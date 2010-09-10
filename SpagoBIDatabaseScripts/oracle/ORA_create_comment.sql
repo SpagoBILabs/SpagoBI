86765453b6f70731d805004bc55103b1c16fed56
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-09-10 14:01:11 +0000

    Added UDP management
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_2.x/Server/trunk@12337 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/oracle/ORA_create_comment.sql
+++ SpagoBIDatabaseScripts/oracle/ORA_create_comment.sql
@@ -131,3 +131,6 @@ COMMENT ON TABLE SBI_MENU  IS 'The table contains all menu voices';
 COMMENT ON TABLE SBI_MENU_ROLE  IS 'The table contains all relations between menu and roles';
 
 COMMENT ON TABLE SBI_CONFIG  IS 'The table contains all configuration parameters';
+
+COMMENT ON  TABLE SBI_UDP IS 'The table contains all generic attributes definition';
+COMMENT ON  TABLE SBI_UDP_VALUE IS 'The table contains all generci attributes values';
