615f17956f832c3487c000d2d0c852e1608890af
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-09-28 08:00:49 +0000

    dataset execution throws specific exceptions
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_2.x/Server/trunk@12554 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/behaviour/QuerableBehaviour.java
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/behaviour/QuerableBehaviour.java
@@ -21,15 +21,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  **/
 package it.eng.spagobi.tools.dataset.common.behaviour;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Vector;
-
-import org.apache.log4j.Logger;
-
 import it.eng.spago.base.SourceBeanException;
 import it.eng.spago.error.EMFErrorSeverity;
 import it.eng.spago.error.EMFInternalError;
@@ -42,7 +33,19 @@ import it.eng.spagobi.tools.dataset.bo.IDataSet;
 import it.eng.spagobi.tools.dataset.bo.JDBCDataSet;
 import it.eng.spagobi.tools.dataset.bo.ScriptDataSet;
 import it.eng.spagobi.tools.dataset.common.query.IQueryTransformer;
+import it.eng.spagobi.tools.dataset.exceptions.ParameterDsException;
+import it.eng.spagobi.tools.dataset.exceptions.ProfileAttributeDsException;
 import it.eng.spagobi.utilities.assertion.Assert;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import org.apache.log4j.Logger;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
@@ -59,14 +62,14 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 
 	public String getStatement() throws EMFInternalError, EMFUserError{
 		String statement;
-		
+
 		logger.debug("IN");
 		try {
 			Assert.assertNotNull(getTargetDataSet(), "Target dataset of a QuerableBehaviour cannot be null");
-						
+
 			logger.debug("Querable dataset [" + getTargetDataSet().getName() + "] is of type [" + getTargetDataSet().getClass().getName() + "]");
-			
-			
+
+
 			if (getTargetDataSet() instanceof ScriptDataSet) {
 				statement = (String) ((ScriptDataSet)getTargetDataSet()).getScript();
 			} else if (getTargetDataSet() instanceof JDBCDataSet) {
@@ -75,41 +78,51 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 				// maybe better to delete getQuery from IDataSet
 				statement = (String)getTargetDataSet().getQuery();
 			}
-			
 			logger.debug("Original dataset statement [" + statement + "]");
 			Assert.assertNotNull(statement, "Querable dataset statment cannot be null");
-				
 			// if script substitute profile attributes in a strict way
 			if (getTargetDataSet() instanceof ScriptDataSet) {
 				try{
 					HashMap attributes = getAllProfileAttributes(getTargetDataSet().getUserProfile()); // to be cancelled, now substitutution inline
 					statement = substituteProfileAttributes(statement, attributes);
 				} catch (EMFInternalError e) {
-					logger.error("Errore nella valorizzazione degli attributi i profilo del dataset Script",e);
-					throw(e);
+					ProfileAttributeDsException parException= new ProfileAttributeDsException(EMFErrorSeverity.ERROR, 9221, e, "");
+					logger.error("Error in query Execution",e);
+					throw parException;				
 				}
 			} else if (getTargetDataSet() instanceof JDBCDataSet) {	
 				try {
 					statement = StringUtilities.substituteProfileAttributesInString(statement, getTargetDataSet().getUserProfile() );
 				} catch (Exception e) {
-					EMFUserError userError = new EMFUserError(EMFErrorSeverity.ERROR, 9213);
-					logger.error("Errore nella valorizzazione degli attributi di profilodel dataset JDBC", e);
-					throw userError;
+					List list = checkProfileAttributesUnfilled(statement);
+					logger.error("there are profile attributes without values");
+					String atts = "";
+					for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+						String string = (String) iterator.next();
+						atts += string;
+						if(iterator.hasNext()){
+							atts += ", ";
+						}
+					}
+					ProfileAttributeDsException parException= new ProfileAttributeDsException(EMFErrorSeverity.ERROR, 9221, e, atts);
+					throw parException;	
+
+					
 				}
 			}
-			
+
 			logger.debug("Dataset statement after profile attributes substitution [" + statement + "]");
 			logger.debug("Dataset paramMap [" + getTargetDataSet().getParamsMap() + "]");
-	
+
 			//check if there are parameters filled
 			//if( getTargetDataSet().getParamsMap() != null && !getTargetDataSet().getParamsMap().isEmpty()){
 			if( getTargetDataSet().getParamsMap() != null){
 				logger.debug("Dataset paramMap contains [" + getTargetDataSet().getParamsMap().size() + "] parameters");
-			
+
 				// if a parameter has value '' put null!
 				Map parameterValues = getTargetDataSet().getParamsMap();
 				Vector<String> parsToChange = new Vector<String>();
-				
+
 				for (Iterator iterator = parameterValues.keySet().iterator(); iterator.hasNext();) {
 					String parName = (String) iterator.next();
 					Object val = parameterValues.get(parName);
@@ -125,24 +138,49 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 					parameterValues.remove(parName);
 					parameterValues.put(parName, null);
 				}
-				
-				
-				
-				
+
 				try{
 					Map parTypeMap = getParTypeMap(getTargetDataSet());
 					statement = StringUtilities.substituteDatasetParametersInString(statement, getTargetDataSet().getParamsMap(), parTypeMap ,false );
-				}catch (Exception e) {
-					EMFUserError userError = new EMFUserError(EMFErrorSeverity.ERROR, 9220);
-					logger.error("Errore nella valorizzazione dei parametri",e);
-					throw userError;
 				}
-	
+				catch (Exception e) {
+					if(e instanceof NumberFormatException){
+						String attribute = ((NumberFormatException)e).getMessage() + " value should be of type number";
+						ParameterDsException parException= new ParameterDsException(EMFErrorSeverity.ERROR, 9221, e, attribute);
+						throw parException;			
+					}
+					else{
+						logger.error("Errore nella valorizzazione dei parametri",e);
+						String attribute = e.getMessage();
+						ParameterDsException parException= new ParameterDsException(EMFErrorSeverity.ERROR, 9221, e, attribute);
+						throw parException;			
+					}
+				}
 			}	
-			
+
+			// after having substituted all parameters check there are not other parameters unfilled otherwise throw an exception;
+			List<String> parsUnfilled = checkParametersUnfilled(statement);
+			if(parsUnfilled != null){
+				// means there are parameters not valorized, throw exception
+				logger.error("there are parameters without values");
+				String pars = "";
+				for (Iterator iterator = parsUnfilled.iterator(); iterator.hasNext();) {
+					String string = (String) iterator.next();
+					pars += string;
+					if(iterator.hasNext()){
+						pars += ", ";
+					}
+				}
+				pars += " have no value specified";
+				ParameterDsException parException= new ParameterDsException(EMFErrorSeverity.ERROR, 
+						9221, new Exception(), pars);
+				throw parException;	
+			}
+
+
 			logger.debug("Dataset statement after  attributes substitution [" + statement + "]");
-			
-	
+
+
 			if(queryTransformer != null) {
 				statement = (String)queryTransformer.transformQuery( statement );
 			}
@@ -150,7 +188,7 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 		} finally {
 			logger.debug("OUT");
 		}
-		
+
 		return statement;
 	}
 
@@ -180,23 +218,23 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 
 
 	public Map getParTypeMap(IDataSet dataSet) throws SourceBeanException{
-		
+
 		Map parTypeMap;
 		String parametersXML;
 		List parameters;
-		
+
 		logger.debug("IN");
-		
+
 		try {		
 			parTypeMap = new HashMap();
 			parametersXML= dataSet.getParameters();	
-			
+
 			logger.debug("Dataset parameters string is equals to [" + parametersXML + "]");
-			
+
 			if ( !StringUtilities.isEmpty(parametersXML) ) {
 				parameters = DataSetParametersList.fromXML(parametersXML).getItems();
 				logger.debug("Dataset have  [" + parameters.size() + "] parameters");
-				
+
 				for (int i = 0; i < parameters.size(); i++) {
 					DataSetParameterItem dsDet = (DataSetParameterItem) parameters.get(i); 
 					String name = dsDet.getName();
@@ -206,12 +244,12 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 					parTypeMap.put(name, type);
 				}
 			}	
-			
-			
+
+
 		} finally {
 			logger.debug("OUT");
 		}
-		
+
 		return parTypeMap;
 	}
 
@@ -241,6 +279,44 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 		}
 		logger.debug("OUT");
 		return profileattrs;
+	}
+
+
+	/** search if there are parameters unfilled and return their names
+	 * 
+	 * @param statement
+	 * @return
+	 */
+
+	public static List checkParametersUnfilled(String statement){
+		List toReturn = null;
+		int index = statement.indexOf("$P{");
+		while (index != -1){
+			int endIndex = statement.indexOf('}', index);
+			if(endIndex != -1){
+				String nameAttr = statement.substring(index, endIndex+1);
+				if(toReturn == null) toReturn = new ArrayList<String>();
+				toReturn.add(nameAttr);
+				index = statement.indexOf("$P{", endIndex);
+			}
+		}
+		return toReturn;
+	}
+
+	public static List checkProfileAttributesUnfilled(String statement){
+		List toReturn = null;
+		int index = statement.indexOf("${");
+		while (index != -1){
+
+			int endIndex = statement.indexOf('}', index);
+			if(endIndex != -1){
+				String nameAttr = statement.substring(index, endIndex+1);
+				if(toReturn == null) toReturn = new ArrayList<String>();
+				toReturn.add(nameAttr);
+				index = statement.indexOf("${", endIndex);
+			}
+		}
+		return toReturn;
 	}
 
 	public IQueryTransformer getQueryTransformer() {
