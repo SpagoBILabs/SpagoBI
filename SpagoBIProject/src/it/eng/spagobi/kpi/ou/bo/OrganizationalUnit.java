d978effeee04023c5450071c08a96169731f8397
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-11-10 10:24:57 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_2.x/Server/trunk@12914 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/kpi/ou/bo/OrganizationalUnit.java
+++ SpagoBIProject/src/it/eng/spagobi/kpi/ou/bo/OrganizationalUnit.java
@@ -94,12 +94,36 @@ public class OrganizationalUnit {
         this.description = description;
     }
  
-    @Override
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrganizationalUnit other = (OrganizationalUnit) obj;
+		if (label == null) {
+			if (other.label != null)
+				return false;
+		} else if (!label.equals(other.label))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 	@Override
@@ -134,20 +158,5 @@ public class OrganizationalUnit {
 		return true;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		OrganizationalUnit other = (OrganizationalUnit) obj;
-		if (label == null) {
-			if (other.label != null)
-				return false;
-		} else if (!label.equals(other.label))
-			return false;
-		return true;
-	}
+
 }
