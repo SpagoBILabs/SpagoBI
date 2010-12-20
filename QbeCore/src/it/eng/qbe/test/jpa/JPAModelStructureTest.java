b5e9e3b7a0f94cd390173b37059b20f433ee0250
antonella giachino <antonella.giachino@eng.it>
2010-12-20 13:34:56 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_2.x/Server/trunk@13165 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/it/eng/qbe/test/jpa/JPAModelStructureTest.java
+++ QbeCore/src/it/eng/qbe/test/jpa/JPAModelStructureTest.java
@@ -4,6 +4,8 @@
 package it.eng.qbe.test.jpa;
 
 import it.eng.qbe.datasource.jpa.JPADataSource;
+import it.eng.qbe.model.structure.DataMartEntity;
+import it.eng.qbe.model.structure.DataMartField;
 import it.eng.qbe.model.structure.DataMartModelStructure;
 import it.eng.qbe.model.structure.builder.DataMartStructureBuilderFactory;
 import it.eng.qbe.model.structure.builder.IDataMartStructureBuilder;
@@ -12,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.metamodel.Metamodel;
 
 /**
  * @author giachino
@@ -44,6 +45,38 @@ public class JPAModelStructureTest {
 		IDataMartStructureBuilder dmb = DataMartStructureBuilderFactory.getDataMartStructureBuilder(jpaDS);
 		//builds the jpa structure
 		DataMartModelStructure dms = dmb.build();
+		
+		//gets structure's informations
+		List allEntities = dms.getRootEntities("TEST_JPA");
+		for (int i=0; i< allEntities.size(); i++){
+			DataMartEntity entity = (DataMartEntity)allEntities.get(i);
+			System.out.println("*** Entity uniqueName: " + entity.getUniqueName());
+			System.out.println("* Entity name: " + entity.getName());
+			System.out.println("* Entity uniqueType: " + entity.getUniqueType());
+			System.out.println("* Entity type: " + entity.getType());			
+			System.out.println("* Entity path: " + entity.getPath());
+			System.out.println("* Entity role: " + entity.getRole());
+			
+			List keyFields = entity.getKeyFields();
+			for (int k=0; k< keyFields.size(); k++){
+				DataMartField key = (DataMartField)keyFields.get(k);				
+				System.out.println("*** key Unique Name: " + key.getUniqueName());
+				System.out.println("* key Name: " + key.getName());
+				System.out.println("* key type: " + key.getType());
+				System.out.println("* key Length: " + key.getLength());
+				System.out.println("* key Precision: " + key.getPrecision());
+			}
+			
+			List fields = entity.getAllFields();
+			for (int j=0; j< fields.size(); j++){
+				DataMartField field = (DataMartField)fields.get(j);				
+				System.out.println("*** Field Unique Name: " + field.getUniqueName());
+				System.out.println("* Field Name: " + field.getName());
+				System.out.println("* Field type: " + field.getType());
+				System.out.println("* Field Length: " + field.getLength());
+				System.out.println("* Field Precision: " + field.getPrecision());
+			}
+		}
 		
 		
 		//close operations
