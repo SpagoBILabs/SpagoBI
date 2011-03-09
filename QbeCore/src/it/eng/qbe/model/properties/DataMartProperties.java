bd5120fbcba3412cf4a6d420a3700a84dea95549
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-03-09 17:41:12 +0000

    Created class DataMartProperties (replace enum in the initializer)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@13667 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/it/eng/qbe/model/properties/DataMartProperties.java
+++ QbeCore/src/it/eng/qbe/model/properties/DataMartProperties.java
@@ -21,10 +21,47 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.qbe.model.properties;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
  *
  */
 public class DataMartProperties {
-
+	
+	public final static DataMartProperty[] globalProperties = new DataMartProperty[]{
+		new DataMartProperty("recursiveFiltering", false, true, "enabled")
+	};
+	
+	public final static DataMartProperty[] entityProperties = new DataMartProperty[]{
+		new DataMartProperty("visible", false, true, "true"),
+		new DataMartProperty("type", false, true, "dimension"),
+		new DataMartProperty("position", false, true, "" + Integer.MAX_VALUE)
+	};
+	
+	public final static DataMartProperty[] fieldProperties = new DataMartProperty[]{
+		new DataMartProperty("visible", false, true, "true"),
+		new DataMartProperty("type", false, true, "attribute"),
+		new DataMartProperty("position", false, true, "" + Integer.MAX_VALUE),
+		new DataMartProperty("format", false, true, null)
+	};
+	
+	static Map<String, DataMartProperty> globalPropertiesMap;
+	static Map<String, DataMartProperty> entityPropertiesMap;
+	static Map<String, DataMartProperty> fieldPropertiesMap;
+	
+	static {
+		globalPropertiesMap = new HashMap<String, DataMartProperty>();
+		entityPropertiesMap = new HashMap<String, DataMartProperty>();
+		fieldPropertiesMap = new HashMap<String, DataMartProperty>();
+		
+		for(int i = 0; i < globalProperties.length; i++) globalPropertiesMap.put(globalProperties[i].getName(), globalProperties[i]);
+		for(int i = 0; i < entityProperties.length; i++) entityPropertiesMap.put(entityProperties[i].getName(), entityProperties[i]);
+		for(int i = 0; i < fieldProperties.length; i++) fieldPropertiesMap.put(fieldProperties[i].getName(), fieldProperties[i]);
+	}
+	
+	public DataMartProperty getGlobalProperty(String name) {return globalPropertiesMap.get(name);}
+	public DataMartProperty getEntityProperties(String name) {return entityPropertiesMap.get(name);}
+	public DataMartProperty getFieldProperties(String name) {return fieldPropertiesMap.get(name);}
 }
