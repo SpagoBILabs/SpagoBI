c53e025eb77958b4c66884807c4816b8d9cb5804
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-03-11 09:50:31 +0000

    factored all serialization classes in a dedicated src folder (crosstab.serializer)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@13696 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ QbeCore/serialization/it/eng/qbe/crosstab/serializer/json/CrosstabSerializationConstants.java
@@ -0,0 +1,41 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2009 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
+package it.eng.qbe.crosstab.serializer.json;
+
+/**
+ * @author Davide Zerbetto (davide.zerbetto@eng.it)
+ */
+public class CrosstabSerializationConstants {
+	
+	public static final String ROWS = "rows";
+	public static final String COLUMNS = "columns";
+	public static final String MEASURES = "measures";
+	public static final String ID = "id";
+	public static final String ALIAS = "alias";
+	public static final String ICON_CLS = "iconCls";
+	public static final String NATURE = "nature";
+	public static final String FUNCTION = "funct";
+	public static final String CONFIG = "config";
+	public static final String CALCULATED_FIELDS = "calculatedFields";
+	public static final String MEASURESON = "measureson";
+	
+}
