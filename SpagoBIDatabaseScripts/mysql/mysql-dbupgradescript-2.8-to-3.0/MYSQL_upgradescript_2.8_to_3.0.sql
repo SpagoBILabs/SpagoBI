6f25e4fc03d4469a1b61de1de7e4368695e456c7
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-03-23 11:29:46 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@13954 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-2.8-to-3.0/MYSQL_upgradescript_2.8_to_3.0.sql
+++ SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-2.8-to-3.0/MYSQL_upgradescript_2.8_to_3.0.sql
@@ -1,3 +1,11 @@
+INSERT into SBI_DOMAINS (VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS) values ('Groovy','Groovy','SCRIPT_TYPE','Script Type','Script Type');
+INSERT into SBI_DOMAINS (VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS) values ('Javascript','Javascript','SCRIPT_TYPE','Script Type','Script Type');
+INSERT into SBI_DOMAINS (VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS) values ('File','SbiFileDataSet','DATA_SET_TYPE','Data Set Type','SbiFileDataSet');
+INSERT into SBI_DOMAINS (VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS) values ('Query','SbiQueryDataSet','DATA_SET_TYPE','Data Set Type','SbiQueryDataSet');
+INSERT into SBI_DOMAINS (VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS) values ('Java Class','SbiJClassDataSet','DATA_SET_TYPE','Data Set Type','SbiJClassDataSet');
+INSERT into SBI_DOMAINS (VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS) values ('Web Service','SbiWSDataSet','DATA_SET_TYPE','Data Set Type','SbiWSDataSet');
+INSERT into SBI_DOMAINS (VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS) values ('Script','SbiScriptDataSet','DATA_SET_TYPE','Data Set Type','SbiScriptDataSet');
+
 ALTER TABLE SBI_DATA_SET DROP COLUMN EXECUTOR_CLASS;
 ALTER TABLE SBI_DATA_SET ADD COLUMN CATEGORY_ID INTEGER;
 
