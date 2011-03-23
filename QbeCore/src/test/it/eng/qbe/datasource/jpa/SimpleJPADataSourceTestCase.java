6e2ecf7428df76d76c77d1e9e8350e8138a8d216
gioia <gioia>
2011-03-23 09:35:13 +0000

    added junit support
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@13940 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ QbeCore/src/test/it/eng/qbe/datasource/jpa/SimpleJPADataSourceTestCase.java
@@ -0,0 +1,64 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2010 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
+package it.eng.qbe.datasource.jpa;
+
+import it.eng.qbe.AbstractQbeTestCase;
+import it.eng.qbe.datasource.AbstractDataSourceTestCase;
+import it.eng.qbe.datasource.DBConnection;
+import it.eng.qbe.datasource.DriverManager;
+import it.eng.qbe.datasource.IDataSource;
+import it.eng.qbe.datasource.configuration.CompositeDataSourceConfiguration;
+import it.eng.qbe.datasource.configuration.FileDataSourceConfiguration;
+import it.eng.qbe.datasource.configuration.IDataSourceConfiguration;
+import it.eng.qbe.datasource.hibernate.HibernateDriver;
+import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Andrea Gioia (andrea.gioia@eng.it)
+ *
+ */
+public class SimpleJPADataSourceTestCase extends AbstractDataSourceTestCase {
+	
+	
+	private static final String QBE_FILE = "resources/jpa/foodmart/datamart.jar";
+	
+	@Override
+	protected void setUpDataSource() {
+		IDataSourceConfiguration configuration;
+		
+		File file = new File(QBE_FILE);
+		configuration = new FileDataSourceConfiguration("foodmart", file);
+		configuration.loadDataSourceProperties().put("connection", connection);
+		dataSource = DriverManager.getDataSource(JPADriver.DRIVER_ID, configuration);
+		
+		testEntityUniqueName = "it.eng.spagobi.meta.Customer::Customer";
+	}
+}
