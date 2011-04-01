37254975e9ee88a9c993fb106ee4e85e274e8816
gioia <gioia>
2011-04-01 16:22:09 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14198 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/test2/it/eng/spagobi/common/service/DomainTestCase.java
+++ SpagoBIProject/test2/it/eng/spagobi/common/service/DomainTestCase.java
@@ -1,8 +1,13 @@
 package it.eng.spagobi.common.service;
 
+import java.io.InputStream;
 import java.util.Iterator;
 import java.util.List;
 
+import it.eng.spago.base.SourceBean;
+import it.eng.spago.base.SourceBeanException;
+import it.eng.spago.configuration.ConfigSingleton;
+import it.eng.spago.configuration.IConfigurationCreator;
 import it.eng.spago.error.EMFUserError;
 import it.eng.spagobi.commons.bo.Domain;
 import it.eng.spagobi.commons.dao.DomainDAOHibImpl;
@@ -10,6 +15,25 @@ import junit.framework.TestCase;
 
 public class DomainTestCase extends TestCase {
 
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		ConfigSingleton.setConfigurationCreation(new IConfigurationCreator() {
+
+			public SourceBean createConfiguration(String arg0) throws SourceBeanException {
+				//System.err.println("create: " + arg0);
+				return new SourceBean("MASTER");
+			}
+
+			public InputStream getInputStream(String arg0) throws Exception {
+				System.err.println("getInputStream");
+				return null;
+			}
+			
+		});
+		
+		//System.setProperty("AF_ROOT_PATH", "D:\\Documenti\\Sviluppo\\workspaces\\helios\\spagobi\\server\\SpagoBIProject\\web-content");
+	}
 	public void testLoadListDomains() throws EMFUserError {
 		DomainDAOHibImpl dao=new DomainDAOHibImpl();
 		List<Domain> lista=dao.loadListDomains();
@@ -17,7 +41,7 @@ public class DomainTestCase extends TestCase {
 		while(iter.hasNext()){
 			System.out.println("Elemento="+iter.next().getValueCd());
 		}
-		fail("Not yet implemented");
+		
 	}
 
 	public void testSaveDomain() {
