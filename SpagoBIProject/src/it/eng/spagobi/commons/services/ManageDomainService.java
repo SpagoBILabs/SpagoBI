e14ef1726e4234dc564b12081ff3fde1c360b318
mospinel <mospinel>
2011-04-11 17:08:25 +0000

    create methods for update domain to server and database
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14329 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/commons/services/ManageDomainService.java
+++ SpagoBIProject/src/it/eng/spagobi/commons/services/ManageDomainService.java
@@ -1,3 +1,48 @@
+/**
+ * SpagoBI - The Business Intelligence Free Platform
+ *
+ * Copyright (C) 2004 - 2008 Engineering Ingegneria Informatica S.p.A.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ **/
+
+/**
+ * Object name
+ * 
+ * ManageDomains
+ * 
+ * 
+ * Public Properties
+ * 
+ * [list]
+ * 
+ * 
+ * Public Methods
+ * 
+ * [list]
+ * 
+ * 
+ * Public Events
+ * 
+ * [list]
+ * 
+ * Authors
+ * 
+ * Monia Spinelli (monia.spinelli@eng.it)
+ */
 package it.eng.spagobi.commons.services;
 
 import java.util.List;
@@ -20,97 +65,98 @@ import it.eng.spagobi.utilities.service.JSONAcknowledge;
 import it.eng.spagobi.utilities.service.JSONFailure;
 import it.eng.spagobi.utilities.service.JSONSuccess;
 
-
 public class ManageDomainService extends AbstractSpagoBIAction {
 
 	/**
 	 * 
 	 */
-	 private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	// logger component
 	private static Logger logger = Logger.getLogger(ManageDomainService.class);
 
-	//Service parameter
+	// Service parameter
 	private final String MESSAGE_DET = "MESSAGE_DET";
-	
+
 	private static final String DOMAIN_LIST = "DOMAIN_LIST";
 	private static final String DOMAIN_DELETE = "DOMAIN_DELETE";
-	private static final String DOMAIN_SAVE = "DOMAIN_SAVE";	
-	
+	private static final String DOMAIN_SAVE = "DOMAIN_SAVE";
+
 	@Override
 	public void doService() {
 		IDomainDAO domainDao;
 		String serviceType;
-		
+
 		logger.debug("IN");
-		
+
 		try {
 			domainDao = DAOFactory.getDomainDAO();
 		} catch (EMFUserError e1) {
 			logger.error(e1.getMessage(), e1);
-			throw new SpagoBIServiceException(SERVICE_NAME,	"Error occurred");
+			throw new SpagoBIServiceException(SERVICE_NAME, "Error occurred");
 		}
-		
 
 		serviceType = this.getAttributeAsString(MESSAGE_DET);
-		logger.debug("Parameter [" + MESSAGE_DET +"] is equal to [" + serviceType + "]");
-		
-		if(serviceType != null) {
+		logger.debug("Parameter [" + MESSAGE_DET + "] is equal to ["
+				+ serviceType + "]");
+
+		if (serviceType != null) {
 			if (serviceType.equalsIgnoreCase(DOMAIN_LIST)) {
-				 doDomainList();
-			}else if (serviceType.equalsIgnoreCase(DOMAIN_DELETE)) {
+				doDomainList();
+			} else if (serviceType.equalsIgnoreCase(DOMAIN_DELETE)) {
 				doDelete();
-			}else if (serviceType.equalsIgnoreCase(DOMAIN_SAVE)) {
+			} else if (serviceType.equalsIgnoreCase(DOMAIN_SAVE)) {
 				doSave();
 			} else {
-				throw new SpagoBIServiceException(SERVICE_NAME,	"Unable to execute service [" + serviceType + "]");
+				throw new SpagoBIServiceException(SERVICE_NAME,
+						"Unable to execute service [" + serviceType + "]");
 			}
 		}
-		
-		
+
 		logger.debug("OUT");
 
 	}
-	
+
 	public void doSave() {
-		try {	
-			//Recupero le informazioni della richiesta
+		
+		logger.debug("IN");
+		
+		try {
 			
-			//poi chiamo le set dei vari parametri dell'oggetto domain (meglio creare un metodo dedicato?)
+			logger.debug("Save domain");
 			
-			//Salvo su db
-			
-			
-			//domainDao.saveDomain(codeDomain, codeValue);
-			writeBackToClient(new JSONAcknowledge("Operation succeded"));
-			//writeBackToClient(new JSONFailure(new SpagoBIServiceException(SERVICE_NAME, "Qualcosa è andato male!")));
+			Domain domain = this.setDomain();
+			DAOFactory.getDomainDAO().saveDomain(domain);
+			JSONObject response = new JSONObject();
+			response.put("VALUE_ID", domain.getValueId());
+			writeBackToClient(new JSONSuccess(response));
 
 		} catch (Throwable e) {
-			logger.error("Exception occurred while retrieving domain data", e);
-			throw new SpagoBIServiceException(SERVICE_NAME,
-					"Exception occurred while retrieving domain data", e);
-		}		
+			throw new SpagoBIServiceException(SERVICE_NAME, "Impossible to save domain", e);
+		} finally  {
+			logger.debug("OUT");
+		}
 	}
-	
+
 	public void doDelete() {
-		try {	
-			//domainDao.delete(codeDomain, codeValue);
+		try {
+			// domainDao.delete(codeDomain, codeValue);
 			writeBackToClient(new JSONAcknowledge("Operation succeded"));
 
 		} catch (Throwable e) {
 			logger.error("Exception occurred while retrieving domain data", e);
 			throw new SpagoBIServiceException(SERVICE_NAME,
 					"Exception occurred while retrieving domain data", e);
-		}	
+		}
 	}
-	
+
 	public void doDomainList() {
-		try {				
-//			List<Domain> domainList = domainDao.loadListDomains();
-//			logger.debug("Loaded domain list");
-//			JSONArray domainListJSON = (JSONArray) SerializerFactory.getSerializer("application/json").serialize(domainList,locale);
-//			writeBackToClient(new JSONSuccess(domainListJSON));
+		try {
+			// List<Domain> domainList = domainDao.loadListDomains();
+			// logger.debug("Loaded domain list");
+			// JSONArray domainListJSON = (JSONArray)
+			// SerializerFactory.getSerializer("application/json").serialize(domainList,locale);
+			// writeBackToClient(new JSONSuccess(domainListJSON));
 			writeBackToClient(new JSONAcknowledge("Operation succeded"));
 		} catch (Throwable e) {
 			logger.error("Exception occurred while retrieving domain data", e);
@@ -119,6 +165,19 @@ public class ManageDomainService extends AbstractSpagoBIAction {
 		}
 	}
 	
-	
+	public Domain setDomain()
+	{
+		Domain domain = new Domain();
+		
+		domain.setValueId(this.getAttributeAsInteger("VALUE_ID"));
+		domain.setValueCd(this.getAttributeAsString("VALUE_CD"));
+		domain.setValueName(this.getAttributeAsString("VALUE_NM"));
+		domain.setDomainCode(this.getAttributeAsString("DOMAIN_CD"));
+		domain.setDomainName(this.getAttributeAsString("DOMAIN_NM"));
+		domain.setValueDescription(this.getAttributeAsString("VALUE_DS"));
+		
+		return domain;
+		
+	}
 
 }
