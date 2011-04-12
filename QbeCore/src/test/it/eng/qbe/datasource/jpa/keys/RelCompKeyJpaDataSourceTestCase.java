f63056afcf86b4ffb1145fbb9a4f388ea197e526
gioia <gioia>
2011-04-12 21:05:16 +0000

    added tests on datamarts with kyes (PK only and PK + FK)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14363 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ QbeCore/src/test/it/eng/qbe/datasource/jpa/keys/RelCompKeyJpaDataSourceTestCase.java
@@ -0,0 +1,61 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2010 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
+package it.eng.qbe.datasource.jpa.keys;
+
+import it.eng.qbe.datasource.DriverManager;
+import it.eng.qbe.datasource.configuration.FileDataSourceConfiguration;
+import it.eng.qbe.datasource.configuration.IDataSourceConfiguration;
+
+import it.eng.qbe.datasource.jpa.JPADriver;
+
+import java.io.File;
+
+
+/**
+ * @author Andrea Gioia (andrea.gioia@eng.it)
+ *
+ */
+public class RelCompKeyJpaDataSourceTestCase extends AbstractKeyJpaDataSourceTestCase {
+	
+	private static final String QBE_FILE = "test-resources/jpa/keys/relCompKey/dist/datamart.jar";
+	
+	@Override
+	protected void setUpDataSource() {
+		IDataSourceConfiguration configuration;
+		
+		modelName = "My Model";  
+		
+		File file = new File(QBE_FILE);
+		configuration = new FileDataSourceConfiguration(modelName, file);
+		configuration.loadDataSourceProperties().put("connection", connection);
+		dataSource = DriverManager.getDataSource(JPADriver.DRIVER_ID, configuration, false);
+	}
+	
+	public void testQbeWithKeys() {
+		doTests() ;
+	}
+	
+	public void doTests() {
+		super.doTests();
+		// add custom tests here
+	}
+}
