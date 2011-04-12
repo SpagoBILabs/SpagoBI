b573a88996f2dfd989720fcb380724a4d1be3859
Davide Zerbetto <davide.zerbetto@eng.it>
2011-04-12 16:18:17 +0000

    Compilation error
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14357 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/tools/dataset/service/ManageDatasets.java
+++ SpagoBIProject/src/it/eng/spagobi/tools/dataset/service/ManageDatasets.java
@@ -1,9 +1,29 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2008 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ **/
 package it.eng.spagobi.tools.dataset.service;
 
 import it.eng.qbe.datasource.DBConnection;
 import it.eng.qbe.datasource.DriverManager;
 import it.eng.qbe.datasource.configuration.CompositeDataSourceConfiguration;
-import it.eng.qbe.datasource.configuration.DAOException;
 import it.eng.qbe.datasource.configuration.FileDataSourceConfiguration;
 import it.eng.qbe.query.Query;
 import it.eng.qbe.query.catalogue.QueryCatalogue;
@@ -782,7 +802,7 @@ public class ManageDatasets extends AbstractSpagoBIAction {
         logger.debug("IN: Check the driver name. Looking if "+jarFile+" is a jpa jar file..");
         JarInputStream zis;
         JarEntry zipEntry;
-        String dialectName;
+        String dialectName = null;
         boolean isJpa = false;
            
         try {
@@ -804,7 +824,7 @@ public class ManageDatasets extends AbstractSpagoBIAction {
             }
         } catch (Throwable t) {
             logger.error("Impossible to read jar file [" + jarFile + "]",t);
-            throw new DAOException("Impossible to read jar file [" + jarFile + "]");
+            throw new SpagoBIServiceException(SERVICE_NAME, "Impossible to read jar file [" + jarFile + "]", t);
         }
 
 
