7b191fce4d323bf71fdf28ce2722c3366ca857bb
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-04-19 15:36:46 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14602 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIProject/src/it/eng/spagobi/tools/dataset/constants/DataSetConstants.java
@@ -0,0 +1,96 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2008 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
+package it.eng.spagobi.tools.dataset.constants;
+
+/**
+ * Defines all SpagoBI's constants.
+ */
+public class DataSetConstants {
+
+	public static final String MESSAGE_DET = "MESSAGE_DET";
+	// type of service
+	public static final String DATASETS_LIST = "DATASETS_LIST";
+	public static final String DATASET_INSERT = "DATASET_INSERT";
+	public static final String DATASET_DELETE = "DATASET_DELETE";
+	public static final String DATASET_TEST = "DATASET_TEST";
+	
+	public static final String DATASET_VERSION_RESTORE = "DATASET_VERSION_RESTORE";
+	public static final String DATASET_VERSION_DELETE = "DATASET_VERSION_DELETE";
+	public static final String DATASET_ALL_VERSIONS_DELETE = "DATASET_ALL_VERSIONS_DELETE";
+	
+	public static final String DATASETS_FOR_KPI_LIST = "DATASETS_FOR_KPI_LIST";
+	
+	public static final String CATEGORY_DOMAIN_TYPE = "CATEGORY_TYPE";
+	public static final String SCRIPT_TYPE = "SCRIPT_TYPE";
+	public static final String DATA_SET_TYPE = "DATA_SET_TYPE";	
+	public static final String TRANSFORMER_TYPE = "TRANSFORMER_TYPE";		
+	
+	public static final String DS_ID = "dsId";
+	public static final String VERSION_ID = "versId";
+	public static final String VERSION_NUM = "versNum";
+		
+	public static final String ID = "id";
+	public static final String NAME = "name";
+	public static final String DESCRIPTION = "description";
+	public static final String LABEL = "label";
+	
+	public static final String CATEGORY_TYPE_CD = "catTypeCd";
+		
+	public static final String PARS = "pars";
+	public static final String METADATA = "meta";
+	
+	public static final String DS_TYPE_CD = "dsTypeCd";
+	public static final String FILE_NAME = "fileName";
+	public static final String QUERY = "query";
+	public static final String DATA_SOURCE = "dataSource";
+	public static final String WS_ADDRESS = "wsAddress";
+	public static final String WS_OPERATION = "wsOperation";
+	public static final String SCRIPT = "script";
+	public static final String SCRIPT_LANGUAGE = "scriptLanguage";
+	public static final String JCLASS_NAME = "jclassName";
+	
+	public static final String TRASFORMER_TYPE_CD = "trasfTypeCd";
+	public static final String PIVOT_COL_NAME = "pivotColName";
+	public static final String PIVOT_COL_VALUE = "pivotColValue";
+	public static final String PIVOT_ROW_NAME = "pivotRowName";
+	public static final String PIVOT_IS_NUM_ROWS = "pivotIsNumRows";
+	
+	public static final String DS_WS = "SbiWSDataSet";
+	public static final String DS_FILE = "SbiFileDataSet";
+	public static final String DS_JCLASS = "SbiJClassDataSet";
+	public static final String DS_QUERY = "SbiQueryDataSet";
+	public static final String DS_SCRIPT = "SbiScriptDataSet";
+	public static final String DS_QBE = "SbiQbeDataSet";
+	
+	public static final String QBE_DATA_SOURCE = "qbeDataSource";
+	public static final String QBE_DATAMARTS = "qbeDatamarts";
+	public static final String QBE_JSON_QUERY = "qbeJSONQuery";
+	public static final String QBE_SQL_QUERY = "qbeSQLQuery";
+	
+	public static final String START = "start";
+	public static final String LIMIT = "limit";
+	public static final Integer START_DEFAULT = 0;
+	public static final Integer LIMIT_DEFAULT = 14;
+	
+	//filters parameters
+	public static final String FILTERS = "FILTERS";
+}
