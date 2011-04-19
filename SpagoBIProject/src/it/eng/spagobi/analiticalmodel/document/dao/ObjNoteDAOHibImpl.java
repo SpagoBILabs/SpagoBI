cd1e284b65a32b922d47f214ac9d4ec084699648
Angelo Bernabei <angelo.bernabei@eng.it>
2011-04-19 07:58:11 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14563 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/analiticalmodel/document/dao/ObjNoteDAOHibImpl.java
+++ SpagoBIProject/src/it/eng/spagobi/analiticalmodel/document/dao/ObjNoteDAOHibImpl.java
@@ -230,6 +230,7 @@ public class ObjNoteDAOHibImpl extends AbstractHibernateDAO implements IObjNoteD
 			hibObjNote.setExecReq(objNote.getExecReq());
 			hibObjNote.setIsPublic(objNote.getIsPublic());
 			hibObjNote.setLastChangeDate(now);
+			updateSbiCommonInfo(hibObjNote);
 			tx.commit();
 		} catch (HibernateException he) {
 			logException(he);
