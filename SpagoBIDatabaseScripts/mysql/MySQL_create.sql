9b2c1f8d8b3d424556859c76dd3ea32e0f342465
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-04-21 07:53:32 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14621 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/mysql/MySQL_create.sql
+++ SpagoBIDatabaseScripts/mysql/MySQL_create.sql
@@ -1056,7 +1056,18 @@ CREATE TABLE SBI_DATA_SET (
 	DS_ID 		   INTEGER NOT NULL ,
 	DESCR 		   VARCHAR(160), 
 	LABEL	 	   VARCHAR(50) NOT NULL,
-	NAME	   	   VARCHAR(50) NOT NULL,      
+	NAME	   	   VARCHAR(50) NOT NULL,  
+       USER_IN              VARCHAR(100) NOT NULL,
+       USER_UP              VARCHAR(100),
+       USER_DE              VARCHAR(100),
+       TIME_IN              TIMESTAMP NOT NULL,
+       TIME_UP              TIMESTAMP NULL DEFAULT NULL,
+       TIME_DE              TIMESTAMP NULL DEFAULT NULL,
+       SBI_VERSION_IN       VARCHAR(10),
+       SBI_VERSION_UP       VARCHAR(10),
+       SBI_VERSION_DE       VARCHAR(10),
+       META_VERSION         VARCHAR(100),
+       ORGANIZATION         VARCHAR(20),  	
     PRIMARY KEY (DS_ID)
 ) ENGINE=InnoDB;
 
