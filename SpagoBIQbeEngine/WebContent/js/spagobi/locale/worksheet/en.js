dad48b02935e32dabc5267c7fb52b814eb21db85
Alberto Ghedin <alberto.ghedin@eng.it>
2011-04-21 15:42:31 +0000

    Work on the title pannel
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14639 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/worksheet/en.js
@@ -0,0 +1,12 @@
+Ext.ns("Sbi.locale");
+
+//===================================================================
+// DESIGNER TITLEPANEL
+//===================================================================
+
+Sbi.locale.ln['sbi.worksheet.title'] = 'Title'
+Sbi.locale.ln['sbi.worksheet.size'] = 'Size'
+Sbi.locale.ln['sbi.worksheet.image'] = 'Image'
+Sbi.locale.ln['sbi.worksheet.position'] = 'Position'
+Sbi.locale.ln['sbi.worksheet.msg.invalidinput.title'] = 'Not valid'
+Sbi.locale.ln['sbi.worksheet.msg.invalidinput.msg'] = 'The values of the fields are not valid or incomplete'
---
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/worksheet/it.js
@@ -0,0 +1,12 @@
+Ext.ns("Sbi.locale");
+
+//===================================================================
+// DESIGNER TITLEPANEL
+//===================================================================
+
+Sbi.locale.ln['sbi.worksheet.title'] = 'Titolo'
+Sbi.locale.ln['sbi.worksheet.size'] = 'Dimensione'
+Sbi.locale.ln['sbi.worksheet.image'] = 'Immagine'
+Sbi.locale.ln['sbi.worksheet.position'] = 'Posizione'
+Sbi.locale.ln['sbi.worksheet.msg.invalidinput.title'] = 'Non valido'
+Sbi.locale.ln['sbi.worksheet.msg.invalidinput.msg'] = 'I dati inseriti non sono validi o sono incompleti'
