637a6ddd0ad73faecaa0c01d69a5d9a476df6cb3
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-05-02 09:21:22 +0000

    Deleted method no longer used
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14752 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/wapp/dao/IMenuRolesDAO.java
+++ SpagoBIProject/src/it/eng/spagobi/wapp/dao/IMenuRolesDAO.java
@@ -44,52 +44,5 @@ public interface IMenuRolesDAO extends ISpagoBIDao{
 	 * 
 	 * @throws EMFUserError If an Exception occurred
 	 */
-	public List loadMenuByRoleId(Integer roleId) throws EMFUserError;
-		
-		
-	/**
-	 * Loads the list of MenuRoles associated to the input
-	 * <code>menuId</code> and <code>roleId</code>. All these information,
-	 * archived by a query to the DB, are stored into a List of <code>MenuRoles</code> object,
-	 * which is returned.
-	 * 
-	 * @param menuId The id for the menu to load
-	 * @param roleId The role id for the role to load
-	 * 
-	 * @return A List of <code>MenuRoles</code> object containing all loaded information
-	 * 
-	 * @throws EMFUserError If an Exception occurred
-	 */
-	public MenuRoles loadMenuRoles(Integer menuId, Integer roleId) throws EMFUserError;
-
-	/**
-	 * Implements the query to modify a MenuRole. All information needed is stored
-	 * into the input <code>MenuRoles</code> object.
-	 * 
-	 * @param aMenuRole The MenuRoles containing all modify information
-	 * 
-	 * @throws EMFUserError If an Exception occurred
-	 */
-	public void modifyMenuRole(MenuRoles aMenuRole) throws EMFUserError;
-
-	/**
-	 * Implements the query to insert a MenuRole. All information needed is stored
-	 * into the input <code>MenuRoles</code> object.
-	 * 
-	 * @param aMenuRole the a menu role
-	 * 
-	 * @throws EMFUserError If an Exception occurred
-	 */
-	public void insertMenuRole(MenuRoles aMenuRole) throws EMFUserError;
-
-	/**
-	 * Implements the query to erase a MenuRoles. All information needed is stored
-	 * into the input <code>MenuRoles</code> object.
-	 * 
-	 * @param aMenuRole The object containing all delete information
-	 * 
-	 * @throws EMFUserError If an Exception occurred
-	 */
-	public void eraseMenuRole(MenuRoles aMenuRole) throws EMFUserError;	
-	
+	public List loadMenuByRoleId(Integer roleId) throws EMFUserError;		
 }
