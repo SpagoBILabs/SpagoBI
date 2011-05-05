bbe9d3f20a2834b8d509f23107502c716b703cfb
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-05-05 11:30:05 +0000

    Worksheet runtime: filters panel
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@14861 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/worksheet/runtime/it.js
@@ -0,0 +1,6 @@
+Ext.ns("Sbi.locale");
+
+//===================================================================
+//Sbi.worksheet.runtime.RuntimeSheetPanel
+//===================================================================
+Sbi.locale.ln['sbi.worksheet.runtime.runtimesheetpanel.filterspanel.title'] = 'Filtri';
