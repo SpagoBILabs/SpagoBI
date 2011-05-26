89ff582d9cb63dd4f4858216795e7f44b0ee2fce
Alberto Ghedin <alberto.ghedin@eng.it>
2011-05-26 07:55:35 +0000

    bug fix
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@15237 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/worksheet/designer/SheetFilterContentPanel.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/worksheet/designer/SheetFilterContentPanel.js
@@ -59,7 +59,7 @@ Sbi.worksheet.designer.SheetFilterContentPanel = function(config, filterStore) {
 
 	this.addEvents('addDesigner');
 	this.contentPanel = new Sbi.worksheet.designer.SheetContentPanel({style:'padding: 5px 15px 0px 15px;'});
-	this.contentPanel.contentPanel.on('addDesigner', function(sheet, state){this.fireEvent('addDesigner',sheet, state);}, this);
+	this.contentPanel.on('addDesigner', function(sheet, state){this.fireEvent('addDesigner',sheet, state);}, this);
 	
 	this.filtersPanel = new Sbi.worksheet.designer.DesignSheetFiltersPanel({
 		style:'padding:5px 10px 0px 15px; float: left'
