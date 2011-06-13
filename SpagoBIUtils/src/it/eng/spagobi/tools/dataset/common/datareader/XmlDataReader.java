c991895b04b3b58feff1ddc832fb512b10cf91e3
gioia <gioia>
2011-06-13 13:55:00 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@15462 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datareader/XmlDataReader.java
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datareader/XmlDataReader.java
@@ -3,18 +3,6 @@
  */
 package it.eng.spagobi.tools.dataset.common.datareader;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringBufferInputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.apache.log4j.Logger;
-import org.xml.sax.InputSource;
-
-import it.eng.spago.base.SourceBean;
-import it.eng.spago.base.SourceBeanAttribute;
 import it.eng.spagobi.tools.dataset.common.datastore.DataStore;
 import it.eng.spagobi.tools.dataset.common.datastore.DataStoreMetaData;
 import it.eng.spagobi.tools.dataset.common.datastore.Field;
@@ -24,16 +12,38 @@ import it.eng.spagobi.tools.dataset.common.datastore.IField;
 import it.eng.spagobi.tools.dataset.common.datastore.IRecord;
 import it.eng.spagobi.tools.dataset.common.datastore.Record;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 /**
- * @author Angelo Bernabei
- *         angelo.bernabei@eng.it
+ * @authors 
+ * Angelo Bernabei (angelo.bernabei@eng.it)
+ * Andrea Gioia (andrea.gioia@eng.it)
  */
 public class XmlDataReader extends AbstractDataReader {
 
+	DocumentBuilderFactory domFactory;
+	
 	private static transient Logger logger = Logger.getLogger(XmlDataReader.class);
 
 	public XmlDataReader() {
 		super();
+		domFactory = DocumentBuilderFactory.newInstance();
+        
 	}
 
 
@@ -43,7 +53,7 @@ public class XmlDataReader extends AbstractDataReader {
 		DataStoreMetaData dataStoreMeta;
 
 		InputStream inputDataStream;
-		InputSource inputDataSource;
+		DocumentBuilder documentBuilder;
 
 		logger.debug("IN");
 
@@ -59,43 +69,46 @@ public class XmlDataReader extends AbstractDataReader {
 		dataStore.setMetaData(dataStoreMeta);
 
 
-		try{
-			inputDataSource = new InputSource(inputDataStream);
-
-			SourceBean rowsSourceBean = null;
-			List colNames = new ArrayList();
-
-			rowsSourceBean = SourceBean.fromXMLStream(inputDataSource);
-			if(rowsSourceBean != null){
-
-				List rows =rowsSourceBean.getAttributeAsList("ROW");
-				Iterator iterator = rows.iterator(); 						
-				boolean firstRow=true;
-				while(iterator.hasNext()){												
-					SourceBean rowSB = (SourceBean) iterator.next();
-					IRecord record = new Record(dataStore);
-
-					List columns = rowSB.getContainedAttributes();
-					for (int i = 0; i < columns.size(); i++) {								
-						SourceBeanAttribute columnSB = (SourceBeanAttribute) columns.get(i);
-
-						if(firstRow==true) {
-							FieldMetadata fieldMeta = new FieldMetadata();
-							fieldMeta.setName( columnSB.getKey() );
-							fieldMeta.setType( columnSB.getValue().getClass() );
-							dataStoreMeta.addFiedMeta(fieldMeta);
-						}				
-
-						IField field = new Field(columnSB.getValue());
-						record.appendField(field);
-					}
-					dataStore.appendRecord(record);
-				if(firstRow==true)firstRow=false;
-				}
+		try {
+			documentBuilder = domFactory.newDocumentBuilder();
+			
+			Document document =  documentBuilder.parse(inputDataStream);
+			
+			NodeList nodes = readXMLNodes(document, "/ROWS/ROW");
+			
+			if(nodes == null) {
+				throw new RuntimeException("Malformed data. Impossible to find tag rows.row");
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error("Exception reading File data");
+
+			int rowNumber = nodes.getLength();
+			boolean firstRow = true;
+			for (int i = 0; i < rowNumber; i++, firstRow = false) {
+				IRecord record = new Record(dataStore);
+				
+			    NamedNodeMap nodeAttributes = nodes.item(i).getAttributes();
+			    for(int j = 0; j < nodeAttributes.getLength(); j++) {
+				    Node attribute = nodeAttributes.item(j);
+				    String columnName = attribute.getNodeName();
+				    String columnValue = attribute.getNodeValue();
+				    Class columnType = attribute.getNodeValue().getClass();
+				    
+				    if(firstRow==true) {
+						FieldMetadata fieldMeta = new FieldMetadata();
+						fieldMeta.setName( columnName );
+						fieldMeta.setType( columnType );
+						dataStoreMeta.addFiedMeta(fieldMeta);
+					}
+				    
+				    IField field = new Field(columnValue);
+					record.appendField(field);
+			    }
+			    
+			    dataStore.appendRecord(record);
+			}
+			 
+			 
+		} catch (Throwable t) {
+			logger.error("Exception reading data", t);
 		} finally{
 			if(inputDataStream!=null)
 				try {
@@ -108,5 +121,15 @@ public class XmlDataReader extends AbstractDataReader {
 
 		return dataStore;
 	}
+	
+	private NodeList readXMLNodes(Document doc, String xpathExpression) throws Exception {
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        XPathExpression expr = xpath.compile(xpathExpression);
+ 
+        Object result = expr.evaluate(doc, XPathConstants.NODESET);
+        NodeList nodes = (NodeList) result;
+ 
+        return nodes;
+    }
 
 }
