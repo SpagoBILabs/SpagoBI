ecc4d1686610e991a40146ecbb5566161fc79cb0
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-06-20 12:58:54 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@15776 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIGeoReportEngine/src/it/eng/spagobi/engines/georeport/services/GeoReportEngineStartAction.java
+++ SpagoBIGeoReportEngine/src/it/eng/spagobi/engines/georeport/services/GeoReportEngineStartAction.java
@@ -86,7 +86,7 @@ public class GeoReportEngineStartAction extends AbstractEngineStartServlet {
     			throw new SpagoBIServiceException(ENGINE_NAME, "An error occurred while dispatching request to [" + REQUEST_DISPATCHER_URL + "]", t);
     		} 
         } catch(Throwable t) {
-        	logger.debug("Impossible to execute document", t);
+        	logger.error("Impossible to execute document", t);
         	t.printStackTrace();
         	throw new SpagoBIServiceException(ENGINE_NAME, t);
         } finally {
