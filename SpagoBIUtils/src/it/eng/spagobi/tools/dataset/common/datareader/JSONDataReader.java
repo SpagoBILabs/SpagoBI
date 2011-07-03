2e33832c6e0d53a44f8070ada60dd50a837a29c8
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-07-03 17:20:42 +0000

    added JSONDataReader class
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@15855 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datareader/JSONDataReader.java
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datareader/JSONDataReader.java
@@ -9,12 +9,19 @@ import it.eng.spagobi.tools.dataset.common.datastore.Field;
 import it.eng.spagobi.tools.dataset.common.datastore.FieldMetadata;
 import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
 import it.eng.spagobi.tools.dataset.common.datastore.IField;
+import it.eng.spagobi.tools.dataset.common.datastore.IFieldMetaData;
 import it.eng.spagobi.tools.dataset.common.datastore.IRecord;
 import it.eng.spagobi.tools.dataset.common.datastore.Record;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringBufferInputStream;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -24,6 +31,8 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
@@ -34,7 +43,8 @@ import org.w3c.dom.NodeList;
  */
 public class JSONDataReader extends AbstractDataReader {
 
-	DocumentBuilderFactory domFactory;
+	private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat( "dd/MM/yyyy" );
+	private static final SimpleDateFormat TIMESTAMP_FORMATTER = new SimpleDateFormat( "dd/MM/yyyy HH:mm:ss" );
 	
 	private static transient Logger logger = Logger.getLogger(JSONDataReader.class);
 
@@ -42,91 +52,121 @@ public class JSONDataReader extends AbstractDataReader {
 		super();        
 	}
 
-
-
 	public IDataStore read( Object data ) {
 		DataStore dataStore;
 		DataStoreMetaData dataStoreMeta;
-
-	
+		JSONObject parsedData;
 
 		logger.debug("IN");
 		
 		dataStore = null;
+		parsedData = null;
 
-//		if (!(data instanceof InputStream)) {
-//			inputDataStream = new StringBufferInputStream((String)data);
-//		}
-//		else{
-//			inputDataStream = (InputStream)data;
-//		}
-//
-//		dataStore = new DataStore();
-//		dataStoreMeta = new DataStoreMetaData();
-//		dataStore.setMetaData(dataStoreMeta);
-//
-//
-//		try {
-//			documentBuilder = domFactory.newDocumentBuilder();
-//			
-//			Document document =  documentBuilder.parse(inputDataStream);
-//			
-//			NodeList nodes = readXMLNodes(document, "/ROWS/ROW");
-//			
-//			if(nodes == null) {
-//				throw new RuntimeException("Malformed data. Impossible to find tag rows.row");
-//			}
-//
-//			int rowNumber = nodes.getLength();
-//			boolean firstRow = true;
-//			for (int i = 0; i < rowNumber; i++, firstRow = false) {
-//				IRecord record = new Record(dataStore);
-//				
-//			    NamedNodeMap nodeAttributes = nodes.item(i).getAttributes();
-//			    for(int j = 0; j < nodeAttributes.getLength(); j++) {
-//				    Node attribute = nodeAttributes.item(j);
-//				    String columnName = attribute.getNodeName();
-//				    String columnValue = attribute.getNodeValue();
-//				    Class columnType = attribute.getNodeValue().getClass();
-//				    
-//				    if(firstRow==true) {
-//						FieldMetadata fieldMeta = new FieldMetadata();
-//						fieldMeta.setName( columnName );
-//						fieldMeta.setType( columnType );
-//						dataStoreMeta.addFiedMeta(fieldMeta);
-//					}
-//				    
-//				    IField field = new Field(columnValue);
-//					record.appendField(field);
-//			    }
-//			    
-//			    dataStore.appendRecord(record);
-//			}
-//			 
-//			 
-//		} catch (Throwable t) {
-//			logger.error("Exception reading data", t);
-//		} finally{
-//			if(inputDataStream!=null)
-//				try {
-//					inputDataStream.close();
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//					logger.error("IOException during File Closure");
-//				}
-//		}
+		dataStore = new DataStore();
+		dataStoreMeta = new DataStoreMetaData();
+		dataStore.setMetaData(dataStoreMeta);
+
+
+		try {
+			if (!(data instanceof JSONObject)) {
+				parsedData = new JSONObject( (String)data );
+			}
+			else{
+				parsedData = (JSONObject)data;
+			}
+			JSONObject parsedMeta = parsedData.optJSONObject("metaData");
+			
+			
+			// init meta 
+			if(parsedMeta == null) {
+				throw new RuntimeException("Malformed data. Impossible to find attribute [" + "metaData" + "]");
+			}
+			String root = parsedMeta.optString("root");
+			if(root == null) {
+				throw new RuntimeException("Malformed meta data. Impossible to find attribute [" + "root" + "]");
+			}
+			JSONArray parsedFieldsMeta = parsedMeta.optJSONArray("fields");
+			if(parsedFieldsMeta == null) {
+				throw new RuntimeException("Malformed meta data. Impossible to find attribute [" + "fields" + "]");
+			}
+			for(int i = 1; i < parsedFieldsMeta.length(); i++) { // skip "recNo"
+				JSONObject parsedFieldMeta = parsedFieldsMeta.getJSONObject(i);
+				String columnName = parsedFieldMeta.getString("dataIndex");
+			    String columnHeader = parsedFieldMeta.getString("header");
+			    String columnType = parsedFieldMeta.getString("type");
+			    
+			    FieldMetadata fieldMeta = new FieldMetadata();
+				fieldMeta.setName( columnName );
+				fieldMeta.setAlias(columnHeader);
+				if(columnType.equalsIgnoreCase("string")) {
+					fieldMeta.setType( String.class );
+				}else if(columnType.equalsIgnoreCase("int")) {
+					fieldMeta.setType( BigInteger.class );
+				} else if(columnType.equalsIgnoreCase("float")) {
+					fieldMeta.setType( Double.class );
+				} else if(columnType.equalsIgnoreCase("date")) {
+					String subtype = parsedFieldMeta.optString("subtype");
+					if(subtype != null && subtype.equalsIgnoreCase("timestamp")) {
+						fieldMeta.setType( Timestamp.class );
+					} else {
+						fieldMeta.setType( Date.class );
+					}
+				} else if(columnType.equalsIgnoreCase("boolean")) {
+					fieldMeta.setType( Boolean.class );
+				} else {
+					throw new RuntimeException("Impossible to resolve column type [" + columnType + "]");
+				}
+				
+				dataStoreMeta.addFiedMeta(fieldMeta);
+			}
+			
+			
+			// init results 
+			JSONArray parsedRows = parsedData.optJSONArray(root);
+			if(parsedRows == null) {
+				throw new RuntimeException("Malformed data. Impossible to find attribute [" + root + "]");
+			}
+
+			int rowNumber = parsedRows.length();
+			for (int i = 0; i < rowNumber; i++) {
+				JSONObject parsedRow = parsedRows.getJSONObject(i);
+				IRecord record = new Record(dataStore);
+				for(int j = 0; j < dataStoreMeta.getFieldCount(); j++) {
+					String columnName = dataStoreMeta.getFieldName(j);
+					String columnValue = parsedRow.getString(columnName);
+					IFieldMetaData fieldMeta = dataStoreMeta.getFieldMeta(j);
+			
+					
+					Class fieldType = fieldMeta.getType();
+					Object value = null;
+					if(fieldType == String.class) {
+						value = columnValue;
+					} else if(fieldType == BigInteger.class ) {
+						value = new BigInteger(columnValue);
+					} else if(fieldType == Double.class) {
+						value = new Double(columnValue);
+					} else if(fieldType ==  Timestamp.class) {
+						value = TIMESTAMP_FORMATTER.parse(columnValue);
+					} else if(fieldType ==  Date.class) {
+						value = DATE_FORMATTER.parse(columnValue);
+					} else if(fieldType ==  Boolean.class) {
+						value = new Boolean(columnValue);
+					} else {
+						throw new RuntimeException("Impossible to resolve field type [" + fieldType + "]");
+					}
+					IField field = new Field(value);
+					record.appendField(field);
+				}
+			    dataStore.appendRecord(record);
+			}
+			 
+			 
+		} catch (Throwable t) {
+			logger.error("Exception reading data", t);
+		} finally{
+			logger.debug("OUT");
+		}
 
 		return dataStore;
 	}
-	
-	private NodeList readXMLNodes(Document doc, String xpathExpression) throws Exception {
-        XPath xpath = XPathFactory.newInstance().newXPath();
-        XPathExpression expr = xpath.compile(xpathExpression);
- 
-        Object result = expr.evaluate(doc, XPathConstants.NODESET);
-        NodeList nodes = (NodeList) result;
- 
-        return nodes;
-    }
-
 }
---
+++ SpagoBIUtils/test/it/eng/spagobi/tools/dataset/bo/JDBCDataSetTest.java
@@ -0,0 +1,102 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2010 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
+package it.eng.spagobi.tools.dataset.bo;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import it.eng.spagobi.services.dataset.bo.SpagoBiDataSet;
+import it.eng.spagobi.services.datasource.bo.SpagoBiDataSource;
+import it.eng.spagobi.tools.dataset.common.datareader.JSONDataReader;
+import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
+import it.eng.spagobi.tools.dataset.common.datawriter.JSONDataWriter;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * @author Andrea Gioia (andrea.gioia@eng.it)
+ *
+ */
+public class JDBCDataSetTest  extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testDataSet() throws Exception {
+		SpagoBiDataSet dataSetConfig = new SpagoBiDataSet();
+		dataSetConfig.setQuery("SELECT fullname as 'Full Name' FROM CUSTOMER LIMIT 2");
+		
+		SpagoBiDataSource dataSourceConfig = new SpagoBiDataSource();
+		dataSourceConfig.setDriver(TestCaseConstants.CONNECTION_DRIVER);
+		dataSourceConfig.setHibDialectClass(TestCaseConstants.CONNECTION_DIALECT);
+		dataSourceConfig.setHibDialectName(TestCaseConstants.CONNECTION_DIALECT);
+		dataSourceConfig.setMultiSchema(false);
+		dataSourceConfig.setUser(TestCaseConstants.CONNECTION_USER);
+		dataSourceConfig.setPassword(TestCaseConstants.CONNECTION_PWD);
+		dataSourceConfig.setUrl(TestCaseConstants.CONNECTION_URL);
+		
+		dataSetConfig.setDataSource(dataSourceConfig);
+		
+		IDataSet dataset = new JDBCDataSet(dataSetConfig);
+		dataset.loadData();
+		IDataStore dataStore1 = dataset.getDataStore();
+		JSONDataWriter dataWriter = new JSONDataWriter();
+		
+		JSONObject o1 = (JSONObject)dataWriter.write(dataStore1);
+				
+		JSONObject m1 = o1.getJSONObject("metaData");
+		System.out.print(m1.toString(3));
+
+		System.out.print("----------------------");
+		
+		JSONArray d1 = o1.getJSONArray("rows");
+		System.out.print(d1.toString(3));
+		
+		System.out.print("\n\n==========================\n\n");
+		
+		JSONDataReader dataReader = new JSONDataReader();
+		IDataStore dataStore2 = dataReader.read(o1);
+		
+		
+		JSONObject o2 = (JSONObject)dataWriter.write(dataStore2);
+		
+		JSONObject m2 = o2.getJSONObject("metaData");
+		System.out.print(m2.toString(3));
+
+		System.out.print("----------------------");
+		
+		JSONArray d2 = o2.getJSONArray("rows");
+		System.out.print(d2.toString(3));
+		
+		
+		assertEquals(m1.toString(), m2.toString());
+		
+		assertEquals(d1.toString(), d2.toString());
+		
+	}
+
+}
