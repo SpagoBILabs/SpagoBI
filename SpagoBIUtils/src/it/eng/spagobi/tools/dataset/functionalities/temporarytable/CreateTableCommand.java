c330a3a66161569c5590dcdcbe998ad231a31400
Giulio Gavardi <giulio.gavardi@eng.it>
2011-09-28 14:49:31 +0000

    Temporary table created by dataset
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16256 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/functionalities/temporarytable/CreateTableCommand.java
@@ -0,0 +1,228 @@
+package it.eng.spagobi.tools.dataset.functionalities.temporarytable;
+
+
+import it.eng.spagobi.tools.dataset.common.metadata.IFieldMetaData;
+import it.eng.spagobi.tools.dataset.persist.DataSetTableDescriptor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+/** Utility class to crate dataset table creation command
+ * 
+ * @author gavardi
+ *
+ */
+
+public class CreateTableCommand {
+
+
+	public static transient Logger logger = Logger.getLogger(CreateTableCommand.class);
+
+	/**
+	 *  mapping String type to db type
+	 */
+	Map<String, String> sqlTypeMapping = null;
+	{
+		sqlTypeMapping = new HashMap<String, String>();
+		sqlTypeMapping.put("java.lang.Integer", "INTEGER");
+		sqlTypeMapping.put("java.lang.String", "VARCHAR");
+		sqlTypeMapping.put("java.lang.String0", "TEXT");
+		sqlTypeMapping.put("java.lang.Boolean", "TINYINT(1)");
+		sqlTypeMapping.put("java.lang.Float", "FLOAT");
+		sqlTypeMapping.put("java.lang.Double", "FLOAT");
+		sqlTypeMapping.put("java.util.Date", "DATE");
+	}
+
+	Map<String, String> oracleTypeMapping = null;
+	{
+		oracleTypeMapping = new HashMap<String, String>();
+		oracleTypeMapping.put("java.lang.Integer", "NUMBER");
+		oracleTypeMapping.put("java.lang.String", "VARCHAR2");
+		oracleTypeMapping.put("java.lang.String0", "CLOB");
+		oracleTypeMapping.put("java.lang.Boolean", "VARCHAR2(1)");
+		oracleTypeMapping.put("java.lang.Float", "NUMBER");
+		oracleTypeMapping.put("java.lang.Double", "NUMBER");
+		oracleTypeMapping.put("java.util.Date", "DATE");
+		oracleTypeMapping.put("java.sql.Date", "DATE");
+		oracleTypeMapping.put("java.sql.Timestamp", "TIMESTAMP");
+		oracleTypeMapping.put("oracle.sql.TIMESTAMP", "TIMESTAMP");
+		oracleTypeMapping.put("java.math.BigDecimal", "NUMBER");
+		
+		
+	}
+
+	// properties
+	public static final String SIZE ="size";
+	public static final String FORMAT ="format";
+	public static final String decimal ="decimal";
+
+	/**
+	 *  
+	 */
+	String tableName;
+	List<ColumnMeta> columns;
+
+	/**
+	 *  Mapping physical column to rea names
+	 */
+	//Map<String, String> physicalColMapping;
+	DataSetTableDescriptor dsTableDescriptor;
+
+	int counter=0;
+
+
+
+	public CreateTableCommand(String tableName) {
+		super();
+		this.tableName = tableName;
+		dsTableDescriptor = new DataSetTableDescriptor();
+		dsTableDescriptor.setTableName(tableName);
+	}
+
+
+	public void addColumn(IFieldMetaData fieldMeta) {
+		logger.debug("IN");
+		if(columns == null) columns = new ArrayList<ColumnMeta>();
+
+		Class fieldClass = fieldMeta.getType();
+		String name = fieldMeta.getName();
+		String alias = fieldMeta.getAlias();
+
+		Map<String, Object> properties = fieldMeta.getProperties();
+
+		//add column deifnition
+		ColumnMeta columnMeta = new ColumnMeta(name, fieldClass, properties);
+		columns.add(columnMeta);
+		logger.debug("OUT");
+	}
+
+
+	public String getTableName() {
+		return tableName;
+	}
+
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+
+
+	public String createSQLQuery(){
+		String query ="CREATE TABLE ";
+		query+=tableName+ " (";
+
+		// run al columns
+		for (Iterator iterator = columns.iterator(); iterator.hasNext();) {
+			ColumnMeta columnMeta = (ColumnMeta) iterator.next();
+
+			// assign physical name to column
+			String physicalName = "COL_"+counter;
+			query+=physicalName;
+
+			// type
+			//Integer size = columnMeta.getSize();
+			String typeJavaName = columnMeta.getType().getName();
+			query += writeType(typeJavaName, columnMeta.getProperties());
+
+			// semicolon separator
+			if(iterator.hasNext()){
+				query+=", ";
+			}
+
+			// ad field description
+			dsTableDescriptor.addField(columnMeta.getName(), physicalName, columnMeta.getType());
+			counter++;
+		}
+
+		//query+=");";
+		query+=")";
+		
+		logger.debug("Query is "+query);
+		System.out.println(query);
+		logger.debug("OUT");
+		return query;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private String writeType(String typeJavaName, Map properties){
+		// convert java type in SQL type
+		String queryType ="";
+		String typeSQL ="";
+
+		// proeprties
+		Integer size = null;
+		Integer precision = null;
+		Integer scale = null;
+
+		if(properties.get("size") != null) 
+			size = Integer.valueOf(properties.get("size").toString());
+		if(properties.get("precision") != null) 
+			precision = Integer.valueOf(properties.get("precision").toString());
+		if(properties.get("scale") != null) 
+			scale = Integer.valueOf(properties.get("scale").toString());
+
+
+		// particular case of VARCHAR and CLOB
+
+		if(typeJavaName.equalsIgnoreCase(String.class.getName())){
+			// varchar with no size is text
+			if((size == null || size == 0) ){
+				typeSQL = oracleTypeMapping.get(typeJavaName+"0");
+			}
+			else {
+				typeSQL = oracleTypeMapping.get(typeJavaName);				
+			}
+		}
+		else typeSQL = oracleTypeMapping.get(typeJavaName);
+
+
+		// write Type
+		queryType +=" "+typeSQL+""; 
+
+		if(typeJavaName.equalsIgnoreCase(String.class.getName())){
+			if( size != null && size!= 0){
+				queryType +="("+size+")";
+			}
+		}
+		else
+			if(typeJavaName.equalsIgnoreCase(Integer.class.getName())
+					||
+					typeJavaName.equalsIgnoreCase(Double.class.getName())
+					||
+					typeJavaName.equalsIgnoreCase(Float.class.getName())
+			){
+				if(precision != null && scale != null){
+					queryType+="("+precision+","+scale+")";
+				}
+
+			}
+				queryType+=" ";
+				return queryType;
+			}
+
+
+		public DataSetTableDescriptor getDsTableDescriptor() {
+			return dsTableDescriptor;
+		}
+
+
+
+
+
+
+	}
---
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/functionalities/temporarytable/DatasetTempTable.java
@@ -0,0 +1,156 @@
+package it.eng.spagobi.tools.dataset.functionalities.temporarytable;
+
+
+import it.eng.spagobi.tools.dataset.common.metadata.FieldMetadata;
+import it.eng.spagobi.tools.dataset.common.metadata.IFieldMetaData;
+import it.eng.spagobi.tools.dataset.common.metadata.MetaData;
+import it.eng.spagobi.tools.dataset.persist.DataSetTableDescriptor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+import java.util.Random;
+
+import org.apache.log4j.Logger;
+
+
+public class DatasetTempTable {
+
+	private static transient Logger logger = Logger.getLogger(DatasetTempTable.class);
+
+	/**
+	 *  Creates a table with columns got from metadata
+	 * @param conn
+	 * @param meta
+	 * @param tableName
+	 * @return
+	 * @throws Exception
+	 */
+
+	public static DataSetTableDescriptor createTemporaryTable(Connection conn, MetaData meta, String tableName){
+		logger.debug("IN");
+
+		DataSetTableDescriptor dstd = null;
+		boolean result= false;
+		Statement st = null;
+		String hqlQuery = null;
+
+		try {
+			CreateTableCommand createTableCommand = new CreateTableCommand(tableName);
+
+			// run through all columns in order to build the SQL columndefinition
+			for (Iterator iterator = meta.getFieldsMeta().iterator(); iterator.hasNext();) {
+				IFieldMetaData fieldMeta = (IFieldMetaData) iterator.next();
+				createTableCommand.addColumn(fieldMeta);
+			}
+
+			// after built columns create SQL Query
+			hqlQuery = createTableCommand.createSQLQuery();
+
+			// excute 
+			st = conn.createStatement();
+			result =  st.execute(hqlQuery);
+
+			dstd = createTableCommand.getDsTableDescriptor();
+
+		} catch (SQLException e) {
+			logger.error("Error in excuting statement "+hqlQuery, e);
+			System.out.println("Errore "+e);
+			return null;
+		}
+		finally{
+			try{
+				if( conn != null) conn.close();
+				if( st != null)	st.close();
+			}
+			catch (SQLException e) {
+				logger.warn("could not free resources ",e);
+			}
+		}
+
+		System.out.println("Query "+hqlQuery);
+		logger.debug("OUT");
+		return dstd;
+	}
+
+
+
+
+	//	public static void main(String[] args) throws Exception {
+	//
+	//		MetaData meta = new MetaData();
+	//
+	//		IFieldMetaData fieldMetaDataIntero = new FieldMetadata();
+	//		fieldMetaDataIntero.setAlias("Colonna alias intero");
+	//		fieldMetaDataIntero.setName("ColonnaIntera");
+	//		fieldMetaDataIntero.setType(java.math.BigDecimal.class);
+	//
+	//		IFieldMetaData fieldMetaDataStringa = new FieldMetadata();
+	//		fieldMetaDataStringa.setAlias("Colonna alias stringa");
+	//		fieldMetaDataStringa.setName("ColonnaStringa");
+	//		fieldMetaDataStringa.setProperty("size", 50);
+	//		fieldMetaDataStringa.setType(java.lang.String.class);
+	//		
+	//		IFieldMetaData fieldMetaDataBool = new FieldMetadata();
+	//		fieldMetaDataBool.setAlias("Colonna alias Bool");
+	//		fieldMetaDataBool.setName("ColonnaBool");
+	//		fieldMetaDataBool.setType(java.lang.Boolean.class);
+	//
+	//		IFieldMetaData fieldMetaDataStringa2 = new FieldMetadata();
+	//		fieldMetaDataStringa2.setAlias("Colonna alias text");
+	//		fieldMetaDataStringa2.setName("ColonnaTextCLOb");
+	//		fieldMetaDataStringa2.setType(java.lang.String.class);
+	//
+	//		IFieldMetaData fieldMetaDataFloat = new FieldMetadata();
+	//		fieldMetaDataFloat.setAlias("Colonna alias text");
+	//		fieldMetaDataFloat.setName("ColonnaFloatt");
+	//		fieldMetaDataFloat.setProperty("precision", 10);
+	//		fieldMetaDataFloat.setProperty("scale", 5);
+	//		fieldMetaDataFloat.setType(java.lang.Float.class);
+	//		
+	//		IFieldMetaData fieldMetaDataDate = new FieldMetadata();
+	//		fieldMetaDataDate.setAlias("Colonna alias Date");
+	//		fieldMetaDataDate.setName("ColonnaDate");
+	//		fieldMetaDataDate.setType(java.sql.Date.class);
+	//		
+	//		IFieldMetaData fieldMetaDataTime = new FieldMetadata();
+	//		fieldMetaDataTime.setAlias("Colonna alias Time");
+	//		fieldMetaDataTime.setName("ColonnaTime");
+	//		fieldMetaDataTime.setType(oracle.sql.TIMESTAMP.class);
+	//		
+	//		
+	//		meta.addFiedMeta(fieldMetaDataIntero);
+	//		meta.addFiedMeta(fieldMetaDataStringa);
+	//		meta.addFiedMeta(fieldMetaDataStringa2);
+	//		meta.addFiedMeta(fieldMetaDataFloat);
+	//		meta.addFiedMeta(fieldMetaDataBool);
+	//		meta.addFiedMeta(fieldMetaDataDate);
+	//		meta.addFiedMeta(fieldMetaDataTime);
+	//		
+	//		Connection connection = null;
+	//		try{
+	////			Class.forName( "com.mysql.jdbc.Driver" );
+	////			connection = DriverManager.getConnection("jdbc:mysql://localhost/foodmart", "root", "admin");
+	//			Class.forName( "oracle.jdbc.OracleDriver" );
+	//			connection = DriverManager.getConnection("jdbc:oracle:thin:@sibilla2:1521:REPO", "spagobi", "bispago");
+	//		}
+	//		catch (Exception e) {
+	//			// TODO: handle exception
+	//		}
+	//
+	//		// map alias to index
+	//
+	//		int r =new Random().nextInt();
+	//		if(r <0 ) r*=-1;
+	//		String tableName = "DatasetName"+r;
+	//		System.out.println(tableName);
+	//		DatasetTempTable.createTemporaryTable(connection, meta, tableName);
+	//	}
+
+
+
+}
+
+
