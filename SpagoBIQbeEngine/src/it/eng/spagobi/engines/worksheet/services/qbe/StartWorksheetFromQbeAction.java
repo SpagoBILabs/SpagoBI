e76db2df28d5d2740359414c14fe88e157621b84
Davide Zerbetto <davide.zerbetto@eng.it>
2011-10-03 13:20:03 +0000

    Bug fix: Qbe was not properly initialized
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16293 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/src/it/eng/spagobi/engines/worksheet/services/qbe/StartWorksheetFromQbeAction.java
+++ SpagoBIQbeEngine/src/it/eng/spagobi/engines/worksheet/services/qbe/StartWorksheetFromQbeAction.java
@@ -20,11 +20,14 @@
  **/
 package it.eng.spagobi.engines.worksheet.services.qbe;
 
+import it.eng.qbe.query.Query;
+import it.eng.qbe.query.catalogue.QueryCatalogue;
 import it.eng.spago.base.SourceBean;
 import it.eng.spagobi.engines.qbe.QbeEngineInstance;
 import it.eng.spagobi.engines.worksheet.WorksheetEngineInstance;
 import it.eng.spagobi.engines.worksheet.services.initializers.WorksheetEngineStartAction;
 import it.eng.spagobi.utilities.engines.EngineConstants;
+import it.eng.spagobi.utilities.engines.SpagoBIEngineRuntimeException;
 
 import org.apache.log4j.Logger;
 
@@ -46,6 +49,17 @@ public class StartWorksheetFromQbeAction extends WorksheetEngineStartAction {
     @Override
     protected QbeEngineInstance getQbeEngineInstance(WorksheetEngineInstance worksheetEngineInstance) {
     	QbeEngineInstance qbeEngineInstance = (QbeEngineInstance) getAttributeFromSession(EngineConstants.ENGINE_INSTANCE);
+    	// the following instructions are needed because: if a user enter a saved query and then click on the worksheet tab,
+    	// there is no active query and therefore worksheet initialization fails.
+    	Query query = qbeEngineInstance.getActiveQuery();
+    	if (query == null) {
+    		QueryCatalogue catalogue = qbeEngineInstance.getQueryCatalogue();
+    		if (catalogue == null || catalogue.getAllQueries().size() == 0) {
+    			logger.error("Query catalogue is empty: no queries are defined");
+    			throw new SpagoBIEngineRuntimeException("No queries are defined");
+    		}
+    		qbeEngineInstance.setActiveQuery(catalogue.getFirstQuery()); // this builds the statement object
+    	}
 		return qbeEngineInstance;
 	}
 }
