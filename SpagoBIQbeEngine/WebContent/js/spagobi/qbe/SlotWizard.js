5dfa5e2f6994e2603769365879f9380de15c5a63
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-10-05 15:00:44 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16322 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIQbeEngine/WebContent/js/spagobi/qbe/SlotWizard.js
@@ -0,0 +1,133 @@
+/**
+ * SpagoBI - The Business Intelligence Free Platform
+ *
+ * Copyright (C) 2004 - 2008 Engineering Ingegneria Informatica S.p.A.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.ZONE
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ **/
+ 
+/**
+  * CalculatedFieldWizard - short description
+  * 
+  * Object documentation ...
+  * 
+  * by Monica Franceschini
+  */
+
+Ext.ns("Sbi.qbe");
+
+Sbi.qbe.SlotWizard = function(config) {	
+	
+	var c = Ext.apply({}, config || {}, {
+		title: 'Slot wizard ...'
+		, width: 600
+		, height: 300
+		, hasBuddy: false		
+	});
+
+	Ext.apply(this, c);
+	
+	this.initMainPanel(c);	
+	
+	if(c.hasBuddy === 'true') {
+		this.buddy = new Sbi.commons.ComponentBuddy({
+    		buddy : this
+    	});
+	}
+	Sbi.qbe.SlotWizard.superclass.constructor.call(this, c);  
+	this.add(this.mainPanel);
+};
+
+Ext.extend(Sbi.qbe.SlotWizard, Ext.Window, {
+	
+	hasBuddy: null
+    , buddy: null
+   
+    , mainPanel: null
+    , firstCalculatedFiledPanel : null
+    , secondSlotDefinitionPanel: null
+    , getMainPanel : function(){
+		
+		return this.mainPanel;
+	}
+	, initMainPanel: function(c) {
+		var navHandler = function(direction){
+			if(this.mainPanel !== null){
+				var curr = this.mainPanel.layout.activeItem;
+				if(direction == 1){
+					this.mainPanel.layout.setActiveItem(1);
+					//this.mainPanel.toolbars.items[2].disabled = false;
+				}else{
+					//back
+					this.mainPanel.layout.setActiveItem(0);
+					//this.mainPanel.toolbars.items[0].disabled = true;
+				}
+			}
+		};
+		
+		var save = function(){
+			this.save();
+		};
+		
+		this.firstCalculatedFiledPanel = new Ext.Panel({
+		        id: 'card-0',
+		        layout: 'fit',
+		        height: 100,
+		        html: '<h1>Welcome to the Demo Wizard!</h1><p>Step 1 of 3</p><p>Please click the "Next" button to continue...</p>'  
+		});
+		this.secondSlotDefinitionPanel = new Ext.Panel({
+			id: 'card-1',  
+	        layout: 'fit',
+	        html: '<p>Step 2 of 2: complete!!!</p><p>Almost there.  Please click the "Next" button to continue...</p>'  
+	    });
+		
+		this.mainPanel = new Ext.Panel({  
+			    title: 'Card Layout (Wizard)',  
+			    layout: 'card',  
+			    activeItem: 0,  
+			    bodyStyle: 'padding:15px',
+			    scope: this,
+			    defaults: {border:false},  
+			    bbar: [
+			           {
+			               id: 'move-prev',
+			               text: 'Back',
+			               handler: navHandler.createDelegate(this, [-1]),
+			               disabled: true
+			           },
+			           '->', // greedy spacer so that the buttons are aligned to each side
+			           {
+			               id: 'move-next',
+			               text: 'Next',
+			               handler: navHandler.createDelegate(this, [1])
+			           },
+			           {
+			               id: 'finish',
+			               text: 'Finish',
+			               disabled: false,
+			               scope: this,
+			               handler : save.createDelegate(this) 
+			               
+			           }
+			    ], 
+			    items: [this.firstCalculatedFiledPanel,this.secondSlotDefinitionPanel]  
+		});    
+		this.mainPanel.doLayout();
+    }
+	, save: function(){
+		alert('save');
+	}
+});
