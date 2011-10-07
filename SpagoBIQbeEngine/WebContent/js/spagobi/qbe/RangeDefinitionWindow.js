2d547ad1903d26144c137a1e99838ddc9e700637
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-10-07 12:40:26 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16342 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIQbeEngine/WebContent/js/spagobi/qbe/RangeDefinitionWindow.js
@@ -0,0 +1,175 @@
+/**
+ * SpagoBI - The Business Intelligence Free Platform
+ *
+ * Copyright (C) 2004 - 2008 Engineering Ingegneria Informatica S.p.A.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.ZONE
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ **/
+ 
+/**
+  * RangeDefinitionWindow - short description
+  * 
+  * Object documentation ...
+  * 
+  * by Monica Franceschini
+  */
+
+Ext.ns("Sbi.qbe");
+
+Sbi.qbe.RangeDefinitionWindow = function(config) {	
+	
+	var c = Ext.apply({}, config || {}, {
+		title: 'Range Definition'
+		, width: 400
+		, height: 150
+		, hasBuddy: false	
+		
+	});
+
+	Ext.apply(this, c);
+	
+	this.initMainPanel(c);	
+	
+	if(c.hasBuddy === 'true') {
+		this.buddy = new Sbi.commons.ComponentBuddy({
+    		buddy : this
+    	});
+	}
+	Sbi.qbe.RangeDefinitionWindow.superclass.constructor.call(this, c);  
+	this.add(this.mainPanel);
+  
+};
+
+Ext.extend(Sbi.qbe.RangeDefinitionWindow, Ext.Window, {
+	
+	hasBuddy: null
+    , buddy: null
+    , slotPanel : null
+   
+    , mainPanel: null
+    , rangeFrom: null
+    , rangeTo: null
+    , rangeToSave: {}
+    
+	, initMainPanel: function(c) {
+		
+		this.slotPanel = c.slotPanel;
+		var record = c.record;
+		
+		var btnFinish = new Ext.Button({
+	        text: 'Save',
+	        disabled: false,
+	        scope: this,
+	        handler : this.save.createDelegate(this, [record])
+		});
+		
+	      var valuesFrom = new Ext.data.SimpleStore({
+	          fields: ['id', 'value'],
+	          data : [['1','&gt;'],['2','&gt;=']]
+	      });
+	      var valuesTo = new Ext.data.SimpleStore({
+	          fields: ['id', 'value'],
+	          data : [['1',Ext.util.Format.htmlEncode('<')],['2','&lt;=']]
+	      });
+	      
+		this.rangeFrom = new Ext.form.ComboBox({
+		    allowBlank: false,
+		    width: 50,
+		    triggerAction: 'all',
+		    lazyRender:true,
+		    mode: 'local',
+		    store: valuesFrom,
+		    valueField: 'id',
+		    displayField: 'value'
+		});
+		this.rangeFromValue = new Ext.form.TextField({
+			width: 70
+		});
+		
+		this.rangeTo = new Ext.form.ComboBox({
+		    allowBlank: false,
+		    width: 50,
+		    triggerAction: 'all',
+		    lazyRender:true,
+		    mode: 'local',
+		    store: valuesTo,
+		    valueField: 'id',
+		    displayField: 'value'
+		});
+		this.rangeToValue = new Ext.form.TextField({
+			width: 70
+		});
+		
+		this.mainPanel = new Ext.form.FormPanel({  
+			    layout: 'column',  
+			    scope: this,
+				width: 385,
+				height: 120,
+				
+			    defaults: {border:false},  
+			    bbar: ['->',
+			           btnFinish
+			    ], 
+			    items: [{
+			    	//rang from
+			        xtype:'fieldset',
+			        columnWidth: 0.48,
+			        layout: 'hbox',   
+			        title: 'MIN',
+			        labelWidth: 0,
+			        //autoHeight:true,
+			        style: 'margin: 2px; border: 1px solid silver; float: left;',
+			        defaultType: 'textfield',
+			        padding:5,
+			        items :[
+			                this.rangeFrom, this.rangeFromValue
+			        ]
+			    } , {
+			    	//rang to
+			        xtype:'fieldset',
+			        columnWidth: 0.48,
+			        layout: 'hbox',   
+			        labelWidth: 0,
+			        style: 'margin: 2px; border: 1px solid silver; float: left;',
+			        title: 'MAX',
+			        //autoHeight:true,
+			        padding:5,
+			        defaultType: 'textfield',
+			        items :[
+			                this.rangeTo, this.rangeToValue
+			        ]
+			    } ]
+		});  
+
+		this.mainPanel.doLayout();
+
+    }
+	, save: function(rec){
+		
+		this.rangeToSave.from ={};
+		this.rangeToSave.to ={};
+		this.rangeToSave.from.operand = this.rangeFrom.value;
+		this.rangeToSave.from.value = this.rangeFromValue.value;
+		this.rangeToSave.to.operand = this.rangeTo.value;
+		this.rangeToSave.to.value = this.rangeToValue.value;
+		
+		this.slotPanel.rangeToSave = this.rangeToSave;
+		this.slotPanel.addRange(this.rangeToSave, rec);
+		
+		this.close();
+	}
+
+});
--- SpagoBIQbeEngine/WebContent/js/spagobi/qbe/SlotEditorPanel.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/qbe/SlotEditorPanel.js
@@ -54,6 +54,8 @@ Ext.extend(Sbi.qbe.SlotEditorPanel, Ext.Panel, {
     gridPanel: null 
     , panelToolbar: null
     , valuesItemTemplate: null
+    , rangeWindow : null
+    , rangeToSave : null
     
 	, initToolbar: function(c){
 	
@@ -77,18 +79,20 @@ Ext.extend(Sbi.qbe.SlotEditorPanel, Ext.Panel, {
 	    // create the data store
 	    var store = new Ext.data.ArrayStore({
 	        fields: ['name', 'values'],
-	        data  : [['eta 1','da 0 a 10']]
+	        data  : [['eta 1',['da 0 a 10']]]
 	    });
 
 	    // manually load local data
 	    //store.loadData(emptyData);
 	    
         var template = new Ext.XTemplate(
-        		'<div style="border: 1px solid silver;border-radius:5px; padding: 2px;">' + 
-                '<span width="70px" style="vertical-align: top;">{values}</span>' + 
+        		'<tpl for="values">'+
+        		'<span width="150px" style="border: 1px solid silver;border-radius:5px; padding: 2px;">' + 
+                '<span style="vertical-align: top;">{values}</span>' + 
                 '<span align="right"><img onClick="test();" style="vertical-align: top;" src="../img/actions/close_icon-15.png"/>' +
                 '</span>'+
-                '</div>'
+                '</span>'+
+                '</tpl>'
              );  
              
         template.compile();
@@ -99,7 +103,45 @@ Ext.extend(Sbi.qbe.SlotEditorPanel, Ext.Panel, {
             xtype: 'templatecolumn',
             tpl : template
         });
-
+	    // button-columns
+	    var rangeButtonColumn = new Ext.grid.ButtonColumn(
+		    Ext.apply({
+		       dataIndex: 'range'
+		       , imgSrc: '../img/actions/range.gif'
+		       , clickHandler:function(e, t){
+		          var index = this.scope.gridPanel.getView().findRowIndex(t);
+		          var record = this.scope.gridPanel.store.getAt(index);
+		          //this.gridPanel.store.remove(record);
+		          //openpopup
+		          this.scope.openiInsertRangeWindow(record);
+		       }
+		       //, width: 20
+		       , header: 'Range'
+		       , renderer : function(v, p, record){
+		           return '<center><img class="x-mybutton-'+this.id+'" width="29px" height="16px" src="' + this.imgSrc + '"/></center>';
+		       }
+		       , scope: this
+		    })
+	    );
+	    var punctualButtonColumn = new Ext.grid.ButtonColumn(
+			    Ext.apply({
+			       dataIndex: 'dots'
+			       , imgSrc: '../img/actions/dots.gif'
+			       , clickHandler:function(e, t){
+			          var index = this.scope.gridPanel.getView().findRowIndex(t);
+			          var record = this.scope.gridPanel.store.getAt(index);
+			          //this.gridPanel.store.remove(record);
+			          //openpopup
+			       }
+			       //, width: 20
+			       , header: 'Punctual'
+				   , renderer : function(v, p, record){
+			           return '<center><img class="x-mybutton-'+this.id+'" width="21px" height="13px" src="' + this.imgSrc + '"/></center>';
+			       }
+			       , scope: this
+			    })
+		    );
+	    
 		this.gridPanel = new Ext.grid.EditorGridPanel({
 			store: store,
 			columns: [
@@ -110,9 +152,11 @@ Ext.extend(Sbi.qbe.SlotEditorPanel, Ext.Panel, {
                    dataIndex: 'name'
                },
                	   valuesColumn
+               	,  rangeButtonColumn
+               	,  punctualButtonColumn
                ],
 	        tbar: this.toolbar,
-	        clicksToEdit:1,
+	        clicksToEdit:2,
 	        frame: true,
 	        border:true,  
 	        style:'padding:0px',
@@ -121,23 +165,42 @@ Ext.extend(Sbi.qbe.SlotEditorPanel, Ext.Panel, {
 	        layout: 'fit',
 	        viewConfig: {
 	            forceFit: true
-	        },	
-
+	        },
+	        plugins :[rangeButtonColumn,  punctualButtonColumn],
 	        enableDragDrop:false	
 	    });
 		var btnAdd = this.panelToolbar.items.items[0];
-		btnAdd.on('click', function(){
-            // access the Record constructor through the grid's store
-            var Slot = this.gridPanel.getStore().recordType;
-            var p = new Slot({
-                name: 'New Slot 1',
-                values: 'New values'
-            });
-            this.gridPanel.stopEditing();
-            this.gridPanel.store.insert(0, p);
-            this.gridPanel.startEditing(0, 0);
-        }, this);
+		var Slot = this.gridPanel.getStore().recordType;
+        var p = new Slot({
+            name: 'New Slot 1',
+            values: []
+        });
+		btnAdd.on('click', this.createSlotRowToDisplay(p), this);
 	}
+	, openiInsertRangeWindow: function(rec){
+		this.rangeWindow = new Sbi.qbe.RangeDefinitionWindow({slotPanel: this, record: rec});
+		
+		this.rangeWindow.mainPanel.doLayout();
+		this.rangeWindow.show();
+	}
+	, createSlotRowToDisplay: function(p){
+        // access the Record constructor through the grid's store
+		
+        this.gridPanel.stopEditing();
+        this.gridPanel.store.insert(0, p);
+        this.gridPanel.startEditing(0, 0);
+	}
+	, addRange: function(rowIndex, rec){
+		var opFrom = rowIndex.from.operand;
+		var valFrom = rowIndex.from.value;
+		var opTo = rowIndex.to.operand;
+		var valTo = rowIndex.to.value;
+		var newVal = opFrom+' '+valFrom+ ' '+opTo+' '+valTo;
+
+		rec.data.values.push(newVal);
+		rec.commit();
+    }
+	
 });
 test= function(itemGroupName, expression) {
 	alert('funziona!!');
