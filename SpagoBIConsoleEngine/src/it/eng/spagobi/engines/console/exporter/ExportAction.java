8401b423b068e2cf6dc0f6a71539f504eb56abd7
Davide Zerbetto <davide.zerbetto@eng.it>
2011-10-11 15:28:42 +0000

    Changed field identifier on inner metadata fields map: before is was alias or name (if alias was not null), now it is just the name
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16398 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIConsoleEngine/src/it/eng/spagobi/engines/console/exporter/ExportAction.java
+++ SpagoBIConsoleEngine/src/it/eng/spagobi/engines/console/exporter/ExportAction.java
@@ -174,12 +174,12 @@ public class ExportAction extends AbstractConsoleEngineAction {
 					String key = (String)it.next();
 					JSONObject header = resultHeaders.getJSONObject(key);
 					String fieldHeader = header.optString("header", "");
-					String fieldHeaderType =  header.optString("headerType", "");		
-					// in case of dynamic headers gets the value from the dataset
-					if (fieldHeaderType.equalsIgnoreCase("dataset")){
-						int posHeader = dataStore.getMetaData().getFieldIndex(fieldHeader);
-						fieldHeader =((List)dataStore.getFieldValues(posHeader)).get(0).toString();
-					}
+//					String fieldHeaderType =  header.optString("headerType", "");		
+//					// in case of dynamic headers gets the value from the dataset
+//					if (fieldHeaderType.equalsIgnoreCase("dataset")){
+//						int posHeader = dataStore.getMetaData().getFieldIndex(fieldHeader);
+//						fieldHeader =((List)dataStore.getFieldValues(posHeader)).get(0).toString();
+//					}
 					
 					Field headerF = new Field(fieldHeader, "java.lang.String", 100);
 					extractedFields.add(headerF);
