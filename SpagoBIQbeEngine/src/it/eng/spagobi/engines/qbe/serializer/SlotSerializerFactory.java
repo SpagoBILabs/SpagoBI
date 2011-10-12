29829d6ea931b0baa657bc1db17d01688f9488fc
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-10-12 15:08:24 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16419 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIQbeEngine/src/it/eng/spagobi/engines/qbe/serializer/SlotSerializerFactory.java
@@ -0,0 +1,68 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2010 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
+package it.eng.spagobi.engines.qbe.serializer;
+
+import it.eng.qbe.model.structure.ModelCalculatedField.Slot;
+import it.eng.qbe.serializer.ISerializer;
+import it.eng.qbe.serializer.ISerializerFactory;
+import it.eng.qbe.serializer.SerializationManager;
+import it.eng.spagobi.engines.qbe.serializer.json.SlotJSONSerializer;
+import it.eng.spagobi.engines.worksheet.bo.Measure;
+import it.eng.spagobi.engines.worksheet.serializer.json.MeasureJSONSerializer;
+import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
+
+/**
+ * @author Andrea Gioia (andrea.gioia@eng.it)
+ *
+ */
+public class SlotSerializerFactory implements ISerializerFactory{
+
+	static SlotSerializerFactory instance;
+	
+	static SlotSerializerFactory getIntsnce() {
+		return instance;
+	}
+	
+	static {
+		instance = new SlotSerializerFactory();
+		SerializationManager.registerSerializerFactory(Slot.class, instance);
+	}
+	
+	
+	public static SlotSerializerFactory getInstance() {
+		if (instance == null) {
+			instance = new SlotSerializerFactory();
+		}
+		return instance;
+	}
+	
+	private SlotSerializerFactory() {}
+
+	
+	public ISerializer getSerializer(String mimeType) {
+		if (mimeType != null && !mimeType.equalsIgnoreCase("application/json")) {
+			throw new SpagoBIRuntimeException("Serializer for mimeType " + mimeType + " not implemented");
+		}
+		return new SlotJSONSerializer();
+	}
+
+}
