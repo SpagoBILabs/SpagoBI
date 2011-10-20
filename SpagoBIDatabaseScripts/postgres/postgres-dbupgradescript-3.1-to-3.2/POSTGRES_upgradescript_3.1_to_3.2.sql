0ca9cac7f6c3eda784e51b78b37b2a5936b907c0
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-10-20 09:33:28 +0000

    Worksheet engine is able to work with datasets
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16574 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/postgres/postgres-dbupgradescript-3.1-to-3.2/POSTGRES_upgradescript_3.1_to_3.2.sql
+++ SpagoBIDatabaseScripts/postgres/postgres-dbupgradescript-3.1-to-3.2/POSTGRES_upgradescript_3.1_to_3.2.sql
@@ -30,4 +30,6 @@ INSERT INTO SBI_DOMAINS (VALUE_ID, VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_D
 VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),
 	'Custom','SbiCustomDataSet','DATA_SET_TYPE','Data Set Type','SbiCustomDataSet', 'biadmin');
 	
-	ALTER TABLE sbi_data_set_history MODIFY COLUMN DS_METADATA TEXT
+ALTER TABLE sbi_data_set_history MODIFY COLUMN DS_METADATA TEXT;
+
+UPDATE SBI_ENGINES SET USE_DATASET = TRUE WHERE DRIVER_NM = 'it.eng.spagobi.engines.drivers.worksheet.WorksheetDriver';
