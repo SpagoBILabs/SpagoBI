4170950e00bdb0f86f57488b7c7d1c891c27dc2c
gioia <gioia>
2011-10-21 18:48:02 +0000

    removed the commented block of old constants (now they are located in Constants.js)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16601 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/commons/Settings.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/commons/Settings.js
@@ -2,34 +2,6 @@ Ext.ns("Sbi.settings");
 
 Sbi.settings.qbe = {
 		
-		
-		// TODO move this to a dedicated file
-		/*
-		constants : {
-			// select clause field types (twins java constants are defined in class ISelectField)
-			FIELD_TYPE_SIMPLE: 'simple.field'
-			, FIELD_TYPE_CALCULATED: 'calculated.field'
-			, FIELD_TYPE_INLINE_CALCULATED: 'inline.calculated.field'
-			
-			// where clause operand types (twins java constants are defined in class AbstractStatement)
-			, OPERAND_TYPE_STATIC_VALUE: 'static.value'
-			, OPERAND_TYPE_SUBQUERY: 'subquery'
-			, OPERAND_TYPE_PARENT_FIELD: 'parent.query.field'
-			, OPERAND_TYPE_SIMPLE_FIELD: 'simple.field'
-			, OPERAND_TYPE_CALCULATED_FIELD: 'calculated.field'
-			, OPERAND_TYPE_INLINE_CALCULATED_FIELD: 'inline.calculated.field'
-				
-			// tree field types (twins java constants are defined in class ExtJsQbeTreeBuilder)
-			, NODE_TYPE_ENTITY: 'entity'
-			, NODE_TYPE_SIMPLE_FIELD: 'field'
-			, NODE_TYPE_CALCULATED_FIELD: 'calculatedField'
-			, NODE_TYPE_INLINE_CALCULATED_FIELD: 'inLineCalculatedField'
-			
-		}
-
-		*/
-
-		
 		queryBuilderPanel: {
 			enableTreeToolbar: true,
 			enableTreeTbPinBtn: true,
