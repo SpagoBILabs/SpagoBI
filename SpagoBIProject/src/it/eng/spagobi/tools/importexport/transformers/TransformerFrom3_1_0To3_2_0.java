35b0afd16103af8690af9e43590b072bdcb5cf61
Giulio Gavardi <giulio.gavardi@eng.it>
2011-11-03 09:56:16 +0000

    import-export from 3.1 to 3.2
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16681 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIProject/src/it/eng/spagobi/tools/importexport/transformers/TransformerFrom3_1_0To3_2_0.java
@@ -0,0 +1,100 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2008 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ **/
+package it.eng.spagobi.tools.importexport.transformers;
+
+import it.eng.spagobi.commons.utilities.GeneralUtilities;
+import it.eng.spagobi.tools.importexport.ITransformer;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.log4j.Logger;
+
+public class TransformerFrom3_1_0To3_2_0 implements ITransformer {
+
+	static private Logger logger = Logger.getLogger(TransformerFrom3_1_0To3_2_0.class);
+
+	public byte[] transform(byte[] content, String pathImpTmpFolder, String archiveName) {
+		logger.debug("IN");
+		try {
+			TransformersUtilities.decompressArchive(pathImpTmpFolder, archiveName, content);
+		} catch(Exception e) {
+			logger.error("Error while unzipping 3.1.0 exported archive", e);	
+		}
+		archiveName = archiveName.substring(0, archiveName.lastIndexOf('.'));
+		changeDatabase(pathImpTmpFolder, archiveName);
+		// compress archive
+		try {
+			content = TransformersUtilities.createExportArchive(pathImpTmpFolder, archiveName);
+		} catch (Exception e) {
+			logger.error("Error while creating creating the export archive", e);	
+		}
+		// delete tmp dir content
+		File tmpDir = new File(pathImpTmpFolder);
+		GeneralUtilities.deleteContentDir(tmpDir);
+		logger.debug("OUT");
+		return content;
+	}
+
+	private void changeDatabase(String pathImpTmpFolder, String archiveName) {
+		logger.debug("IN");
+		Connection conn = null;
+		try {
+			conn = TransformersUtilities.getConnectionToDatabase(pathImpTmpFolder, archiveName);
+			fixParView(conn);
+			conn.commit();
+		} catch (Exception e) {
+			logger.error("Error while changing database", e);	
+		} finally {
+			logger.debug("OUT");
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				logger.error("Error closing connection to export database", e);
+			}
+		}
+	}
+
+	private void fixParView(Connection conn) throws Exception {
+		logger.debug("IN");
+
+		Statement stmt = conn.createStatement();
+		
+		String sql = "CREATE TABLE SBI_OBJ_PARVIEW ( "+
+				  "OBJ_PAR_ID INTEGER NOT NULL, "+
+				   "OBJ_PAR_FATHER_ID  INTEGER NOT NULL, "+
+				   "OPERATION  VARCHAR(20) NOT NULL, "+
+				   "COMPARE_VALUE  VARCHAR(200) NOT NULL, "+
+				   "VIEW_LABEL  VARCHAR(50), "+
+				   "PROG INTEGER)";
+
+		stmt.executeUpdate(sql);
+
+		logger.debug("OUT");
+	}
+
+
+}
