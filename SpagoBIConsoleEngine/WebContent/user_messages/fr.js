97439c4b77b05040be0b614039356e0d980997fc
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-11-17 14:20:30 +0000

    Managed internationalization for grid's headers (from file .js or from specific dataset)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16779 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIConsoleEngine/WebContent/user_messages/fr.js
@@ -0,0 +1,9 @@
+Ext.ns("Sbi.locale");
+Sbi.locale.ln = Sbi.locale.ln || new Array();
+
+
+//===================================================================
+// USER MESSAGE EXAMPLES
+//===================================================================
+Sbi.locale.ln['cod_StoreSales'] = 'Revenus';
+Sbi.locale.ln['cod_StoreCost'] = 'Couts';
