26975dca794792c3f8312b1cd1f11a2cbb1ccabe
Alberto Ghedin <alberto.ghedin@eng.it>
2011-12-01 09:19:13 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16840 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/runtime/it/eng/qbe/query/Query.java
+++ QbeCore/src/runtime/it/eng/qbe/query/Query.java
@@ -21,7 +21,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.qbe.query;
 
-import it.eng.qbe.model.structure.ModelCalculatedField.Slot;
 import it.eng.spagobi.utilities.assertion.Assert;
 
 import java.util.ArrayList;
