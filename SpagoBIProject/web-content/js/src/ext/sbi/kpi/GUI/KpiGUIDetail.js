f4c1795d2b6b95de5ce3e9a7f15daedf6b88d107
monica franceschini <francesm@eng.it>
2011-12-01 08:17:24 +0000

    spedometer chart (Highcharts API)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16833 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext/sbi/kpi/GUI/KpiGUIDetail.js
+++ SpagoBIProject/web-content/js/src/ext/sbi/kpi/GUI/KpiGUIDetail.js
@@ -66,20 +66,85 @@ Ext.extend(Sbi.kpi.KpiGUIDetail , Ext.form.FormPanel, {
 	valueItem: null,
 	weightItem: null,
 	targetItem: null,
+	chartPanel : null,
+	chartid: null,
+	//chart
+	dial: null,
+	maxChartValue: 0,
+	ranges: new Array(),
 	
 	initDetail: function(){	
-		this.defaultType= 'label';		
+		this.chartid = Ext.id();
+		
+		this.defaultType= 'label';
+		this.style= 'padding-left:10px;';
+		this.border=false;
+		
+		
+		this.chartPanel = new Ext.Panel({
+			id: this.chartid,
+			border: false,
+			height: 5
+		});
+
 		this.threshFields = new Ext.form.FieldSet({
 	        // Fieldset thresholds
 	        xtype:'fieldset',
-	        layout:'fit',
 	        border: false,
 	        defaultType: 'fieldset',
 	        items: []
 	    });
-		this.items =[this.threshFields];
+		this.items =[this.chartPanel, this.threshFields];
 	}
-	, update:  function(field){
+	, calculateMax: function(threshold){
+		if(threshold.max > this.maxChartValue){
+			this.maxChartValue = threshold.max;
+		}
+	}
+	, calculateRange: function(thr){
+		var range = {from: thr.min, to: thr.max, color: thr.color};
+		this.ranges.push(range);
+	}
+	, drawChart: function(value){
+		if(this.dial == null){
+			// Build the dial
+			this.dial = drawDial({
+			    renderTo: this.chartid,
+			    value: value,
+			    centerX: 150,
+			    centerY: 130,
+			    min: 0,
+			    max: this.maxChartValue,
+			    
+			    minAngle: -Math.PI,
+			    maxAngle: 0,
+			    tickInterval: 20,
+			    ranges: this.ranges
+			    , pivotLength: 70 //arrow length
+			    , backgroundRadius: 120
+			    , arcMinRadius : 70
+			    , arcMaxRadius : 100
+			    , textRadius : 105
+			    , renderX : 300 //width of the area of the chart
+			    , renderY : 145 //height of the area of the chart
+			});
+		}else{
+			this.dial.setMax(this.maxChartValue);			
+			this.dial.setRanges(this.ranges);
+			this.dial.setTicks(this.maxChartValue);
+			this.dial.setValue(value);
+			this.dial.setCircle();
+		}
+		
+	}
+	, cleanPanel: function(){
+		if(this.chartPanel != null){
+			this.chartPanel.removeAll(true);
+			
+		}
+		if(this.threshFields != null){
+			this.threshFields.removeAll();
+		}
 		if(this.valueItem != null){
 			this.remove(this.valueItem);
 		}
@@ -89,15 +154,30 @@ Ext.extend(Sbi.kpi.KpiGUIDetail , Ext.form.FormPanel, {
 		if(this.targetItem != null){
 			this.remove(this.targetItem);
 		}
-		//value
+		this.maxChartValue =0;
+		this.ranges = new Array();
+	}
+	, update:  function(field){
+
+		//chart
 		var val = field.attributes.actual;
+
+		this.on('afterlayout',function(){
+			this.drawChart(val);
+		},this);
+		
+		
+		this.cleanPanel();
+		//value
 		this.valueItem = new Ext.form.DisplayField({fieldLabel: 'Valore', 
 													value: val, 
-													style: 'font-style: italic;'});
+													style: 'padding-left:5px; font-style: italic;'});
 		this.add(this.valueItem );
 		
+		//thresholds
+
 		var thrArray = field.attributes.thresholds;
-		this.threshFields.removeAll();
+
 		if(thrArray != undefined && thrArray !== null){
 			for(i=0; i<thrArray.length; i++){
 				var bItems = [];
@@ -127,19 +207,23 @@ Ext.extend(Sbi.kpi.KpiGUIDetail , Ext.form.FormPanel, {
 				              colorBox]);
 				
 				this.threshFields.add(thrLine);
-
-
+				//calculate chart options
+				this.calculateMax(thr);
+				this.calculateRange(thr);
 			}
 
 		}
+
 		//weight
 		var weight = field.attributes.weight;
-		this.weightItem = new Ext.form.DisplayField({fieldLabel: 'Peso', 
+		this.weightItem = new Ext.form.DisplayField({fieldLabel: 'Peso',
+													style: 'margin-left:5px;',
 													value: weight});
 		this.add(this.weightItem );
 		//target
 		var target = field.attributes.target;
 		this.targetItem = new Ext.form.DisplayField({fieldLabel: 'Target', 
+													style: 'padding-left:5px;',
 													value: target});
 		this.add(this.targetItem );
 		this.doLayout();
