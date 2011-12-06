b2492d9039ac08a1c17576c6f739712923707ae1
dzerbett <dzerbett"@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-12-06 08:39:07 +0000

    Basic configuration for XLSX exporters for QbE and Worksheet
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16866 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-3.2-to-3.3/MYSQL_upgradescript_3.2_to_3.3.sql
@@ -0,0 +1,15 @@
+INSERT INTO SBI_DOMAINS (VALUE_ID,VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS,USER_IN)
+	VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),	
+	'XLSX','XLSX','EXPORT_TYPE','Exporters type','Export type', 'biadmin');
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_DOMAINS';
+commit;
+INSERT INTO SBI_EXPORTERS (ENGINE_ID,DOMAIN_ID,DEFAULT_VALUE) 
+	VALUES ((SELECT ENGINE_ID FROM SBI_ENGINES WHERE DRIVER_NM = 'it.eng.spagobi.engines.drivers.worksheet.WorksheetDriver'),
+	(SELECT VALUE_ID FROM SBI_DOMAINS WHERE DOMAIN_CD = 'EXPORT_TYPE' AND VALUE_CD = 'XLSX'), 
+	false);
+commit;
+INSERT INTO SBI_EXPORTERS (ENGINE_ID,DOMAIN_ID,DEFAULT_VALUE) 
+	VALUES ((SELECT ENGINE_ID FROM SBI_ENGINES WHERE DRIVER_NM = 'it.eng.spagobi.engines.drivers.qbe.QbeDriver'),
+	(SELECT VALUE_ID FROM SBI_DOMAINS WHERE DOMAIN_CD = 'EXPORT_TYPE' AND VALUE_CD = 'XLSX'), 
+	false);
+commit;
