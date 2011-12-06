2c2aeb56a46e873dbb8218699eaaf3b0d1a0bad2
Giulio Gavardi <giulio.gavardi@eng.it>
2011-12-06 14:46:13 +0000

    Added table SBI_I18N_MESSAGES and domains relative to four languages (ITA; FRA; ENG; SPA)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@16888 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-3.2-to-3.3/INGRES_upgradescript_3.2_to_3.3.sql
+++ SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-3.2-to-3.3/INGRES_upgradescript_3.2_to_3.3.sql
@@ -1,15 +1,33 @@
-INSERT INTO SBI_DOMAINS (VALUE_ID,VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS,USER_IN)
-	VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),	
-	'XLSX','XLSX','EXPORT_TYPE','Exporters type','Export type', 'biadmin');\p\g
-UPDATE hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_DOMAINS';\p\g
-COMMIT;\p\g
-INSERT INTO SBI_EXPORTERS (ENGINE_ID,DOMAIN_ID,DEFAULT_VALUE) 
-	VALUES ((SELECT ENGINE_ID FROM SBI_ENGINES WHERE DRIVER_NM = 'it.eng.spagobi.engines.drivers.worksheet.WorksheetDriver'),
-	(SELECT VALUE_ID FROM SBI_DOMAINS WHERE DOMAIN_CD = 'EXPORT_TYPE' AND VALUE_CD = 'XLSX'), 
-	false);\p\g
-commit;\p\g
-INSERT INTO SBI_EXPORTERS (ENGINE_ID,DOMAIN_ID,DEFAULT_VALUE) 
-	VALUES ((SELECT ENGINE_ID FROM SBI_ENGINES WHERE DRIVER_NM = 'it.eng.spagobi.engines.drivers.qbe.QbeDriver'),
-	(SELECT VALUE_ID FROM SBI_DOMAINS WHERE DOMAIN_CD = 'EXPORT_TYPE' AND VALUE_CD = 'XLSX'), 
-	false);\p\g
-commit;\p\g
+CREATE TABLE SBI_I18N_MESSAGES (
+  LANGUAGE_CD INTEGER NOT NULL,
+  LABEL VARCHAR(200) NOT NULL,
+  MESSAGE NVARCHAR,
+  PRIMARY KEY (LANGUAGE_CD, LABEL)
+);\p\g
+
+ALTER TABLE SBI_I18N_MESSAGES ADD CONSTRAINT FK_SBI_I18N_MESSAGES FOREIGN KEY (LANGUAGE_CD) REFERENCES SBI_DOMAINS(VALUE_ID) ON DELETE RESTRICT;;
+
+
+INSERT INTO SBI_DOMAINS (VALUE_ID, VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS, USER_IN)
+VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),
+	'ITA','Italian','LANG','language ISO Code','Italian', 'biadmin');
+update hibernate_sequences set next_val = next_val+1 where  sequence_name = 'SBI_DOMAINS';
+commit;
+
+INSERT INTO SBI_DOMAINS (VALUE_ID, VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS, USER_IN)
+VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),
+	'ENG','English','LANG','language ISO Code','English', 'biadmin');
+update hibernate_sequences set next_val = next_val+1 where  sequence_name = 'SBI_DOMAINS';
+commit;
+
+INSERT INTO SBI_DOMAINS (VALUE_ID, VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS, USER_IN)
+VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),
+	'FRA','French','LANG','language ISO Code','French', 'biadmin');
+update hibernate_sequences set next_val = next_val+1 where  sequence_name = 'SBI_DOMAINS';
+commit;
+
+INSERT INTO SBI_DOMAINS (VALUE_ID, VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS, USER_IN)
+VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),
+	'SPA','Spanish','LANG','language ISO Code','Spanish', 'biadmin');
+update hibernate_sequences set next_val = next_val+1 where  sequence_name = 'SBI_DOMAINS';
+commit;
