89973d3dba311a675ca255fede5190e2e761a06f
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-12-20 21:25:56 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17020 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/wapp/presentation/MenuConfigurationHTMLTreeGenerator.java
+++ SpagoBIProject/src/it/eng/spagobi/wapp/presentation/MenuConfigurationHTMLTreeGenerator.java
@@ -4,7 +4,7 @@ package it.eng.spagobi.wapp.presentation;
 
 	SpagoBI - The Business Intelligence Free Platform
 
-	Copyright (C) 2005-2008 Engineering Ingegneria Informatica S.p.A.
+	Copyright (C) 2004 - 2011 Engineering Ingegneria Informatica S.p.A.
 
 	This library is free software; you can redistribute it and/or
 	modify it under the terms of the GNU Lesser General Public
