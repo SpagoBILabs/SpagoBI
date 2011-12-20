0de84732aed25694bd25f630961800a75806fbb3
Angelo Bernabei <angelo.bernabei@eng.it>
2011-12-20 21:25:56 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17020 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBITalendEngine/src/it/eng/spagobi/engines/talend/client/SpagoBITalendEngineClient.java
+++ SpagoBITalendEngine/src/it/eng/spagobi/engines/talend/client/SpagoBITalendEngineClient.java
@@ -1,5 +1,5 @@
 /**
-Copyright (c) 2005-2008, Engineering Ingegneria Informatica s.p.a.
+Copyright (C) 2004 - 2011, Engineering Ingegneria Informatica s.p.a.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
