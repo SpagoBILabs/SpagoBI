0411b380db59da9cba411d7c5d8c77abd3ab3674
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-12-21 14:06:44 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17084 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBITalendEngineCilentAPI/src/it/eng/spagobi/engines/talend/client/demo/SpagoBITalendEngineClientDemo.java
+++ SpagoBITalendEngineCilentAPI/src/it/eng/spagobi/engines/talend/client/demo/SpagoBITalendEngineClientDemo.java
@@ -1,34 +1,14 @@
-/**
-Copyright (C) 2004 - 2011, Engineering Ingegneria Informatica s.p.a.
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of 
-      conditions and the following disclaimer.
-      
-    * Redistributions in binary form must reproduce the above copyright notice, this list of 
-      conditions and the following disclaimer in the documentation and/or other materials 
-      provided with the distribution.
-      
-    * Neither the name of the Engineering Ingegneria Informatica s.p.a. nor the names of its contributors may
-      be used to endorse or promote products derived from this software without specific
-      prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
-**/
+/*
+ * SpagoBI, the Open Source Business Intelligence suite
+ * © 2005-2015 Engineering Group
+ *
+ * This file is part of SpagoBI. SpagoBI is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 2.1 of the License, or any later version. 
+ * SpagoBI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received
+ * a copy of the GNU Lesser General Public License along with SpagoBI. If not, see: http://www.gnu.org/licenses/.
+ * The complete text of SpagoBI license is included in the COPYING.LESSER file. 
+ */
 package it.eng.spagobi.engines.talend.client.demo;
 
 import it.eng.spagobi.engines.talend.client.ISpagoBITalendEngineClient;
