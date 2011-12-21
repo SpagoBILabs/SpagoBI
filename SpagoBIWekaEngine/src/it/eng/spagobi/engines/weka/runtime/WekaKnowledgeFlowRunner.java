5bc60e0f384fa4cb457e9e5c08de6459e0b65a59
monica franceschini <francesm@eng.it>
2011-12-21 16:10:14 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17120 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWekaEngine/src/it/eng/spagobi/engines/weka/runtime/WekaKnowledgeFlowRunner.java
+++ SpagoBIWekaEngine/src/it/eng/spagobi/engines/weka/runtime/WekaKnowledgeFlowRunner.java
@@ -1,13 +1,9 @@
 /*
  * SpagoBI, the Open Source Business Intelligence suite
  * © 2005-2015 Engineering Group
- *
- * This file is part of SpagoBI. SpagoBI is free software: you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation, either version 2.1 of the License, or any later version. 
- * SpagoBI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received
- * a copy of the GNU Lesser General Public License along with SpagoBI. If not, see: http://www.gnu.org/licenses/.
- * The complete text of SpagoBI license is included in the COPYING.LESSER file. 
+ * 
+ * LICENSE: see WEKA.LICENSE.txt file
+ * 
  */
 package it.eng.spagobi.engines.weka.runtime;
 
