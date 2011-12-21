3cc0b532dc802e2131374dda25e4847217a3103f
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-12-21 16:14:40 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17122 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIJPaloEngine/src/com/tensegrity/wpalo/client/ui/mvc/fasttree/DoubleClickListener.java
+++ SpagoBIJPaloEngine/src/com/tensegrity/wpalo/client/ui/mvc/fasttree/DoubleClickListener.java
@@ -1,3 +1,10 @@
+/*
+ * SpagoBI, the Open Source Business Intelligence suite
+ * © 2005-2015 Engineering Group
+ * 
+ * LICENSE: see JPALO.LICENSE.txt file
+ * 
+ */
 package com.tensegrity.wpalo.client.ui.mvc.fasttree;
 
 public interface DoubleClickListener {
