b351b2e29553d6f0dde343fa6b48553d127f832f
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-12-21 13:44:43 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17075 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/kpi/config/metadata/SbiKpi.java
+++ SpagoBIProject/src/it/eng/spagobi/kpi/config/metadata/SbiKpi.java
@@ -1,3 +1,14 @@
+/*
+ * SpagoBI, the Open Source Business Intelligence suite
+ * © 2005-2015 Engineering Group
+ *
+ * This file is part of SpagoBI. SpagoBI is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 2.1 of the License, or any later version. 
+ * SpagoBI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received
+ * a copy of the GNU Lesser General Public License along with SpagoBI. If not, see: http://www.gnu.org/licenses/.
+ * The complete text of SpagoBI license is included in the COPYING.LESSER file. 
+ */
 package it.eng.spagobi.kpi.config.metadata;
 
 // Generated 18-feb-2009 17.14.10 by Hibernate Tools 3.2.2.GA
