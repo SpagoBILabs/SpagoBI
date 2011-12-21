6c6b66d15fbe24d05f9f6eeaf36fd0bfef1b9fb9
monica franceschini <francesm@eng.it>
2011-12-21 16:06:57 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17117 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIJPXMLAEngine/src/com/tonbeller/jpivot/mondrian/script/ScriptColumn.java
+++ SpagoBIJPXMLAEngine/src/com/tonbeller/jpivot/mondrian/script/ScriptColumn.java
@@ -1,6 +1,8 @@
-/**
+/*
+ * SpagoBI, the Open Source Business Intelligence suite
+ * © 2005-2015 Engineering Group
  * 
- * LICENSE: see LICENSE.html file
+ * LICENSE: see JPIVOT.LICENSE.txt file
  * 
  */
 package com.tonbeller.jpivot.mondrian.script;
