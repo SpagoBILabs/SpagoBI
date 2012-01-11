a3ba96dbb59ec40bcabc6ffca5622c205a33b6db
dzerbett <dzerbett"@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-01-11 16:40:38 +0000

    RESOLVED - issue SPAGOBI-697: Multivalue analytical drivers don't work on QbE https://spagobi.eng.it/jira/browse/SPAGOBI-697
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17194 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/jpa/it/eng/qbe/statement/jpa/JPQLStatement.java
+++ QbeCore/src/jpa/it/eng/qbe/statement/jpa/JPQLStatement.java
@@ -29,12 +29,17 @@ import it.eng.qbe.query.Query;
 import it.eng.qbe.statement.AbstractStatement;
 import it.eng.spagobi.utilities.StringUtils;
 import it.eng.spagobi.utilities.assertion.Assert;
+import it.eng.spagobi.utilities.engines.EngineConstants;
 import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -70,7 +75,8 @@ public class JPQLStatement extends AbstractStatement {
 
 		if(getParameters() != null) {
 			try {
-				queryStr = StringUtils.replaceParameters(queryStr.trim(), "P", getParameters());
+				Map parametersAsString = getParametersAsString();
+				queryStr = StringUtils.replaceParameters(queryStr.trim(), "P", parametersAsString);
 			} catch (IOException e) {
 				throw new SpagoBIRuntimeException("Impossible to set parameters in query", e);
 			}
@@ -79,6 +85,59 @@ public class JPQLStatement extends AbstractStatement {
 		
 		setQueryString(queryStr);
 		
+	}
+	
+	private Map<String, String> getParametersAsString() {
+		logger.debug("IN");
+		Map<String, String> toReturn = new HashMap<String, String>();
+		Map parameters = this.getParameters();
+		Set keys = parameters.keySet();
+		Iterator keysIt = keys.iterator();
+		while (keysIt.hasNext()) {
+			String aKey = (String) keysIt.next();
+			logger.debug("Evaluating parameter [" + aKey + "] ...");
+			Object value = parameters.get(aKey);
+			logger.debug("It's value is [" + aKey + "] ...");
+			if (value instanceof List) {
+				// only Lists are actual parameters
+				List values = (List) value;
+				String valuesAsString = getParametersAsString(values);
+				logger.debug("String representation is [" + valuesAsString + "] ...");
+				toReturn.put(aKey, valuesAsString);
+			}
+		}
+		logger.debug("OUT");
+		return toReturn;
+	}
+
+	private String getParametersAsString(List values) {
+		logger.debug("IN");
+		StringBuffer toReturn = new StringBuffer("");
+		if (values != null && !values.isEmpty()) {
+			Iterator it = values.iterator();
+			while ( it.hasNext() ) {
+				Object aValue = it.next();
+				logger.debug("Value is [" + aValue + "]");
+				Class clazz = aValue.getClass();
+				logger.debug("Class is [" + clazz + "]");
+				if ( aValue instanceof Date ) {
+					logger.debug("Value is a date");
+					String userDateFormatPattern = (String) getParameters().get(EngineConstants.ENV_USER_DATE_FORMAT);
+					logger.debug("Using date format [" + userDateFormatPattern + "] ...");
+					DateFormat userDataFormat = new SimpleDateFormat(userDateFormatPattern);
+					aValue = userDataFormat.format(aValue);
+					logger.debug("Date as a string is [" + aValue + "]");
+				}
+				String valueStr = getValueBounded(aValue.toString(), clazz.getName());
+				logger.debug("Value as a string is [" + valueStr + "]");
+				toReturn.append(valueStr);
+				if ( it.hasNext() ) {
+					toReturn.append(",");
+				}
+			}
+		}
+		logger.debug("OUT");
+		return toReturn.toString();
 	}
 	
 	/*
