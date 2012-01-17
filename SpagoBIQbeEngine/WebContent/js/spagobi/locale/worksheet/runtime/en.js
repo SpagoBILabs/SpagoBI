65aea38c023073b4815b4f986417c9662665bb5c
aghedin <aghedin@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-01-17 16:30:27 +0000

    hide series
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17232 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/locale/worksheet/runtime/en.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/worksheet/runtime/en.js
@@ -23,3 +23,9 @@ Sbi.locale.ln['sbi.worksheet.runtimeSheetFiltersPanel.errorwindow.title'] = 'Fil
 Sbi.locale.ln['sbi.worksheet.runtimeSheetFiltersPanel.errorwindow.text.mandatory'] = 'Those filters should have at least one value:';
 Sbi.locale.ln['sbi.worksheet.runtimeSheetFiltersPanel.errorwindow.text.toomuch'] = 'Those filters have too much values:';
 Sbi.locale.ln['sbi.worksheet.runtimeSheetFiltersPanel.errorwindow.text.maxselection'] = 'maximum allowed';
+
+
+//===================================================================
+//Sbi.worksheet.runtime.xxxChart
+//===================================================================
+Sbi.locale.ln['sbi.worksheet.runtime.worksheetruntimepanel.chart.visibleseries']= 'Visible series';
