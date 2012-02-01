09d01435bab53f4e76ec55de5812e217e8561e70
Giulio Gavardi <giulio.gavardi@eng.it>
2012-02-01 13:41:29 +0000

    Get parameters for metadata via web service
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17354 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/src/it/eng/spagobi/engines/worksheet/services/initializers/MassiveExportWorksheetEngineStartAction.java
+++ SpagoBIQbeEngine/src/it/eng/spagobi/engines/worksheet/services/initializers/MassiveExportWorksheetEngineStartAction.java
@@ -12,8 +12,14 @@
 package it.eng.spagobi.engines.worksheet.services.initializers;
 
 import it.eng.spago.base.SourceBean;
+import it.eng.spagobi.engines.worksheet.services.export.ExportWorksheetAction;
+import it.eng.spagobi.services.proxy.SbiDocumentServiceProxy;
+
+import java.util.Locale;
 
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * @authors Giulio Gavardi
@@ -28,30 +34,63 @@ public class MassiveExportWorksheetEngineStartAction extends WorksheetEngineStar
 
 	@Override
 	public void service(SourceBean serviceRequest, SourceBean serviceResponse) {
+		logger.debug("IN");
 		super.service(serviceRequest, serviceResponse);
+		Locale locale = getLocale();
+		String docId = getDocumentId();
+		logger.debug("document Id: "+docId);
 
-	
+		SbiDocumentServiceProxy proxy = new SbiDocumentServiceProxy(getUserProfile().getUserId().toString(), getHttpSession());
+		logger.debug("recover paramters for metadata");		
+		try {
+			String jSonPars = proxy.getDocumentAnalyticalDriversJSON(Integer.valueOf(docId), locale.getLanguage(), locale.getCountry());
+			logger.debug("parameters for metadata "+jSonPars);		
+			if(jSonPars != null){
+				JSONArray array = new JSONArray(jSonPars);
+				// add name and values
+				for (int i =0; i <array.length(); i++){
+					JSONObject par = (JSONObject) array.get(i);
+					String name = par.getString("label");
+					String nameDescription = getAttributeAsString(name+"_description");
+					String value = getAttributeAsString(name);
+					// put name, description and value
+					par.put("name", name);
+					if(nameDescription != null) par.put("description", nameDescription);
+					if(value != null) par.put("value", value);				
+				}
+				logger.debug("add parameters JSON array "+array);
+				serviceResponse.setAttribute(ExportWorksheetAction.PARAMETERS, array);
+			}
 
+		} catch (Exception e) {
+			logger.debug("Error in retrieving parameters information for metadata purpose, go on aniway");
+		}
+
+
+
+
+
+		logger.debug("OUT");
 	}
 
-	
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
