0fa844daf36f37da0ac28fd73c08ac134b1bf8b5
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-02-01 12:34:21 +0000

    refactored object MassiveExportWizardWindow.js
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17349 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext/sbi/tools/massiveExport/MassiveExportWizardWindow.js
+++ SpagoBIProject/web-content/js/src/ext/sbi/tools/massiveExport/MassiveExportWizardWindow.js
@@ -58,293 +58,335 @@ Sbi.tools.massiveExport.MassiveExportWizardWindow = function(config) {
 			, hasBuddy: false
 			, resizable: true
 	};
-
-	var c = Ext.apply(defaultSettings, config || {});
-
-	Ext.apply(this, c);
+	if (Sbi.settings && Sbi.settings.tools 
+			&& Sbi.settings.tools.massiveExport && Sbi.settings.tools.massiveExport.massiveExportWizardWindow) {
+		defaultSettings = Ext.apply(defaultSettings, Sbi.settings.execution.parametersPanel);
+	}
 	
+	var c = Ext.apply(defaultSettings, config || {});	
+	Ext.apply(this, c);
 
-
-	this.services = new Array();
+	this.services = this.services || new Array();
 
 	var params = {LIGHT_NAVIGATOR_DISABLED: 'TRUE', SBI_EXECUTION_ID: null, TYPE: 'WORKSHEET'};
 	this.services['StartMassiveExportExecutionProcessAction'] = this.services['StartMassiveExportExecutionProcessAction'] || Sbi.config.serviceRegistry.getServiceUrl({
 		serviceName: 'START_MASSIVE_EXPORT_EXECUTION_PROCESS_ACTION'
 		, baseParams: new Object()
-		});
+	});	
 	this.services['getAnalyticalDriversFromDocsInFolderAction'] = this.services['getAnalyticalDriversFromDocsInFolderAction'] || Sbi.config.serviceRegistry.getServiceUrl({
-	serviceName: 'GET_ANALYTICAL_DRIVER_FROM_DOCS_IN_FOLDER_ACTION'
-	, baseParams: new Object()
-	});
+		serviceName: 'GET_ANALYTICAL_DRIVER_FROM_DOCS_IN_FOLDER_ACTION'
+			, baseParams: new Object()
+	});	
 	this.services['startMassiveExportThreadAction'] = this.services['startMassiveExportThreadAction'] || Sbi.config.serviceRegistry.getServiceUrl({
 		serviceName: 'START_MASSIVE_EXPORT_THREAD_ACTION'
 		, baseParams: new Object()
-		});
+	});
 	
-	this.addEvents();
-
+	//this.addEvents();
+	this.initMainPanel(c);	
 	c = Ext.apply(c, {
 		layout: 'fit'
-		//,  html:'swswsws'
+		, items: [this.mainPanel]	
 	});
 
 	// constructor
 	Sbi.tools.massiveExport.MassiveExportWizardWindow.superclass.constructor.call(this, c);
 	
-	this.initMainPanel(c);
-	
-	this.add(this.mainPanel);
-	
-	this.addEvents();
-	
-
-	
-
+	//this.addEvents();
 };
 
 Ext.extend(Sbi.tools.massiveExport.MassiveExportWizardWindow, Ext.Window, {
 
 	services: null
     , mainPanel: null
-    , optionsPanel: null
-    , parametersPanel : null
+    , btnPrev: null
+	, btnNext: null
+	, btnFinish: null
+	, activePageNumber: null
+	, pages: null
+	
+    , optionsPage: null
+    , parametersPage: null
+    , OPTIONS_PAGE_NUMBER: null
+	, PARAMETERS_PAGE_NUMBER: null
+    
     , functId: null
     , functCd: null
 	, executionInstances: null
-    , btnPrev: null
-	, btnNext: null
-	 , btnFinish: null
     
-    , initMainPanel: function(c) {
-			var config = {functId: this.functId};
+	// ----------------------------------------------------------------------------------------
+	// private methods
+	// ----------------------------------------------------------------------------------------
 	
-			var navHandler = function(page){
-				if(this.mainPanel !== null){
-					var curr = this.mainPanel.layout.activeItem;
-				if(page == 1){ // PARAMETER PANEL CASE
-						// clear the fields in case you are coming to panel for the second time
-						this.parametersPanel.clear();
-
-						this.mainPanel.layout.setActiveItem(1);
-						this.btnNext.disable();
-						this.btnPrev.enable();
-						this.btnFinish.enable();
-				
-						// create ExecutionInstances and  get parameters 
-						var selRole = this.mainPanel.getComponent(0).getSelectedRole();		
-							var pars = {
-									selectedRole : selRole
-									, functId : this.functId
-									, type : 'WORKSHEET'						
-							}				
-						this.createExecutionInstances(pars);
+	/**
+	 * @returns whether this wizard could be finished without further user interaction. 
+	 * Typically, this is used by the wizard to enable or disable the Finish button.
+	 */
+	, canFinish: function() {
+		return this.activePageNumber == this.PARAMETERS_PAGE_NUMBER;
+	}
+    
+	/**
+	 * Called by the wizard when the Finish button is pressed.
+	 */
+	, performFinish: function() {
+		// get all values
+		var state = this.getParametersPanelFormState();
+		var jsonState = Sbi.commons.JSON.encode( state );
+		var selRole = this.optionsPage.getSelectedRole();
+		var splittingFiltersB = this.optionsPage.isCycleOnFilterSelected();
+		var params = {
+	    	selectedRole : selRole
+	       	, functId : this.functId
+	       	, type : 'WORKSHEET'
+	       	, splittingFilter : splittingFiltersB
+	       	, parameterValues : jsonState
+		};
 		
+		// Start mamssive export
+		Ext.Ajax.request({
+		        url: this.services['startMassiveExportThreadAction']
+		        , params: params
+		        , success : function(response, options){}
+				, failure: Sbi.exception.ExceptionHandler.handleFailure      
+				, scope: this
+		});
+		
+		var messageBox = Ext.MessageBox.show({
+				title: 'Status',
+				msg: LN('Export thread started for worksheet in functionality '+this.functCd+"; check Progress Panel on the left to know progress"),
+				modal: true,
+				buttons: Ext.MessageBox.OK,
+				width:500,
+				icon: Ext.MessageBox.INFO,
+				animEl: 'root-menu'        			
+		});
+		this.close();
+	}
+	
+    // ----------------------------------------------------------------------------------------
+	// private methods
+	// ----------------------------------------------------------------------------------------
+	
+    , initMainPanel: function(c) {
+    	
+    	this.initButtons();
+		this.initPages()
+		
+	
+		this.mainPanel = new Ext.Panel({  
+			layout: 'card',  
+			activeItem: 0,  
+			scope: this,
+			height: 420,
+			autoWidth: true,
+			resizable: true,
+			defaults: {border:false},  
+			bbar: [
+			       this.btnPrev,
+			       '->', // greedy spacer so that the buttons are aligned to each side
+			       this.btnNext,
+			       this.btnFinish
+			], 
+			items: this.pages
+		});
+		
+		this.mainPanel.doLayout();
+		
+		this.activePageNumber = 0;
+    }
 
-
-			} else{
-				//back
-				this.mainPanel.layout.setActiveItem(0);
-				//this.firstCalculatedFiledPanel.detailsFormPanel.syncSize()();
-				this.mainP
-				this.btnPrev.disabled = true;
-				this.btnNext.disabled = false;
-				this.btnFinish.disabled = true;
-				
-				this.btnPrev.disable();
-				this.btnNext.enable();
-				this.btnFinish.disable();
-			}
-		}
-	};
-	
-	this.btnPrev = new Ext.Button({
-        id: 'move-prev',
-        text: LN('Sbi.tools.massiveExport.MassiveExportWizardWindow.back'),
-        handler: navHandler.createDelegate(this, [-1]),
-        disabled : true
-	});
-	
-	this.btnNext = new Ext.Button({
-        id: 'move-next',
-        text: LN('Sbi.tools.massiveExport.MassiveExportWizardWindow.next'),
-        handler: navHandler.createDelegate(this, [1])
-	});
-	
-	this.btnFinish = new Ext.Button({
-        id: 'finish',
-        text: LN('Sbi.tools.massiveExport.MassiveExportWizardWindow.finish'),
-        disabled: false,
-        scope: this,
-        disabled: true,
-        handler: function(){
-			  this.finishButtonPressed();
-    			var messageBox = Ext.MessageBox.show({
-      				title: 'Status',
-      				msg: LN('Export thread started for worksheet in functionality '+this.functCd+"; check Progress Panel on the left to know progress"),
-      				modal: true,
-      				buttons: Ext.MessageBox.OK,
-      				width:500,
-      				icon: Ext.MessageBox.INFO,
-      				animEl: 'root-menu'        			
-      			});
-		      this.close();
-			}
+    , initButtons: function() {
+    	this.btnPrev = new Ext.Button({
+	        text: LN('Sbi.tools.massiveExport.MassiveExportWizardWindow.back')
+	        , handler: this.moveToPreviousPage
+	        , scope: this
+	        , disabled : true
 		});
 	
+		this.btnNext = new Ext.Button({
+	        text: LN('Sbi.tools.massiveExport.MassiveExportWizardWindow.next')
+	        , handler: this.moveToNextPage
+	        , scope: this
+	        , bdisabled : false
+		});
+	
+		this.btnFinish = new Ext.Button({
+	        text: LN('Sbi.tools.massiveExport.MassiveExportWizardWindow.finish')
+	        , handler: this.performFinish
+	        , scope: this
+	        , disabled: true
+		});
+    }
+    
+	, initPages: function() {
+		this.pages = [];
+		this.initOptionsPage();
+		this.initParametersPage();
+	}
+	
+	, initOptionsPage: function() {
+		var config = {functId: this.functId};
+		this.optionsPage = new Sbi.tools.massiveExport.MassiveExportOptionsPanel(config);
+		this.optionsPage.on('noDocsEvent', 
+				function() {
+					this.btnNext.disable();
+					this.btnFinish.disable();
+				}
+			, this
+		);
+		
+		this.OPTIONS_PAGE_NUMBER = this.pages.length;
+		this.pages.push(this.optionsPage);
+	}
+	
+	, initParametersPage: function() {
+		var ser = new Array();
+		var params = {LIGHT_NAVIGATOR_DISABLED: 'TRUE'
+					   , SBI_EXECUTION_ID: null
+					   
+		};
+		ser['getParametersForExecutionService'] = Sbi.config.serviceRegistry.getServiceUrl({
+			serviceName: 'GET_ANALYTICAL_DRIVER_FROM_DOCS_IN_FOLDER_ACTION'
+			, baseParams: params
+		});
+	
+		var config = {
+			services : ser	
+			, contest : 'massiveExport'
+			, drawHelpMessage : false	
+		};
+		
+		this.parametersPage = new Sbi.execution.ParametersPanel(config);
+		
+		this.PARAMETERS_PAGE_NUMBER = this.pages.length;
+		this.pages.push(this.parametersPage);
+	}
 
-	
-//	config = {
-//			services : ser	
-//		};
-	
-	this.optionsPanel = new Sbi.tools.massiveExport.MassiveExportOptionsPanel(config);
-	this.optionsPanel.on('noDocsEvent', 
-			function() {
-				this.btnNext.disable();
-				this.btnFinish.disable();
-			}
-		, this
-	);
-	
-	
-	var firstPage = this.firstCalculatedFiledPanel;
-	
-	var ser = new Array();
-	var params = {LIGHT_NAVIGATOR_DISABLED: 'TRUE'
-				   , SBI_EXECUTION_ID: null
-				   
-	};
-	ser['getParametersForExecutionService'] = Sbi.config.serviceRegistry.getServiceUrl({
-		serviceName: 'GET_ANALYTICAL_DRIVER_FROM_DOCS_IN_FOLDER_ACTION'
-		, baseParams: params
-	});
-
-	config = {
-		services : ser	
-		, contest : 'massiveExport'
-		, drawHelpMessage : false	
-	};
-	
-	this.parametersPanel = new Sbi.execution.ParametersPanel(config);
-
-	var wizardPages = [this.optionsPanel, this.parametersPanel ];
-
-	this.mainPanel = new Ext.Panel({  
-		layout: 'card',  
-		activeItem: 0,  
-		scope: this,
-		height: 420,
-		autoWidth: true,
-		resizable: true,
-		defaults: {border:false},  
-		bbar: [
-		       this.btnPrev,
-		       '->', // greedy spacer so that the buttons are aligned to each side
-		       this.btnNext,
-		       this.btnFinish
-		       ], 
-		  items: wizardPages
-	});
-	this.mainPanel.doLayout();
-	
-}
-, createExecutionInstances: function(pars) {
-	pars = Ext.apply(pars, {modality: 'CREATE_EXEC_CONTEST_ID_MODALITY'});
-	Ext.Ajax.request({
-        url: this.services['StartMassiveExportExecutionProcessAction'],
-       
-        params: pars,
-        
-        //callback : function(options , success, response){
-        success : function(response, options) {
-        if(response !== undefined) {   
-      		if(response.responseText !== undefined) {
-      			var content = Ext.util.JSON.decode( response.responseText );
-      			if(content !== undefined) {
-      				this.executionInstances = {
-      						SBI_EXECUTION_ID: content.execContextId
-      					};
-      		  		for(p in this.parametersPanel.fields){
-      		  			var field = this.parametersPanel.fields[p];
-      		  			field.enable();
-      		  		}
-      		  		this.btnFinish.enable();
-      				pars = Ext.apply(pars, this.executionInstances);
-      				this.parametersPanel.loadParametersForExecution(pars);
-      			} 
-      		} else {
-      			Sbi.exception.ExceptionHandler.showErrorMessage('Server response is empty', 'Service Error');
-      		}
-	  	}
-	  	else{
-	  	//clear preceding store if error happened
-	  		for(p in this.parametersPanel.fields){
-	  			var field = this.parametersPanel.fields[p];
-	  			field.disable();
-	  		}
-	  		this.btnFinish.disable();
-	  	}
-      },
-        scope: this,
-		failure: Sbi.exception.ExceptionHandler.handleFailure      
-   });
-}	
-, finishButtonPressed: function() {
-	// get all values
-	var state = this.getParametersPanelFormState();
-	var jsonState = Sbi.commons.JSON.encode( state );
-	// call the action
-	var selRole = this.mainPanel.getComponent(0).getSelectedRole();
-	var splittingFiltersB = this.mainPanel.getComponent(0).isCycleOnFilterSelected();
-	var pars = {
-    		selectedRole : selRole
-         	, functId : this.functId
-         	, type : 'WORKSHEET'
-         	, splittingFilter : splittingFiltersB
-         	, parameterValues : jsonState
+	, createExecutionInstances: function(pars) {
+		pars = Ext.apply(pars, {modality: 'CREATE_EXEC_CONTEST_ID_MODALITY'});
+		Ext.Ajax.request({
+	        url: this.services['StartMassiveExportExecutionProcessAction'],
+	       
+	        params: pars,
+	        
+	        //callback : function(options , success, response){
+	        success : function(response, options) {
+	        if(response !== undefined) {   
+	      		if(response.responseText !== undefined) {
+	      			var content = Ext.util.JSON.decode( response.responseText );
+	      			if(content !== undefined) {
+	      				this.executionInstances = {
+	      						SBI_EXECUTION_ID: content.execContextId
+	      					};
+	      		  		for(p in this.parametersPage.fields){
+	      		  			var field = this.parametersPage.fields[p];
+	      		  			field.enable();
+	      		  		}
+	      		  		this.btnFinish.enable();
+	      				pars = Ext.apply(pars, this.executionInstances);
+	      				this.parametersPage.loadParametersForExecution(pars);
+	      			} 
+	      		} else {
+	      			Sbi.exception.ExceptionHandler.showErrorMessage('Server response is empty', 'Service Error');
+	      		}
+		  	}
+		  	else{
+		  	//clear preceding store if error happened
+		  		for(p in this.parametersPage.fields){
+		  			var field = this.parametersPage.fields[p];
+		  			field.disable();
+		  		}
+		  		this.btnFinish.disable();
+		  	}
+	      },
+	        scope: this,
+			failure: Sbi.exception.ExceptionHandler.handleFailure      
+	   });
 	}	
 	
-	// Start amssive export
-	Ext.Ajax.request({
-        url: this.services['startMassiveExportThreadAction'],
-       
-        params: pars,
-       
-        success : function(response, options){
-        },
-        scope: this,
-		failure: Sbi.exception.ExceptionHandler.handleFailure      
-   });
+	/**
+	 * returns the value selected of the parameters in parametersPanel,
+	 * and for each also the objparameterId (for label rinomination: name  => nameB)
+	 */
+	, getParametersPanelFormState: function() {
+		var state;
+		//to avoid synchronization problem
+		
+		state = {};
+		for(p in this.parametersPage.fields) {
+			var field = this.parametersPage.fields[p];
+			var value = field.getValue();
+			state[field.name] = value;
+			var rawValue = field.getRawValue();
+			if(value == "" && rawValue != ""){
+				state[field.name] = rawValue;
+			}
+			
+			// add objParsId information if present (massive export case)
+			if(field.objParameterIds){
+				for(pr=0;pr < field.objParameterIds.length;pr++){
+					val = field.objParameterIds[pr];
+					state[val+ '_objParameterId']=field.name;
+				}
+			}
+		}
+		return state;
+	}
 	
-}
-/**
- * returns the value selecte dof the parameters in parametersPanel,
- * and for each also the objparameterId (for label rinomination: name  => nameB)
- */
-, getParametersPanelFormState: function() {
-	var state;
-	//to avoid synchronization problem
+	, moveToPreviousPage: function() {
+		this.moveToPage(this.activePageNumber - 1);
+	}
 	
-	state = {};
-	for(p in this.parametersPanel.fields) {
-		var field = this.parametersPanel.fields[p];
-		var value = field.getValue();
-		state[field.name] = value;
-		var rawValue = field.getRawValue();
-		if(value == "" && rawValue != ""){
-			state[field.name] = rawValue;
+	, moveToNextPage: function() {
+		this.moveToPage(this.activePageNumber + 1);
+	}
+
+	, moveToPage: function(page) {
+		
+		var curr = this.mainPanel.layout.activeItem;
+		if(page == this.PARAMETERS_PAGE_NUMBER){ 
+			
+			// clear the fields in case you are coming to panel for the second time
+			this.parametersPage.clear();
+
+			this.mainPanel.layout.setActiveItem(this.PARAMETERS_PAGE_NUMBER);
+			this.activePageNumber = this.PARAMETERS_PAGE_NUMBER;
+
+			// create ExecutionInstances and  get parameters 
+			var selectedRole = this.optionsPage.getSelectedRole();		
+			var params = {
+				selectedRole : selectedRole
+				, functId : this.functId
+				, type : 'WORKSHEET'						
+			}				
+			this.createExecutionInstances(params);
+			
+		} else if(page == this.OPTIONS_PAGE_NUMBER) { 
+				this.mainPanel.layout.setActiveItem(this.OPTIONS_PAGE_NUMBER);
+				this.activePageNumber = this.OPTIONS_PAGE_NUMBER;
+		} else {
+			alert('page [' + page + '] not available in wizard');
 		}
 		
-		// add objParsId information if present (massive export case)
-		if(field.objParameterIds){
-			for(pr=0;pr < field.objParameterIds.length;pr++){
-			val = field.objParameterIds[pr];
-			state[val+ '_objParameterId']=field.name;
+		
+		if(page === 0) {
+			this.btnPrev.disable();
+		} else {
+			this.btnPrev.enable();
 		}
+		
+		if(page === (this.pages.length -1)) {
+			this.btnNext.disable();
+		} else {
+			this.btnNext.enable();
 		}
-}
-			//state[field.name + '_objParameterIds']=field.objParameterIds;
-	return state;
-}
+				
+		if(this.canFinish() === true) {
+			this.btnFinish.enable();
+		} else {
+			this.btnFinish.disable();
+		}
+	}
 
 });
