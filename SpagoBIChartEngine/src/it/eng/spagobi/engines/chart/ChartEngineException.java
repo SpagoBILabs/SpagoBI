2a20423d82ded9cbe67587c76e511c4846f1ce37
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-02-01 10:23:42 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17347 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIChartEngine/src/it/eng/spagobi/engines/chart/ChartEngineException.java
@@ -0,0 +1,61 @@
+/*
+ * SpagoBI, the Open Source Business Intelligence suite
+ * © 2005-2015 Engineering Group
+ *
+ * This file is part of SpagoBI. SpagoBI is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 2.1 of the License, or any later version. 
+ * SpagoBI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received
+ * a copy of the GNU Lesser General Public License along with SpagoBI. If not, see: http://www.gnu.org/licenses/.
+ * The complete text of SpagoBI license is included in the COPYING.LESSER file. 
+ */
+
+package it.eng.spagobi.engines.chart;
+
+import it.eng.spagobi.utilities.engines.SpagoBIEngineException;
+
+
+/**
+ * The Class ChartEngineException.
+ */
+public class ChartEngineException extends SpagoBIEngineException {
+    
+	/** The hints. 
+	List hints;
+	*/
+	
+	ChartEngineInstance engineInstance;
+	
+	
+	/**
+	 * Builds a <code>ChartEngineException</code>.
+	 * 
+	 * @param message Text of the exception
+	 */
+    public ChartEngineException(String message) {
+    	super(message);
+    }
+	
+    /**
+     * Builds a <code>ChartEngineException</code>.
+     * 
+     * @param message Text of the exception
+     * @param ex previous Throwable object
+     */
+    public ChartEngineException(String message, Throwable ex) {
+    	super(message, ex);
+    }
+    
+    public ChartEngineInstance getEngineInstance() {
+		return engineInstance;
+	}
+
+	public void setEngineInstance(ChartEngineInstance engineInstance) {
+		this.engineInstance = engineInstance;
+	}
+    
+   
+
+
+}
+
---
+++ SpagoBIChartEngine/src/it/eng/spagobi/engines/chart/services/AbstractChartEngineAction.java
@@ -0,0 +1,41 @@
+/*
+ * SpagoBI, the Open Source Business Intelligence suite
+ * © 2005-2015 Engineering Group
+ *
+ * This file is part of SpagoBI. SpagoBI is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 2.1 of the License, or any later version. 
+ * SpagoBI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received
+ * a copy of the GNU Lesser General Public License along with SpagoBI. If not, see: http://www.gnu.org/licenses/.
+ * The complete text of SpagoBI license is included in the COPYING.LESSER file. 
+ */
+package it.eng.spagobi.engines.chart.services;
+
+import it.eng.spagobi.engines.chart.ChartEngineInstance;
+import it.eng.spagobi.utilities.engines.AbstractEngineAction;
+
+import org.apache.log4j.Logger;
+
+/**
+ * The Class AbstractChartEngineAction.
+ * 
+ * @author Antonella Giachino (antonella.giachino@eng.it)
+ */
+public class AbstractChartEngineAction extends AbstractEngineAction {
+	
+	
+	/** Logger component. */
+    private static transient Logger logger = Logger.getLogger(AbstractChartEngineAction.class);
+    	
+		
+	/**
+	 * Gets the console engine instance.
+	 * 
+	 * @return the console engine instance
+	 */
+	public ChartEngineInstance getChartEngineInstance() {
+		return (ChartEngineInstance)getEngineInstance();
+	}
+	
+	
+}
