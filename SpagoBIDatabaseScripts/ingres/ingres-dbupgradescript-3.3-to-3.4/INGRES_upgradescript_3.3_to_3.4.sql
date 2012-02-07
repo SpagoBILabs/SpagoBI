436db69f95458bca17a9c89ed164cb555301bc03
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-02-07 13:23:23 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17490 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-3.3-to-3.4/INGRES_upgradescript_3.3_to_3.4.sql
+++ SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-3.3-to-3.4/INGRES_upgradescript_3.3_to_3.4.sql
@@ -4,8 +4,9 @@ CREATE TABLE SBI_PROGRESS_THREAD (
        PARTIAL              INTEGER,
        TOTAL        	      INTEGER,
        FUNCTION_CD         VARCHAR(200),
-       MESSAGE              VARCHAR(4000),
-       RANDOM_KEY			VARCHAR(4000),	
+       STATUS              VARCHAR(4000),
+       RANDOM_KEY			      VARCHAR(4000),	
+       TYPE			          VARCHAR(200),	
               CONSTRAINT XAK1SBI_PROGRESS_THREAD UNIQUE  (PROGRESS_THREAD_ID),
        PRIMARY KEY (PROGRESS_THREAD_ID)
 ) ;\p\g
@@ -15,3 +16,5 @@ ALTER TABLE SBI_EXT_ROLES ADD COLUMN DO_MASSIVE_EXPORT TINYINT DEFAULT 1;
 insert into hibernate_sequences(next_val,sequence_name) values (ifnull((select max(PROGRESS_THREAD_ID)+1 from SBI_PROGRESS_THREAD) ,1),'SBI_PROGRESS_THREAD');
 
 ALTER TABLE SBI_UDP_VALUE MODIFY COLUMN VALUE VARCHAR(1000) DEFAULT NULL;
+
+INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, USER_IN) VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'),'JNDI_THREAD_MANAGER', 'JNDI_THREAD_MANAGER', 'Jndi to build work manager', true, 'java:/comp/env/wm/SpagoWorkManager',(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'biadmin');
--- SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-3.3-to-3.4/MYSQL_upgradescript_3.3_to_3.4.sql
+++ SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-3.3-to-3.4/MYSQL_upgradescript_3.3_to_3.4.sql
@@ -4,8 +4,9 @@ CREATE TABLE SBI_PROGRESS_THREAD (
        PARTIAL              INTEGER,
        TOTAL        	      INTEGER,
        FUNCTION_CD         VARCHAR(200),
-       MESSAGE              VARCHAR(4000),
+       STATUS              VARCHAR(4000),
        RANDOM_KEY			VARCHAR(4000),	
+       TYPE           VARCHAR(200),
        PRIMARY KEY (PROGRESS_THREAD_ID)
 )
  
@@ -14,3 +15,5 @@ insert into hibernate_sequences(next_val,sequence_name) values (ifnull((select m
 ALTER TABLE SBI_EXT_ROLES ADD COLUMN DO_MASSIVE_EXPORT BOOLEAN DEFAULT TRUE;
 
 ALTER TABLE SBI_UDP_VALUE MODIFY COLUMN VALUE VARCHAR(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL;
+
+INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, USER_IN) VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'),'JNDI_THREAD_MANAGER', 'JNDI_THREAD_MANAGER', 'Jndi to build work manager', true, 'java:/comp/env/wm/SpagoWorkManager',(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'biadmin');
