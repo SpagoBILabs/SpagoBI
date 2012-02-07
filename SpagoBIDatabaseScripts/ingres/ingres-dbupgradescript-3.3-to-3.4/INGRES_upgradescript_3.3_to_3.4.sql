4320c714d7a9b2066c2fc258030d88a29158c998
dzerbett <dzerbett"@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-02-07 15:51:23 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17494 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-3.3-to-3.4/INGRES_upgradescript_3.3_to_3.4.sql
+++ SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-3.3-to-3.4/INGRES_upgradescript_3.3_to_3.4.sql
@@ -9,12 +9,26 @@ CREATE TABLE SBI_PROGRESS_THREAD (
        TYPE			          VARCHAR(200),	
               CONSTRAINT XAK1SBI_PROGRESS_THREAD UNIQUE  (PROGRESS_THREAD_ID),
        PRIMARY KEY (PROGRESS_THREAD_ID)
-) ;\p\g
+);\p\g
 
-ALTER TABLE SBI_EXT_ROLES ADD COLUMN DO_MASSIVE_EXPORT TINYINT DEFAULT 1;
+insert into hibernate_sequences(next_val,sequence_name) values (1, 'SBI_PROGRESS_THREAD');\p\g
 
-insert into hibernate_sequences(next_val,sequence_name) values (ifnull((select max(PROGRESS_THREAD_ID)+1 from SBI_PROGRESS_THREAD) ,1),'SBI_PROGRESS_THREAD');
+ALTER TABLE SBI_EXT_ROLES ADD COLUMN DO_MASSIVE_EXPORT TINYINT DEFAULT 1;\p\g
 
-ALTER TABLE SBI_UDP_VALUE MODIFY COLUMN VALUE VARCHAR(1000) DEFAULT NULL;
+insert into hibernate_sequences(next_val,sequence_name) values (ifnull((select max(PROGRESS_THREAD_ID)+1 from SBI_PROGRESS_THREAD) ,1),'SBI_PROGRESS_THREAD');\p\g
 
-INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, USER_IN) VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'),'JNDI_THREAD_MANAGER', 'JNDI_THREAD_MANAGER', 'Jndi to build work manager', true, 'java:/comp/env/wm/SpagoWorkManager',(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'biadmin');
+ALTER TABLE SBI_UDP_VALUE MODIFY COLUMN VALUE VARCHAR(1000) DEFAULT NULL;\p\g
+
+INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, USER_IN) VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'),'JNDI_THREAD_MANAGER', 'JNDI_THREAD_MANAGER', 'Jndi to build work manager', true, 'java:/comp/env/wm/SpagoWorkManager',(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'biadmin');\p\g
+
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';\p\g
+
+INSERT INTO SBI_DOMAINS (VALUE_ID, VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS, USER_IN, TIME_IN)
+	VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),
+	'XLSX','XLSX','EXPORT_TYPE','Exporters type','Export type', 'biadmin', current_timestamp);\p\g
+	
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_DOMAINS';\p\g
+
+INSERT INTO SBI_EXPORTERS (ENGINE_ID,DOMAIN_ID,DEFAULT_VALUE)values((SELECT ENGINE_ID FROM SBI_ENGINES WHERE LABEL='WorksheetEngine'),(SELECT VALUE_ID FROM SBI_DOMAINS WHERE DOMAIN_CD = 'EXPORT_TYPE' AND VALUE_CD = 'XLSX'), false);\p\g
+
+commit;\p\g
--- SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-3.3-to-3.4/MYSQL_upgradescript_3.3_to_3.4.sql
+++ SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-3.3-to-3.4/MYSQL_upgradescript_3.3_to_3.4.sql
@@ -10,10 +10,22 @@ CREATE TABLE SBI_PROGRESS_THREAD (
        PRIMARY KEY (PROGRESS_THREAD_ID)
 )
  
-insert into hibernate_sequences(next_val,sequence_name) values (ifnull((select max(PROGRESS_THREAD_ID)+1 from SBI_PROGRESS_THREAD) ,1),'SBI_PROGRESS_THREAD');
+insert into hibernate_sequences(next_val,sequence_name) values (1, 'SBI_PROGRESS_THREAD');
 
 ALTER TABLE SBI_EXT_ROLES ADD COLUMN DO_MASSIVE_EXPORT BOOLEAN DEFAULT TRUE;
 
+INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, USER_IN) VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'),'JNDI_THREAD_MANAGER', 'JNDI_THREAD_MANAGER', 'Jndi to build work manager', true, 'java:/comp/env/wm/SpagoWorkManager',(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'biadmin');
+
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
+
 ALTER TABLE SBI_UDP_VALUE MODIFY COLUMN VALUE VARCHAR(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL;
 
-INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, USER_IN) VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'),'JNDI_THREAD_MANAGER', 'JNDI_THREAD_MANAGER', 'Jndi to build work manager', true, 'java:/comp/env/wm/SpagoWorkManager',(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'biadmin');
+INSERT INTO SBI_DOMAINS (VALUE_ID, VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS, USER_IN, TIME_IN)
+	VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),
+	'XLSX','XLSX','EXPORT_TYPE','Exporters type','Export type', 'biadmin', current_timestamp);
+	
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_DOMAINS';
+
+INSERT INTO SBI_EXPORTERS (ENGINE_ID,DOMAIN_ID,DEFAULT_VALUE)values((SELECT ENGINE_ID FROM SBI_ENGINES WHERE LABEL='WorksheetEngine'),(SELECT VALUE_ID FROM SBI_DOMAINS WHERE DOMAIN_CD = 'EXPORT_TYPE' AND VALUE_CD = 'XLSX'), false);
+
+commit;
