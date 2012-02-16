9e9846a7c0295d9f8276ec7670fbf66741e9a1e4
monica franceschini <francesm@eng.it>
2012-02-16 08:15:18 +0000

    tap event on preview
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17570 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIMobileEngine/WebContent/js/spagobi/DocumentPreview.js
+++ SpagoBIMobileEngine/WebContent/js/spagobi/DocumentPreview.js
@@ -4,10 +4,7 @@ app.views.DocumentPreview = Ext.extend(Ext.Panel,
 	    dockedItems: [],
 	    flex:2,
 	    cls: 'spagobi-bg',
-	    previewItem: null,
-	    listeners: {
-	
-		},
+	    layout: 'vbox',
 		initComponent: function ()	{
 			this.title = 'Document preview';
 			this.items = [];
@@ -16,24 +13,100 @@ app.views.DocumentPreview = Ext.extend(Ext.Panel,
 			app.views.DocumentPreview.superclass.initComponent.apply(this, arguments);
 			
 		}
-		, showPreview: function (tpl){
+		, showPreview: function (imageClass, rec){
 			
 			this.removeAll();
-			this.update('');
-		    this.previewItem = new Ext.DataView({
-			    frame:true
-			    , overItemCls: 'over'
-			    , itemSelector: 'dd'
-			    , groups: null
-			    , lookup: null
-			    , viewState: null
-			    , ready: false				    
-			    , tpl : tpl	
-			    , store: null
-			    
+
+			var documentTpl = new Ext.XTemplate(
+					'<tpl for=".">',
+					'<div class="preview-item">',
+					'<div class="'+imageClass+'">' ,		
+					'<img src="' + Ext.BLANK_IMAGE_URL + '" ></img>' ,
+					'</div>' ,
+				    '<div class="item-desc">{name}</div>',
+				    '<div class="item-desc"><b>engine: </b>{engine}</div>',
+				    '<div class="item-desc"><b>description: </b>{description}</div>',
+				    '<div class="item-desc">{creationDate}</div>',
+				    '</div>',
+				    '</tpl>'
+				 );
+
+			
+			var html = documentTpl.applyTemplate( rec ) ;
+			
+		    app.views.previewItem = new Ext.Panel({
+		    	html: html,
+		    	listeners: { el:{ tap:function(e){ 
+		  			  Ext.dispatch({
+						  controller: app.controllers.mobileController,
+						  action: 'executeDocument',
+						  id: rec.id,
+						  label: rec.label, 
+						  engine: rec.engine, 
+						  typeCode: rec.typeCode
+					  });
+		    		} } }
+		    });			
+		    this.add(app.views.previewItem);
+		    
+//			DO NOT REMOVE; WE CAN USE THIS SNIPPET IF THE PREVIEW PANEL SHOULD CONTAIN MORE THAN ONE DOCUMENT PREVIW 
+//			var data = [rec];
+//			//this store can be used several times
+//			new Ext.data.Store({
+//			    model: 'browserItems',
+//			    storeId: 'previewStore',
+//			    type: 'memory',
+//			    data: data
+//			});		
+//		    app.views.previewItem = new Ext.DataView({
+//			    overItemCls: 'over'
+//			    , itemSelector: 'dd'
+//			    , groups: null
+//			    , lookup: null
+//			    , viewState: null
+//			    , ready: false				    
+//			    , tpl : documentTpl	
+//			    , store: 'previewStore'
+//			    , listeners: { el:{ tap:function(e){ alert('click'); } } }
+//		    });
+//		    app.views.previewItem.on('tap', alert('on container tap'));
+			
+			
+/*			var docTpl = new Ext.XTemplate(
+					'<div class="preview-item">',
+					'<div class="'+imageClass+'">' ,		
+					'<img src="' + Ext.BLANK_IMAGE_URL + '" ></img>' ,
+					'</div>' ,
+				    '<div class="item-desc">{name}</div>',
+				    '<div class="item-desc"><b>engine: </b>{engine}</div>',
+				    '<div class="item-desc"><b>description: </b>{description}</div>',
+				    '<div class="item-desc">{creationDate}</div>',
+				    '</div>'
+				 );
+		    var button = new Ext.Button({
+		        tpl: docTpl,
+		        data: rec,
+		        ui  : 'plain',
+                text: 'Drastic',
+                flex: 1,
+		        listeners:{
+		    		'tap': function(btn, e ){
+		    			  alert('tap');
+		    			  Ext.dispatch({
+		    				  controller: app.controllers.mobileController,
+		    				  action: 'executeDocument',
+		    				  id: id,
+		    				  label: label, 
+		    				  engine: engine, 
+		    				  typeCode: typeCode
+		    			  });
+		    		}
+		    	}
 		    });
-		    this.add(this.previewItem);
-		    this.update(tpl);
+		    //p.on('tap', alert('on tap 1'));
+		    this.add(button);*/
+		    //this.update(tpl);
+		    this.doLayout();
 		    console.log('preview items: '+this.items.length);
 		}
 
