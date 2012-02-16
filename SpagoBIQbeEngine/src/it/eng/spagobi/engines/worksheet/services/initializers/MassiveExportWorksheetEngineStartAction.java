2b70aaf32854b0352a50e5a464d8bb9bbd88beca
dzerbett <dzerbett>
2012-02-16 14:07:50 +0000

    Bug fix: wrong exception management
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17574 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/src/it/eng/spagobi/engines/worksheet/services/initializers/MassiveExportWorksheetEngineStartAction.java
+++ SpagoBIQbeEngine/src/it/eng/spagobi/engines/worksheet/services/initializers/MassiveExportWorksheetEngineStartAction.java
@@ -14,8 +14,8 @@ package it.eng.spagobi.engines.worksheet.services.initializers;
 import it.eng.spago.base.SourceBean;
 import it.eng.spagobi.engines.worksheet.services.export.ExportWorksheetAction;
 import it.eng.spagobi.services.proxy.SbiDocumentServiceProxy;
+import it.eng.spagobi.utilities.engines.SpagoBIEngineServiceException;
 import it.eng.spagobi.utilities.engines.SpagoBIEngineStartupException;
-import it.eng.spagobi.utilities.exceptions.SpagoBIServiceException;
 
 import java.util.Locale;
 
@@ -25,91 +25,78 @@ import org.json.JSONObject;
 
 /**
  * @authors Giulio Gavardi
- *          
- *
+ * 
+ * 
  */
-public class MassiveExportWorksheetEngineStartAction extends WorksheetEngineStartAction {	
-
+public class MassiveExportWorksheetEngineStartAction extends
+		WorksheetEngineStartAction {
 
 	/** Logger component. */
-	private static transient Logger logger = Logger.getLogger(MassiveExportWorksheetEngineStartAction.class);
+	private static transient Logger logger = Logger
+			.getLogger(MassiveExportWorksheetEngineStartAction.class);
 
 	private final String SERVICE_NAME = "MASSIVE_EXPORT_WORKSHEET_ENGINE_START_ACTION";
-	
+
 	@Override
 	public void service(SourceBean serviceRequest, SourceBean serviceResponse) {
 		logger.debug("IN");
-		try{
-		super.service(serviceRequest, serviceResponse);
-		Locale locale = getLocale();
-		String docId = getDocumentId();
-		logger.debug("document Id: "+docId);
-
-		SbiDocumentServiceProxy proxy = new SbiDocumentServiceProxy(getUserProfile().getUserId().toString(), getHttpSession());
-		logger.debug("recover paramters for metadata");		
 		try {
-			String jSonPars = proxy.getDocumentAnalyticalDriversJSON(Integer.valueOf(docId), locale.getLanguage(), locale.getCountry());
-			logger.debug("parameters for metadata "+jSonPars);		
-			if(jSonPars != null){
-				JSONArray array = new JSONArray(jSonPars);
-				// add name and values
-				for (int i =0; i <array.length(); i++){
-					JSONObject par = (JSONObject) array.get(i);
-					//String name = par.getString("label");
-					String id = par.getString("id");
-					String name = par.getString("label");
+			super.service(serviceRequest, serviceResponse);
+			Locale locale = getLocale();
+			String docId = getDocumentId();
+			logger.debug("document Id: " + docId);
 
-					String nameDescription = getAttributeAsString(id+"_description");
-					String value = getAttributeAsString(id);
-					// put name, description and value
-					par.put("name", name);
-					if(nameDescription != null) par.put("description", nameDescription);
-					if(value != null) par.put("value", value);
-					else par.put("value", "");
+			SbiDocumentServiceProxy proxy = new SbiDocumentServiceProxy(
+					getUserProfile().getUserId().toString(), getHttpSession());
+			logger.debug("recover paramters for metadata");
+			try {
+				String jSonPars = proxy.getDocumentAnalyticalDriversJSON(
+						Integer.valueOf(docId), locale.getLanguage(),
+						locale.getCountry());
+				logger.debug("parameters for metadata " + jSonPars);
+				if (jSonPars != null) {
+					JSONArray array = new JSONArray(jSonPars);
+					// add name and values
+					for (int i = 0; i < array.length(); i++) {
+						JSONObject par = (JSONObject) array.get(i);
+						// String name = par.getString("label");
+						String id = par.getString("id");
+						String name = par.getString("label");
+
+						String nameDescription = getAttributeAsString(id
+								+ "_description");
+						String value = getAttributeAsString(id);
+						// put name, description and value
+						par.put("name", name);
+						if (nameDescription != null)
+							par.put("description", nameDescription);
+						if (value != null)
+							par.put("value", value);
+						else
+							par.put("value", "");
+					}
+					logger.debug("add parameters JSON array " + array);
+					serviceResponse.setAttribute(
+							ExportWorksheetAction.PARAMETERS, array);
 				}
-				logger.debug("add parameters JSON array "+array);
-				serviceResponse.setAttribute(ExportWorksheetAction.PARAMETERS, array);
+
+			} catch (Exception e) {
+				logger.debug("Error in retrieving parameters information for metadata purpose, go on aniway");
 			}
 
-		} catch (Exception e) {
-			logger.debug("Error in retrieving parameters information for metadata purpose, go on aniway");
-		}
-
-		}
-		catch (SpagoBIEngineStartupException e) {
-			throw new SpagoBIServiceException(SERVICE_NAME, "Error during initialziation of worksheet document", e);
-		}
-		catch (SpagoBIServiceException e) {
+		} catch (SpagoBIEngineStartupException e) {
+			throw new SpagoBIEngineServiceException(SERVICE_NAME,
+					"Error during initialziation of worksheet document", e);
+		} catch (SpagoBIEngineServiceException e) {
 			throw e;
+		} catch (Throwable e) {
+			logger.error("Error during initialziation of worksheet document ",
+					e);
+			throw new SpagoBIEngineServiceException(SERVICE_NAME,
+					"Error during initialziation of worksheet document ", e);
 		}
-		catch (Throwable e) {
-			logger.error("Error during initialziation of worksheet document ",e);
-			throw new SpagoBIServiceException(SERVICE_NAME, "Error during initialziation of worksheet document ", e);
-		}
-
-
 
 		logger.debug("OUT");
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
