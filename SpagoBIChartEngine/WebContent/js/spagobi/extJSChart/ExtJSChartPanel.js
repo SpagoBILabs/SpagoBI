055d313413d0b36f17ef278fe3a6dc8129779485
antonella giachino <antonella.giachino@eng.it>
2012-02-17 17:30:09 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17603 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIChartEngine/WebContent/js/spagobi/extJSChart/ExtJSChartPanel.js
+++ SpagoBIChartEngine/WebContent/js/spagobi/extJSChart/ExtJSChartPanel.js
@@ -1,4 +1,3 @@
-
 /**
  * SpagoBI - The Business Intelligence Free Platform
  *
@@ -45,88 +44,127 @@
 Ext.define('Sbi.extjs.chart.ExtJSChartPanel', {
     alias: 'widget.ExtJSChartPanel',
     extend: 'Ext.panel.Panel',
+    chart: null,
+    template: null, 
+    store: null,
+    storeManager: null,
+    chartStore: null,
+    
     constructor: function(config) {
-    	//alert(config.toSource());
     	var defaultSettings = {
     	};
 
     	var c = Ext.apply(defaultSettings, config || {});
-
+    	
+    	c = Ext.apply(c, {id: 'ExtJSChartPanel'});
+    	
+    	c.storeId = c.dsLabel;
+    	
     	Ext.apply(this, c);
     	
-    	//this.init();
-    	this.createExample(c);
-    	
-        this.callParent(this, c);
+    	this.template = c.template || {};
+    	this.template.divId = c.divId || {};
+    	delete c.template;
+    	this.init(c);
+        this.callParent(arguments);
     }
 
-  , createExample: function(config){
-	   var store = Ext.create('Ext.data.JsonStore', {
-		    fields: ['name', 'data1', 'data2', 'data3', 'data4', 'data5'],
-		    data: [
-		        { 'name': 'metric one',   'data1':10, 'data2':12, 'data3':14, 'data4':8,  'data5':13 },
-		        { 'name': 'metric two',   'data1':7,  'data2':8,  'data3':16, 'data4':10, 'data5':3  },
-		        { 'name': 'metric three', 'data1':5,  'data2':2,  'data3':14, 'data4':12, 'data5':7  },
-		        { 'name': 'metric four',  'data1':2,  'data2':14, 'data3':6,  'data4':1,  'data5':23 },
-		        { 'name': 'metric five',  'data1':27, 'data2':38, 'data3':36, 'data4':13, 'data5':33 }
-		    ]
-		});
-	   
-	   	config.animate=true;
-	   	config.store=store;
-	   	config.width=500;
-	   	config.height=300;
-	   	config.renderTo=config.divId;
-	   	config.theme='Base:gradients';
-	   	
-	   	/*
-		this.chart = Ext.create('Ext.chart.Chart', {
-							    renderTo: config.divId ,
-							    width: 500,
-							    height: 300,
-							    animate: true,
-							    store: store,
-							    axes: [{
-							        type: 'Numeric',
-							        position: 'bottom',
-							        fields: ["data1"],
-							        label: {
-							            renderer: Ext.util.Format.numberRenderer('0,0')
-							        },
-							        title: 'Sample Values',
-							        grid: true,
-							        minimum: 0
-							    }, {
-							        type: 'Category',
-							        position: 'left',
-							        fields: ["name"],
-							        title: 'Sample Metrics'
-							    }],
-							    series: [{
-							        type: 'bar',
-							        axis: 'bottom',
-							        highlight: true,
-							        tips: {
-							          trackMouse: true,
-							          width: 140,
-							          height: 28,
-							          renderer: function(storeItem, item) {
-							            this.setTitle(storeItem.get('name') + ': ' + storeItem.get('data1') + ' views');
-							          }
-							        },
-							        label: {
-							          display: 'insideEnd',
-							            field: 'data1',
-							            renderer: Ext.util.Format.numberRenderer('0'),
-							            orientation: 'horizontal',
-							            color: '#333',
-							            'text-anchor': 'middle'
-							        },
-							        xField: 'name',
-							        yField: ["data1"]
-							    }]
-							});*/
-	   	this.chart = Ext.create('Ext.chart.Chart',config);
+	, init : function (dataConfig) {
+		//gets dataset values
+		this.loadChartData(dataConfig);
+		
+		//show the loading mask
+		if(this.rendered){
+			this.showMask();
+		} else{
+		//	this.on('afterlayout',this.showMask,this);
+		}
+	}
+	
+	, loadChartData: function(dataConfig){
+		var requestParameters = {
+			    id: dataConfig.dsLabel
+			  , label: dataConfig.dsLabel
+			  , refreshTime: dataConfig.refreshTime || 0
+			  , dsTypeCd: dataConfig.dsTypeCd
+			  , pars: dataConfig.dsPars || []
+			  , trasfTypeCd: dataConfig.dsTransformerType || ""
+		};
+		var datasets = [];
+		datasets.push(requestParameters);	
+		this.initStore(datasets, dataConfig.dsLabel);
+	}
+	
+    , initStore: function(config, dsLabel) {
+    	this.storeManager = Ext.create('Sbi.extjs.chart.data.StoreManager',{datasetsConfig: config});
+		this.store = this.storeManager.getStore(dsLabel);
+		if (this.store === undefined) {
+			Sbi.exception.ExceptionHandler.showErrorMessage('Dataset with identifier [' + this.storeId + '] is not correctly configurated');			
+		}else{		
+			this.store.on('load', this.onLoad, this);
+			this.store.on('exception', Sbi.exception.ExceptionHandler.onStoreLoadException, this);
+		}		
+	}
 
-  	} 
-});
+    , adaptStoreForChart: function () {
+    	if(this.store!=null){
+        	var meta = [],
+    	    fields = [],
+    	    storeRec = {},
+    	    store = {};
+        	
+        	//defines new fields for the store chart
+        	for (var m in this.store.alias2FieldMetaMap){
+        		var tmpMeta = this.store.alias2FieldMetaMap[m];
+        		var tmpField = tmpMeta[0];
+        		meta.push(tmpField.header);        		
+        	}
+        	// defines new data for the store chart
+        	var records = this.store.getRange();
+        	for (var i = 0; i < records.length; i++) {
+        		storeRec = {};
+	    		var rec = records[i];
+				if(rec ) {
+					for (var j in meta){
+						var fieldName =	this.store.getFieldNameByAlias(meta[j]); 
+						var fieldValue = rec.get(fieldName);
+						if (fieldValue !== undefined) storeRec[meta[j]] = fieldValue;
+					}	
+					fields.push(storeRec);
+				}
+	        }
+
+        	this.chartStore  = Ext.create('Ext.data.JsonStore', {
+     		    fields: meta,
+     		    data: fields
+     		});        	
+
+    	}
+    }
+    
+  , onLoad: function(){
+	  this.adaptStoreForChart();
+      this.createChart();
+  }
+  
+  , createChart: function(){
+	    var config =  Ext.apply(this.template || {});	    
+		config.renderTo = config.divId;
+	   	config.store = this.chartStore;
+	   	config.animate = (!config.animate)?true:config.animate;	
+	   
+	   	//da recuperare del template:	   	
+	   	config.width=500;
+	   	config.height=300;	   	
+	   	config.theme='Base:gradients';
+        if (this.chart){
+        	//update the store and redraw the chart
+        	this.chart.store = this.chartStore;
+        	this.chart.redraw();
+        }else{
+        	//Creates the new (initial) instance of chart
+        	this.chart = Ext.create('Ext.chart.Chart',config);
+        }
+  }
+
+  });
