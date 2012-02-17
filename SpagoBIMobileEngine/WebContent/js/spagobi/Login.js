18c3af8bfd623f663cd257f0062e0060c669c8ab
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-02-17 09:44:09 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17576 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIMobileEngine/WebContent/js/spagobi/Login.js
+++ SpagoBIMobileEngine/WebContent/js/spagobi/Login.js
@@ -1,74 +1,11 @@
-app.views.LoginForm = Ext.extend(Ext.form.FormPanel,
+app.views.LoginView = Ext.extend(Ext.Panel,
 		{
-		name: 'login-form',	
-        autoRender: true,
-        floating: true,
-        modal: true,
-        centered: true,
-        hideOnMaskTap: false,
-        height: 385,
-        width: 480,
-        scroll : 'vertical',
-        url    : this.loginUrl,
-        standardSubmit : false,
+		fullscreen: true,
         userIDField: null, 
         pwdField: null,
         loginUrl: null,
-        fullscreen : false,
-	
-        dockedItems: [
-            {
-                xtype: 'toolbar',
-                dock: 'bottom',
-                scope:this,
-                items: [
-                    {
-                        text: 'Login',
-                        ui: 'confirm',
-                        scope: this,
-                        handler: function() {
-                    		  var form = app.views.loginForm;
-                    		  var userid = form.userIDField.getValue();
-                    		  var pwd = form.pwdField.getValue();
-                              Ext.Ajax.request({
-                                  url: form.loginUrl,
-                                  scope: this,
-                                  method: 'post',
-                                  params: {userID: userid, password : pwd},
-                                  failure : function(response){
-                                        console.log('call Error! ');
-                                  }
-                                  
-                                  ,success: function(response, opts) {
-                                	  if(response.responseText.indexOf('<') == -1){
-	                                	  var content = Ext.util.JSON.decode( response.responseText );	              		      			 
-	                                      var esito = content.text;
-	                                      if(esito=='userhome'){
-	                                    	  //alert('login OK!!!!');
-	                                    	  Ext.dispatch({
-	                                    		  controller: app.controllers.mobileController,
-	                                    		  action: 'login',
-	                                    		  animation: {
-	                                    		  type: 'slide',
-	                                    		  direction: 'right'
-	                                    		  }
-	                                    	  });
-	                                      }else{
-	                                    	  alert('Authentication failure!');
-	                                    	  return;
-	                                      }
-                                	  }else{
-                                		  alert('Authentication failure!');
-                                    	  return;
-                                	  }
-                                  }
-                                  
-                              });
-                        }
-                    }
-                ]
-            }
-        ],
+
+        
         initComponent: function() 	{
 
 		    Sbi.config = {};
@@ -89,35 +26,104 @@ app.views.LoginForm = Ext.extend(Ext.form.FormPanel,
 				serviceName: 'LOGIN_ACTION'
 			});	
 			console.log(this.loginUrl);
-		
-			this.userIDField = new Ext.form.Text({                                
+			
+ 		  this.userIDField = new Ext.form.Text({                                
 				xtype: 'textfield',
 		        name : 'userID',
 		        label: 'Username',
+		        placeHolder : 'Username',
 		        useClearIcon: true});
 			
 			this.pwdField = new Ext.form.Password({                                
 		        xtype: 'passwordfield',
 		        name : 'password',
 		        label: 'Password',
+		        placeHolder : 'Password',
 		        useClearIcon: false});
-			
-			this.items =[
-			             {
-			                 xtype: 'fieldset',
-			                 title: 'SpagoBI Mobile Login',
-			                 instructions: 'Please login.',
-			                 defaults: {
-			                     required: true,
-			                     labelAlign: 'left',
-			                     labelWidth: '45%'
-			                 },
-			                 items: [
-			                 	this.userIDField, 
-			                 	this.pwdField]
-			             }];
-			
-			app.views.LoginForm.superclass.initComponent.apply(this, arguments);
+
+				//creates form panel
+				app.views.form = new Ext.form.FormPanel({
+	                autoRender: true,
+	                floating: true,
+	                modal: true,
+	                centered: true,
+	                hideOnMaskTap: false,
+	                height: 350,
+	                width: 450,
+	                items: [
+				             {
+				                 xtype: 'fieldset',
+				                 title: 'SpagoBI Mobile Login',
+				                 instructions: 'Please login.',
+
+				                 defaults: {
+				                     required: true,
+				                     labelAlign: 'left',
+				                     labelWidth: '45%'
+				                 },
+				                 items: [
+				                 	this.userIDField, 
+				                 	this.pwdField]
+				             }],
+				   dockedItems: [
+                             {
+                                 xtype: 'toolbar',
+                                 dock: 'bottom',
+                                 scope:this,
+                                 items: [
+                                     {
+                                         text: 'Login',
+                                         ui: 'confirm',
+                                         scope: this,
+                                         handler: function() {
+
+                                     		  var userid = app.views.loginView.userIDField.getValue();
+                                     		  var pwd = app.views.loginView.pwdField.getValue();
+                                               Ext.Ajax.request({
+                                                   url: app.views.loginView.loginUrl,
+                                                   scope: this,
+                                                   method: 'post',
+                                                   params: {userID: userid, password : pwd},
+                                                   failure : function(response){
+                                                         console.log('call Error! ');
+                                                   }
+                                                   
+                                                   ,success: function(response, opts) {
+                                                 	  if(response.responseText.indexOf('<') == -1){
+                 	                                	  var content = Ext.util.JSON.decode( response.responseText );	              		      			 
+                 	                                      var esito = content.text;
+                 	                                      if(esito=='userhome'){
+                 	                                    	  //alert('login OK!!!!');
+                 	                                    	  app.views.form.hide(); 
+                 	                                    	  Ext.dispatch({
+                 	                                    		  controller: app.controllers.mobileController,
+                 	                                    		  action: 'login',
+                 	                                    		  animation: {
+                 	                                    		  type: 'slide',
+                 	                                    		  direction: 'right'
+                 	                                    		  }
+                 	                                    	  });
+                 	                                      }else{
+                 	                                    	  alert('Authentication failure!');
+                 	                                    	  return;
+                 	                                      }
+                                                 	  }else{
+                                                 		  alert('Authentication failure!');
+                                                     	  return;
+                                                 	  }
+                                                   }
+                                                   
+                                               });
+                                         }
+                                     }
+                                 ]
+                             }
+                         ]
+				});  
+
+		        app.views.form.show(); 
+		        //this.add(this.form);
+		        app.views.LoginView.superclass.initComponent.apply(this, arguments);
 
 		}
 
