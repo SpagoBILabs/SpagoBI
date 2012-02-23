057b6de9a62e22084ee4601fc4127c6b80fe311b
Alberto Ghedin <alberto.ghedin@eng.it>
2012-02-23 14:04:10 +0000

    Add title and values to chart
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17655 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIMobileEngine/WebContent/js/spagobi/execution/chart/ChartExecutionPanel.js
+++ SpagoBIMobileEngine/WebContent/js/spagobi/execution/chart/ChartExecutionPanel.js
@@ -17,9 +17,25 @@ app.views.ChartExecutionPanel = Ext.extend(app.views.WidgetPanel, {
 		config.animate = true;
 
 		config.listeners = {
-            'itemtap': function(series, item, event) {  }
+            'itemtap': function(series, item, event) {  
+            	var t =5;
+            }
         };
+		
+		if(config.dockedItems==undefined || config.dockedItems==undefined){
+			config.dockedItems = new Array();
+		}
+		
+		if(config.interactions==undefined || config.interactions==undefined){
+			config.interactions = new Array();
+		}
+		
+		if(config.options.showValueTip){
+			this.addValueTip(config);
+		}
+
 		var chartConfig = {
+
 			items : [ config ]
 		};
 
@@ -28,17 +44,54 @@ app.views.ChartExecutionPanel = Ext.extend(app.views.WidgetPanel, {
 			chartConfig.height = '100%';
 			chartConfig.defaultType = 'chart';
 			chartConfig.layout = 'fit';
+			if(config.title){
+				chartConfig.dockedItems = [{
+	                dock: 'top',
+	                xtype: 'toolbar',
+	                ui: 'light',
+	                title: config.title.value
+	            }];
+			}
 			r = new Ext.Panel(chartConfig);
 			this.insert(0, r);
 			this.doLayout();
 		} else {
 			chartConfig.bodyMargin = '10% 1px 60% 1px';
 			chartConfig.fullscreen = true;
+			if(config.title){
+				chartConfig.title = config.title.value;
+			}
 			app.views.chart = new Ext.chart.Panel(chartConfig);
 		}
 		if(this.IS_FROM_COMPOSED){
 			this.loadingMask.hide();
 		}
+	}
+	
+	, addValueTip: function(config){
+		config.interactions.push({
+            type: 'iteminfo',
+            listeners: {
+                show: function(interaction, item, panel) {
+                	panel.setWidth(400);
+                	var str = "";
+                	var storeItem = item.storeItem;
+                	var values = item.value;
+                	for(var propertyName in storeItem.data){
+                	   if((storeItem.data).hasOwnProperty(propertyName) ){
+                		   var propertyValue = (storeItem.data)[propertyName];
+                		   if(values.indexOf(propertyValue)>=0){
+                			   str = str +"<li><b><span>"+propertyName+"</b>: "+propertyValue+"</span></li>";
+                		   }
+                	   }
+                	}
+                	if(str.length>0){
+                		str = "<ul>"+str+"</ul>";
+                		 panel.update(str);
+                	}
+                }
+            }
+        });
 	}
 
 });
