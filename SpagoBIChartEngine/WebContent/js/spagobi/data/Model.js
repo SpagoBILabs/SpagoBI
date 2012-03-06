d2ea4a29335dc4a51277aacdc97ab275e23b4b31
antonella giachino <antonella.giachino@eng.it>
2012-03-06 08:03:23 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17704 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIChartEngine/WebContent/js/spagobi/data/Model.js
+++ SpagoBIChartEngine/WebContent/js/spagobi/data/Model.js
@@ -47,7 +47,6 @@
 
 Ext.define('Sbi.extjs.chart.data.Model', {
     extend: 'Ext.data.Model',
-    //fields: ["recNo",{"type":"string","header":"name","dataIndex":"column_1","name":"column_1"},{"type":"int","header":"data1","dataIndex":"column_2","name":"column_2"},{"type":"int","header":"data2","dataIndex":"column_3","name":"column_3"},{"type":"int","header":"data3","dataIndex":"column_4","name":"column_4"},{"type":"int","header":"data4","dataIndex":"column_5","name":"column_5"},{"type":"int","header":"data5","dataIndex":"column_6","name":"column_6"}]
     fields: ["recNo",{"type":"string","header":"name","dataIndex":"column_1","name":"column_1"}]
 
 	, constructor: function(config) {
@@ -61,18 +60,3 @@ Ext.define('Sbi.extjs.chart.data.Model', {
     }
 	
 });
-/*
-var proxyUrl = Sbi.config.serviceRegistry.getServiceUrl({serviceName:  'GET_CHART_DATA_ACTION'
-	   , baseParams:params
-	    });
-//The Store contains the AjaxProxy as an inline configuration
-var store = Ext.create('Ext.data.Store', {
-    model: 'Sbi.extjs.chart.data.Model',
-    proxy: {
-        type: 'ajax',
-        url : proxyUrl
-    }
-});
-
-store.load();
-*/
