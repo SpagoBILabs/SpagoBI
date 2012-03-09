3005ac1e3542dff01ee8fed9b179357ac439b8c3
Marco Cortella <macortel@eng.it>
2012-03-09 11:00:24 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17741 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIChartEngine/WebContent/js/spagobi/extJSChart/ExtJSChartPanel.js
+++ SpagoBIChartEngine/WebContent/js/spagobi/extJSChart/ExtJSChartPanel.js
@@ -86,6 +86,148 @@ Ext.define('Sbi.extjs.chart.ExtJSChartPanel', {
 		    config.theme = 'ExtJSChartTheme';
 	   	}
 	   	
+	   	var themeConfig = {
+            axis: {
+                fill: localBaseColor,
+                stroke: localBaseColor
+            },
+            axisLabelLeft: {
+                fill: localBaseColor
+            },
+            axisLabelBottom: {
+                fill: localBaseColor
+            },
+            axisTitleLeft: {
+                fill: localBaseColor
+            },
+            axisTitleBottom: {
+                fill: localBaseColor
+            },
+            colors: localColors,
+    	    baseColor: localBaseColor
+        };
+
+
+	   	var theme = Ext.create('Ext.chart.theme.ExtJSChartTheme', themeConfig);
+	   
+	   	/*
+	   	Ext.define('Ext.chart.theme.ExtJSChartTheme', {
+	   	    extend: 'Ext.chart.theme.Base',
+	   	    colors : ['#b1da5a', '#4ce0e7', '#e84b67', '#da5abd', '#4d7fe6', '#fec935'],
+
+	   	   	baseColor : '#b1da5a',
+	   	        
+	   	    constructor: function(config) {
+	   	        this.callParent([Ext.apply({
+	   	            axis: {
+	   	                fill: config.baseColor,
+	   	                stroke: baseColor
+	   	            },
+	   	            axisLabelLeft: {
+	   	                fill: baseColor
+	   	            },
+	   	            axisLabelBottom: {
+	   	                fill: baseColor
+	   	            },
+	   	            axisTitleLeft: {
+	   	                fill: baseColor
+	   	            },
+	   	            axisTitleBottom: {
+	   	                fill: baseColor
+	   	            },
+	   	            colors: colors
+	   	        }, config)]);
+	   	    }});
+*/
+	  	config.theme = 'ExtJSChartTheme';
+	  	
+	  	var docLabel = this.documentLabel;
+	  	
+	  	//Adding click listener for Cross Navigation
+	  	for(var j = 0; j< config.series.length; j++){
+		  	config.series[j].listeners = {
+		  			itemmousedown:function(obj) {
+		  				var categoryField ;
+		  				var valueField ;
+		  				
+		  				if (obj.series.type == 'bar'){
+		  					categoryField = obj.storeItem.data[obj.series.xField];
+			  				valueField = obj.storeItem.data[obj.yField];
+		  				}
+		  				else if (obj.series.type == 'pie'){
+		  					categoryField = obj.storeItem.data[obj.series.label.field];
+		  					valueField = obj.slice.value;	
+		  				} else if (obj.series.type == 'gauge'){
+		  					categoryField = obj.storeItem.data[obj.series.label.field];
+		  					valueField = obj.slice.value;	
+		  				} else if (obj.series.type == 'area'){
+		  					categoryField = obj.storeItem.data[obj.series.xField];
+		  					valueField = obj.storeItem.data[obj.storeField];	
+		  				} else if (obj.series.type == 'line'){
+		  					categoryField = obj.storeItem.data[obj.series.xField];
+		  					valueField = obj.storeItem.data[obj.series.yField];	
+		  				}  else if (obj.series.type == 'radar'){
+		  					categoryField = obj.storeItem.data[obj.series.xField];
+		  					valueField = obj.storeItem.data[obj.series.yField];	
+		  				} else if (obj.series.type == 'scatter'){
+		  					categoryField = obj.storeItem.data[obj.series.xField];
+		  					valueField = obj.storeItem.data[obj.series.yField];	
+		  				}
+
+		  				// alert(categoryField + ' &' + valueField);
+		  				 
+		  				//Cross Navigation
+		  			  	var drill = config.drill;
+		  		    	if(drill != null && drill !== undefined){
+		  		    		var doc = drill.document;
+		  		    		
+		  		    		var params = "";
+		  		    		for(var i = 0; i< drill.param.length; i++){
+		  		    			if(drill.param[i].type == 'ABSOLUTE'){
+		  		    				if(params !== ""){
+		  			    				params+="&";
+		  			    			}
+		  		    				params+= drill.param[i].name +"="+drill.param[i].value;
+		  		    			}
+		  		    		}
+		  		    		
+		  			    		for(var i = 0; i< drill.param.length; i++){
+		  			    			if(drill.param[i].type == 'CATEGORY'){
+		  			    				if(params !== ""){
+		  		    	    				params+="&";
+		  		    	    			}
+		  			    				if(categoryField !== undefined){
+		  			    					params+= drill.param[i].name +"="+categoryField;
+		  			    				}	    				
+		  			    			}
+		  			    			
+		  			    		}
+		  			    		for(var i = 0; i< drill.param.length; i++){
+		  			    			if(drill.param[i].type == 'SERIE'){
+		  			    				if(params !== ""){
+		  		    	    				params+="&";
+		  		    	    			}
+		  			    				if (valueField !== undefined){
+			  			    				params+= drill.param[i].name +"="+valueField;
+		  			    				} 
+		  			    					
+		  			    			}
+		  			    			
+		  			    		}    		
+		  		    		
+		  			    		//execute Cross navigation
+		  			    		//alert("Document Label: "+docLabel);
+		  						parent.execCrossNavigation("iframe_"+docLabel, doc, params);
+
+		  		    	}	 
+		  				 
+		  		    }
+		  	};
+	  	}
+	  	
+
+
+	   	
         if (this.chart){
         	//update the store and redraw the chart
         	this.chart.store = this.chartStore;
