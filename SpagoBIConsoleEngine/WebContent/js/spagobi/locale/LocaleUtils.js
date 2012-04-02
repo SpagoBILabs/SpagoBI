220bb922d2ec92530324c75c489932eb8aad2227
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-04-02 15:50:23 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17870 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIConsoleEngine/WebContent/js/spagobi/locale/LocaleUtils.js
+++ SpagoBIConsoleEngine/WebContent/js/spagobi/locale/LocaleUtils.js
@@ -3,13 +3,13 @@ Ext.ns("Sbi.locale");
 Sbi.locale.dummyFormatter = function(v){return v;};
 
 Sbi.locale.formatters = {
-	int: Sbi.locale.dummyFormatter,
-	float: Sbi.locale.dummyFormatter,
-	string: Sbi.locale.dummyFormatter,		
-	date: Sbi.locale.dummyFormatter,		
-	timestamp: Sbi.locale.dummyFormatter,
-	boolean: Sbi.locale.dummyFormatter,
-	html: Sbi.locale.dummyFormatter,
+	'int': Sbi.locale.dummyFormatter,
+	'float': Sbi.locale.dummyFormatter,
+	'string': Sbi.locale.dummyFormatter,		
+	'date': Sbi.locale.dummyFormatter,		
+	'timestamp': Sbi.locale.dummyFormatter,
+	'boolean': Sbi.locale.dummyFormatter,
+	'html': Sbi.locale.dummyFormatter,
 	inlineBar: Sbi.locale.dummyFormatter,
 	inlinePoint: Sbi.locale.dummyFormatter
 };
@@ -17,23 +17,23 @@ Sbi.locale.formatters = {
 
 if(Sbi.console.commons.Format){
 	if(Sbi.locale.formats) {
-		Sbi.locale.formatters.int  = Sbi.console.commons.Format.numberRenderer(Sbi.locale.formats['int']);		
-		Sbi.locale.formatters.float  = Sbi.console.commons.Format.numberRenderer(Sbi.locale.formats['float']);		
-		Sbi.locale.formatters.string  = Sbi.console.commons.Format.stringRenderer(Sbi.locale.formats['string']);		
-		Sbi.locale.formatters.date    = Sbi.console.commons.Format.dateRenderer(Sbi.locale.formats['date']);		
-		Sbi.locale.formatters.timestamp   = Sbi.console.commons.Format.dateRenderer(Sbi.locale.formats['timestamp']);
-		Sbi.locale.formatters.boolean = Sbi.console.commons.Format.booleanRenderer(Sbi.locale.formats['boolean']);
-		Sbi.locale.formatters.html    = Sbi.console.commons.Format.htmlRenderer();
+		Sbi.locale.formatters['int']  = Sbi.console.commons.Format.numberRenderer(Sbi.locale.formats['int']);		
+		Sbi.locale.formatters['float']  = Sbi.console.commons.Format.numberRenderer(Sbi.locale.formats['float']);		
+		Sbi.locale.formatters['string']  = Sbi.console.commons.Format.stringRenderer(Sbi.locale.formats['string']);		
+		Sbi.locale.formatters['date']   = Sbi.console.commons.Format.dateRenderer(Sbi.locale.formats['date']);		
+		Sbi.locale.formatters['timestamp']   = Sbi.console.commons.Format.dateRenderer(Sbi.locale.formats['timestamp']);
+		Sbi.locale.formatters['boolean'] = Sbi.console.commons.Format.booleanRenderer(Sbi.locale.formats['boolean']);
+		Sbi.locale.formatters['html']    = Sbi.console.commons.Format.htmlRenderer();
 		Sbi.locale.formatters.inlineBar    = Sbi.console.commons.Format.inlineBarRenderer();
 		Sbi.locale.formatters.inlinePoint = Sbi.console.commons.Format.inlinePointRenderer();
 	} else {
-		Sbi.locale.formatters.int  = Sbi.console.commons.Format.numberRenderer( );	
-		Sbi.locale.formatters.float  = Sbi.console.commons.Format.numberRenderer( );	
-		Sbi.locale.formatters.string  = Sbi.console.commons.Format.stringRenderer( );		
-		Sbi.locale.formatters.date    = Sbi.console.commons.Format.dateRenderer( );		
-		Sbi.locale.formatters.timestamp = Sbi.console.commons.Format.timestampRenderer( );
-		Sbi.locale.formatters.boolean = Sbi.console.commons.Format.booleanRenderer( );
-		Sbi.locale.formatters.html    = Sbi.console.commons.Format.htmlRenderer();
+		Sbi.locale.formatters['int'] = Sbi.console.commons.Format.numberRenderer( );	
+		Sbi.locale.formatters['float'] = Sbi.console.commons.Format.numberRenderer( );	
+		Sbi.locale.formatters['string']  = Sbi.console.commons.Format.stringRenderer( );		
+		Sbi.locale.formatters['date']   = Sbi.console.commons.Format.dateRenderer( );		
+		Sbi.locale.formatters['timestamp'] = Sbi.console.commons.Format.timestampRenderer( );
+		Sbi.locale.formatters['boolean'] = Sbi.console.commons.Format.booleanRenderer( );
+		Sbi.locale.formatters['html']     = Sbi.console.commons.Format.htmlRenderer();
 		Sbi.locale.formatters.inlineBar   = Sbi.console.commons.Format.inlineBarRenderer();
 		Sbi.locale.formatters.inlinePoint = Sbi.console.commons.Format.inlinePointRenderer();
 	}
