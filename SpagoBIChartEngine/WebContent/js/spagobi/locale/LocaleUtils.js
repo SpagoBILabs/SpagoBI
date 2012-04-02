8162db2a250654a016ff193964ec4cede6c2d4b9
Marco Cortella <macortel@eng.it>
2012-04-02 16:00:41 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17874 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIChartEngine/WebContent/js/spagobi/locale/LocaleUtils.js
+++ SpagoBIChartEngine/WebContent/js/spagobi/locale/LocaleUtils.js
@@ -84,36 +84,36 @@ Ext.define('Sbi.locale', {
 Ext.ns("Sbi.locale");
 
 Sbi.locale.dummyFormatter = function(v){return v;};
-/*
+
 Sbi.locale.formatters = {
 	//number: Sbi.locale.dummyFormatter,
-	int: Sbi.locale.dummyFormatter,
-	float: Sbi.locale.dummyFormatter,
-	string: Sbi.locale.dummyFormatter,		
-	date: Sbi.locale.dummyFormatter,		
-	boolean: Sbi.locale.dummyFormatter,
-	html: Sbi.locale.dummyFormatter
+	'int': Sbi.locale.dummyFormatter,
+	'float': Sbi.locale.dummyFormatter,
+	'string': Sbi.locale.dummyFormatter,		
+	'date': Sbi.locale.dummyFormatter,		
+	'boolean': Sbi.locale.dummyFormatter,
+	'html': Sbi.locale.dummyFormatter
 };
 
 
 if(Sbi.chart.commons.Format){
 	if(Sbi.locale.formats) {
-		Sbi.locale.formatters.int  = Sbi.chart.commons.Format.numberRenderer(Sbi.locale.formats['int']);		
-		Sbi.locale.formatters.float  = Sbi.chart.commons.Format.numberRenderer(Sbi.locale.formats['float']);		
-		Sbi.locale.formatters.string  = Sbi.chart.commons.Format.stringRenderer(Sbi.locale.formats['string']);		
-		Sbi.locale.formatters.date    = Sbi.chart.commons.Format.dateRenderer(Sbi.locale.formats['date']);		
-		Sbi.locale.formatters.boolean = Sbi.chart.commons.Format.booleanRenderer(Sbi.locale.formats['boolean']);
-		Sbi.locale.formatters.html    = Sbi.chart.commons.Format.htmlRenderer();
+		Sbi.locale.formatters['int']  = Sbi.chart.commons.Format.numberRenderer(Sbi.locale.formats['int']);		
+		Sbi.locale.formatters['float'] = Sbi.chart.commons.Format.numberRenderer(Sbi.locale.formats['float']);		
+		Sbi.locale.formatters['string'] = Sbi.chart.commons.Format.stringRenderer(Sbi.locale.formats['string']);		
+		Sbi.locale.formatters['date'] = Sbi.chart.commons.Format.dateRenderer(Sbi.locale.formats['date']);		
+		Sbi.locale.formatters['boolean'] = Sbi.chart.commons.Format.booleanRenderer(Sbi.locale.formats['boolean']);
+		Sbi.locale.formatters['html']   = Sbi.chart.commons.Format.htmlRenderer();
 	} else {
-		Sbi.locale.formatters.int  = Sbi.chart.commons.Format.numberRenderer( );	
-		Sbi.locale.formatters.float  = Sbi.chart.commons.Format.numberRenderer( );	
-		Sbi.locale.formatters.string  = Sbi.chart.commons.Format.stringRenderer( );		
-		Sbi.locale.formatters.date    = Sbi.chart.commons.Format.dateRenderer( );		
-		Sbi.locale.formatters.boolean = Sbi.chart.commons.Format.booleanRenderer( );
-		Sbi.locale.formatters.html    = Sbi.chart.commons.Format.htmlRenderer();
+		Sbi.locale.formatters['int']   = Sbi.chart.commons.Format.numberRenderer( );	
+		Sbi.locale.formatters['float']  = Sbi.chart.commons.Format.numberRenderer( );	
+		Sbi.locale.formatters['string'] = Sbi.chart.commons.Format.stringRenderer( );		
+		Sbi.locale.formatters['date']   = Sbi.chart.commons.Format.dateRenderer( );		
+		Sbi.locale.formatters['boolean']= Sbi.chart.commons.Format.booleanRenderer( );
+		Sbi.locale.formatters['html']  = Sbi.chart.commons.Format.htmlRenderer();
 	}
 }
-*/
+
 
 
 Sbi.locale.localize = function(key) {
