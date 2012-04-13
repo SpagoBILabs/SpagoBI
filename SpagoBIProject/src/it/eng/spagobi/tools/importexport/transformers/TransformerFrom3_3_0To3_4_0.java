966c64c33d66fe22489abd6a11bd0d443250daeb
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-04-13 16:49:01 +0000

    domains and engine rinomination (transformer from 3.3. to 3.4 modified accordingly)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18016 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/tools/importexport/transformers/TransformerFrom3_3_0To3_4_0.java
+++ SpagoBIProject/src/it/eng/spagobi/tools/importexport/transformers/TransformerFrom3_3_0To3_4_0.java
@@ -16,6 +16,7 @@ import it.eng.spagobi.tools.importexport.ITransformer;
 
 import java.io.File;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -54,6 +55,7 @@ public class TransformerFrom3_3_0To3_4_0 implements ITransformer {
 			conn = TransformersUtilities.getConnectionToDatabase(pathImpTmpFolder, archiveName);
 			fixExtRoles(conn);
 			fixDataset(conn);
+			fixDomains(conn);
 			conn.commit();
 		} catch (Exception e) {
 			logger.error("Error while changing database", e);	
@@ -117,4 +119,143 @@ public class TransformerFrom3_3_0To3_4_0 implements ITransformer {
 	}
 
 
+	/** In order to have import work, some domains have changed name: in particular
+	 * @param conn
+	 * @throws Exception
+	 */
+	private void fixDomains(Connection conn) throws Exception {
+		logger.debug("IN");
+
+		try{
+			Statement stmt = conn.createStatement();
+
+			// SOme domaiins have been changed
+			logger.debug("change some domains name that have been changed in 3.4");
+
+			String[] updates = {
+					"update SBI_DOMAINS set VALUE_CD = 'FREE_INQUIRY'" +
+					", VALUE_NM ='sbidomains.nm.freeinquiry', VALUE_DS = 'sbidomains.ds.freeinquiry'" +
+					" where  VALUE_CD = 'DATAMART' AND DOMAIN_CD = 'BIOBJ_TYPE';",
+
+					"update SBI_DOMAINS set VALUE_CD = 'ADHOC_REPORTING'" +
+					", VALUE_NM ='sbidomains.nm.adhoc_reporting', VALUE_DS = 'sbidomains.ds.adhoc_reporting'" +
+					" where  VALUE_CD = 'WORKSHEET' AND DOMAIN_CD = 'BIOBJ_TYPE';",
+
+					"update SBI_DOMAINS set VALUE_CD = 'LOCATION_INTELLIGENCE', VALUE_NM ='sbidomains.nm.location_intelligence', " +
+					" VALUE_DS = 'sbidomains.ds.location_intelligence'" +
+					" where  VALUE_CD = 'GEO' AND DOMAIN_CD = 'BIOBJ_TYPE';",
+
+					"update SBI_DOMAINS set VALUE_CD = 'COCKPIT', VALUE_NM ='sbidomains.nm.cockpit', " +
+					" VALUE_DS = 'sbidomains.ds.cockpit' " +
+					"where  VALUE_CD = 'DOCUMENT_COMPOSITE' AND DOMAIN_CD = 'BIOBJ_TYPE';",
+
+					"update SBI_DOMAINS set VALUE_CD = 'COLLABORATION', VALUE_NM ='sbidomains.nm.collaboration', " +
+					"VALUE_DS = 'sbidomains.ds.collaboration' " +
+					" where  VALUE_CD = 'DOSSIER' AND DOMAIN_CD = 'BIOBJ_TYPE';",
+					
+					"update SBI_DOMAINS set VALUE_CD = 'LOCATION_INTELLIGENCE', VALUE_NM ='sbidomains.nm.location_intelligence', " +
+					"VALUE_DS = 'sbidomains.ds.location_intelligence' " +
+					" where  VALUE_CD = 'MAP' AND DOMAIN_CD = 'BIOBJ_TYPE';",
+
+					"update SBI_DOMAINS set VALUE_CD = 'EXTERNAL_PROCESS', VALUE_NM ='sbidomains.nm.external_process'" +
+					" , VALUE_DS = 'sbidomains.ds.external_process' " +
+					" where  VALUE_CD = 'PROCESS' AND DOMAIN_CD = 'BIOBJ_TYPE';"
+			};
+
+			executeSQL(conn, updates);	
+
+			logger.debug("Insert new domains in 3.4");
+
+			int maxId = getDomainsMaxId(conn);
+
+			String[] sqls = {
+					//"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'FREE_INQUIRY', 'sbidomains.nm.freeinquiry','BIOBJ_TYPE','BI Object type','sbidomains.ds.freeinquiry');",
+					"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'ADHOC_REPORTING', 'sbidomains.nm.adhoc_reporting','BIOBJ_TYPE','BI Object type','sbidomains.ds.adhoc_reporting');",
+					"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'REAL_TIME', 'sbidomains.nm.realtime','BIOBJ_TYPE','BI Object type','sbidomains.ds.realtime');",
+					//"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'LOCATION_INTELLIGENCE', 'sbidomains.nm.location_intelligence','BIOBJ_TYPE','BI Object type','sbidomains.ds.location_intelligence');",
+					"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'COCKPIT', 'sbidomains.nm.cockpit','BIOBJ_TYPE','BI Object type','sbidomains.ds.cockpit');",
+					"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'COLLABORATION', 'sbidomains.nm.collaboration','BIOBJ_TYPE','BI Object type','sbidomains.ds.collaboration');",
+					"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'EXTERNAL_PROCESS', 'sbidomains.nm.external_process','BIOBJ_TYPE','BI Object type','sbidomains.ds.external_process');",
+					"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'MOBILE_REPORT', 'sbidomains.nm.mobile.report','BIOBJ_TYPE','BI Object type','sbidomains.ds.mobile.report');",
+					"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'MOBILE_CHART', 'sbidomains.nm.mobile.chart','BIOBJ_TYPE','BI Object type','sbidomains.ds.mobile.chart');",
+					"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'MOBILE_COCKPIT', 'sbidomains.nm.mobile.cockpit','BIOBJ_TYPE','BI Object type','sbidomains.ds.mobile.cockpit');",
+					"INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS) values (" + ++maxId + ", 'CHART', 'sbidomains.nm.chart','BIOBJ_TYPE','BI Object type','sbidomains.ds.chart');"
+			};
+			executeSQL(conn, sqls);
+
+			logger.debug("Change engine that have changed their domain type");
+
+			String[] engineUpdates = {	
+					"update SBI_ENGINES set BIOBJ_TYPE = (SELECT VALUE_ID FROM sbi_domains s WHERE VALUE_CD = 'CHART' AND DOMAIN_CD = 'BIOBJ_TYPE') WHERE CLASS_NM = 'it.eng.spagobi.engines.chart.SpagoBIChartInternalEngine'",
+					"update SBI_ENGINES set BIOBJ_TYPE = (SELECT VALUE_ID FROM sbi_domains s WHERE VALUE_CD = 'REAL_TIME' AND DOMAIN_CD = 'BIOBJ_TYPE') WHERE CLASS_NM = 'it.eng.spagobi.engines.dashboard.SpagoBIDashboardInternalEngine'",
+					"update SBI_ENGINES set BIOBJ_TYPE = (SELECT VALUE_ID FROM sbi_domains s WHERE VALUE_CD = 'REPORT' AND DOMAIN_CD = 'BIOBJ_TYPE') WHERE DRIVER_NM = 'it.eng.spagobi.engines.drivers.accessibility.AccessibilityDriver'",
+					"update SBI_ENGINES set BIOBJ_TYPE = (SELECT VALUE_ID FROM sbi_domains s WHERE VALUE_CD = 'EXTERNAL_PROCESS' AND DOMAIN_CD = 'BIOBJ_TYPE') WHERE DRIVER_NM = 'it.eng.spagobi.engines.drivers.commonj.CommonjDriver'",
+					"update SBI_ENGINES set BIOBJ_TYPE = (SELECT VALUE_ID FROM sbi_domains s WHERE VALUE_CD = 'REAL_TIME' AND DOMAIN_CD = 'BIOBJ_TYPE') WHERE DRIVER_NM = 'it.eng.spagobi.engines.drivers.generic.GenericDriver' AND MAIN_URL='/SpagoBIConsoleEngine/servlet/AdapterHTTP?ACTION_NAME=CONSOLE_ENGINE_START_ACTION'",
+					"update SBI_ENGINES set BIOBJ_TYPE = (SELECT VALUE_ID FROM sbi_domains s WHERE VALUE_CD = 'FREE_INQUIRY' AND DOMAIN_CD = 'BIOBJ_TYPE') WHERE DRIVER_NM = 'it.eng.spagobi.engines.drivers.smartfilter.SmartFilterDriver'"
+			};
+			
+			executeSQL(conn, engineUpdates);
+
+			String[] objectUpdates = {	
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'REAL_TIME' WHERE BIOBJ_TYPE_CD = 'DASH'",
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'FREE_INQUIRY' WHERE BIOBJ_TYPE_CD = 'DATAMART'",
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'ADHOC_REPORTING' WHERE BIOBJ_TYPE_CD = 'WORKSHEET'",
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'LOCATION_INTELLIGENCE' WHERE BIOBJ_TYPE_CD = 'GEO'",
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'COCKPIT' WHERE BIOBJ_TYPE_CD = 'DOCUMENT_COMPOSITE'",
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'COLLABORATION' WHERE BIOBJ_TYPE_CD = 'DOSSIER'",
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'LOCATION_INTELLIGENCE' WHERE BIOBJ_TYPE_CD = 'MAP'",
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'EXTERNAL_PROCESS' WHERE BIOBJ_TYPE_CD = 'PROCESS'",
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'REPORT' WHERE BIOBJ_TYPE_CD = 'ACCESSIBLE_HTML'",
+					"update SBI_OBJECTS set BIOBJ_TYPE_CD = 'REAL_TIME' WHERE BIOBJ_TYPE_CD = 'CONSOLE'"
+					
+			};
+			executeSQL(conn, objectUpdates);
+
+		
+		}
+		catch (Exception e) {
+			logger.error("Error in updating domains from previous version. ", e);
+			throw e;
+		}
+		logger.debug("OUT");
+	}
+
+
+	private void executeSQL(Connection conn, String[] sqls) throws Exception {
+		logger.debug("IN");
+		for (int i = 0; i < sqls.length; i++) {
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(sqls[i]);
+		}
+		logger.debug("OUT");
+	}
+
+
+	private int getDomainsMaxId(Connection conn) throws Exception {
+		logger.debug("IN");
+		Statement statement = null;
+		ResultSet resultSet = null;
+		int toReturn = 0;
+		try {
+			statement = conn.createStatement();
+			String sql = "SELECT MAX(VALUE_ID) FROM SBI_DOMAINS";
+			resultSet = statement.executeQuery(sql);
+			if (resultSet.next()) {
+				toReturn = resultSet.getInt(1);
+			} else {
+				throw new Exception("Query SELECT MAX(VALUE_ID) FROM SBI_DOMAINS did not get any result!!!!");
+			} 
+		} finally {
+			if (resultSet != null) {
+				resultSet.close();
+			}
+			if (statement != null) {
+				statement.close();
+			}
+		}
+		logger.debug("OUT");
+		return toReturn;
+	}
+
 }
+
