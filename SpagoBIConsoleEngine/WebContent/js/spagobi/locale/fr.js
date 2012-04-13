0d506454692fbfefc9daf3cb81e90e83d8420524
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-04-13 10:10:51 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@17997 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIConsoleEngine/WebContent/js/spagobi/locale/fr.js
+++ SpagoBIConsoleEngine/WebContent/js/spagobi/locale/fr.js
@@ -60,38 +60,38 @@ Sbi.locale.ln = Sbi.locale.ln || new Array();
 //===================================================================
 // MESSAGE WINDOW
 //===================================================================
-Sbi.locale.ln['sbi.console.messagewin.warning.title'] = 'Warning message';
-Sbi.locale.ln['sbi.console.messagewin.error.title'] = 'Error message';
-Sbi.locale.ln['sbi.console.messagewin.info.title'] = 'Info message';
+Sbi.locale.ln['sbi.qbe.messagewin.warning.title'] = 'Message d\'alerte';
+Sbi.locale.ln['sbi.qbe.messagewin.error.title'] = 'Message d\'erreur';
+Sbi.locale.ln['sbi.qbe.messagewin.info.title'] = 'Message d\'information'
 
-Sbi.locale.ln['sbi.console.detailpage.title'] = 'Detail Page';
+Sbi.locale.ln['sbi.console.detailpage.title'] = 'Page de d\u00e9tails';
 Sbi.locale.ln['sbi.console.consolepanel.title'] = 'Console';
 
 //error / alarms window
-Sbi.locale.ln['sbi.console.error.btnClose'] = 'Close';
-Sbi.locale.ln['sbi.console.error.btnSetChecked'] = 'Marck as checked';
-Sbi.locale.ln['sbi.console.error.btnSetNotChecked'] = 'Mark as not checked';
+Sbi.locale.ln['sbi.console.error.btnClose'] = 'Fermer';
+Sbi.locale.ln['sbi.console.error.btnSetChecked'] = 'Marquer comme v\u00e9rifi\u00e9' //oppure: coch\u00e9 la case
+Sbi.locale.ln['sbi.console.error.btnSetNotChecked'] = 'Marquer comme non v\u00e9rifi\u00e9';
 
 //download window
-Sbi.locale.ln['sbi.console.downloadlogs.title'] = 'Specify parameters to download log files:';
-Sbi.locale.ln['sbi.console.downloadlogs.initialDate'] = 'Initial date';
-Sbi.locale.ln['sbi.console.downloadlogs.finalDate'] = 'Final date';
-Sbi.locale.ln['sbi.console.downloadlogs.initialTime'] = 'Initial time';
-Sbi.locale.ln['sbi.console.downloadlogs.finalTime'] = 'Final time';
-Sbi.locale.ln['sbi.console.downloadlogs.path'] = 'Path';
-Sbi.locale.ln['sbi.console.downloadlogs.btnClose'] = 'Close';
-Sbi.locale.ln['sbi.console.downloadlogs.btnDownload'] = 'Download';
-Sbi.locale.ln['sbi.console.downloadlogs.initialDateMandatory'] = 'The start date is mandatory';
-Sbi.locale.ln['sbi.console.downloadlogs.finalDateMandatory'] = 'The final date is mandatory';
-Sbi.locale.ln['sbi.console.downloadlogs.initialTimeMandatory'] = 'The initial time is mandatory';
-Sbi.locale.ln['sbi.console.downloadlogs.finalTimeMandatory'] = 'The final time is mandatory';
-Sbi.locale.ln['sbi.console.downloadlogs.pathsMandatory'] = 'The download path is mandatory'
-Sbi.locale.ln['sbi.console.downloadlogs.rangeInvalid'] = 'Incorrect range of date'
+Sbi.locale.ln['sbi.console.downloadlogs.title'] = 'Sp\u00e9cifiez les param\u00e8tres pour t\u00e9l\u00e9charger des fichiers de log'; 
+Sbi.locale.ln['sbi.console.downloadlogs.initialDate'] = 'Date de d\u00e9part';
+Sbi.locale.ln['sbi.console.downloadlogs.finalDate'] = 'Date d\'arriv\u00e9e';
+Sbi.locale.ln['sbi.console.downloadlogs.initialTime'] = 'Temps de d\u00e9part';
+Sbi.locale.ln['sbi.console.downloadlogs.finalTime'] = 'Temps d\'arriv\u00e9e';
+Sbi.locale.ln['sbi.console.downloadlogs.path'] = 'Chemin';
+Sbi.locale.ln['sbi.console.downloadlogs.btnClose'] = 'Fermer';
+Sbi.locale.ln['sbi.console.downloadlogs.btnDownload'] = 'T\u00e9l\u00e9charger';
+Sbi.locale.ln['sbi.console.downloadlogs.initialDateMandatory'] = 'La date de d\u00e9part est obligatoire';
+Sbi.locale.ln['sbi.console.downloadlogs.finalDateMandatory'] = 'La date d\'arriv\u00e9e est obligatoire';
+Sbi.locale.ln['sbi.console.downloadlogs.initialTimeMandatory'] = 'Le temps de d\u00e9par est obligatoire';
+Sbi.locale.ln['sbi.console.downloadlogs.finalTimeMandatory'] = 'Le temps d\'arriv\u00e9e est obligatoire';
+Sbi.locale.ln['sbi.console.downloadlogs.pathsMandatory'] = 'Le chemin de t\u00e9l\u00e9chargement est obligatoire' //da verificare
+Sbi.locale.ln['sbi.console.downloadlogs.rangeInvalid'] = '\u00c9ventail incorrecte des dates'
 //propmtables window
 Sbi.locale.ln['sbi.console.promptables.btnOK'] = 'OK';
-Sbi.locale.ln['sbi.console.promptables.btnClose'] = 'Close';
-Sbi.locale.ln['sbi.console.promptables.lookup.Annulla'] = 'Cancel';
-Sbi.locale.ln['sbi.console.promptables.lookup.Confirm'] = 'Confirm';
-Sbi.locale.ln['sbi.console.promptables.lookup.Select'] = 'Select';
+Sbi.locale.ln['sbi.console.promptables.btnClose'] = 'Fermer';
+Sbi.locale.ln['sbi.console.promptables.lookup.Annulla'] = 'Annuler';
+Sbi.locale.ln['sbi.console.promptables.lookup.Confirm'] = 'Confirmer';
+Sbi.locale.ln['sbi.console.promptables.lookup.Select'] = 'S\u00e9lectionner';
 //internationalization
-Sbi.locale.ln['sbi.console.localization.columnsKO'] = 'Error during the internationalizzation. Check the dataset!';
+Sbi.locale.ln['sbi.console.localization.columnsKO'] = 'Erreur lors de la internationalizzation. V\u00e9rifiez l\'ensemble des donn\u00e9es!';
