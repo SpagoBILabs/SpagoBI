964330ff95ff38caf439beca5f3f0d0fe3e7da47
Davide Zerbetto <davide.zerbetto@eng.it>
2012-04-13 13:35:44 +0000

    French messages adjustment
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18008 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/locale/registry/fr.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/registry/fr.js
@@ -1,1 +1,16 @@
+Ext.ns("Sbi.locale");
+
+//===================================================================
+//Sbi.registry.RegistryPanel 
+//===================================================================
+Sbi.locale.ln['sbi.registry.registrypanel.title']= 'Registry';
+
+//===================================================================
+//Sbi.registry.RegistryEditorGridPanel
+//===================================================================
+Sbi.locale.ln['sbi.registry.registryeditorgridpanel.mandatory']= ': mandatory field';
+Sbi.locale.ln['sbi.registry.registryeditorgridpanel.saveconfirm.title']= 'Status';
+Sbi.locale.ln['sbi.registry.registryeditorgridpanel.saveconfirm.message']= 'Data updated correctly';
+Sbi.locale.ln['sbi.registry.registryeditorgridpanel.saveconfirm.message.ko']= 'Operation failed';
+Sbi.locale.ln['sbi.registry.registryeditorgridpanel.validation']= 'Validation error: numeric field';
 Sbi.locale.ln['sbi.registry.registryeditorgridpanel.validation.unsigned']= 'Erreur de validation: les valeurs sign\u00e9es ne sont pas admises';
