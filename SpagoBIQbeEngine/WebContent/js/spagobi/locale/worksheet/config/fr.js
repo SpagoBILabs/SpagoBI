f80b6b720a648db34a88a41d59f0c5f0b7b5fa20
monica franceschini <francesm@eng.it>
2012-04-13 11:53:26 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18002 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/worksheet/config/fr.js
@@ -0,0 +1,17 @@
+Ext.ns("Sbi.locale");
+
+Sbi.locale.ln['sbi.worksheet.config.options.title'] = 'Options';
+
+Sbi.locale.ln['sbi.worksheet.config.options.attributepresentation.label'] = 'Afficher les attributs ';
+Sbi.locale.ln['sbi.worksheet.config.options.attributepresentation.code'] = 'code';
+Sbi.locale.ln['sbi.worksheet.config.options.attributepresentation.description'] = 'description';
+Sbi.locale.ln['sbi.worksheet.config.options.attributepresentation.both'] = 'code et description';
+
+Sbi.locale.ln['sbi.config.optionswindow.title'] = 'Options';
+Sbi.locale.ln['sbi.worksheet.config.optionswindow.buttons.text.apply'] = 'Appliquer';
+Sbi.locale.ln['sbi.worksheet.config.optionswindow.buttons.text.cancel'] = 'Annuler';
+
+Sbi.locale.ln['sbi.worksheet.config.options.measurepresentation.NONE'] = 'aucun';
+Sbi.locale.ln['sbi.worksheet.config.options.measurepresentation.K'] = 'milliers';
+Sbi.locale.ln['sbi.worksheet.config.options.measurepresentation.M'] = 'millions';
+Sbi.locale.ln['sbi.worksheet.config.options.measurepresentation.G'] = 'milliards';
