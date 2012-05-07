0bb0ce470d74f8cf1c73b48d8660e599cc36d585
Marco Cortella <macortel@eng.it>
2012-05-07 08:22:53 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18112 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIDatabaseScripts/hsql/hsql-dbupgradescript-3.4-to-3.5/HSQL_upgradescript_3.4_to_3.5.sql
@@ -0,0 +1,11 @@
+ALTER TABLE SBI_LOV ADD COLUMN DATASET_ID INTEGER AFTER ORGANIZATION,
+ ADD CONSTRAINT FK_SBI_LOV_2 FOREIGN KEY FK_SBI_LOV_2 (DATASET_ID)
+    REFERENCES SBI_DATA_SET (DS_ID)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT;
+    
+INSERT INTO SBI_DOMAINS (VALUE_ID, VALUE_CD,VALUE_NM,DOMAIN_CD,DOMAIN_NM,VALUE_DS, USER_IN, TIME_IN)
+    VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'),
+    'DATASET','sbidomains.nm.dataset','INPUT_TYPE','Input mode and values','sbidomains.ds.dataset', 'server', current_timestamp);
+update hibernate_sequences set next_val = next_val+1 where  sequence_name = 'SBI_DOMAINS';
+commit;    
