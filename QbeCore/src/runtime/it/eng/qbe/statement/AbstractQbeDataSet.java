3d484755cc808977a59ddf9424867819e7c461eb
gioia <gioia>
2012-05-10 17:12:01 +0000

    RESOLVED - issue SPAGOBI-859: Extend QuerableBehaviour object in order to generate the executable statement from the query as the resut of the execution of one script over it https://spagobi.eng.it/jira/browse/SPAGOBI-859
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18125 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/runtime/it/eng/qbe/statement/AbstractQbeDataSet.java
+++ QbeCore/src/runtime/it/eng/qbe/statement/AbstractQbeDataSet.java
@@ -276,24 +276,7 @@ public abstract class AbstractQbeDataSet extends AbstractDataSet {
 	}
 
 
-	public void printInfo() {
-		ScriptEngineManager mgr = new ScriptEngineManager();
-		List<ScriptEngineFactory> factories = mgr.getEngineFactories();
-
-		for (ScriptEngineFactory factory: factories) {
-			System.out.println("ScriptEngineFactory Info");
-			String engName = factory.getEngineName();
-			String engVersion = factory.getEngineVersion();
-			String langName = factory.getLanguageName();
-			String langVersion = factory.getLanguageVersion();
-			System.out.printf("\tScript Engine: %s (%s)\n", engName, engVersion);
-			List<String> engNames = factory.getNames();
-			for(String name: engNames) {
-				System.out.printf("\tEngine Alias: %s\n", name);
-			}
-			System.out.printf("\tLanguage: %s (%s)\n", langName, langVersion);
-		}   
-	}
+	
 
 	public IStatement getStatement() {
 		return statement;
