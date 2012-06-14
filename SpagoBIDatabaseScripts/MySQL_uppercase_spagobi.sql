2245b16c22b1ecf28cfd3247f224314d68df9ca9
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-06-14 12:31:23 +0000

    Multi-tenancy support: commit 3 (MySQL history and uppercase script) See http://spagobi.eng.it/jira/browse/SPAGOBI-884
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18235 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/MySQL_uppercase_spagobi.sql
+++ SpagoBIDatabaseScripts/MySQL_uppercase_spagobi.sql
@@ -129,6 +129,7 @@ ALTER TABLE `sbi_goal_hierarchy` RENAME `SBI_GOAL_HIERARCHY`;
 ALTER TABLE `sbi_goal_kpi` RENAME `SBI_GOAL_KPI`;
 ALTER TABLE `sbi_data_set_history` RENAME `SBI_DATA_SET_HISTORY`;
 ALTER TABLE `hibernate_sequences` RENAME `hibernate_sequences`;
+ALTER TABLE `sbi_organizations` RENAME `SBI_ORGANIZATIONS`;
 
 select concat('rename table ', table_name, ' to ' , upper(table_name) , ';') from information_schema.tables where table_schema = 'your_schema_name';
 
