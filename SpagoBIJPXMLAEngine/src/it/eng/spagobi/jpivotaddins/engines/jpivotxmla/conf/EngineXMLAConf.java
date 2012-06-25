5729ac8568785e0e8e3c0c7a53e444e55a7b5903
monica franceschini <francesm@eng.it>
2012-06-25 14:37:29 +0000

    file header: new license MPL 2.0
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18274 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIJPXMLAEngine/src/it/eng/spagobi/jpivotaddins/engines/jpivotxmla/conf/EngineXMLAConf.java
+++ SpagoBIJPXMLAEngine/src/it/eng/spagobi/jpivotaddins/engines/jpivotxmla/conf/EngineXMLAConf.java
@@ -1,10 +1,9 @@
-/*
- * SpagoBI, the Open Source Business Intelligence suite
- * © 2005-2015 Engineering Group
- * 
- * LICENSE: see JPIVOT.LICENSE.txt file
- * 
- */
+/* SpagoBI, the Open Source Business Intelligence suite
+
+* © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.jpivotaddins.engines.jpivotxmla.conf;
 
 import it.eng.spago.base.SourceBean;
