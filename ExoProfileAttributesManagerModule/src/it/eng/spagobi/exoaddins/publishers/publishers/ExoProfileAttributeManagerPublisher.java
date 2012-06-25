5e71f77dd684b94804c1fe145499eebd9daa8228
monica franceschini <francesm@eng.it>
2012-06-25 15:28:10 +0000

    file header: new license MPL 2.0
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18307 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- ExoProfileAttributesManagerModule/src/it/eng/spagobi/exoaddins/publishers/publishers/ExoProfileAttributeManagerPublisher.java
+++ ExoProfileAttributesManagerModule/src/it/eng/spagobi/exoaddins/publishers/publishers/ExoProfileAttributeManagerPublisher.java
@@ -1,24 +1,9 @@
-/**
+/* SpagoBI, the Open Source Business Intelligence suite
 
-Copyright 2005 Engineering Ingegneria Informatica S.p.A.
-
-This file is part of SpagoBI.
-
-SpagoBI is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-any later version.
-
-SpagoBI is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Spago; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
-
-**/
+* © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.exoaddins.publishers;
 
 import org.apache.log4j.Logger;
