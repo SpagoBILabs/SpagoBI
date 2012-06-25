8ec35ed38e91a295ece0aa61ab047a71cf8393e3
monica franceschini <francesm@eng.it>
2012-06-25 14:56:53 +0000

    file header: new license MPL 2.0
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18282 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/commons/bo/Config.java
+++ SpagoBIProject/src/it/eng/spagobi/commons/bo/Config.java
@@ -1,14 +1,9 @@
-/*
- * SpagoBI, the Open Source Business Intelligence suite
- * © 2005-2015 Engineering Group
- *
- * This file is part of SpagoBI. SpagoBI is free software: you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation, either version 2.1 of the License, or any later version. 
- * SpagoBI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received
- * a copy of the GNU Lesser General Public License along with SpagoBI. If not, see: http://www.gnu.org/licenses/.
- * The complete text of SpagoBI license is included in the COPYING.LESSER file. 
- */
+/* SpagoBI, the Open Source Business Intelligence suite
+
+* © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.commons.bo;
 
 import java.io.Serializable;
