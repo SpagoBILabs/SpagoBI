96db9ca30320711c9729ab4f735e48107833ec85
monica franceschini <francesm@eng.it>
2012-06-26 15:25:01 +0000

    keep original license header
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18340 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIJPaloEngine/src/com/tensegrity/wpalo/client/i18n/ILocalMessages.java
+++ SpagoBIJPaloEngine/src/com/tensegrity/wpalo/client/i18n/ILocalMessages.java
@@ -1,10 +1,3 @@
-/*
- * SpagoBI, the Open Source Business Intelligence suite
- * © 2005-2015 Engineering Group
- * 
- * LICENSE: see JPALO.LICENSE.txt file
- * 
- */
 package com.tensegrity.wpalo.client.i18n;
 
 import com.google.gwt.i18n.client.Messages;
--- SpagoBIJPaloEngine/src/com/tensegrity/wpalo/client/ui/mvc/cubeview/LoDHierarchyTree.java
+++ SpagoBIJPaloEngine/src/com/tensegrity/wpalo/client/ui/mvc/cubeview/LoDHierarchyTree.java
@@ -1,10 +1,3 @@
-/*
- * SpagoBI, the Open Source Business Intelligence suite
- * © 2005-2015 Engineering Group
- * 
- * LICENSE: see JPALO.LICENSE.txt file
- * 
- */
 package com.tensegrity.wpalo.client.ui.mvc.cubeview;
 
 import java.util.List;
