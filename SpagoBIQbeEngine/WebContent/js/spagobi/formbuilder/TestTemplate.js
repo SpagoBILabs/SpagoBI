ca652325712b1afa92c83c3d0500a9dba452095d
monica franceschini <francesm@eng.it>
2012-06-27 09:09:44 +0000

    added license header for MPL 2.0 license
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18344 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/formbuilder/TestTemplate.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/formbuilder/TestTemplate.js
@@ -1,4 +1,11 @@
-Ext.ns("Sbi.formbuilder.template");
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+ Ext.ns("Sbi.formbuilder.template");
 
 Sbi.formbuilder.template = {
 	 "staticClosedFilters": [
--- SpagoBIQbeEngine/WebContent/js/spagobi/locale/commons/en.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/commons/en.js
@@ -1,4 +1,11 @@
-Ext.ns("Sbi.locale");
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+ Ext.ns("Sbi.locale");
 Sbi.locale.ln = Sbi.locale.ln || new Array();
 
 Sbi.locale.ln['sbi.designertable.tableValidation.noElement'] = 'At least one attribute or measure must be inserted';
