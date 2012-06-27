ff67a40e69515a1caf9319fed8360ab711b3de68
monica franceschini <francesm@eng.it>
2012-06-27 09:49:32 +0000

    added license header for MPL 2.0 license
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18350 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIMobileEngine/WebContent/js/spagobi/Login.js
+++ SpagoBIMobileEngine/WebContent/js/spagobi/Login.js
@@ -1,4 +1,13 @@
-app.views.LoginView = Ext.extend(Ext.Panel, {
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+  
+ 
+ app.views.LoginView = Ext.extend(Ext.Panel, {
 	fullscreen : true,
 	userIDField : null,
 	pwdField : null,
--- SpagoBIMobileEngine/WebContent/js/spagobi/execution/composed/ComposedExecutionController.js
+++ SpagoBIMobileEngine/WebContent/js/spagobi/execution/composed/ComposedExecutionController.js
@@ -1,4 +1,13 @@
-app.controllers.ComposedExecutionController = Ext.extend(Ext.Controller,{
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+  
+ 
+ app.controllers.ComposedExecutionController = Ext.extend(Ext.Controller,{
 	
 	executeSubDocument : function(executionInstance, subDocumentPanel){
 		var option = {};
