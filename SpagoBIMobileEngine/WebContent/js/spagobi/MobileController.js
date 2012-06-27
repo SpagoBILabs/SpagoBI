34a6b0312af9145585be62f1cde458f627cfa945
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-06-27 09:49:32 +0000

    added license header for MPL 2.0 license
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18350 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIMobileEngine/WebContent/js/spagobi/MobileController.js
+++ SpagoBIMobileEngine/WebContent/js/spagobi/MobileController.js
@@ -1,4 +1,13 @@
-app.controllers.MobileController = Ext.extend(Ext.Controller,{
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+  
+ 
+ app.controllers.MobileController = Ext.extend(Ext.Controller,{
 	
 	init: function()  {
 		var params = {LIGHT_NAVIGATOR_DISABLED: 'TRUE', SBI_EXECUTION_ID: null};
--- SpagoBIMobileEngine/WebContent/js/spagobi/main.js
+++ SpagoBIMobileEngine/WebContent/js/spagobi/main.js
@@ -1,4 +1,13 @@
-Ext.setup({
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+  
+ 
+ Ext.setup({
 	tabletStartupScreen : "/images/tablet_startup.png",
 	phoneStartupScreen  : "phone_startup.png",
 	icon                : "icon.png",
