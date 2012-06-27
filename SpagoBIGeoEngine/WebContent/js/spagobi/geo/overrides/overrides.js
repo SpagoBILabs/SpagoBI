5affcda05215521811a7787896e7703149a7553e
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-06-27 09:36:15 +0000

    added license header for MPL 2.0 license
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18349 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIGeoEngine/WebContent/js/spagobi/geo/overrides/overrides.js
+++ SpagoBIGeoEngine/WebContent/js/spagobi/geo/overrides/overrides.js
@@ -1,4 +1,11 @@
-/* =============================================================================
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * © 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+ /* =============================================================================
 * Change the behaviour of method setRoot. Thanks to this override everytime the 
 * root node change the tree is properly refreshed in order to reflect the 
 * modifications  in the structure.
