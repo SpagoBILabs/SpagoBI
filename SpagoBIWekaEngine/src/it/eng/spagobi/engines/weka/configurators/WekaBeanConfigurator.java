f053b1f83d922aa06f99faa01122e083940997ba
monica franceschini <francesm@eng.it>
2012-06-28 13:33:25 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18426 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWekaEngine/src/it/eng/spagobi/engines/weka/configurators/WekaBeanConfigurator.java
+++ SpagoBIWekaEngine/src/it/eng/spagobi/engines/weka/configurators/WekaBeanConfigurator.java
@@ -1,10 +1,11 @@
-/*
- * SpagoBI, the Open Source Business Intelligence suite
- * © 2005-2015 Engineering Group
- * 
- * LICENSE: see WEKA.LICENSE.txt file
- * 
- */
+/* SpagoBI, the Open Source Business Intelligence suite
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This program is free software: you can redistribute it and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation, either version 2.1 
+ * of the License, or (at your option) any later version. This program is distributed in the hope that 
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU  General Public License for more details. You should have received a copy of the GNU  General Public License along with 
+ * this program. If not, see: http://www.gnu.org/licenses/. */
 package it.eng.spagobi.engines.weka.configurators;
 
 /**
