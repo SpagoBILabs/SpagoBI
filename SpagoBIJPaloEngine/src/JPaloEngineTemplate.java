e593815e41fe4a253adf466addcb9d0645a2243e
monica franceschini <francesm@eng.it>
2012-06-28 10:21:23 +0000

    spagobi GPL2 license
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18408 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIJPaloEngine/src/JPaloEngineTemplate.java
+++ SpagoBIJPaloEngine/src/JPaloEngineTemplate.java
@@ -1,29 +1,11 @@
-/*
-*
-*
-* Copyright (C) 2006-2009 Tensegrity Software GmbH
-*
-* This program is free software; you can redistribute it and/or modify it
-* under the terms of the GNU General Public License (Version 2) as published
-* by the Free Software Foundation at http://www.gnu.org/copyleft/gpl.html.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-* more details.
-*
-* You should have received a copy of the GNU General Public License along with
-* this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-* Place, Suite 330, Boston, MA 02111-1307 USA
-*
-* If you are developing and distributing open source applications under the
-* GPL License, then you are free to use JPalo Modules under the GPL License.  For OEMs,
-* ISVs, and VARs who distribute JPalo Modules with their products, and do not license
-* and distribute their source code under the GPL, Tensegrity provides a flexible
-* OEM Commercial License.
-*
-*
-*/
+/* SpagoBI, the Open Source Business Intelligence suite
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This program is free software: you can redistribute it and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation, either version 2.1 
+ * of the License, or (at your option) any later version. This program is distributed in the hope that 
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU  General Public License for more details. You should have received a copy of the GNU  General Public License along with 
+ * this program. If not, see: http://www.gnu.org/licenses/. */
 
 import it.eng.spago.base.SourceBean;
 
