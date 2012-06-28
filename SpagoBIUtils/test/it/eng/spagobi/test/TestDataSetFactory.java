ceccc02b2b058d159f0c8d566d97da8a93f7fe67
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-06-28 13:25:31 +0000

    added Incompatible With Secondary Licenses to license header
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18425 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/test/it/eng/spagobi/test/TestDataSetFactory.java
+++ SpagoBIUtils/test/it/eng/spagobi/test/TestDataSetFactory.java
@@ -1,14 +1,8 @@
-/*
- * SpagoBI, the Open Source Business Intelligence suite
- * © 2005-2015 Engineering Group
- *
- * This file is part of SpagoBI. SpagoBI is free software: you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation, either version 2.1 of the License, or any later version. 
- * SpagoBI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received
- * a copy of the GNU Lesser General Public License along with SpagoBI. If not, see: http://www.gnu.org/licenses/.
- * The complete text of SpagoBI license is included in the COPYING.LESSER file. 
- */
+/* SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.test;
 
 import it.eng.spagobi.services.datasource.bo.SpagoBiDataSource;
