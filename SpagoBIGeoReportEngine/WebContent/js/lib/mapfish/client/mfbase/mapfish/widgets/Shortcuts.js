d94b10f1f3a96e54c8417a3630c18a29bbe7fb9e
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-06-29 09:34:03 +0000

    mapfish client-SpagoBI license
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18464 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIGeoReportEngine/WebContent/js/lib/mapfish/client/mfbase/mapfish/widgets/Shortcuts.js
+++ SpagoBIGeoReportEngine/WebContent/js/lib/mapfish/client/mfbase/mapfish/widgets/Shortcuts.js
@@ -1,4 +1,14 @@
 /*
+* SpagoBI, the Open Source Business Intelligence suite
+* 
+* Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+* This file is part of MapFish Client, Copyright (C) 2007 Camptocamp 
+* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the “Incompatible With Secondary Licenses” notice, according to the ExtJS Open Source License Exception for Development, version 1.03, January 23rd, 2012 http://www.sencha.com/legal/open-source-faq/open-source-license-exception-for-development/ 
+* If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+* This file is an extension to Ext JS Library that is distributed under the terms of the GNU GPL v3 license. For any information, visit: http://www.sencha.com/license.
+* 
+* The original copyright notice of this file follows.*/
+/*
  * Copyright (C) 2007-2008  Camptocamp
  *
  * This file is part of MapFish Client
