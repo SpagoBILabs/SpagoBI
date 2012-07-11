61a645aa90e8eb1e5dbc6eea7665ac1e7b90b109
gioia <gioia>
2012-07-11 12:17:41 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18524 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext/sbi/execution/ParametersSelectionPage.js
+++ SpagoBIProject/web-content/js/src/ext/sbi/execution/ParametersSelectionPage.js
@@ -1,12 +1,23 @@
-/** SpagoBI, the Open Source Business Intelligence suite
+/**
+ * SpagoBI - The Business Intelligence Free Platform
+ *
+ * Copyright (C) 2004 - 2011 Engineering Ingegneria Informatica S.p.A.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
 
- * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
- * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
- 
-  
- 
-  
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ **/
  
 /**
   * Object name 
@@ -391,7 +402,7 @@ Ext.extend(Sbi.execution.ParametersSelectionPage, Ext.Panel, {
 	, initParametersPanel: function( config ) {
 		Ext.apply(config, {pageNumber: 2}); // this let the ParametersPanel know that it is on parameters selection page
 		if(this.isFromCross == true) {
-			
+			//alert(config.toSource());
 		}
 		
 		config.isFromCross = this.isFromCross;
