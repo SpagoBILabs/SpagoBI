2aeabb1aaaa9322481d7fc713c18297dea66d834
antonella giachino <antonella.giachino@eng.it>
2012-07-19 07:21:05 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18565 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIConsoleEngine/WebContent/js/spagobi/console/SummaryPanel.js
+++ SpagoBIConsoleEngine/WebContent/js/spagobi/console/SummaryPanel.js
@@ -51,28 +51,127 @@ Sbi.console.SummaryPanel = function(config) {
 		if(Sbi.settings && Sbi.settings.console && Sbi.settings.console.summaryPanel) {
 			defaultSettings = Ext.apply(defaultSettings, Sbi.settings.console.summaryPanel);
 		}
-		var widgetPanelConfig = config.layoutManagerConfig || {};
 		
 		var c = Ext.apply(defaultSettings, config || {});
-		widgetPanelConfig.executionContext = c.executionContext;
 		Ext.apply(this, c);
+
+		var widgetPanelConfig = this.initWidgetPanelConfig(c);
+		var widgetPanel = new Sbi.console.WidgetPanel(widgetPanelConfig);
 		
+		c = Ext.apply(c, {  	
+			items: [widgetPanel]
+		});
+			
+		// constructor
+		Sbi.console.SummaryPanel.superclass.constructor.call(this, c);	
+		
+		//this.on('expand', this.storeManager.forceRefresh, this);
+		//this.on('collapse', this.collapsePanel, this);
+		this.on('expand', this.expandPanel, this);
+		 
+		//add task: checks if all widgets have the isSWFReady setted to true. In this case force the refresh of datasets
+
+		var datasetsTask = this.datasets;
+		var isCompositeTask = this.isComposite;
+		var task = {};
+		task = {
+					run: function(){
+						var allWidgetsReady = false;
+						//if (!isComposite){		
+						if (!isCompositeTask){		
+							//each panel could contains more charts					
+							for (var k=0, l3 = widgetPanelConfig.items.length; k < l3; k++){
+								var tmpWidget = widgetPanelConfig.items[k].chart;
+								if (tmpWidget === undefined) break;
+								for (var x=0, l4 = tmpWidget.items.length; x < l4; x++){
+									var tmpChart = tmpWidget.items.get(x);
+									
+									if (tmpChart !== null && tmpChart !== undefined && !tmpChart.isSwfReady ){							
+										allWidgetsReady = false;
+										break;
+									}else {								
+										allWidgetsReady = true;								
+									}
+									
+								}
+								
+								if (!allWidgetsReady){
+									break;
+								}
+							}
+						} else{								
+							//each panel could contains more charts 
+							for (var k=0, l3 = widgetPanelConfig.items.length; k < l3; k++){
+								
+								for (p = 0; p < widgetPanelConfig.items[k].items.length; p++){
+									var tmpWidget = widgetPanelConfig.items[k].items.get(p).chart;
+									if (tmpWidget === undefined || tmpWidget === null) break;
+									for (var x=0, l4 = tmpWidget.items.length; x < l4; x++){
+										var tmpChart = tmpWidget.items.get(x);
+										
+										if (tmpChart !== null && tmpChart !== undefined && !tmpChart.isSwfReady ){							
+											allWidgetsReady = false;
+											break;
+										}else {								
+											allWidgetsReady = true;								
+										}										
+									}
+									
+									if (!allWidgetsReady){
+										break;
+									}
+								}
+								if (!allWidgetsReady){
+									break;
+								}
+							}								
+						} 
+						if (allWidgetsReady && k == l3  ) {
+							if ( widgetPanelConfig.storeManager !== null && widgetPanelConfig.storeManager !== undefined){
+								//for (ds in datasets){
+								for (ds in datasetsTask){									
+									//sets the single store as refreshable 
+									var tmpStore = widgetPanelConfig.storeManager.getStore(ds);									
+									if (tmpStore !== undefined){										
+										tmpStore.stopped = false;
+									}
+								}								
+								widgetPanelConfig.storeManager.forceRefresh();
+								//stops the task
+								Ext.TaskMgr.stop(task);								
+							}
+						}
+							
+					},
+					interval: 10 //milliseconds
+				};
+			
+				//starts the task
+				Ext.TaskMgr.start(task);
+};
+		
+
+Ext.extend(Sbi.console.SummaryPanel, Ext.Panel, {
+    
+    services: null,
+	datasets: [],
+	isComposite: false,
+	
+    //  -- public methods ---------------------------------------------------------
+    
+    
+    
+    //  -- private methods ---------------------------------------------------------	
+	initWidgetPanelConfig: function(config){
+		var widgetPanelConfig = config.layoutManagerConfig || {};
+		widgetPanelConfig.executionContext = config.executionContext;
 		widgetPanelConfig.storeManager = this.storeManager;
 		widgetPanelConfig.items = [];
 		
-		var datasets = [];
-		var isComposite = false;
-		
-		//test
-		/*
-		var d = new Date();
-		var start = d.getTime();
-		alert('start: ' + start);
-		*/
 		for(var i = 0, l1 = config.charts.length ; i < l1; i++) {
 			if(config.charts[i].widgetConfig.type === 'chart.composite') {
 				//composite widget
-				isComposite = true;
+				this.isComposite = true;
 				
 				//sets the general style of the table 
 				widgetPanelConfig.layoutConfig = config.layoutManagerConfig.layoutConfig || {};
@@ -123,8 +222,8 @@ Sbi.console.SummaryPanel = function(config) {
 						else{
 							configSubChart.dataset = defaultDataset;
 						}
-					}
-					datasets[configSubChart.dataset] = configSubChart.dataset ;
+					}					
+					this.datasets[configSubChart.dataset] = configSubChart.dataset ;
 					
 					//sets the WIDTH of single element; the order for getting values are: single widget, single panel, table
 					if (configSubChart.width === undefined){
@@ -180,119 +279,37 @@ Sbi.console.SummaryPanel = function(config) {
 
 			} else {
 				//simple widget
-				datasets[config.charts[i].dataset] = config.charts[i].dataset;
+				this.datasets[config.charts[i].dataset] = config.charts[i].dataset;				
 				widgetPanelConfig.items.push(new Sbi.console.ChartWidget(config.charts[i]));
-
 			}
 		}		
 		
 		widgetPanelConfig.autoScroll = {};
 		widgetPanelConfig.autoScroll = true;
-		var widgetPanel = new Sbi.console.WidgetPanel(widgetPanelConfig);
-		
-		c = Ext.apply(c, {  	
-			items: [widgetPanel]
-		});
-			
-		// constructor
-		Sbi.console.SummaryPanel.superclass.constructor.call(this, c);			
-		 
-		//add task: checks if all widgets have the isSWFReady setted to true. In this case force the refresh of datasets
-		var task = {};
-		 
-			task = {
-					run: function(){
-				//		alert('run function');
-						var allWidgetsReady = false;
-						if (!isComposite){							
-							//each panel could contains more charts					
-							for (var k=0, l3 = widgetPanelConfig.items.length; k < l3; k++){
-								var tmpWidget = widgetPanelConfig.items[k].chart;
-								if (tmpWidget === undefined) break;
-								for (var x=0, l4 = tmpWidget.items.length; x < l4; x++){
-									var tmpChart = tmpWidget.items.get(x);
-									
-									if (tmpChart !== null && tmpChart !== undefined && !tmpChart.isSwfReady ){							
-										allWidgetsReady = false;
-										break;
-									}else {								
-										allWidgetsReady = true;								
-									}
-									
-								}
-								
-								if (!allWidgetsReady){
-									break;
-								}
-							}
-						} else{								
-							//each panel could contains more charts 
-							for (var k=0, l3 = widgetPanelConfig.items.length; k < l3; k++){
-								
-								for (p = 0; p < widgetPanelConfig.items[k].items.length; p++){
-									var tmpWidget = widgetPanelConfig.items[k].items.get(p).chart;
-									if (tmpWidget === undefined || tmpWidget === null) break;
-									for (var x=0, l4 = tmpWidget.items.length; x < l4; x++){
-										var tmpChart = tmpWidget.items.get(x);
-										
-										if (tmpChart !== null && tmpChart !== undefined && !tmpChart.isSwfReady ){							
-											allWidgetsReady = false;
-											break;
-										}else {								
-											allWidgetsReady = true;								
-										}										
-									}
-									
-									if (!allWidgetsReady){
-										break;
-									}
-								}
-								if (!allWidgetsReady){
-									break;
-								}
-							}								
-						} 
-						if (allWidgetsReady && k == l3  ) {
-							/*
-							var d2 = new Date();
-							var end = d2.getTime();
-							alert('end: ' + end);
-							var diff = end - start;
-							alert(' - diff: ' + diff);
-							*/
-							if ( widgetPanelConfig.storeManager !== null && widgetPanelConfig.storeManager !== undefined){	
-								for (ds in datasets){
-									//sets the single store as refreshable 
-									var tmpStore = widgetPanelConfig.storeManager.getStore(ds);
-									
-									if (tmpStore !== undefined){										
-										tmpStore.stopped = false;
-									}
-								}								
-								widgetPanelConfig.storeManager.forceRefresh();
-								//stops the task
-								Ext.TaskMgr.stop(task);
-							}
-						}
-							
-					},
-					interval: 10 //milliseconds
-				};
-			
-				//starts the task
-				Ext.TaskMgr.start(task);
-};
-		
+		return widgetPanelConfig;		
+	}
 
-Ext.extend(Sbi.console.SummaryPanel, Ext.Panel, {
-    
-    services: null
-   
-    //  -- public methods ---------------------------------------------------------
-    
-    
-    
-    //  -- private methods ---------------------------------------------------------
+	, expandPanel: function(p){
+		//WORK-AROUND for IE: that's the problem: if the user collapse the summaryPanel and expand it
+		// the widgets don't refresh its values. Probably the problem is in the call of the method loadData
+		//of the single swf widget because the dataset is regularly loaded
+		/*
+		// if (Ext.isIE){
+			 //var bckItems = [];
+			 var bckItems = Ext.apply({}, this.items);    
+			 //this.items.each(function(item){            
+		     //       this.items.remove(item);
+		     //       item.destroy();           
+		     //   }, this); 
+			 this.items = [];
+			 this.items = bckItems;
+			 this.doLayout();
+		 //}
+	//	 this.resumeEvents();
+		 */
+	}
 	
-    
+	//, collapsePanel: function(){
+	//	this.suspendEvents();						
+	//} 
 });
