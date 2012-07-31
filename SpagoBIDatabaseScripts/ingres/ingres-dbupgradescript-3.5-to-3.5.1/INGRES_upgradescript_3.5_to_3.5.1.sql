bba6f9d9fa146648d6ea086d727e51f21435a01b
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-07-31 07:26:31 +0000

    Deleted DEFAULT_DATE as parameter type . The only PAR_TYPE available are : STRING, NUM and DATE.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18634 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-3.5-to-3.5.1/INGRES_upgradescript_3.5_to_3.5.1.sql
+++ SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-3.5-to-3.5.1/INGRES_upgradescript_3.5_to_3.5.1.sql
@@ -15,3 +15,10 @@ INSERT INTO SBI_ROLE_TYPE_USER_FUNC (ROLE_TYPE_ID, USER_FUNCT_ID)
 commit;\p\g
 
 ALTER TABLE SBI_EXT_ROLES ADD COLUMN MANAGE_USERS TINYINT DEFAULT 0;\p\g
+
+--Deleted 'DATE_DEFAULT' as parameter type. The only parameters types usable are: date, num and string!
+update SBI_PARAMETERS set PAR_TYPE_CD = 'DATE', PAR_TYPE_ID = 
+	(select VALUE_ID from SBI_DOMAINS where value_cd = 'DATE' AND domain_cd = 'PAR_TYPE') WHERE PAR_TYPE_CD = 'DATE_DEFAULT';\p\g
+delete from SBI_DOMAINS  where VALUE_NM = 'sbidomains.nm.date.default';\p\g
+commit;\p\g
+
