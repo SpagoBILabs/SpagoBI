9a17c9c2f0c5d2cdabefa9b81375738380f86f1e
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-08-30 09:39:25 +0000

    Improved exception management
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18707 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- cas/src/it/eng/spagobi/authentication/handler/BaseAuthenticationHandler.java
+++ cas/src/it/eng/spagobi/authentication/handler/BaseAuthenticationHandler.java
@@ -9,20 +9,21 @@ import it.eng.spago.base.SourceBean;
 import it.eng.spago.base.SourceBeanAttribute;
 import it.eng.spagobi.authentication.utility.AuthenticationUtility;
 
-
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.jasig.cas.authentication.handler.AuthenticationException;
+import org.jasig.cas.authentication.handler.BadCredentialsAuthenticationException;
 import org.jasig.cas.authentication.handler.support.AbstractUsernamePasswordAuthenticationHandler;
 import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
 
 
 
 /**
- * @author Giachino (antonella.giachino@eng.it)
+ * @author 
+ * 	Giachino (antonella.giachino@eng.it)
+ *  Davide Zerbetto (davide.zerbetto@eng.it)
  **/
 
 /**
@@ -33,43 +34,51 @@ public class BaseAuthenticationHandler extends AbstractUsernamePasswordAuthentic
 	
     protected boolean authenticateUsernamePasswordInternal(UsernamePasswordCredentials credentials) 
     	throws AuthenticationException {
-        logger.debug("IN");
+		logger.debug("IN");
 
-        
-        String username = credentials.getUsername();
-        String password = credentials.getPassword();
-        
-        logger.debug("user  "+username);
-        logger.debug("psw  "+password);
-        
-        String correctPassword = null;
-        String encrPass = null;
-        logger.debug("Start validating password for the user " + username);
-        List lstResult = null;
-        //define query to get pwd from database
-        try{
-        	encrPass = Password.encriptPassword(password);
-        	AuthenticationUtility utility = new AuthenticationUtility();
-        	List pars = new LinkedList();
-        	// CASE INSENSITVE SEARCH ON USER ID
-        	pars.add(username.toUpperCase());
-        	lstResult = utility.executeQuery("SELECT PASSWORD FROM SBI_USER WHERE UPPER(USER_ID) = ?", pars);
-        }catch(Exception e){
-        	logger.error("Error while check pwd: " + e);
-        	e.printStackTrace();
-        }
-        
-      //gets the pwd presents in db
-		Iterator iter_sb_pwd = lstResult.iterator();
-		while(iter_sb_pwd.hasNext()) {
-			SourceBeanAttribute tmp_attributeSB = (SourceBeanAttribute)iter_sb_pwd.next();
-			SourceBean attributeSB = (SourceBean)tmp_attributeSB.getValue();
-			String attribute = (String)attributeSB.getAttribute("PASSWORD");
-			correctPassword = attribute;
+		String username = credentials.getUsername();
+		String password = credentials.getPassword();
+		logger.debug("user : " + username);
+		logger.debug("psw : " + password);
+
+		String correctPassword = null;
+		String encrPass = null;
+		logger.debug("Start validating password for the user " + username);
+		List lstResult = null;
+		// define query to get pwd from database
+		try {
+			encrPass = Password.encriptPassword(password);
+			AuthenticationUtility utility = new AuthenticationUtility();
+			List pars = new LinkedList();
+			// CASE INSENSITVE SEARCH ON USER ID
+			pars.add(username.toUpperCase());
+			lstResult = utility.executeQuery(
+					"SELECT PASSWORD FROM SBI_USER WHERE UPPER(USER_ID) = ?",
+					pars);
+		} catch (Exception e) {
+			logger.error("Error while check pwd: " + e);
+			throw new RuntimeException("Cannot authenticate user", e);
 		}
-        
-        logger.debug("OUT");
-        return encrPass.equals(correctPassword);
+
+		if (lstResult == null || lstResult.size() == 0) {
+			logger.error("No user with the specified user identifier : [" + username + "]");
+			throw new BadCredentialsAuthenticationException();
+		}
+		
+		if (lstResult.size() > 1) {
+			logger.error("There are different users with the same user identifier : " + username + ". " +
+					"Remember that the check is case INSENSITIVE");
+			throw new RuntimeException("There are different users with the same user identifier : " + username + ". " +
+					"Remember that the check is case INSENSITIVE");
+		}
+		
+		// gets the pwd presents in db
+		SourceBeanAttribute sbAttribute = (SourceBeanAttribute) lstResult.get(0);
+		SourceBean value = (SourceBean) sbAttribute.getValue();
+		correctPassword = (String) value.getAttribute("PASSWORD");
+
+		logger.debug("OUT");
+		return encrPass.equals(correctPassword);
     }
     
     
