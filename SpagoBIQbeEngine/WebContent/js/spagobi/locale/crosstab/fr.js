19c54d07f49b5f82898c79b1f401e0d2c70660bd
Angelo Bernabei <angelo.bernabei@eng.it>
2012-11-22 09:09:58 +0000

    Modifiche fornite da Alliadis (alessandra)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18936 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/locale/crosstab/fr.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/crosstab/fr.js
@@ -15,27 +15,27 @@ Sbi.locale.ln = Sbi.locale.ln || new Array();
 //===================================================================
 //CROSSTAB DESIGNER
 //===================================================================
-Sbi.locale.ln['sbi.crosstab.crosstabdesignerpanel.title'] = 'Concepteur Tableau Crois\u00E9';
-Sbi.locale.ln['sbi.crosstab.crosstabdefinitionpanel.tools.preview'] = 'Pr\u00E9visualiser le Tableau crois\u00E9';
-Sbi.locale.ln['sbi.crosstab.crosstabdefinitionpanel.title'] = 'D\u00E9finition Tableau Crois\u00E9';
+Sbi.locale.ln['sbi.crosstab.crosstabdesignerpanel.title'] = 'Concepteur de tableau crois\u00E9';
+Sbi.locale.ln['sbi.crosstab.crosstabdefinitionpanel.tools.preview'] = 'Pr\u00E9visualiser le tableau crois\u00E9';
+Sbi.locale.ln['sbi.crosstab.crosstabdefinitionpanel.title'] = 'D\u00E9finition du tableau crois\u00E9';
 Sbi.locale.ln['sbi.crosstab.crosstabdefinitionpanel.rows'] = 'Lignes';
 Sbi.locale.ln['sbi.crosstab.crosstabdefinitionpanel.columns'] = 'Colonnes';
 Sbi.locale.ln['sbi.crosstab.crosstabdefinitionpanel.measures'] = 'Mesures';
-Sbi.locale.ln['sbi.crosstab.attributescontainerpanel.cannotdrophere.title'] = 'Suppression Impossible';
+Sbi.locale.ln['sbi.crosstab.attributescontainerpanel.cannotdrophere.title'] = 'Suppression impossible';
 Sbi.locale.ln['sbi.crosstab.attributescontainerpanel.cannotdrophere.attributealreadypresent'] = 'L\'attribut a d\u00E9j\u00E0 \u00E9t\u00E9 choisi';
 //Sbi.locale.ln['sbi.crosstab.attributescontainerpanel.cannotdrophere.attributealreadypresentfilters'] = 'L\'attribut a d\u00E9j\u00E0 \u00E9t\u00E9 choisi';
 Sbi.locale.ln['sbi.crosstab.attributescontainerpanel.cannotdrophere.measures'] = 'Impossible de glisser la mesure sur les lignes ou les colonnes : vous devez les glisser dans la section centrale du tableau crois\u00E9';
 Sbi.locale.ln['sbi.crosstab.attributescontainerpanel.tools.tt.removeall'] = ['Tout Supprimer'];
 
-Sbi.locale.ln['sbi.crosstab.measurescontainerpanel.cannotdrophere.title'] = 'Impossible supprimer';
-Sbi.locale.ln['sbi.crosstab.measurescontainerpanel.cannotdrophere.measurealreadypresent'] = 'La mesure est a d\u00E9j\u00E0 \u00E9t\u00E9 choisie';
+Sbi.locale.ln['sbi.crosstab.measurescontainerpanel.cannotdrophere.title'] = 'Suppression impossible';
+Sbi.locale.ln['sbi.crosstab.measurescontainerpanel.cannotdrophere.measurealreadypresent'] = 'La mesure a d\u00E9j\u00E0 \u00E9t\u00E9 choisie';
 Sbi.locale.ln['sbi.crosstab.measurescontainerpanel.cannotdrophere.attributes'] = 'Impossible de glisser les attributs dans la section centrale : vous devez les placer sur les lignes ou les colonnes';
 Sbi.locale.ln['sbi.crosstab.measurescontainerpanel.cannotdrophere.postlinecalculated'] = 'Vous ne pouvez pas utiliser les champs calcul\u00E9s dans un tableau crois\u00E9';
 
 Sbi.locale.ln['sbi.crosstab.measurescontainerpanel.tools.tt.showdetailswizard'] = 'Pr\u00E9senter les d\u00E9tails';
 Sbi.locale.ln['sbi.crosstab.measurescontainerpanel.tools.tt.removeall'] = ['Tout supprimer'];
 
-Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.title'] = 'D\u00E9tails Tableau Crois\u00E9';
+Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.title'] = 'D\u00E9tails du tableau crois\u00E9';
 Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.measureson'] = 'Mesures en';
 Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.rows'] = 'Ligne';
 Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.columns'] = 'Colonne';
@@ -51,20 +51,20 @@ Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.percenton'] = 'Pourcentage cal
 Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.row'] = 'Ligne';
 Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.column'] = 'Colonne';
 Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.nopercent'] = 'non';
-Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.maxcellnumber'] = 'Maximum nombre de cellules';
+Sbi.locale.ln['sbi.crosstab.crosstabdetailswizard.maxcellnumber'] = 'Nombre maxium de cellules';
 
 Sbi.locale.ln['sbi.crosstab.chooseaggregationfunctionwindow.title'] = 'Choisissez la fonction d\'agr\u00E9gation pour la mesure';
 Sbi.locale.ln['sbi.crosstab.chooseaggregationfunctionwindow.buttons.apply'] = 'Appliquer';
 Sbi.locale.ln['sbi.crosstab.chooseaggregationfunctionwindow.buttons.cancel'] = 'Annuler';
 
-Sbi.locale.ln['sbi.crosstab.crosstabpreviewpanel.title'] = 'Aper\u00E7u Tableau crois\u00E9';
-Sbi.locale.ln['sbi.crosstab.crosstabpreviewpanel.overflow.warning'] = 'Le nombre de cellules est sup\u00e9rieure \u00e0 celui par d\u00e9faut. Vous pouvez trouver toutes les donn\u00e9es avec l\'exportation en XLS.';
+Sbi.locale.ln['sbi.crosstab.crosstabpreviewpanel.title'] = 'Aper\u00E7u du tableau crois\u00E9';
+Sbi.locale.ln['sbi.crosstab.crosstabpreviewpanel.overflow.warning'] = 'Le nombre de cellules est sup\u00e9rieur \u00e0 celui par d\u00e9faut. Vous pouvez trouver toutes les donn\u00e9es avec l\'exportation en XLS.';
 
-Sbi.locale.ln['sbi.crosstab.crossTabValidation.title'] = 'cross Tab n\'est pas valide';
-Sbi.locale.ln['sbi.crosstab.crossTabValidation.noMeasure'] = 'Utilise au moin une mesure';
-Sbi.locale.ln['sbi.crosstab.crossTabValidation.noAttribute'] = 'Utilise au moin un attribut';
-Sbi.locale.ln['sbi.crosstab.crossTabValidation.noSegmentAttribute'] = 'Il faut utiliser segment attribut';
-Sbi.locale.ln['sbi.crosstab.crossTabValidation.noMandatoryMeasure'] = 'Il faut utiliser le mesure obbligatoire';
+Sbi.locale.ln['sbi.crosstab.crossTabValidation.title'] = 'Le tableau crois\u00E9 n\'est pas valide';
+Sbi.locale.ln['sbi.crosstab.crossTabValidation.noMeasure'] = 'Utiliser au moins une mesure';
+Sbi.locale.ln['sbi.crosstab.crossTabValidation.noAttribute'] = 'Utiliser au moins un attribut';
+Sbi.locale.ln['sbi.crosstab.crossTabValidation.noSegmentAttribute'] = 'Il faut utiliser un segment attribut';
+Sbi.locale.ln['sbi.crosstab.crossTabValidation.noMandatoryMeasure'] = 'Il faut utiliser une mesure';
 
 //===================================================================
 //CROSSTAB
@@ -72,9 +72,9 @@ Sbi.locale.ln['sbi.crosstab.crossTabValidation.noMandatoryMeasure'] = 'Il faut u
 
 Sbi.locale.ln['sbi.crosstab.header.total.text'] = 'Total';
 
-Sbi.locale.ln['sbi.crosstab.menu.addcalculatedfield'] = 'Ajouter champ calcul\u00E9';
-Sbi.locale.ln['sbi.crosstab.menu.removecalculatedfield'] = 'Supprimer champ calcul\u00E9';
-Sbi.locale.ln['sbi.crosstab.menu.modifycalculatedfield'] = 'Modifier champ calcul\u00E9';
+Sbi.locale.ln['sbi.crosstab.menu.addcalculatedfield'] = 'Ajouter un champ calcul\u00E9';
+Sbi.locale.ln['sbi.crosstab.menu.removecalculatedfield'] = 'Supprimer un champ calcul\u00E9';
+Sbi.locale.ln['sbi.crosstab.menu.modifycalculatedfield'] = 'Modifier un champ calcul\u00E9';
 Sbi.locale.ln['sbi.crosstab.menu.hideheader'] = 'Masquer l\'ent\u00EAte';
 Sbi.locale.ln['sbi.crosstab.menu.hideheadertype'] = 'Masquer toutes les ent\u00EAtes de ce type';
 Sbi.locale.ln['sbi.crosstab.menu.hidemeasure'] = 'Mesures';
