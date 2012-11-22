6fdc4802c144379b8ec12448d2e017ae7cb1811e
Angelo Bernabei <angelo.bernabei@eng.it>
2012-11-22 09:15:47 +0000

    Modifiche fornite da Alliadis (alessandra)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18941 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/locale/widgets/lookupfield/fr.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/widgets/lookupfield/fr.js
@@ -13,6 +13,6 @@ Sbi.locale.ln = Sbi.locale.ln || new Array();
 
 Sbi.locale.ln['sbi.lookup.Confirm'] = 'Confirmer';
 Sbi.locale.ln['sbi.lookup.Annulla'] = 'Annuler';
-Sbi.locale.ln['sbi.lookup.Select']= 'Selectionner Valeur ...';
+Sbi.locale.ln['sbi.lookup.Select']= 'S\u00E9lectionner une valeur ...';
 Sbi.locale.ln['sbi.lookup.ValueOfColumn'] ='La valeur d\'une colonne';
 Sbi.locale.ln['sbi.lookup.asA'] = 'Comme un';
