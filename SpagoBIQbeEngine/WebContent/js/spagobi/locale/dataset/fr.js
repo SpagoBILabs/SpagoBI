e8a2d857d9c81b1b0eef6941ce507f1bec33893d
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-11-22 09:13:11 +0000

    Modifiche fornite da Alliadis (alessandra)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18937 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/locale/dataset/fr.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/dataset/fr.js
@@ -13,6 +13,6 @@ Sbi.locale.ln = Sbi.locale.ln || new Array();
 
 Sbi.locale.ln['sbi.dataset.parametersgridpanel.title'] = 'Param\u00E8tres';
 Sbi.locale.ln['sbi.dataset.parametersgridpanel.buttons.add'] = 'Ajouter';
-Sbi.locale.ln['sbi.dataset.parametersgridpanel.buttons.remove'] = 'Supprimez';
+Sbi.locale.ln['sbi.dataset.parametersgridpanel.buttons.remove'] = 'Supprimer';
 Sbi.locale.ln['sbi.dataset.parametersgridpanel.columns.name'] = 'Nom';
 Sbi.locale.ln['sbi.dataset.parametersgridpanel.columns.type'] = 'Type';
