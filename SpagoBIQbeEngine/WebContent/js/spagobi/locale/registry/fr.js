e99a2b0b4037bd2c97da140de9b4ee9a70584f01
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-11-22 09:15:01 +0000

    Modifiche fornite da Alliadis (alessandra)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18940 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/locale/registry/fr.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/locale/registry/fr.js
@@ -13,13 +13,13 @@ Ext.ns("Sbi.locale");
 //===================================================================
 //Sbi.registry.RegistryPanel 
 //===================================================================
-Sbi.locale.ln['sbi.registry.registrypanel.title']= 'Registry';
+Sbi.locale.ln['sbi.registry.registrypanel.title']= 'Registre';
 
 //===================================================================
 //Sbi.registry.RegistryEditorGridPanel
 //===================================================================
 Sbi.locale.ln['sbi.registry.registryeditorgridpanel.mandatory']= ': champ obligatoire';
-Sbi.locale.ln['sbi.registry.registryeditorgridpanel.saveconfirm.title']= 'Status';
+Sbi.locale.ln['sbi.registry.registryeditorgridpanel.saveconfirm.title']= 'Statut';
 Sbi.locale.ln['sbi.registry.registryeditorgridpanel.saveconfirm.message']= 'Donn\u00e9es mises \u00e0 jour correctement';
 Sbi.locale.ln['sbi.registry.registryeditorgridpanel.saveconfirm.message.ko']= 'L\'op\u00e9ration a \u00e9chou\u00e9';
 Sbi.locale.ln['sbi.registry.registryeditorgridpanel.validation']= 'Erreur de validation: champ num\u00e9rique';
