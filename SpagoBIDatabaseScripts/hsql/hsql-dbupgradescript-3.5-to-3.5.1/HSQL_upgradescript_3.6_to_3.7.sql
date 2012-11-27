a89d5eb0e7a50cc1fbc3be81b166ff279e86ddd7
antonella giachino <antonella.giachino@eng.it>
2012-11-27 11:39:36 +0000

    Added an optional check on the user role received into the request : it must be available on the user that makes the login. The check is actived by two new configuration: - SPAGOBI.SECURITY.CHECK_ROLE_LOGIN (boolean to activate the check) - SPAGOBI.SECURITY.ROLE_LOGIN (string. Identifies the attribute in the request with the role to check)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@18957 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIDatabaseScripts/hsql/hsql-dbupgradescript-3.5-to-3.5.1/HSQL_upgradescript_3.6_to_3.7.sql
@@ -0,0 +1,7 @@
+--inserts configuration for check of role in login module
+INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN) VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'),'SPAGOBI.SECURITY.CHECK_ROLE_LOGIN', 'SPAGOBI.SECURITY.CHECK_ROLE_LOGIN', 'Check the correct role in login action', false, 'false',(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SECURITY', 'biadmin', current_timestamp);
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
+INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN) VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'),'SPAGOBI.SECURITY.ROLE_LOGIN', 'SPAGOBI.SECURITY.ROLE_LOGIN', 'The value of the role to check at login module', false, '',(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SECURITY', 'biadmin', current_timestamp);
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
+
+COMMIT;
