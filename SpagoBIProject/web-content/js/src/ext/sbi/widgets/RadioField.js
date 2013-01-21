758dcb258e156a56099b05be1de2aa48708e7c74
gioia <gioia>
2013-01-21 17:28:59 +0000

    OPEN - issue SPAGOBI-1044: As SpagoBI user I want to have document's parameters whose selector is a radiobutton/checkbox group https://spagobi.eng.it/jira/browse/SPAGOBI-1044
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@19068 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIProject/web-content/js/src/ext/sbi/widgets/RadioField.js
@@ -0,0 +1,45 @@
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
+
+/**
+ * Sbi.widgets.CheckboxFiel
+ * 
+ * Authors
+ *  - Andrea Gioia (mail)
+ */
+
+Ext.ns("Sbi.widgets");
+
+Sbi.widgets.RadioField = Ext.extend(Sbi.widgets.CheckboxField, {
+    allowBlank : true,
+    blankText : "You must select one item in this group",
+    defaultType : 'radio',
+    groupCls: 'x-form-radio-group'
+	
+    , createOptionItem: function(optionConfig) {
+    	var radio = new Ext.form.Radio(optionConfig);
+    	return radio;
+    }
+
+	, getValue : function(){
+        var out = [];
+        this.eachItem(function(item){
+            if(item.checked){
+                out.push(item.value);
+            }
+        });
+        if(out.length > 1) alert("Assertion failed. In a radio field only one value can be checked at the same time");
+        return out[0];
+    }
+    
+	, getValues : function() {
+    	return [this.getValue()];
+	}	
+	
+	, getRawValue : function() {
+		return this.getValues().join();
+	}
+});
