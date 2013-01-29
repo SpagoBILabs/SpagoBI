c84ae7386e0e7230e4666f7a3b15994eda8022a4
Davide Zerbetto <davide.zerbetto@eng.it>
2013-01-29 13:35:31 +0000

    Merge with MondrianSchemaCatalogue branch
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Server/trunk@19111 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/sqlserver/sqlserver-dbupgradescript-3.6-to-3.7/SQLSERVER_upgradescript_3.6_to_3.7.sql
+++ SpagoBIDatabaseScripts/sqlserver/sqlserver-dbupgradescript-3.6-to-3.7/SQLSERVER_upgradescript_3.6_to_3.7.sql
@@ -5,3 +5,114 @@ INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, V
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
 
 COMMIT;
+
+INSERT INTO SBI_USER_FUNC (USER_FUNCT_ID, NAME, DESCRIPTION, USER_IN, TIME_IN)
+    VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_USER_FUNC'), 
+    'ArtifactCatalogueManagement','ArtifactCatalogueManagement', 'server', current_timestamp);
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_USER_FUNC';
+commit;
+
+INSERT INTO SBI_ROLE_TYPE_USER_FUNC (ROLE_TYPE_ID, USER_FUNCT_ID)
+    VALUES ((SELECT VALUE_ID FROM SBI_DOMAINS WHERE VALUE_CD = 'ADMIN' AND DOMAIN_CD = 'ROLE_TYPE'), 
+    (SELECT USER_FUNCT_ID FROM SBI_USER_FUNC WHERE NAME = 'ArtifactCatalogueManagement'));
+commit;
+
+INSERT INTO SBI_USER_FUNC (USER_FUNCT_ID, NAME, DESCRIPTION, USER_IN, TIME_IN)
+    VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_USER_FUNC'), 
+    'MetaModelsCatalogueManagement','MetaModelsCatalogueManagement', 'server', current_timestamp);
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_USER_FUNC';
+commit;
+
+INSERT INTO SBI_ROLE_TYPE_USER_FUNC (ROLE_TYPE_ID, USER_FUNCT_ID)
+    VALUES ((SELECT VALUE_ID FROM SBI_DOMAINS WHERE VALUE_CD = 'ADMIN' AND DOMAIN_CD = 'ROLE_TYPE'), 
+    (SELECT USER_FUNCT_ID FROM SBI_USER_FUNC WHERE NAME = 'MetaModelsCatalogueManagement'));
+commit;
+
+CREATE TABLE SBI_META_MODELS (
+       ID                   INTEGER NOT NULL,
+       NAME                 VARCHAR(100) NOT NULL,
+       DESCR                VARCHAR(500) NULL,
+       USER_IN              VARCHAR(100) NOT NULL,
+       USER_UP              VARCHAR(100),
+       USER_DE              VARCHAR(100),
+       TIME_IN              DATETIME NOT NULL,
+       TIME_UP              DATETIME DEFAULT NULL,    
+       TIME_DE              DATETIME DEFAULT NULL,
+       SBI_VERSION_IN       VARCHAR(10),
+       SBI_VERSION_UP       VARCHAR(10),
+       SBI_VERSION_DE       VARCHAR(10),
+       META_VERSION         VARCHAR(100),
+       ORGANIZATION         VARCHAR(20), 
+       CONSTRAINT XAK1SBI_META_MODELS UNIQUE (NAME, ORGANIZATION),
+       PRIMARY KEY (ID)
+) ON [PRIMARY];
+
+CREATE TABLE SBI_META_MODELS_VERSIONS (
+        ID                   INTEGER NOT NULL,
+        MODEL_ID             INTEGER NOT NULL,
+        CONTENT              IMAGE NOT NULL,
+        NAME                 VARCHAR(100),  
+        PROG                 INTEGER,
+        CREATION_DATE        DATETIME DEFAULT NULL,
+        CREATION_USER        VARCHAR(50) NOT NULL, 
+        ACTIVE               BIT,  
+        USER_IN              VARCHAR(100) NOT NULL,
+        USER_UP              VARCHAR(100),
+        USER_DE              VARCHAR(100),
+        TIME_IN              DATETIME NOT NULL,
+        TIME_UP              DATETIME DEFAULT NULL,    
+        TIME_DE              DATETIME DEFAULT NULL,
+        SBI_VERSION_IN       VARCHAR(10),
+        SBI_VERSION_UP       VARCHAR(10),
+        SBI_VERSION_DE       VARCHAR(10),
+        META_VERSION         VARCHAR(100),
+        ORGANIZATION         VARCHAR(20), 
+        PRIMARY KEY (ID)
+) ON [PRIMARY];
+
+ALTER TABLE SBI_META_MODELS_VERSIONS ADD CONSTRAINT FK_SBI_META_MODELS_VERSIONS_1 FOREIGN KEY ( MODEL_ID ) REFERENCES SBI_META_MODELS( ID ) ON DELETE CASCADE;
+
+CREATE TABLE SBI_ARTIFACTS (
+       ID                   INTEGER NOT NULL,
+       NAME                 VARCHAR(100) NOT NULL,
+       DESCR                VARCHAR(500) NULL,
+       TYPE                 VARCHAR(50) NULL,
+       USER_IN              VARCHAR(100) NOT NULL,
+       USER_UP              VARCHAR(100),
+       USER_DE              VARCHAR(100),
+       TIME_IN              DATETIME NOT NULL,
+       TIME_UP              DATETIME DEFAULT NULL,    
+       TIME_DE              DATETIME DEFAULT NULL,
+       SBI_VERSION_IN       VARCHAR(10),
+       SBI_VERSION_UP       VARCHAR(10),
+       SBI_VERSION_DE       VARCHAR(10),
+       META_VERSION         VARCHAR(100),
+       ORGANIZATION         VARCHAR(20), 
+       CONSTRAINT XAK1SBI_ARTIFACTS UNIQUE (NAME, TYPE, ORGANIZATION),
+       PRIMARY KEY (ID)
+) ON [PRIMARY];
+
+CREATE TABLE SBI_ARTIFACTS_VERSIONS (
+        ID                   INTEGER NOT NULL,
+        ARTIFACT_ID          INTEGER NOT NULL,
+        CONTENT              IMAGE NOT NULL,
+        NAME                 VARCHAR(100),  
+        PROG                 INTEGER,
+        CREATION_DATE        DATETIME DEFAULT NULL,
+        CREATION_USER        VARCHAR(50) NOT NULL, 
+        ACTIVE               BIT,
+        USER_IN              VARCHAR(100) NOT NULL,
+        USER_UP              VARCHAR(100),
+        USER_DE              VARCHAR(100),
+        TIME_IN              DATETIME NOT NULL,
+        TIME_UP              DATETIME DEFAULT NULL,    
+        TIME_DE              DATETIME DEFAULT NULL,
+        SBI_VERSION_IN       VARCHAR(10),
+        SBI_VERSION_UP       VARCHAR(10),
+        SBI_VERSION_DE       VARCHAR(10),
+        META_VERSION         VARCHAR(100),
+        ORGANIZATION         VARCHAR(20), 
+        PRIMARY KEY (ID)
+) ON [PRIMARY];
+
+ALTER TABLE SBI_ARTIFACTS_VERSIONS ADD CONSTRAINT FK_SBI_ARTIFACTS_VERSIONS_1 FOREIGN KEY ( ARTIFACT_ID ) REFERENCES SBI_ARTIFACTS( ID ) ON DELETE CASCADE;
