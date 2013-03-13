eda136fa1b9d39447a5a9af89ffd38c13aef6758
gioia <gioia>
2013-03-13 12:55:06 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@19300 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIUtils/src/it/eng/spagobi/services/metamodel/MetamodelService.java
@@ -0,0 +1,29 @@
+/*
+ * SpagoBI, the Open Source Business Intelligence suite
+ * © 2005-2015 Engineering Group
+ *
+ * This file is part of SpagoBI. SpagoBI is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 2.1 of the License, or any later version. 
+ * SpagoBI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received
+ * a copy of the GNU Lesser General Public License along with SpagoBI. If not, see: http://www.gnu.org/licenses/.
+ * The complete text of SpagoBI license is included in the COPYING.LESSER file. 
+ */
+package it.eng.spagobi.services.metamodel;
+
+import javax.activation.DataHandler;
+
+/**
+ * @author Andrea Gioia (andrea.gioia@eng.it)
+ *
+ */
+public interface MetamodelService {
+	/**
+	 * return the metamodel by the name
+	 * @param token. The token.
+	 * @param user. The user.
+	 * @param id. The metamodel's name.
+	 * @return the content of the metamodel.
+	 */
+	DataHandler getMetamodelContentByName(String token, String user, String name);
+}
