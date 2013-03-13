727400ee2ff794200241d0389fb760c879c15fc4
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-03-13 14:11:25 +0000

    IN PROGRESS - issue SPAGOBI-1071: Added Metamodel Service proxy and associated classes
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@19303 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/utilities/engines/EngineConstants.java
+++ SpagoBIUtils/src/it/eng/spagobi/utilities/engines/EngineConstants.java
@@ -35,7 +35,8 @@ public class EngineConstants {
 	public static final String ENV_USER_PROFILE = "ENV_USER_PROFILE";	
 	public static final String ENV_EXECUTION_ROLE = "ENV_EXECUTION_ROLE";	
 	public static final String ENV_USER_DATE_FORMAT = "ENV_USER_DATE_FORMAT";
-	public static final String ENV_DB_DATE_FORMAT = "ENV_DB_DATE_FORMAT";	
+	public static final String ENV_DB_DATE_FORMAT = "ENV_DB_DATE_FORMAT";
+	public static final Object ENV_METAMODEL_PROXY = "ENV_METAMODEL_PROXY";	
 	
 
 
