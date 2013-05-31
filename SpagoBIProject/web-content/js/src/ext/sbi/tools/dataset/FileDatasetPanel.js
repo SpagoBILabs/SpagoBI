ce97bcf54b7e1922f10817b8bb60ba06f61793ea
aghedin <aghedin@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-05-31 13:27:34 +0000

    Test
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@19769 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext/sbi/tools/dataset/FileDatasetPanel.js
+++ SpagoBIProject/web-content/js/src/ext/sbi/tools/dataset/FileDatasetPanel.js
@@ -36,53 +36,17 @@
 Ext.ns("Sbi.tools.dataset");
 
 Sbi.tools.dataset.FileDatasetPanel = function(config) {
-	
-	var defaultSettings = {		
-			height: 300,
-			width: 300,
-			layout: 'form'
-//			frame: true,
-//			header: false,
-//			border: false,
-//			padding: '5 5 5 5'
-			//html: '<p><b> Prova File Dataset Panel </b></p>'
-		};
+
+	var myItems = [];
 	
 
-
-
-
-
-
-
-	var c = Ext.apply(defaultSettings, config || {});
-	Ext.apply(this, c);
-
-	var panelItems;
-	panelItems = this.initUploadForm(panelItems);
+	this.initForm(myItems);
 	
-
 	c = {
-			items: [
-			        panelItems,
-			        {
-			        	xtype:          'button',
-			        	border: 		false,
-			        	//handler:		this.uploadFileButtonHandler,
-			        	columnWidth:	0.1,
-			        	scope: 			this,
-			        	style:			'padding-left: 5px',
-			        	text: 			'upload'
-			        }
-		        ]
+			height : 200,
+		    frame:false,
+          	items: myItems 
 		};
-
-
-//	var c ;
-//	this.layout = 'fit';
-//	this.height = 100;
-//		
-//	this.html = "<p><b> Prova File Dataset Panel </b></p>";
 
 
 	Sbi.tools.dataset.FileDatasetPanel.superclass.constructor.call(this, c);	 		
@@ -91,85 +55,24 @@ Sbi.tools.dataset.FileDatasetPanel = function(config) {
 
 Ext.extend(Sbi.tools.dataset.FileDatasetPanel, Ext.Panel, {
 	
-//	initDetailItems : function(items) {				
-//		this.initUploadForm();		
-//	}
-	
-	initUploadForm : function(items){
-		
+	initForm: function(myItems){
 		this.uploadField = new Ext.form.TextField({
 			inputType : 'file',
+			width: '300',
 			fieldLabel : LN('sbi.generic.upload'),
 			allowBlank : true,
 		});
 		
-		this.fileUploadFields = new Ext.form.FieldSet({
-			labelWidth : 100,
-			defaults : {
-				border : true
-			},
-			defaultType : 'textfield',
-			autoHeight : true,
-			autoScroll : true,
-			border : true,
-			style : {
-				"margin-left" : "3px",
-				"margin-top" : "0px",
-				"margin-right" : Ext.isIE6 ? (Ext.isStrict ? "-3px"
-						: "-5px")
-						: "3px"
-			},
-			items : [ 
-			    this.uploadField
-			]
+		this.optField = new Ext.form.TextField({
+			width: '300',
+			name: "opt1",
+			allowBlank : true,
 		});
 		
-		this.fileUploadFormPanel = new Ext.FormPanel({
-		    title: 'Simple Form with FieldSets',
-		    labelWidth: 75, // label settings here cascade unless overridden
-		    frame:true,
-		    bodyStyle:'padding:5px 5px 0',
-		    width: 700,
-		    layout:'column', // arrange items in columns
-		    defaults: {      // defaults applied to items
-		        layout: 'form',
-		        border: false,
-		        bodyStyle: 'padding:4px'
-		    }
-          	,items: [{
-		                fieldLabel: 'First Name',
-		                name: 'first',
-		                allowBlank:false
-		                , width: 50
-		            },{
-		                fieldLabel: 'Last Name',
-		                name: 'last'
-		                , width: 50
-		            },{
-		                fieldLabel: 'Company',
-		                name: 'company'
-		                , width: 50
-		            }, {
-		                fieldLabel: 'Email',
-		                name: 'email',
-		                vtype:'email'
-		               , width: 50
-		            }, new Ext.form.TimeField({
-		                fieldLabel: 'Time',
-		                name: 'time',
-		                minValue: '8:00am',
-		                maxValue: '6:00pm'
-		                , width: 50
-		            })
-		        ]    
-		});
-		
+		myItems.push(this.uploadField);
+		myItems.push(this.optField);
+	}
 
-		
-		//items.push(this.fileUploadFormPanel);
-		return this.fileUploadFormPanel;
-
-	}	
 	
 });
 
