fd116dff6afb57c9f1011dd0743aae3ada25d2d2
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-06-03 10:58:55 +0000

    First version  for the wizard ...
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@19782 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIProject/web-content/js/src/ext4/sbi/widgets/wizard/WizardWindow.js
@@ -0,0 +1,101 @@
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
+
+
+/**TODO anto: aggiornare documentazione!!!
+ * 
+ * Data view for a browser style. It define a layout and provides the stubs methods for the icons browser.
+ * This methods should be overridden to define the logic. Only the methods onAddNewRow, onGridSelect and onCloneRow are implemented.
+ * The configuration dataView must be passed to the object. It should contains the method setFormState (it is used by onGridSelect)
+ * 
+ * 
+ * 		@example
+ *	Ext.define('Sbi.tools.datasource.DataSourceListDetailPanel', {
+ *   extend: 'Sbi.widgets.compositepannel.ListDetailPanel'
+ *	, constructor: function(config) {
+ *		//init services
+ *		this.initServices();
+ *		//init form
+ *		this.form =  Ext.create('Sbi.tools.datasource.DataSourceDetailPanel',{services: this.services});
+ *		this.columns = [{dataIndex:"DATASOURCE_LABEL", header:"Name"}, {dataIndex:"DESCRIPTION", header:"description"}];
+ *		this.fields = ["DATASOURCE_ID","DATASOURCE_LABEL","DESCRIPTION","DRIVER","DIALECT_ID","DIALECT_CLASS","DIALECT_NAME","JNDI_URL","USER","PASSWORD","SCHEMA","MULTISCHEMA","CONNECTION_URL"];
+ *		this.form.on("save",this.onFormSave,this);
+ *    	this.callParent(arguments);
+ *    }
+ *	, initServices: function(baseParams){
+ *		this.services["getAllValues"]= Sbi.config.serviceRegistry.getRestServiceUrl({
+ *			    							serviceName: 'datasources/listall'
+ *			    							, baseParams: baseParams
+ *			    						});
+ *		...
+ *		    	
+ *	}
+ *	, onDeleteRow: function(record){
+ *		Ext.Ajax.request({
+ *  	        url: this.services["delete"],
+ *  	        params: {DATASOURCE_ID: record.get('DATASOURCE_ID')},
+ *  	       ...
+ *	}
+ *	, onFormSave: function(record){
+ *		Ext.Ajax.request({
+ *  	        url: this.services["save"],
+ *  	        params: record,
+ *  	   ...
+ *	}
+ *});
+ *			... 
+ *
+ * 
+ * @author
+ * Antonella Giachino (antonella.giachino@eng.it)
+ */
+
+Ext.define('Sbi.widgets.wizard.WizardWindow', {
+    extend: 'Ext.Window'
+
+    ,config: {    	  	     	
+    	width: 1000,
+		height: 700,
+		hasBuddy: false,	
+		modal: true,
+	//	layout: 'fit',
+		closeAction:'close',
+		constrain: true,
+		plain: true,
+		modal:true,
+		title: '',
+		buttonAlign : 'center'		
+    }
+
+	/**
+	 * In this constructor you must pass configuration
+	 */
+	, constructor: function(config) {
+		config.layout='fit';
+		this.initConfig(config);				
+		
+		Ext.apply(this,config||{});		
+		
+//		var closeButton = new Ext.Button({
+//			text: 'Close',
+//			handler: function(){
+//	        	this.destroy();
+//	        }
+//	        , scope: this
+//		});
+//		   
+//		this.buttons = [closeButton];
+		
+		var wizardPanel =  Ext.create('Sbi.widgets.wizard.WizardPanel',{});
+		this.items = [wizardPanel];
+				
+		//this.doLayout();
+		this.callParent(arguments);
+	}
+	
+	
+   
+});
