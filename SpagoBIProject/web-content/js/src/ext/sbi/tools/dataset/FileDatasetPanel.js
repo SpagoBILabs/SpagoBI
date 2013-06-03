0328a29796fc90ceb5c3cdf850c380e9f0cb0771
Marco Cortella <macortel@eng.it>
2013-06-03 08:43:03 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@19779 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext/sbi/tools/dataset/FileDatasetPanel.js
+++ SpagoBIProject/web-content/js/src/ext/sbi/tools/dataset/FileDatasetPanel.js
@@ -1,7 +1,7 @@
 /** SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Ceneselli" notice. 
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
  
   
@@ -36,48 +36,80 @@
 Ext.ns("Sbi.tools.dataset");
 
 Sbi.tools.dataset.FileDatasetPanel = function(config) {
-
-	var myItems = [];
 	
-
-	this.initForm(myItems);
 	
-	c = {
+	var defaultSettings =  {
 	        labelWidth: 75, 
 	        frame:false,
-	        width: 350,
-	        defaults: {width: 230},
-	        defaultType: 'textfield',
-	        items: myItems
+	        //width: 350,
+	        //height: 300,
+	        //defaults: {width: 230},
+	        defaultType: 'textfield'
+	        	
 		};
+
+
+
+
+
+
+	var c = Ext.apply(defaultSettings, config || {});
+	Ext.apply(this, c);
+
+	var panelItems;
+	panelItems = this.initUploadForm(panelItems);
 	
+
+	c = {
+			items: [
+			        panelItems        
+			       ]
+		};
+
+
+
 
 
 	Sbi.tools.dataset.FileDatasetPanel.superclass.constructor.call(this, c);	 		
-
+	
 };
 
-Ext.extend(Sbi.tools.dataset.FileDatasetPanel, Ext.form.FormPanel, {
+Ext.extend(Sbi.tools.dataset.FileDatasetPanel, Ext.Panel, {
 	
-	initForm: function(myItems){
+	
+	initUploadForm : function(items){
+		
+//		this.myBtnHandler = function(btn) {
+//		        Ext.MessageBox.alert('You Clicked', btn.text);
+//		    }
+		
 		this.uploadField = new Ext.form.TextField({
 			inputType : 'file',
-			width: '300',
 			fieldLabel : LN('sbi.generic.upload'),
 			allowBlank : true,
+			
 		});
 		
-		this.optField = new Ext.form.TextField({
-			width: '300',
-			fieldLabel: 'Opt1',
-			name: "opt1",
-			allowBlank : true,
-		});
+		this.uploadButton = new Ext.Button({
+	        text: 'Upload File',
+	        id: 'fileUploadButton'	
+	        //handler : this.myBtnHandler
+	    });
 		
-		myItems.push(this.uploadField);
-		myItems.push(this.optField);
-	}
+		this.fileUploadFormPanel = new Ext.Panel({
+		  margins: '50 50 50 50',
+          labelAlign: 'left',
+          bodyStyle:'padding:5px',
+          layout: 'form',
+		  defaultType: 'textfield',
+		  fileUpload: true,
+		  id: 'fileUploadPanel',
+		  items: [this.uploadField, this.uploadButton]
 
+		});
+		
+		return this.fileUploadFormPanel;
+
+	}	
 	
 });
-
