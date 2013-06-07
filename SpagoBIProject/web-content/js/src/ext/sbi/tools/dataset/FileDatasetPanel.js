2f2bb698d8464ac12420048c98b2cce55a2cd5fc
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-06-07 16:57:22 +0000

    Incomplete - task : BZ002 - Come utente di SpagoBI voglio poter eseguire dataset di tipo file xls o csv
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@19823 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext/sbi/tools/dataset/FileDatasetPanel.js
+++ SpagoBIProject/web-content/js/src/ext/sbi/tools/dataset/FileDatasetPanel.js
@@ -76,18 +76,158 @@ Ext.extend(Sbi.tools.dataset.FileDatasetPanel, Ext.Panel, {
 	
 	initUploadForm : function(items){
 		
-	
+		//XLS Options Panel
+		this.skipRowsField = new Ext.form.TextField({
+			fieldLabel : 'Skip Rows',
+			allowBlank : true,
+			name: 'skipRows',
+			width: 100
+		});
+		
+		this.limitRowsField = new Ext.form.TextField({
+			fieldLabel : 'Limit Rows Number',
+			allowBlank : true,
+			name: 'limitRows',
+			width: 100
+		});
+		
+		
+		this.xlsOptionsPanel = new Ext.Panel({
+			  margins: '50 50 50 50',
+	          labelAlign: 'left',
+	          bodyStyle:'padding:5px',
+	          layout: 'form',
+	          width: 500,
+			  labelWidth: 150,
+	          items: [ this.skipRowsField, this.limitRowsField  ]
+		});
+		this.xlsOptionsPanel.setVisible(false);
+
+		
+		
+		//CSV Options Panel
+		this.csvEndOfLineCombo = new Ext.form.ComboBox({
+			name : 'csvEndOfLine',
+			store: new Ext.data.ArrayStore({
+		        fields: [
+		            'csvEndOfLineName',
+		            'csvEndOfLineValue'
+		        ],
+		        data: [['Windows CR LF', '\\r\\n'], ['Unix, Mac Os X LF', '\\n']]
+		    }),
+			width : 150,
+			fieldLabel : 'End of line Character',
+			displayField : 'csvEndOfLineName', 
+			valueField : 'csvEndOfLineValue', 
+			typeAhead : true,
+			forceSelection : true,
+			mode : 'local',
+			triggerAction : 'all',
+			selectOnFocus : true, 
+			editable : false,
+			allowBlank : false, 
+			validationEvent : false
+		});	
+		
+		
+		this.csvQuoteCombo = new Ext.form.ComboBox({
+			name : 'csvQuote',
+			store: new Ext.data.ArrayStore({
+		        fields: [
+		            'csvQuoteName',
+		            'csvQuoteValue'
+		        ],
+		        data: [['"', '"'], ['\'', '\'']]
+		    }),
+			width : 150,
+			fieldLabel : 'Quote Character',
+			displayField : 'csvQuoteName', 
+			valueField : 'csvQuoteValue', 
+			typeAhead : true,
+			forceSelection : true,
+			mode : 'local',
+			triggerAction : 'all',
+			selectOnFocus : true, 
+			editable : false,
+			allowBlank : false, 
+			validationEvent : false
+		});	
+		
+		this.csvDelimiterCombo = new Ext.form.ComboBox({
+			name : 'csvDelimiter',
+			store: new Ext.data.ArrayStore({
+		        fields: [
+		            'csvDelimiterName',
+		            'csvDelimiterValue'
+		        ],
+		        data: [[';', ';'], [',', ','], ['\\t', '\\t']]
+		    }),
+			width : 150,
+			fieldLabel : 'Delimiter Character',
+			displayField : 'csvDelimiterName', 
+			valueField : 'csvDelimiterValue', 
+			typeAhead : true,
+			forceSelection : true,
+			mode : 'local',
+			triggerAction : 'all',
+			selectOnFocus : true, 
+			editable : false,
+			allowBlank : false, 
+			validationEvent : false
+		});	
+		
+		
+		this.csvOptionsPanel = new Ext.Panel({
+			  margins: '50 50 50 50',
+	          labelAlign: 'left',
+	          bodyStyle:'padding:5px',
+	          layout: 'form',
+	          width: 500,
+			  labelWidth: 150,
+	          items: [ this.csvDelimiterCombo, this.csvQuoteCombo, this.csvEndOfLineCombo ]
+		});
+		
+		
+		//Upload file fields
+		
+		this.fileTypeCombo = new Ext.form.ComboBox({
+			name : 'fileType',
+			store: new Ext.data.ArrayStore({
+		        fields: [
+		            'fileTypeName',
+		            'fileTypeValue'
+		        ],
+		        data: [['CSV', 'CSV'], ['Excel 2003', 'XLS']]
+		    }),
+			width : 150,
+			fieldLabel : 'Type',
+			displayField : 'fileTypeName', 
+			valueField : 'fileTypeValue', 
+			typeAhead : true, forceSelection : true,
+			mode : 'local',
+			triggerAction : 'all',
+			selectOnFocus : true, 
+			editable : false,
+			allowBlank : false, 
+			validationEvent : false
+		});		
+		this.fileTypeCombo.addListener('select',this.activateFileTypePanel, this);
+
+		
 		this.uploadField = new Ext.form.TextField({
 			inputType : 'file',
 			fieldLabel : LN('sbi.generic.upload'),
 			allowBlank : true,
-			id: 'fileNameField'
+			id: 'fileNameField',
+			name: 'fileName'
 		});
 		
 		this.uploadButton = new Ext.Button({
 	        text: 'Upload File',
 	        id: 'fileUploadButton'	
 	    });
+		
+		//Main Panel
 		
 		this.fileUploadFormPanel = new Ext.Panel({
 		  margins: '50 50 50 50',
@@ -97,12 +237,25 @@ Ext.extend(Sbi.tools.dataset.FileDatasetPanel, Ext.Panel, {
 		  defaultType: 'textfield',
 		  fileUpload: true,
 		  id: 'fileUploadPanel',
-		  items: [this.uploadField, this.uploadButton]
+		  items: [this.uploadField, this.uploadButton, this.fileTypeCombo, this.csvOptionsPanel, this.xlsOptionsPanel]
 
 		});
 		
 		return this.fileUploadFormPanel;
 
 	}	
+
+	//Listeners
+	,activateFileTypePanel : function(combo, record, index) {
+		var fileTypeSelected = record.get('fileTypeValue');
+		if (fileTypeSelected != null && fileTypeSelected == 'CSV') {
+			this.csvOptionsPanel.setVisible(true);
+			this.xlsOptionsPanel.setVisible(false);
+		} else if (fileTypeSelected != null && fileTypeSelected == 'XLS') {
+			this.csvOptionsPanel.setVisible(false);
+			this.xlsOptionsPanel.setVisible(true);
+		}
+	}
+
 	
 });
