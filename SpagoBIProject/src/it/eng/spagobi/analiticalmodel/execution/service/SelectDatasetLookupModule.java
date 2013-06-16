c251e5fb08f709436ab06fc71bb0dfa0966e4457
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-06-16 15:02:07 +0000

    Added filter on is_active, is_public and owner fields in the list dataset (spago management)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@20002 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/analiticalmodel/execution/service/SelectDatasetLookupModule.java
+++ SpagoBIProject/src/it/eng/spagobi/analiticalmodel/execution/service/SelectDatasetLookupModule.java
@@ -5,12 +5,28 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.analiticalmodel.execution.service;
 
+import java.sql.Connection;
+
+import javax.servlet.http.HttpServletRequest;
+
+import it.eng.spago.base.RequestContainer;
+import it.eng.spago.base.SessionContainer;
 import it.eng.spago.base.SourceBean;
 import it.eng.spago.dispatching.module.list.basic.AbstractBasicListModule;
+import it.eng.spago.error.EMFErrorSeverity;
+import it.eng.spago.error.EMFUserError;
 import it.eng.spago.paginator.basic.ListIFace;
+import it.eng.spago.security.IEngUserProfile;
+import it.eng.spagobi.commons.bo.UserProfile;
 import it.eng.spagobi.commons.services.DelegatedHibernateConnectionListService;
+import it.eng.spagobi.commons.utilities.AuditLogUtilities;
+import it.eng.spagobi.commons.utilities.HibernateUtil;
+import it.eng.spagobi.commons.utilities.UserUtilities;
+import it.eng.spagobi.services.common.SsoServiceInterface;
 
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 
 public class SelectDatasetLookupModule extends AbstractBasicListModule {
 
@@ -20,6 +36,32 @@ public class SelectDatasetLookupModule extends AbstractBasicListModule {
 		 * @see it.eng.spago.dispatching.service.list.basic.IFaceBasicListService#getList(it.eng.spago.base.SourceBean, it.eng.spago.base.SourceBean)
 		 */
 		public ListIFace getList(SourceBean request, SourceBean response) throws Exception {
+			//Start writing log in the DB
+			Session aSession =null;
+			try {
+				aSession = HibernateUtil.currentSession();
+				//Connection jdbcConnection = aSession.connection();
+				Connection jdbcConnection = HibernateUtil.getConnection(aSession);
+				IEngUserProfile profile = UserUtilities.getUserProfile();
+				AuditLogUtilities.updateAudit(((HttpServletRequest)getRequestContainer().getRequestContainer().getInternalRequest()),  profile, "DISTRIBUTION_LIST.OPEN", null, "OK");
+			} catch (HibernateException he) {
+				throw new EMFUserError(EMFErrorSeverity.ERROR, 100);
+			} finally {
+				if (aSession!=null){
+					if (aSession.isOpen()) aSession.close();
+				}
+			}
+			//End writing log in the DB
+
+			RequestContainer aRequestContainer = RequestContainer.getRequestContainer();
+			SessionContainer aSessionContainer = aRequestContainer.getSessionContainer();
+
+			IEngUserProfile userProfile =UserUtilities.getUserProfile();
+			String userId="";
+			if (userProfile!=null) userId=(String)((UserProfile)userProfile).getUserId();
+			//sets the userid as input parameter for the query fo statements.xml
+			aSessionContainer.setAttribute(SsoServiceInterface.USER_ID ,userId);
+
 			return DelegatedHibernateConnectionListService.getList(this, request, response);
 		}
 	
