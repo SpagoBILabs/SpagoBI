ac048faf1495c181452efa9466d36fbd7a170097
Marco Cortella <macortel@eng.it>
2013-06-24 15:56:07 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@20081 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext4/sbi/tools/dataset/ManageDatasetFieldMetadata.js
+++ SpagoBIProject/web-content/js/src/ext4/sbi/tools/dataset/ManageDatasetFieldMetadata.js
@@ -1,9 +1,6 @@
 Ext.define('Sbi.tools.dataset.ManageDatasetFieldMetadata', {
 	extend: 'Ext.Panel'
-
 	
-		
-		
 	,config: {
 		id: 'dsMetaGrid',
 		border: false,
@@ -15,9 +12,6 @@ Ext.define('Sbi.tools.dataset.ManageDatasetFieldMetadata', {
         frame: true,
         autoscroll: true
 	}
-
-
-
 
 
 	, constructor: function(config) {
@@ -32,71 +26,8 @@ Ext.define('Sbi.tools.dataset.ManageDatasetFieldMetadata', {
 
 		
 		Ext.apply(this, config || {});
-//		this.initConfig(config);	
 
-		//Add to the dom the select used from the combo..
-		//it is referenced by Id from the transform
-		/*
-		var selectElement = document.getElementById("fieldTypeSelect");
-		if(!selectElement){
-			var select = '<select name="fieldTypeSelect" id="fieldTypeSelect" style="display: none;">'+
-	    	'<option value="ATTRIBUTE">ATTRIBUTE</option>'+
-	    	'<option value="MEASURE">MEASURE</option>'+
-	    	'</select>';
-			var bodyElement = document.getElementsByTagName('body');
-			Ext.DomHelper.append(bodyElement[0].id, select );
-		}
-		*/
-		
-		
 
-		
-	
-		/*
-		this.fieldsColumns =  [
-		    {
-		    	header: LN('sbi.ds.field.name'), 
-		    	width: '50%', 
-				id:'name',
-				sortable: true, 
-				dataIndex: 'name' 
-		    },{
-	        	header: LN('sbi.ds.field.metadata'),
-	            dataIndex: 'fieldType',
-	            width: '50%',
-	            editor: new Ext.form.ComboBox({
-	            	typeAhead: true,
-	                triggerAction: 'all',
-	                // transform the data already specified in html
-	                transform: 'fieldTypeSelect',
-	                lazyRender: true,
-	                listClass: 'x-combo-list-small',	               
-	            })
-	        }			
-		]; 
-	    
-		
-		
-		this.columns = this.fieldsColumns;
-		 
-		this.fieldStore = new Ext.data.JsonStore({
-			    id : 'metaStoreData',
-			    fields: ['name', 'fieldType','type' ],
-			    idIndex: 0,
-			    data:this.meta || []
-			});
-		 
-
-		this.selModel= {selType: 'cellmodel'};
-		this.store = this.fieldStore;
-	    var cellEditing = Ext.create('Ext.grid.plugin.CellEditing', {
-	        clicksToEdit: 1
-	    });
-		this.plugins = [cellEditing];
-		*/
-		
-
-		
 		
 	    this.callParent(arguments);
 	    
