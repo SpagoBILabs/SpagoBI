31bc8b43b4685b8dddeeb459dfd71d15d096ace3
Davide Zerbetto <davide.zerbetto@eng.it>
2013-07-04 08:17:41 +0000

    Ordering tables on FROM clause: this is a partial fix of the problem regarding tables on FROM clause when using HSQLDB (with certain orders the query execution is very slow)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@20167 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/jpa/it/eng/qbe/statement/jpa/JPQLStatementFromClause.java
+++ QbeCore/src/jpa/it/eng/qbe/statement/jpa/JPQLStatementFromClause.java
@@ -5,95 +5,111 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.qbe.statement.jpa;
 
-import it.eng.qbe.datasource.jpa.IJpaDataSource;
 import it.eng.qbe.model.structure.IModelEntity;
-import it.eng.qbe.model.structure.IModelField;
-import it.eng.qbe.query.AbstractSelectField;
-import it.eng.qbe.query.SimpleSelectField;
-import it.eng.qbe.query.InLineCalculatedSelectField;
 import it.eng.qbe.query.Query;
-import it.eng.spagobi.utilities.objects.Couple;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import javax.persistence.EntityManager;
-import javax.persistence.metamodel.BasicType;
-import javax.persistence.metamodel.EmbeddableType;
-import javax.persistence.metamodel.EntityType;
-import javax.persistence.metamodel.Metamodel;
-import javax.persistence.metamodel.SingularAttribute;
-import javax.persistence.metamodel.Type;
 
 import org.apache.log4j.Logger;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
- *
+ * 
  */
 public class JPQLStatementFromClause extends AbstractJPQLStatementClause {
-	
+
 	public static final String FROM = "FROM";
-	
-	public static transient Logger logger = Logger.getLogger(JPQLStatementFromClause.class);
-	
-	public static String build(JPQLStatement parentStatement, Query query, Map<String, Map<String, String>> entityAliasesMaps){
-		JPQLStatementFromClause clause = new JPQLStatementFromClause(parentStatement);
+
+	public static transient Logger logger = Logger
+			.getLogger(JPQLStatementFromClause.class);
+
+	public static String build(JPQLStatement parentStatement, Query query,
+			Map<String, Map<String, String>> entityAliasesMaps) {
+		JPQLStatementFromClause clause = new JPQLStatementFromClause(
+				parentStatement);
 		return clause.buildClause(query, entityAliasesMaps);
 	}
-	
+
 	protected JPQLStatementFromClause(JPQLStatement statement) {
 		parentStatement = statement;
 	}
-	
+
 	public String buildClause(Query query, Map entityAliasesMaps) {
 		StringBuffer buffer;
-		
-		
+
 		logger.debug("IN");
 		buffer = new StringBuffer();
 		try {
-			Map entityAliases = (Map)entityAliasesMaps.get(query.getId());
-			
-			
-			if(entityAliases == null || entityAliases.keySet().size() == 0) {
+			Map entityAliases = (Map) entityAliasesMaps.get(query.getId());
+
+			if (entityAliases == null || entityAliases.keySet().size() == 0) {
 				return "";
 			}
-			
+
 			buffer.append(" " + FROM + " ");
-			
-			
-			// outer join are not supported by jpa ?? check it! 
-			// so this method is expected to return always an empty string
-			//buffer.append( buildJoinClause(query, entityAliases) );
-			
+
+			List<IModelEntity> cubes = new ArrayList<IModelEntity>();
+			List<IModelEntity> normalEntities = new ArrayList<IModelEntity>();
+			Map<String, String> clauses = new HashMap<String, String>();
+
 			Iterator it = entityAliases.keySet().iterator();
-			while( it.hasNext() ) {
-				String entityUniqueName = (String)it.next();
-				logger.debug("entity [" + entityUniqueName +"]");
-				
-				String entityAlias = (String)entityAliases.get(entityUniqueName);
-				logger.debug("entity alias [" + entityAlias +"]");
-				
-				IModelEntity datamartEntity =  parentStatement.getDataSource().getModelStructure().getEntity(entityUniqueName);
-				
-				
-				String fromClauseElement = datamartEntity.getName() + " " + entityAlias;
-				logger.debug("from clause element [" + fromClauseElement +"]");
-				
-				buffer.append(" " + fromClauseElement);
-				if( it.hasNext() ) {
+			while (it.hasNext()) {
+				String entityUniqueName = (String) it.next();
+				logger.debug("entity [" + entityUniqueName + "]");
+
+				String entityAlias = (String) entityAliases
+						.get(entityUniqueName);
+				logger.debug("entity alias [" + entityAlias + "]");
+
+				IModelEntity modelEntity = parentStatement.getDataSource()
+						.getModelStructure().getEntity(entityUniqueName);
+				if (modelEntity.getProperty("type") == "cube") {
+					cubes.add(modelEntity);
+				} else {
+					normalEntities.add(modelEntity);
+				}
+
+				String fromClauseElement = modelEntity.getName() + " "
+						+ entityAlias;
+				logger.debug("from clause element [" + fromClauseElement + "]");
+
+				clauses.put(entityUniqueName, " " + fromClauseElement);
+
+			}
+
+			Iterator<IModelEntity> cubesIt = cubes.iterator();
+			while (cubesIt.hasNext()) {
+				IModelEntity cube = cubesIt.next();
+				buffer.append(clauses.get(cube.getUniqueName()));
+				if (cubesIt.hasNext()) {
 					buffer.append(",");
 				}
+			}
+
+			if (normalEntities.size() > 0 && cubes.size() > 0)
+				buffer.append(",");
+
+			Iterator<IModelEntity> normalEntitiesIt = normalEntities.iterator();
+			while (normalEntitiesIt.hasNext()) {
+				IModelEntity normalEntity = normalEntitiesIt.next();
+				buffer.append(clauses.get(normalEntity.getUniqueName()));
+				if (normalEntitiesIt.hasNext()) {
+					buffer.append(",");
+				}
+			}
+
+			for (IModelEntity cube : cubes) {
+
 			}
 		} finally {
 			logger.debug("OUT");
 		}
-		
+
 		return buffer.toString().trim();
 	}
-	
-	
+
 }
