e793f52fa1a7be31cadb516bd9aa627fe534721e
Davide Zerbetto <davide.zerbetto@eng.it>
2013-07-08 10:06:16 +0000

    Bug fix
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@20220 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/oracle/oracle-dbupgradescript-3.6-to-3.7/ORACLE_upgradescript_3.6_to_3.7.sql
+++ SpagoBIDatabaseScripts/oracle/oracle-dbupgradescript-3.6-to-3.7/ORACLE_upgradescript_3.6_to_3.7.sql
@@ -40,7 +40,7 @@ COMMIT;
 
 UPDATE sbi_obj_par SET REQ_FL=0;
 UPDATE sbi_obj_par SET REQ_FL=1 WHERE par_id IN (SELECT a.par_id FROM   sbi_parameters a, sbi_paruse m, sbi_paruse_ck r, sbi_checks c
-WHERE a.par_id = m.par_id and m.use_id = r.use_id and r.check_id = c.check_id and c.value_type_cd = 'MANDATORY')
+WHERE a.par_id = m.par_id and m.use_id = r.use_id and r.check_id = c.check_id and c.value_type_cd = 'MANDATORY');
 
 INSERT INTO SBI_USER_FUNC (USER_FUNCT_ID, NAME, DESCRIPTION, USER_IN, TIME_IN)
     VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_USER_FUNC'), 
