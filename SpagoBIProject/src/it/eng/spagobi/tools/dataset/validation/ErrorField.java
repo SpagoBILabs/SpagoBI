02d53b86415f92e2810f73b21f625de3bd6e2fee
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-07-23 09:27:00 +0000

    SPAGOBI-1274: As a SpagoBI User I want that, if I specify that a column of datset is a time reference (eg time_ref_year), the Wizard will perform a validation before saving https://spagobi.eng.it/jira/browse/SPAGOBI-1274
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@20357 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIProject/src/it/eng/spagobi/tools/dataset/validation/ErrorField.java
@@ -0,0 +1,71 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2010 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
+package it.eng.spagobi.tools.dataset.validation;
+
+import it.eng.spagobi.tools.dataset.common.datastore.IField;
+
+/**
+ * @author Marco Cortella (marco.cortella@eng.it)
+ *
+ */
+public class ErrorField {
+	
+	private int columnIndex;
+	private IField field;
+	
+	public ErrorField(int columnIndex, IField field){
+		this.columnIndex = columnIndex;
+		this.field = field;
+	}
+	
+	/**
+	 * @return the columnIndex
+	 */
+	public int getColumnIndex() {
+		return columnIndex;
+	}
+	/**
+	 * @param columnIndex the columnIndex to set
+	 */
+	public void setColumnIndex(int columnIndex) {
+		this.columnIndex = columnIndex;
+	}
+	/**
+	 * @return the field
+	 */
+	public IField getField() {
+		return field;
+	}
+	/**
+	 * @param field the field to set
+	 */
+	public void setField(IField field) {
+		this.field = field;
+	}
+	
+	public String toString() {
+		return "ColumnIndex = "+columnIndex+" FieldValue = "+field.getValue();
+	}
+	
+	
+
+}
