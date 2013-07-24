81532133c77fb57dae545f0097c81147b5ea619b
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-07-24 08:29:47 +0000

    localization
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@20377 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/community/service/MenageCommunityAction.java
+++ SpagoBIProject/src/it/eng/spagobi/community/service/MenageCommunityAction.java
@@ -5,38 +5,32 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.community.service;
 
+import it.eng.spago.base.RequestContainer;
+import it.eng.spago.base.RequestContainerAccess;
+import it.eng.spago.base.SessionContainer;
 import it.eng.spago.error.EMFUserError;
 import it.eng.spago.security.IEngUserProfile;
-import it.eng.spagobi.commons.bo.Domain;
-import it.eng.spagobi.commons.bo.UserProfile;
+import it.eng.spagobi.commons.constants.SpagoBIConstants;
 import it.eng.spagobi.commons.dao.DAOFactory;
-import it.eng.spagobi.commons.dao.IDomainDAO;
-import it.eng.spagobi.commons.services.AbstractSpagoBIAction;
+import it.eng.spagobi.commons.utilities.messages.IMessageBuilder;
+import it.eng.spagobi.commons.utilities.messages.MessageBuilderFactory;
 import it.eng.spagobi.community.dao.ISbiCommunityDAO;
 import it.eng.spagobi.community.mapping.SbiCommunity;
-import it.eng.spagobi.tools.dataset.measurecatalogue.MeasureCatalogue;
-import it.eng.spagobi.tools.dataset.measurecatalogue.MeasureCatalogueSingleton;
-import it.eng.spagobi.tools.datasource.bo.DataSource;
-import it.eng.spagobi.tools.datasource.dao.IDataSourceDAO;
-import it.eng.spagobi.utilities.exceptions.SpagoBIServiceException;
 
-import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
-import org.json.JSONObject;
 
 @Path("/community")
 public class MenageCommunityAction {
-	
+	protected IMessageBuilder msgBuilder = MessageBuilderFactory.getMessageBuilder();;
 	private static Logger logger = Logger.getLogger(MenageCommunityAction.class);
 
 	
@@ -45,12 +39,27 @@ public class MenageCommunityAction {
 	@Produces(MediaType.TEXT_HTML)
 	public String accept(@Context HttpServletRequest req) {
 		
+		RequestContainer reqCont = RequestContainerAccess.getRequestContainer(req);
+		Locale locale = null;	
+		if(reqCont != null){
+			SessionContainer aSessionContainer = reqCont.getSessionContainer();
+	
+			SessionContainer permanentSession = aSessionContainer.getPermanentContainer();
+			String curr_language=(String)permanentSession.getAttribute(SpagoBIConstants.AF_LANGUAGE);
+			String curr_country=(String)permanentSession.getAttribute(SpagoBIConstants.AF_COUNTRY);
+					
+
+			if(curr_language!=null && curr_country!=null && !curr_language.equals("") && !curr_country.equals("")){
+				locale=new Locale(curr_language, curr_country, "");
+			}
+		}
+		
 		IEngUserProfile profile = (IEngUserProfile) req.getSession()
 				.getAttribute(IEngUserProfile.ENG_USER_PROFILE);
 		String userToAccept = (String)req.getParameter("userToAccept");
 		String community = (String)req.getParameter("community");
 		String owner = (String)req.getParameter("owner");
-		String result="Operation succeded: "+userToAccept+" added to the community "+community;
+		String result=msgBuilder.getMessage("community.save.membership.ok.1", "messages", locale)+userToAccept+msgBuilder.getMessage("community.save.membership.ok.2", "messages", locale)+community;
 		
 		if(profile.getUserUniqueIdentifier().equals(owner)){
 
@@ -66,10 +75,10 @@ public class MenageCommunityAction {
 				
 			} catch (EMFUserError e) {
 				logger.error(e.getMessage());
-				result= "Operation failed";
+				result= msgBuilder.getMessage("community.save.membership.ko", "messages", locale);
 			}		
 		}else{
-			result= "User cannot perform action";
+			result= msgBuilder.getMessage("community.save.membership.cannot", "messages", locale);
 		}
 		logger.debug("OUT");
 		return result;
