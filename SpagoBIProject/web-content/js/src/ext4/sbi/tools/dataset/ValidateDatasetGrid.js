728f67becb75ddc6749b8bfb67979c353fd7a996
antonella giachino <antonella.giachino@eng.it>
2013-10-10 07:33:49 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@20825 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext4/sbi/tools/dataset/ValidateDatasetGrid.js
+++ SpagoBIProject/web-content/js/src/ext4/sbi/tools/dataset/ValidateDatasetGrid.js
@@ -3,35 +3,7 @@
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
- 
-  
- 
-  
- 
-  
 
-/**
- * Object name 
- * 
- * 
- * Public Properties
- * 
- * [list]
- * 
- * 
- * Public Methods
- * 
- *  [list]
- * 
- * 
- * Public Events
- * 
- *  [list]
- * 
- * Authors
- * 
- * - Marco Cortella (marco.cortella@eng.it)
- */
 
 Ext.define('Sbi.tools.dataset.ValidateDatasetGrid', {
     extend: 'Sbi.widgets.grid.DynamicGridPanel'
