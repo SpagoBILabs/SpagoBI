f763f21c85b716a9631cfe7c395d200f1bc0b9a2
antonella giachino <antonella.giachino@eng.it>
2013-10-28 11:39:41 +0000

    solved bug on datasource management
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21026 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext4/sbi/widgets/grid/DynamicGridPanel.js
+++ SpagoBIProject/web-content/js/src/ext4/sbi/widgets/grid/DynamicGridPanel.js
@@ -58,13 +58,12 @@ Ext.define('Sbi.widgets.grid.DynamicGridPanel', {
 			this = parentGrid;
 		
     	Sbi.debug('DynamicGridPanel build store');
-//    	config.storeConfig = Ext.apply(config.storeConfig||{},{serviceUrl: config.serviceUrl, autoLoad:true});
     	config.storeConfig = Ext.apply(config.storeConfig||{},{serviceUrl: config.serviceUrl});
     	if ((config.usePost != null) && (config.usePost != undefined ) ){
         	config.storeConfig.params = config.params;
         	config.storeConfig.usePost = config.usePost;
     	}
-
+    	config.storeConfig.isDynamicStore = true;
     	var store;
     	if (config.storeConfig.storeType && config.storeConfig.storeType == 'InMemoryFilteredStore'){    		
     		store = Ext.create('Sbi.widgets.store.InMemoryFilteredStore', config.storeConfig ||{});	
