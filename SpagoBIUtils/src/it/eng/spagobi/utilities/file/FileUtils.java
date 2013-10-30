ce94d915f1c9f1c590efc9907ea7b14ce5111871
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-10-30 13:56:56 +0000

    added preview file management by  administrator
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21059 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/utilities/file/FileUtils.java
+++ SpagoBIUtils/src/it/eng/spagobi/utilities/file/FileUtils.java
@@ -5,16 +5,33 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.utilities.file;
 
+import it.eng.spago.error.EMFErrorSeverity;
+import it.eng.spago.error.EMFUserError;
+import it.eng.spagobi.commons.SingletonConfig;
+import it.eng.spagobi.commons.utilities.SpagoBIUtilities;
 import it.eng.spagobi.utilities.assertion.Assert;
+import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
+import it.eng.spagobi.utilities.exceptions.SpagoBIServiceException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.log4j.Logger;
+import org.safehaus.uuid.UUID;
+import org.safehaus.uuid.UUIDGenerator;
+
+
 
 /**
  * @author Andrea Gioia
  *
  */
 public class FileUtils {
+	
+	private static transient Logger logger = Logger.getLogger(FileUtils.class);
 	
 	public static boolean isAbsolutePath(String path) {
 		if(path == null) return false;
@@ -71,5 +88,97 @@ public class FileUtils {
 		}
 	}
 	
+
+	public static void checkUploadedFile(FileItem uploaded,Integer maxSize, List extFiles ) throws Exception {
+		// check if the uploaded file is empty
+		if (uploaded.getSize() == 0) {
+			logger.error("The uploaded file is empty");
+			throw new SpagoBIServiceException("", "The uploaded file is empty");	
+		}
+		// check if the uploaded file exceeds the maximum dimension
+		if (maxSize != null && uploaded.getSize() > maxSize) {
+			logger.error("The uploaded file exceeds the maximum size, that is " + maxSize + " bytes");
+			throw new SpagoBIServiceException("", "The uploaded file exceeds the maximum size, that is " + maxSize + " bytes");
+		}
+					
+		if (extFiles != null){
+			// check if the extension is valid
+			String fileExtension =  uploaded.getName().lastIndexOf('.') > 0 ?  
+					uploaded.getName().substring( uploaded.getName().lastIndexOf('.') + 1) : null;	
+			
+			if (!extFiles.contains(fileExtension.toLowerCase()) && !extFiles.contains(fileExtension.toUpperCase())){
+				logger.error( "The uploaded file has an invalid extension. Choose a "+ extFiles.toString() +" file.");
+				String msg = "The uploaded file has an invalid extension. Choose a "+ extFiles.toString() +" file.";
+				throw new SpagoBIServiceException("", msg);
+			} 
+		}		
+		return;
+	}
+
+	
+	public static File checkAndCreateDir(FileItem uploaded,String directory) {
+		File toReturn = null;
+		try {
+			String fileName = SpagoBIUtilities.getRelativeFileNames(uploaded.getName());		
+			SingletonConfig configSingleton = SingletonConfig.getInstance();
+			String path  = configSingleton.getConfigValue("SPAGOBI.RESOURCE_PATH_JNDI_NAME");
+			String resourcePath= SpagoBIUtilities.readJndiResource(path);
+			if (directory != null){
+				resourcePath += (!directory.startsWith("/") && !directory.startsWith("\\"))?File.separatorChar+directory:directory;
+			}else{
+				resourcePath += File.separatorChar+"files"; //default: is correct?
+			}
+			File fileDir = new File(resourcePath);
+			if (!fileDir.exists()){
+				//Create Directory if doesn't exist
+				boolean mkdirResult = fileDir.mkdirs();
+				if (!mkdirResult) {
+					logger.error( "Cannot create files directory into server resources");
+					throw new SpagoBIServiceException("", "Cannot create files directory into server resources");
+				}
+			}
+			
+			// uuid generation
+			String uuid = createNewExecutionId();
+			String fileExtension =  uploaded.getName().lastIndexOf('.') > 0 ?  
+					uploaded.getName().substring( uploaded.getName().lastIndexOf('.')) : null;			
+			fileName  =  uuid + fileExtension;
+			toReturn =  new File(fileDir, fileName);
+			
+		} catch (Throwable t) {
+			logger.error("Error while saving file into server", t);
+			throw new SpagoBIRuntimeException( "Error while saving file into server");
+		}
+		return toReturn;
+	}
+	
+	public static void saveFile(FileItem uploaded, File saveTo) {
+		try {
+			uploaded.write(saveTo);
+		} catch (Throwable t) {
+			logger.error("Error while saving file into server: " + t);
+			throw new SpagoBIServiceException("", "Error while saving file into server");
+		}
+	}
+	
+	public static boolean checkFileIfExist(File file){
+		return (file.exists());
+	}
+	
+	public static String createNewExecutionId() {
+		String executionId;
+		
+		executionId = null;
+		try {
+			UUIDGenerator uuidGen  = UUIDGenerator.getInstance();
+			UUID uuidObj = uuidGen.generateTimeBasedUUID();
+			executionId = uuidObj.toString();
+			executionId = executionId.replaceAll("-", "");
+		} catch(Throwable t) {
+			
+		} 
+		
+		return executionId;
+	}
 	
 }
