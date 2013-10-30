f17656cb82f1390855d3e0e7f36954d1847c16a2
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-10-30 19:13:06 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21068 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIGeoReportEngine/src/it/eng/spagobi/engines/georeport/services/GetTargetLayerAction.java
+++ SpagoBIGeoReportEngine/src/it/eng/spagobi/engines/georeport/services/GetTargetLayerAction.java
@@ -10,6 +10,8 @@ import it.eng.spagobi.commons.utilities.StringUtilities;
 import it.eng.spagobi.engines.georeport.GeoReportEngineInstance;
 import it.eng.spagobi.engines.georeport.features.provider.FeaturesProviderDAOFactory;
 import it.eng.spagobi.engines.georeport.features.provider.IFeaturesProviderDAO;
+import it.eng.spagobi.engines.georeport.utils.LayerCache;
+import it.eng.spagobi.engines.georeport.utils.Monitor;
 import it.eng.spagobi.services.proxy.DataSetServiceProxy;
 import it.eng.spagobi.tools.dataset.bo.IDataSet;
 import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
@@ -23,6 +25,7 @@ import it.eng.spagobi.utilities.engines.SpagoBIEngineException;
 import it.eng.spagobi.utilities.service.AbstractBaseServlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -39,6 +42,8 @@ import org.geotools.feature.FeatureIterator;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.geojson.feature.FeatureJSON;
+import org.json.JSONArray;
+import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.AttributeDescriptor;
@@ -60,6 +65,8 @@ public class GetTargetLayerAction extends AbstractBaseServlet {
 	
 	public static final String FEATURE_SOURCE_TYPE = "featureSourceType";
 	public static final String FEATURE_SOURCE = "featureSource";
+	public static final String FEATURE_IDS = "featureIds";
+	public static final String LAYER_ID = "geoId";
 	public static final String LAYER_NAME = "layer";
 	
 	
@@ -74,12 +81,14 @@ public class GetTargetLayerAction extends AbstractBaseServlet {
 		String featureSourceType = null;
 		String featureSource  = null;
 		String layerName = null;
+		String layerId = null;
+		String featureIds = null;
 		
 		
 		logger.debug("IN");
 		
 		try {
-			
+			Monitor.start("GetTargetLayerAction.doService");
 			try {
 				featureSourceType = servletIOManager.getParameterAsString(FEATURE_SOURCE_TYPE); 
 				logger.debug("Parameter [" + FEATURE_SOURCE_TYPE + "] is equal to [" + featureSourceType + "]");
@@ -89,9 +98,17 @@ public class GetTargetLayerAction extends AbstractBaseServlet {
 				logger.debug("Parameter [" + FEATURE_SOURCE + "] is equal to [" + featureSource + "]");
 				Assert.assertTrue(StringUtilities.isNotEmpty(featureSource), "Input parameter [" + FEATURE_SOURCE + "] cannot be empty");
 				
+				featureIds = servletIOManager.getParameterAsString(FEATURE_IDS); 
+				logger.debug("Parameter [" + FEATURE_IDS + "] is equal to [" + featureIds + "]");
+				Assert.assertTrue(StringUtilities.isNotEmpty(featureIds), "Input parameter [" + FEATURE_IDS + "] cannot be empty");
+				
 				layerName = servletIOManager.getParameterAsString(LAYER_NAME); 
 				logger.debug("Parameter [" + LAYER_NAME + "] is equal to [" + layerName + "]");
 				Assert.assertTrue(StringUtilities.isNotEmpty(layerName), "Input parameter [" + LAYER_NAME + "] cannot be empty");
+				
+				layerId = servletIOManager.getParameterAsString(LAYER_ID); 
+				logger.debug("Parameter [" + LAYER_ID + "] is equal to [" + layerId + "]");
+				Assert.assertTrue(StringUtilities.isNotEmpty(layerId), "Input parameter [" + LAYER_ID + "] cannot be empty");
 			} catch(Throwable t1) {
 				logger.error("Impossible to load target layer because input parameter are not correct: " + t1.getMessage());
 				servletIOManager.writeBackToClient(500, 
@@ -100,11 +117,18 @@ public class GetTargetLayerAction extends AbstractBaseServlet {
 				return;
 			}
 			
-			FeatureCollection outputFeatureCollection = null;
+			FeatureCollection outputFeatureCollection = LayerCache.cache.get(layerName);
+			
+			if(outputFeatureCollection == null) {
+			logger.debug("Layer [" + FEATURE_SOURCE_TYPE + "] is not in cache");
 			try {
+				Monitor.start("GetTargetLayerAction.getFeature");
 				IFeaturesProviderDAO featuresProvider = FeaturesProviderDAOFactory.getFeaturesProviderDAO(featureSourceType);
 				outputFeatureCollection = featuresProvider.getAllFeatures(featureSource, layerName);	
 				Assert.assertNotNull(outputFeatureCollection, "The feature source returned a null object");
+				logger.debug("GetTargetLayerAction.getFeature" + Monitor.elapsed("GetTargetLayerAction.getFeature"));
+				
+				LayerCache.cache.put(layerName, outputFeatureCollection);
 			} catch(Throwable t2) {
 				logger.error("Impossible to load layer [" + layerName + "] " +
 						"from source of type [" + featureSourceType + "] " +
@@ -123,11 +147,35 @@ public class GetTargetLayerAction extends AbstractBaseServlet {
 						true ,"service-response", "text/plain");
 				return;
 			}
+			} else {
+				logger.debug("Layer [" + FEATURE_SOURCE_TYPE + "] is in cache");
+			}
 		    
-			FeatureJSON featureJSON = new FeatureJSON();				
-			String responseFeature = featureJSON.toString(outputFeatureCollection);
-			servletIOManager.tryToWriteBackToClient(responseFeature);
+			JSONArray featuresIdJSON = new JSONArray(featureIds);
+			Map<String, String> idIndex = new HashMap<String, String>();
+			for(int i = 0; i < featuresIdJSON.length(); i++) {
+				String s = featuresIdJSON.getString(i);
+				idIndex.put(s,s);
+			}
+			FeatureIterator it = outputFeatureCollection.features();
+			 List<SimpleFeature> list = new ArrayList<SimpleFeature>();
+			while(it.hasNext()) {
+				SimpleFeature f  = (SimpleFeature)it.next();
+				String id = (String)f.getProperty(layerId).getValue();
+				if(idIndex.containsKey(id)) {
+					list.add(f);
+				}
+			}
 			
+			FeatureCollection<SimpleFeatureType, SimpleFeature> filteredOutputFeatureCollection = DataUtilities.collection( list ); 
+			
+			Monitor.start("GetTargetLayerAction.flushResponse");
+			FeatureJSON featureJSON = new FeatureJSON();	
+			String responseFeature = featureJSON.toString(filteredOutputFeatureCollection);
+			servletIOManager.tryToWriteBackToClient(responseFeature);
+			logger.debug("GetTargetLayerAction.flushResponse" + Monitor.elapsed("GetTargetLayerAction.flushResponse"));
+			
+			logger.debug("GetTargetLayerAction.doService" + Monitor.elapsed("GetTargetLayerAction.doService"));
 		} catch(Throwable t) {
 			logger.error("An unexpected error occured while loading target layer", t);
 			try {
