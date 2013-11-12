820a0e8ee355a62e18127a4920dc2dfe7e6293bd
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-11-12 17:30:51 +0000

    RESOLVED - issue SPAGOBI-1461: Error when defining a query on a dataset: wrong fields' descriptions https://www.spagoworld.org/jira/browse/SPAGOBI-1461
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21251 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/runtime/it/eng/qbe/datasource/configuration/DataSetDataSourceConfiguration.java
+++ QbeCore/src/runtime/it/eng/qbe/datasource/configuration/DataSetDataSourceConfiguration.java
@@ -6,7 +6,14 @@
 package it.eng.qbe.datasource.configuration;
 
 
+import java.util.Locale;
+import java.util.Properties;
+
+import it.eng.qbe.model.properties.IModelProperties;
+import it.eng.qbe.model.properties.SimpleModelProperties;
 import it.eng.spagobi.tools.dataset.bo.IDataSet;
+import it.eng.spagobi.tools.dataset.common.metadata.IFieldMetaData;
+import it.eng.spagobi.tools.dataset.common.metadata.IMetaData;
 
 /**
  * @author Alberto Ghedin (alberto.ghedin@eng.it)
@@ -25,7 +32,38 @@ public class DataSetDataSourceConfiguration extends AbstractDataSourceConfigurat
 	public IDataSet getDataset() {
 		return dataset;
 	}
-	
+
+	@Override
+	public IModelProperties loadModelI18NProperties() {
+		Properties props = this.getProperties();
+		SimpleModelProperties toReturn = new SimpleModelProperties(props);
+		return toReturn;
+	}
+
+	private Properties getProperties() {
+		Properties props = new Properties();
+		String entityName = dataset.getTableNameForReading();
+		props.setProperty(entityName + "//" + entityName + ".label", dataset.getName());
+		
+		IMetaData datasetMetadata = dataset.getMetadata();
+		
+		for (int i = 0; i < datasetMetadata.getFieldCount(); i++) {
+			IFieldMetaData fieldMetadata = datasetMetadata.getFieldMeta(i);
+			String fieldUniqueName = entityName + "/" + fieldMetadata.getName();
+			String alias = fieldMetadata.getAlias();
+			if (alias == null || alias.equals("")) {
+				alias = fieldMetadata.getName();
+			}
+			props.setProperty(fieldUniqueName + ".label", alias);
+		}
+		
+		return props;
+	}
+
+	@Override
+	public IModelProperties loadModelI18NProperties(Locale locale) {
+		return this.loadModelI18NProperties();
+	}
 
 }
 
