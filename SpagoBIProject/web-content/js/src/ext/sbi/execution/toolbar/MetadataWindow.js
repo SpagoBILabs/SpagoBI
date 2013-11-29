a0a47084be8978c1b78a4b436e55c9447a63ca56
gioia <gioia>
2013-11-29 09:17:20 +0000

    medium refactoring
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21412 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext/sbi/execution/toolbar/MetadataWindow.js
+++ SpagoBIProject/web-content/js/src/ext/sbi/execution/toolbar/MetadataWindow.js
@@ -3,87 +3,37 @@
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
- 
-  
- 
-  
 
-/**
- * Object name
- * 
- * [description]
- * 
- * 
- * Public Properties
- * 
- * [list]
- * 
- * 
- * Public Methods
- * 
- * [list]
- * 
- * 
- * Public Events
- * 
- * [list]
- * 
- * Authors - Chiara Chiarelli (chiara.chiarelli@eng.it)
- */
 Ext.ns("Sbi.execution.toolbar");
 
+/**
+ * Sbi.execution.toolbar.MetadataWindow
+ * A popup window that shows document's metadata
+ */
 Sbi.execution.toolbar.MetadataWindow = function(config) {
 
-    var params = {
-        LIGHT_NAVIGATOR_DISABLED : 'TRUE',
-        OBJECT_ID: config.OBJECT_ID,
-        SUBOBJECT_ID: config.SUBOBJECT_ID
-    };
-    this.services = new Array();
+	this.validateConfigObject(config);
+	this.adjustConfigObject(config);
 
-    this.services['getMetadataService'] = Sbi.config.serviceRegistry
-            .getServiceUrl({
-                serviceName : 'GET_METADATA_ACTION',
-                baseParams : params
-            });
-
-    this.services['saveMetadataService'] = Sbi.config.serviceRegistry
-            .getServiceUrl({
-                serviceName : 'SAVE_METADATA_ACTION',
-                baseParams : params
-            });
-            
- 	// store for general metadata
-    this.generalMetadataStore = new Ext.data.SimpleStore({
-        fields : ['meta_name', 'meta_content' ]
-    });
-
-    // store for short text metadata
-    this.shortTextMetadataStore = new Ext.data.SimpleStore({
-        fields : [ 'meta_id', 'meta_name', 'meta_content' ]
-    });
-    
-    // store for long text metadata
-    this.longTextMetadataStore = new Ext.data.SimpleStore({
-        fields : [ 'meta_id', 'meta_name', 'meta_content' ]
-    });
-    
-    // store for all metadata
-    this.metadataStore = new Ext.data.JsonStore({
-        autoLoad: false,
-        fields: ['meta_id', 'biobject_id', 'subobject_id', 'meta_name', 'meta_type', 'meta_content',
-                   {name:'meta_creation_date', type:'date', dateFormat: Sbi.config.clientServerTimestampFormat}, 
-                   {name:'meta_change_date', type:'date', dateFormat: Sbi.config.clientServerTimestampFormat}
-                ]
-        , url: this.services['getMetadataService']
-    });
-    this.metadataStore.on('load', this.init, this);
-    this.metadataStore.load();
-    
-    // init  
-    this.initGeneralMetadataGridPanel();
-    this.initShortTextMetadataGridPanel();
-    this.initLongTextMetadataTabPanel();
+	var defaultSettings = {
+		id : 'win_metadata',
+		title : LN('sbi.execution.metadata'),
+		width : 650,
+		height : 400,
+		plain : true,
+		modal: true,
+		resizable: true,
+		autoScroll : true	
+	};
+			
+	if(Sbi.settings && Sbi.settings.execution && Sbi.settings.execution.metadatawindow) {
+		defaultSettings = Ext.apply(defaultSettings, Sbi.settings.execution.metadatawindow);
+	}		
+			
+	var c = Ext.apply(defaultSettings, config || {});
+	Ext.apply(this, c);
+	
+	this.init();
     
     var buttons = [];
    if (Sbi.user.functionalities.contains('SaveMetadataFunctionality')) {
@@ -94,17 +44,9 @@ Sbi.execution.toolbar.MetadataWindow = function(config) {
         });
     }
     
-    var c = Ext.apply( {}, config, {
-        id : 'win_metadata',
-        items : this.generalMetadataPanel ,
-        buttons : buttons,
-        width : 650,
-        height : 460,
-        plain : true,
-        autoScroll : true,
-        title : LN('sbi.execution.metadata')
-    });
-
+    c.buttons = this.buttons;
+    c.items = this.generalMetadataPanel;
+   
     this.buddy = undefined;
     
     // constructor
@@ -119,6 +61,16 @@ Sbi.execution.toolbar.MetadataWindow = function(config) {
 };
 
 Ext.extend(Sbi.execution.toolbar.MetadataWindow, Ext.Window, {
+	
+	// =================================================================================================================
+	// PROPERTIES
+	// =================================================================================================================
+	
+	
+	/**
+     * @property {Array} services
+     * This array contains all the services invoked by this class
+     */
     services : null
     , metadataStore : null
     , shortTextMetadataStore : null
@@ -126,34 +78,176 @@ Ext.extend(Sbi.execution.toolbar.MetadataWindow, Ext.Window, {
     , generalMetadataStore: null
     , shortTextMetadataPanel : null
     , generalMetadataPanel : null
+    
+    // =================================================================================================================
+	// METHODS
+	// =================================================================================================================
+	
+	/**
+	 * @method 
+	 * 
+	 * Controls that the configuration object passed in to the class constructor contains all the compulsory properties. 
+	 * If it is not the case an exception is thrown. Use it when there are properties necessary for the object
+	 * construction for whom is not possible to find out a valid default value.
+	 * 
+	 * @param {Object} the configuration object passed in to the class constructor
+	 * 
+	 * @return {Object} the config object received as input
+	 */
+	, validateConfigObject: function(config) {
+		
+	}
 
-    , init: function () {
+	/**
+	 * @method 
+	 * 
+	 * Modify the configuration object passed in to the class constructor adding/removing properties. Use it for example to 
+	 * rename a property or to filter out not necessary properties.
+	 * 
+	 * @param {Object} the configuration object passed in to the class constructor
+	 * 
+	 * @return {Object} the modified version config object received as input
+	 * 
+	 */
+	, adjustConfigObject: function(config) {
+		
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------
+    // accessor methods
+	// -----------------------------------------------------------------------------------------------------------------
+	// ...
+	
+	// -----------------------------------------------------------------------------------------------------------------
+    // public methods
+	// -----------------------------------------------------------------------------------------------------------------
+	// ...
+
+	//-----------------------------------------------------------------------------------------------------------------
+	// init methods
+	// -----------------------------------------------------------------------------------------------------------------
+
+	, init: function() {
+		this.initServices();
+		this.initStores();
+	    this.initGeneralMetadataGridPanel();
+	    this.initShortTextMetadataGridPanel();
+	    this.initLongTextMetadataTabPanel();
+	    this.initButtons();
+	}
+	
+	/**
+	 * @method 
+	 * 
+	 * Initialize the following services exploited by this component:
+	 * 
+	 *    - getMetadataService: read metadata from server
+	 *    - saveMetadataService: write metadata to server
+	 *    
+	 */
+	, initServices: function() {
+		
+		this.services = this.services || new Array();	
+	
+		var params = {
+			LIGHT_NAVIGATOR_DISABLED : 'TRUE',
+			OBJECT_ID: this.OBJECT_ID,
+			SUBOBJECT_ID: this.SUBOBJECT_ID
+		};
+		
+		this.services['getMetadataService'] = this.services['getMetadataService'] || Sbi.config.serviceRegistry.getServiceUrl({
+			serviceName : 'GET_METADATA_ACTION',
+			baseParams : params
+		});
+
+		this.services['saveMetadataService'] = this.services['saveMetadataService'] || Sbi.config.serviceRegistry.getServiceUrl({
+			serviceName : 'SAVE_METADATA_ACTION',
+			baseParams : params
+		});
+	}
+	
+	/**
+	 * @method 
+	 * 
+	 * Initialize the metadata stores
+	 */
+	, initStores: function() {
+		// store for general metadata
+	    this.generalMetadataStore = new Ext.data.SimpleStore({
+	        fields : ['meta_name', 'meta_content' ]
+	    });
+
+	    // store for short text metadata
+	    this.shortTextMetadataStore = new Ext.data.SimpleStore({
+	        fields : [ 'meta_id', 'meta_name', 'meta_content' ]
+	    });
+	    
+	    // store for long text metadata
+	    this.longTextMetadataStore = new Ext.data.SimpleStore({
+	        fields : [ 'meta_id', 'meta_name', 'meta_content' ]
+	    });
+	    
+	    // store for all metadata
+	    this.metadataStore = new Ext.data.JsonStore({
+	        autoLoad: false,
+	        fields: [
+	           'meta_id'
+	           , 'biobject_id'
+	           , 'subobject_id'
+	           , 'meta_name'
+	           , 'meta_type'
+	           , 'meta_content'
+	           , {name:'meta_creation_date', type:'date', dateFormat: Sbi.config.clientServerTimestampFormat}
+	           , {name:'meta_change_date', type:'date', dateFormat: Sbi.config.clientServerTimestampFormat}
+	        ]
+	    	, url: this.services['getMetadataService']
+	    });
+	    this.metadataStore.on('load', this.onMetadataStoreLoaded, this);
+	    this.metadataStore.load();
+	}
+	
+	/**
+	 * @method 
+	 * 
+	 * Refresh window content when metadata are loaded succesfully from server
+	 */
+    , onMetadataStoreLoaded: function () {
         
+    	Sbi.trace("[MetadataWindow.onMetadataStoreLoaded] : IN");
+    	
         for (var i = 0; i < this.metadataStore.getCount(); i++) {
-            var aRecord = this.metadataStore.getAt(i);
-            if (aRecord.data.meta_type == 'SHORT_TEXT') {
+        	
+        	var record = this.metadataStore.getAt(i);
+        	var metadataType = record.data.meta_type;
+        	
+        	if (metadataType == 'GENERAL_META') {
+                this.generalMetadataStore.add(record);
+            } else if (metadataType == 'SHORT_TEXT') {
+                this.shortTextMetadataStore.add(record);
+            } else if(metadataType == 'LONG_TEXT') {
             	
-                this.shortTextMetadataStore.add(aRecord);
-                
-            }else if (aRecord.data.meta_type == 'GENERAL_META') {
-            	
-                this.generalMetadataStore.add(aRecord);
-            } else {
-            	
-                this.longTextMetadataStore.add(aRecord);
-                var html = aRecord.data.meta_content !== '' ? aRecord.data.meta_content : '<br/>';
+                this.longTextMetadataStore.add(record);
+                var html = record.data.meta_content !== '' ? record.data.meta_content : '<br/>';
                 
                 var editablePanel = new Sbi.widgets.EditablePanel({
-                    "title" : aRecord.data.meta_name
+                    "title" : record.data.meta_name
                     , "html" : html
-                    , "height" : 246
                     , "editable" : Sbi.user.functionalities.contains('SaveMetadataFunctionality')
-                    , "fieldName" : aRecord.data.meta_id
+                    , "fieldName" : record.data.meta_id
                 });
                 
                 editablePanel.on('activate', function(panel) {
-                    panel.doLayout();
+                    this.doLayout();
                 }, this);
+                editablePanel.on('changestate', function(panel) {
+                	this.doLayout();
+//                	var v = panel.htmlEditor.getValue();
+//                	panel.collapse(false);
+//                	panel.expand(false);
+//                	panel.htmlEditor.setFocus();
+//                	panel.htmlEditor.setValue(v);
+                }, this);
+                
                 
                 editablePanel.on('change', function(panel, newValue) {
                     var index = this.longTextMetadataStore.find('meta_id', panel.fieldName);
@@ -162,10 +256,10 @@ Ext.extend(Sbi.execution.toolbar.MetadataWindow, Ext.Window, {
                 }, this);
                 
                 this.longTextMetadataTabPanel.add(editablePanel);
+            } else {
+            	alert("Metadata type [" + metadataType + "] not recognized");
             }
         }
-        
-        
         
         if(this.shortTextMetadataStore.getCount() !== 0 ){
 	    	this.add(this.shortTextMetadataPanel);
@@ -177,8 +271,14 @@ Ext.extend(Sbi.execution.toolbar.MetadataWindow, Ext.Window, {
 	      
 	    this.doLayout();
         
+	    Sbi.trace("[MetadataWindow.onMetadataStoreLoaded] : OUT");
     }
     
+    /**
+	 * @method 
+	 * 
+	 * Initialize the panel containing technical metadata
+	 */
     , initGeneralMetadataGridPanel : function() {
                      
         var generalMetadataGridPanel = new Ext.grid.GridPanel({
@@ -199,7 +299,6 @@ Ext.extend(Sbi.execution.toolbar.MetadataWindow, Ext.Window, {
             viewConfig : {
                 forceFit : true,
                 scrollOffset : 2
-            // the grid will never have scrollbars
             }
         });
 
@@ -234,7 +333,6 @@ Ext.extend(Sbi.execution.toolbar.MetadataWindow, Ext.Window, {
             viewConfig : {
                 forceFit : true,
                 scrollOffset : 2
-            // the grid will never have scrollbars
             },
             singleSelect : true,
             clicksToEdit : 2
@@ -254,22 +352,34 @@ Ext.extend(Sbi.execution.toolbar.MetadataWindow, Ext.Window, {
     , initLongTextMetadataTabPanel : function() {
 
         this.longTextMetadataTabPanel = new Ext.TabPanel({
-            enableTabScroll : true
-            , activeTab : 0
-            , autoScroll : true
-            , height: 210
-            //, hideMode : 'offsets'
+            activeTab : 0
+            //enableTabScroll : true
+            //, autoScroll : true
+            //, height: 210
+            //, frame: false
         });
 
-        this.longTextMetadataPanel = new Ext.form.FieldSet( {
+        this.longTextMetadataPanel = new Ext.Panel( {
             title : LN('sbi.execution.metadata.longtextmetadata'),
+            layout : 'fit',
             collapsible : true,
             collapsed : false,
             items : [ this.longTextMetadataTabPanel ],
-            height : 235,
-            width: 620
+            height : 235
+            //width: 620
         });
         
+    }
+    
+    , initButtons: function() {
+    	this.buttons = [];
+    	if (Sbi.user.functionalities.contains('SaveMetadataFunctionality')) {
+    		this.buttons.push({
+    			text : LN('sbi.execution.metadata.savemeta')
+    	        , scope : this
+    	        , handler : this.saveMetadata
+    	     });
+    	}
     }
 
     , saveMetadata : function() {
