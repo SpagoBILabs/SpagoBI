c0d2227480c5c263b1ffaab82b9ff91637670473
gioia <gioia>
2013-12-02 17:23:18 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21462 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/signup/validation/SignupFieldsValidator.java
+++ SpagoBIProject/src/it/eng/spagobi/signup/validation/SignupFieldsValidator.java
@@ -19,7 +19,7 @@ import org.json.JSONObject;
 public class SignupFieldsValidator implements IFieldsValidator {
 
 	private static transient Logger logger = Logger.getLogger(SignupFieldsValidator.class);
-	private static final String regex_password = "[^\\d][a-zA-Z0-9]{7,9}";
+	private static final String regex_password = "[^\\d][a-zA-Z0-9]{7,15}";
 	private static final String regex_email = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}";
 	private static final String regex_date = "(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\\d\\d";
 	private static final String defaultPassword = "Password";
