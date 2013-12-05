85c766408ab8a1e795b2e68bea577d2282e8a298
Marco Cortella <macortel@eng.it>
2013-12-05 10:42:38 +0000

    fixed bug with internet explorer: trim function not defined
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21498 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext/sbi/tools/dataset/FileDatasetPanel.js
+++ SpagoBIProject/web-content/js/src/ext/sbi/tools/dataset/FileDatasetPanel.js
@@ -27,6 +27,12 @@
     }
 })();
 
+//fix for Internet Explorer: missing trim function
+if(typeof String.prototype.trim !== 'function') {
+	  String.prototype.trim = function() {
+	    return this.replace(/^\s+|\s+$/g, ''); 
+	  }
+	}
 
 Ext.ns("Sbi.tools.dataset");
 
