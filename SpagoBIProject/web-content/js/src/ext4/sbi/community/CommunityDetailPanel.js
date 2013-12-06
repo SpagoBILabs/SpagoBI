58589318034c35f6294b11eb017592085fa58e4c
rosso83 <rosso83@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-12-06 13:28:47 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21520 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext4/sbi/community/CommunityDetailPanel.js
+++ SpagoBIProject/web-content/js/src/ext4/sbi/community/CommunityDetailPanel.js
@@ -26,7 +26,7 @@ Ext.define('Sbi.community.CommunityDetailPanel', {
 			if(this.validateForm()){
 				this.fireEvent("save", this.getValues());
 			}else{
-				Sbi.exception.ExceptionHandler.showErrorMessage('sbi.community.validation.error','sbi.generic.validationError');
+				Sbi.exception.ExceptionHandler.showErrorMessage(LN('sbi.community.validation.error'),LN('sbi.generic.validationError'));
 			}
 			
 		},this)
