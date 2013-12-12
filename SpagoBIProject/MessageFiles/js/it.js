cecf469532062e13027e67679e2d5dc108aa2ec9
rosso83 <rosso83>
2013-12-12 17:08:23 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21577 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/MessageFiles/js/it.js
+++ SpagoBIProject/MessageFiles/js/it.js
@@ -716,6 +716,7 @@ Sbi.locale.ln['sbi.datasource.jndiname.info']= 'Per la configurazione del data s
 Sbi.locale.ln['sbi.datasource.saved']= 'Data source salvato correttamente';
 Sbi.locale.ln['sbi.datasource.tested']= 'Data source configurato correttamente';
 Sbi.locale.ln['sbi.datasource.deleted']= 'Data source cancellato correttamente';
+Sbi.locale.ln['sbi.datasource.delete.forbidden']= 'Non hai le autorizzazioni necessarie per cancellare il data source!';
 Sbi.locale.ln['sbi.datasource.validation.error']= 'Ci sono degli errori di validazione. Controlla i campi marcati.';
 Sbi.locale.ln['sbi.datasource.tested']= 'Test OK';
 Sbi.locale.ln['sbi.datasource.test.failed']= 'Errore nel test del datasource';
