654722462243915cccf10cc7a71ce323f3ddbf87
gioia <gioia>
2014-01-10 17:37:29 +0000

    IN PROGRESS - issue SPAGOBI-1570: added core  javascript calsses that manage widgets within cockpit
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21679 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBICockpitEngine/WebContent/js/src/ext/sbi/cockpit/widgets/DummyWidget.js
@@ -0,0 +1,113 @@
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+  
+ 
+  
+ 
+/**
+  * Object name 
+  * 
+  * [description]
+  * 
+  * 
+  * Public Properties
+  * 
+  * [list]
+  * 
+  * 
+  * Public Methods
+  * 
+  *  [list]
+  * 
+  * 
+  * Public Events
+  * 
+  *  [list]
+  * 
+  * Authors
+  * 
+  * - Andrea Gioia (andrea.gioia@eng.it)
+  */
+
+Ext.ns("Sbi.cockpit.widgets");
+
+Sbi.cockpit.widgets.DummyWidget = function(config) {	
+		
+	Sbi.trace("[DummyWidget]: IN");
+	// init properties...
+	var defaultSettings = {
+	};
+
+	if(Sbi.settings && Sbi.settings.console && Sbi.settings.console.widget) {
+		defaultSettings = Ext.apply(defaultSettings, Sbi.settings.console.widget);
+	}
+	var c = Ext.apply(defaultSettings, config || {});
+		
+	Ext.apply(this, c);
+		
+	// constructor
+	Sbi.cockpit.widgets.DummyWidget.superclass.constructor.call(this, c);
+	Sbi.trace("[DummyWidget]: OUT");
+};
+
+/**
+ * @cfg {Object} config
+ * ...
+ */
+Ext.extend(Sbi.cockpit.widgets.DummyWidget, Sbi.cockpit.widgets.Widget, {
+    
+	// =================================================================================================================
+	// PROPERTIES
+	// =================================================================================================================
+	
+	// ...
+    
+    // =================================================================================================================
+	// METHODS
+	// =================================================================================================================
+	
+    // -----------------------------------------------------------------------------------------------------------------
+    // public methods
+	// -----------------------------------------------------------------------------------------------------------------
+    
+	
+	// -----------------------------------------------------------------------------------------------------------------
+    // private methods
+	// -----------------------------------------------------------------------------------------------------------------
+
+	onRender: function(ct, position) {	
+		Sbi.trace("[DummyWidget.onRender]: IN");
+		Sbi.cockpit.widgets.DummyWidget.superclass.onRender.call(this, ct, position);	
+		
+		this.dummyContent = new Ext.Panel({
+			border: false
+			, bodyBorder: false
+			, hideBorders: true
+			, frame: false
+			, html: 'Bella di padella'
+		});
+		
+		this.items.each( function(item) {
+			this.items.remove(item);
+	        item.destroy();           
+	    }, this);   
+		
+		if(this.chart !== null) {
+			this.add(this.dummyContent);
+			this.doLayout();
+		}	
+		Sbi.trace("[DummyWidget.onRender]: OUT");
+	}
+	
+	// =================================================================================================================
+	// EVENTS
+	// =================================================================================================================
+
+	// ...
+    
+	
+});
