0e148952a458cdb064aafd7d3fb0b4ded77c1883
gioia <gioia>
2014-01-10 09:59:04 +0000

    IN PROGRESS - issue SPAGOBI-1570: Added support for ext 3.4.1
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21674 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBICockpitEngine/WebContent/js/src/ext/sbi/cockpit/MainPanel.js
@@ -0,0 +1,177 @@
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+Ext.ns("Sbi.cockpit");
+
+/**
+ * Class: Sbi.cockpit.MainPanel
+ * Main GUI of SpagoBIGeoReportEngine
+ */
+Sbi.cockpit.MainPanel = function(config) {
+	
+	this.validateConfigObject(config);
+	this.adjustConfigObject(config);
+	
+	var defaultSettings = {
+	
+	};
+		
+	if(Sbi.settings && Sbi.settings.cockpit && Sbi.settings.cockpit.mainpanel) {
+		defaultSettings = Ext.apply(defaultSettings, Sbi.settings.cockpit.mainpanel);
+	}
+		
+	var c = Ext.apply(defaultSettings, config || {});
+	
+	Ext.apply(this, c);
+	
+	this.initServices();
+	//this.initStore();
+	
+	this.init();
+	
+	c = Ext.apply(c, {
+         //layout   : 'fit',
+         hideBorders: true,
+         items    : [this.msgPanel]
+	});
+
+	// constructor
+	Sbi.cockpit.MainPanel.superclass.constructor.call(this, c);
+};
+
+/**
+ * @class Sbi.cockpit.MainPanel
+ * @extends Ext.Panel
+ * 
+ * ...
+ */
+Ext.extend(Sbi.cockpit.MainPanel, Ext.Panel, {
+    
+	// =================================================================================================================
+	// PROPERTIES
+	// =================================================================================================================
+	
+	/**
+     * @property {Array} services
+     * This array contains all the services invoked by this class
+     */
+    services: null
+    
+    , msgPanel: null
+   
+    
+
+    
+    // =================================================================================================================
+	// METHODS
+	// =================================================================================================================
+	
+	/**
+	 * @method 
+	 * 
+	 * Controls that the configuration object passed in to the class constructor contains all the compulsory properties. 
+	 * If it is not the case an exception is thrown. Use it when there are properties necessary for the object
+	 * construction for whom is not possible to find out a valid default value.
+	 * 
+	 * @param {Object} the configuration object passed in to the class constructor
+	 * 
+	 * @return {Object} the config object received as input
+	 */
+	, validateConfigObject: function(config) {
+		
+	}
+
+	/**
+	 * @method 
+	 * 
+	 * Modify the configuration object passed in to the class constructor adding/removing properties. Use it for example to 
+	 * rename a property or to filter out not necessary properties.
+	 * 
+	 * @param {Object} the configuration object passed in to the class constructor
+	 * 
+	 * @return {Object} the modified version config object received as input
+	 * 
+	 */
+	, adjustConfigObject: function(config) {
+	
+	}
+	
+	, validate: function (successHandler, failureHandler, scope) {
+		Sbi.trace("[MainPanel.validate]: IN");
+		
+		var template = this.getAnalysisState();
+		var templeteStr = Ext.util.JSON.encode(template);
+		Sbi.trace("[MainPanel.validate]: template = " + templeteStr);
+		
+		Sbi.trace("[MainPanel.validate]: OUT");
+		return templeteStr;
+	}
+	
+	, getAnalysisState: function () {
+		Sbi.trace("[MainPanel.getAnalysisState]: IN");
+		
+		var analysisState = {};
+		
+		Sbi.trace("[MainPanel.getAnalysisState]: OUT");
+		return analysisState;
+	}
+	
+	// -----------------------------------------------------------------------------------------------------------------
+    // accessor methods
+	// -----------------------------------------------------------------------------------------------------------------
+
+	// ...
+	
+	// -----------------------------------------------------------------------------------------------------------------
+    // public methods
+	// -----------------------------------------------------------------------------------------------------------------
+	
+	// ...
+
+	//-----------------------------------------------------------------------------------------------------------------
+	// init methods
+	// -----------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * @method 
+	 * 
+	 * Initialize the following services exploited by this component:
+	 * 
+	 *    - xxx: ...
+	 *    - yyy: ...
+	 *    - zzz: ...
+	 *    
+	 */
+	, initServices: function() {
+		this.services = this.services || new Array();	
+		
+		var params = {
+			
+		};
+		
+//		this.services['GetTargetDataset'] = this.services['GetTargetDataset'] || Sbi.config.serviceRegistry.getServiceUrl({
+//			serviceName: 'GetTargetDataset'
+//			, baseParams: params
+//		});
+
+	}
+	
+	
+	/**
+	 * @method 
+	 * 
+	 * Initialize the GUI
+	 */
+	, init: function() {
+		this.msgtPanel();
+	}
+	
+	, msgtPanel: function() {  
+		this.msgPanel = new Ext.Panel({
+			html : 'Ciao, sono il nuovo motore cockpit'
+		});
+	}
+});
