920da3a63d0d0d1b118d2f9b61f0dce603dde735
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-01-28 10:03:51 +0000

    first commit
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21822 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIDAO/src/it/eng/spagobi/commons/utilities/HibernateUtil.java
@@ -0,0 +1,58 @@
+/* SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package it.eng.spagobi.commons.utilities;
+
+
+import it.eng.qbe.datasource.transaction.hibernate.HibernateTransaction;
+
+import java.sql.Connection;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	
+	public static transient Logger logger = Logger.getLogger(HibernateUtil.class);
+
+	private static final SessionFactory sessionFactory;
+	static {
+		try {
+
+
+				String fileCfg = "hibernate.cfg.xml";
+				fileCfg = fileCfg.trim();
+				logger.info( "Initializing hibernate Session Factory Described by [" + fileCfg +"]");
+				Configuration conf = new Configuration();
+				conf = conf.configure(fileCfg);
+				sessionFactory = conf.buildSessionFactory();
+
+		} catch (Throwable ex) {
+			// Make sure you log the exception, as it might be swallowed
+			logger.error("Initial SessionFactory creation failed.", ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	
+
+	/**
+	 * Current session.
+	 * 
+	 * @return the session
+	 */
+	public static Session currentSession() {
+		return sessionFactory.openSession();
+	}
+	
+	
+	public static Connection getConnection(Session session) {
+		return HibernateTransaction.getConnection(session);
+	}
+
+	
+}
