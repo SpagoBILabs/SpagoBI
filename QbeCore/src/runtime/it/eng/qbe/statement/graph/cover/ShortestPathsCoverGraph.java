72eff2279bac666e94ef18e5b7f5e134b6ca0965
Alberto Ghedin <alberto.ghedin@eng.it>
2014-02-05 13:30:43 +0000

    Fix a bug in the minimum cover path: consider also the path longer than 2 edges
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21932 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/runtime/it/eng/qbe/statement/graph/cover/ShortestPathsCoverGraph.java
+++ QbeCore/src/runtime/it/eng/qbe/statement/graph/cover/ShortestPathsCoverGraph.java
@@ -21,84 +21,105 @@ import org.apache.log4j.Logger;
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
+import org.jgrapht.UndirectedGraph;
 import org.jgrapht.alg.DijkstraShortestPath;
+import org.jgrapht.alg.KruskalMinimumSpanningTree;
+import org.jgrapht.graph.DirectedSubgraph;
+import org.jgrapht.graph.Subgraph;
+import org.jgrapht.graph.UndirectedSubgraph;
 
 public class ShortestPathsCoverGraph extends AbstractDefaultCover{
 
 	public static transient Logger logger = Logger.getLogger(ShortestPathsCoverGraph.class);
 
 	
-	public QueryGraph getCoverGraph( Graph<IModelEntity, Relationship> rootEntitiesGraph, Set<IModelEntity> entities) {
+	
+	
 
+	public QueryGraph getCoverGraph( Graph<IModelEntity, Relationship> rootEntitiesGraph, Set<IModelEntity> entities) {
+		Subgraph<IModelEntity, Relationship, Graph<IModelEntity, Relationship>> subgraph;
 		Iterator<IModelEntity> it = entities.iterator();
 		Set<Relationship> connectingRelatiosnhips = new HashSet<Relationship>();
+		Set<Relationship> minimumRelatiosnhips = new HashSet<Relationship>();
 
 		Set<IModelEntity> connectedEntities = new HashSet<IModelEntity>();
 		connectedEntities.add( it.next() );
 
+		//build the subgraph that contains all the nodes in entities
+		logger.debug("Building the subgraph that contains only the entities involved in the query");
 		while(it.hasNext()) {
 			IModelEntity entity = it.next();
-			if(connectedEntities.contains(entity)) continue;
-			GraphPath minimumPath = null;
-			double minPathLength = Double.MAX_VALUE;
-			for(IModelEntity connectedEntity : connectedEntities) {
+			for(IModelEntity otherEntity : entities) {
+
+				GraphPath<IModelEntity, Relationship> minimumPath = null;
 				
 				//check the path from entity to connectedEntity
-				DijkstraShortestPath dsp = new DijkstraShortestPath(rootEntitiesGraph, entity, connectedEntity);
+				DijkstraShortestPath<IModelEntity, Relationship> dsp = new DijkstraShortestPath(rootEntitiesGraph, entity, otherEntity);
 				double pathLength = dsp.getPathLength();
-				if(minPathLength > pathLength) {
-					minPathLength = pathLength;
-					minimumPath = dsp.getPath();
-				}
+				minimumPath = dsp.getPath();
+	
 				
 				if(rootEntitiesGraph instanceof DirectedGraph){
 					//check the path from connectedEntity to entity
-					DijkstraShortestPath dsp2 = new DijkstraShortestPath(rootEntitiesGraph, connectedEntity, entity);
+					DijkstraShortestPath dsp2 = new DijkstraShortestPath(rootEntitiesGraph, otherEntity, entity);
 					double pathLength2 = dsp2.getPathLength();
-					if(minPathLength > pathLength2) {
-						minPathLength = pathLength2;
+					if(pathLength > pathLength2) {
 						minimumPath = dsp2.getPath();
 					}
 				} 
 				
-			}
-			if(minimumPath!=null){
-				List<Relationship> relationships = (List<Relationship>)minimumPath.getEdgeList();
-				connectingRelatiosnhips.addAll(relationships);
-				for(Relationship relatioship: relationships) {
-					connectedEntities.add( rootEntitiesGraph.getEdgeSource(relatioship) );
-					connectedEntities.add( rootEntitiesGraph.getEdgeTarget(relatioship) );
+				
+				if(minimumPath!=null){
+					List<Relationship> pathRelations = minimumPath.getEdgeList();
+					
+					if(pathRelations!=null){
+						for(int i=0; i<pathRelations.size(); i++){
+							
+							Relationship connectingRel=	pathRelations.get(i);
+
+							if(connectingRel!=null){
+								connectingRelatiosnhips.add(connectingRel);
+								connectedEntities.add(connectingRel.getSourceEntity());
+								connectedEntities.add(connectingRel.getTargetEntity());
+							}
+
+						}
+					}	
 				}
 			}
-
 		}
 
-		for(Relationship r : connectingRelatiosnhips) {
-			IModelEntity source = rootEntitiesGraph.getEdgeSource(r);
-			IModelEntity target = rootEntitiesGraph.getEdgeTarget(r);
+		if(rootEntitiesGraph instanceof DirectedGraph){
+			subgraph = (Subgraph) new DirectedSubgraph<IModelEntity, Relationship>((DirectedGraph)rootEntitiesGraph, connectedEntities, connectingRelatiosnhips);
+		}else{
+			subgraph = (Subgraph) new UndirectedSubgraph<IModelEntity, Relationship>((UndirectedGraph)rootEntitiesGraph, connectedEntities, connectingRelatiosnhips);
 		}
-
-
-
-
-
+		logger.debug("Subgraph built");
+		
+		logger.debug("Getting the minimum spaning tree on the subgraph");
+		KruskalMinimumSpanningTree<IModelEntity, Relationship> kruscal = new KruskalMinimumSpanningTree<IModelEntity, Relationship>(subgraph);
+		minimumRelatiosnhips = kruscal.getEdgeSet();
+		logger.debug("Finished to load the MST");
+		
+		logger.debug("Building the graph starting from the MST");
 		QueryGraphBuilder qb = new QueryGraphBuilder();
-		QueryGraph monimumGraph = qb.buildGraphFromEdges(connectingRelatiosnhips);
-
+		QueryGraph monimumGraph = qb.buildGraphFromEdges(minimumRelatiosnhips);
+		logger.debug("Built the graph starting from the MST");
+		
 		return monimumGraph;
 	}
-	
+
 	public Map<IModelEntity, Set<GraphPath<IModelEntity, Relationship>>>  getConnectingRelatiosnhips( Graph<IModelEntity, Relationship> rootEntitiesGraph, Set<IModelEntity> entities) {
 		QueryGraph monimumGraph = getCoverGraph(rootEntitiesGraph, entities);
 		return getConnectingRelatiosnhips(monimumGraph, entities);
 	}
-	
+
 	public Map<IModelEntity, Set<GraphPath<IModelEntity, Relationship>>>  getConnectingRelatiosnhips( QueryGraph monimumGraph, Set<IModelEntity> entities) {
 		PathInspector pi = new PathInspector(monimumGraph, monimumGraph.vertexSet());
 		Map<IModelEntity, Set<GraphPath<IModelEntity, Relationship> >> minimumPaths = pi.getAllEntitiesPathsMap();
 
 		return minimumPaths;
 	}
-	
+
 
 }
