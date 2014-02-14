0d846dff5d6043c1dfcff60d7a4778b973ac18ab
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-02-14 11:59:58 +0000

    Added XMLA driver reference
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@21993 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/oracle/oracle-dbupgradescript-4.1-to-4.2/ORACLE_upgradescript_4.1_to_4.2.sql
+++ SpagoBIDatabaseScripts/oracle/oracle-dbupgradescript-4.1-to-4.2/ORACLE_upgradescript_4.1_to_4.2.sql
@@ -479,3 +479,6 @@ INSERT INTO SBI_ORGANIZATION_ENGINE (ENGINE_ID, ORGANIZATION_ID, CREATION_DATE, 
 values((SELECT engine_id from SBI_ENGINES where label='SpagoBICockpitEngine'), (select id from SBI_ORGANIZATIONS where name = 'SPAGOBI'),
 SYSDATE, SYSDATE, 'server', SYSDATE, '4.1');
 COMMIT;
+
+update SBI_ENGINES SET DRIVER_NM = 'it.eng.spagobi.engines.drivers.xmla.XMLADriver' where label = 'XMLAEngine';
+commit;
