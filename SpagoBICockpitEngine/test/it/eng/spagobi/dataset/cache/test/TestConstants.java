b5c28f518a4b1431673b9f74708a1c68a28146f3
Marco Cortella <macortel@eng.it>
2014-02-20 17:49:05 +0000

    IN PROGRESS - issue SPAGOBI-1623: Implementazione dell'oggetto cache https://spagobi.eng.it/jira/browse/SPAGOBI-1623
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22028 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBICockpitEngine/test/it/eng/spagobi/dataset/cache/test/TestConstants.java
+++ SpagoBICockpitEngine/test/it/eng/spagobi/dataset/cache/test/TestConstants.java
@@ -21,10 +21,89 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.dataset.cache.test;
 
+import java.io.File;
+
 /**
  * @author Marco Cortella (marco.cortella@eng.it)
  *
  */
 public class TestConstants {
+	
+	public static String workspaceFolder = "C:/Users/cortella/workspaceJEE";
+	
+	public static String RESOURCE_PATH = workspaceFolder+"/SpagoBICockpitEngine/test/it/eng/spagobi/dataset/cache/impl/sqldbcache/test/";
+	public static String WEBCONTENT_PATH = workspaceFolder+"/SpagoBICockpitEngine/WebContent";
+	public static String AF_CONFIG_FILE = "/WEB-INF/conf/master.xml";
+	
+	public enum DatabaseType { MYSQL, POSTGRES, ORACLE, SQLSERVER };
 
+
+	// =======================================================
+	// MYSQL 
+	// =======================================================
+	public static String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+	public static String MYSQL_DIALECT_CLASS = "org.hibernate.dialect.MySQLInnoDBDialect";
+	public static String MYSQL_DIALECT_NAME = "sbidomains.nm.mysql";
+
+	//-------------
+	// FOR WRITING
+	//-------------
+	public static String MYSQL_LABEL_WRITING = "datasetTest_mysql_write";
+	public static String MYSQL_URL_WRITING = "jdbc:mysql://localhost:3306/writetestschema";
+	public static String MYSQL_USER_WRITING = "root";
+	public static String MYSQL_PWD_WRITING = "root";
+	
+	//-------------
+	// FOR READING
+	//-------------
+	public static String MYSQL_LABEL_READING = "datasetTest_mysql_read";
+	public static String MYSQL_URL_READING = "jdbc:mysql://localhost:3306/foodmart";
+	public static String MYSQL_USER_READING = "root";
+	public static String MYSQL_PWD_READING = "root";
+	
+	// =======================================================
+	// POSTGRES
+	// =======================================================
+	public static String POSTGRES_DRIVER = "org.postgresql.Driver";
+	public static String POSTGRES_DIALECT_CLASS = "org.hibernate.dialect.PostgreSQLDialect";
+	public static String POSTGRES_DIALECT_NAME = "sbidomains.nm.postgresql";
+
+	//-------------
+	// FOR WRITING
+	//-------------
+	public static String POSTGRES_LABEL_WRITING = "datasetTest_postgres_write";
+	public static String POSTGRES_URL_WRITING = "jdbc:postgresql://localhost:5433/testwrite";
+	public static String POSTGRES_USER_WRITING = "postgres";
+	public static String POSTGRES_PWD_WRITING = "postgres";
+	
+	//-------------
+	// FOR READING
+	//-------------
+	public static String POSTGRES_LABEL_READING = "datasetTest_postgres_read";
+	public static String POSTGRES_URL_READING = "jdbc:postgresql://localhost:5433/testwrite";
+	public static String POSTGRES_USER_READING = "postgres";
+	public static String POSTGRES_PWD_READING = "postgres";
+	
+	// =======================================================
+	// ORACLE
+	// =======================================================
+	public static String ORACLE_DRIVER = "oracle.jdbc.OracleDriver";
+	public static String ORACLE_DIALECT_CLASS = "org.hibernate.dialect.Oracle9Dialect";
+	public static String ORACLE_DIALECT_NAME = "sbidomains.nm.oracle_9i10g";
+
+	//-------------
+	// FOR WRITING
+	//-------------
+	public static String ORACLE_LABEL_WRITING = "datasetTest_oracle_write";
+	public static String ORACLE_URL_WRITING = "jdbc:oracle:thin:@172.27.1.83:1521:repo"; //sibilla2
+	public static String ORACLE_USER_WRITING = "bilancio_mi";
+	public static String ORACLE_PWD_WRITING = "bilancio_mi";
+	
+	//-------------
+	// FOR READING
+	//-------------
+	public static String ORACLE_LABEL_READING = "datasetTest_oracle_read";
+	public static String ORACLE_URL_READING = "jdbc:oracle:thin:@172.27.1.83:1521:repo";
+	public static String ORACLE_USER_READING = "bilancio_mi";
+	public static String ORACLE_PWD_READING = "bilancio_mi";
 }
