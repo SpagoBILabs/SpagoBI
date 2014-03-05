0ebf48ba553fbe34a9f1859aa30231dab3608635
aghedin <aghedin@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-03-05 11:15:09 +0000

    comments
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22132 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/utilis/CubeUtilities.java
+++ SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/utilis/CubeUtilities.java
@@ -1,3 +1,14 @@
+ /* SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/**
+ * 
+ * Utilities class that provides some usefull method to access the informations of the cube
+ * 
+ * @author Alberto Ghedin (alberto.ghedin@eng.it)
+ */
 package it.eng.spagobi.engines.whatif.utilis;
 
 import java.util.List;
