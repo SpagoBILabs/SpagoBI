0ee1125d0861ee3f64c70313d5666bf5f8d2d543
gioia <gioia>
2014-03-17 14:24:11 +0000

    added background grid
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22262 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBICockpitEngine/WebContent/js/src/ext/sbi/cockpit/MainPanel.js
+++ SpagoBICockpitEngine/WebContent/js/src/ext/sbi/cockpit/MainPanel.js
@@ -35,7 +35,9 @@ Sbi.cockpit.MainPanel = function(config) {
 	this.init();
 	
 	c = Ext.apply(c, {
-          items    : [this.widgetContainer]
+		//id: "mainPanel",
+		bodyCssClass : "mainPanel",
+        items    : [this.widgetContainer]
 	});
 
 	// constructor
