1f0e546ee6fc9e4c4314349b5cf928c973491429
monica franceschini <francesm@eng.it>
2014-03-19 15:08:22 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22313 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/services/serializer/json/PivotJsonHTMLSerializer.java
+++ SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/services/serializer/json/PivotJsonHTMLSerializer.java
@@ -90,9 +90,9 @@ public class PivotJsonHTMLSerializer extends JsonSerializer<PivotModel> {
 		
 		logger.debug("Setting the properties of the renderer");
 		
-		renderer.setShowDimensionTitle(false); // Optionally hide the dimension title headers.
+
 		renderer.setShowParentMembers(false); // Optionally make the parent members visible.
-		
+		renderer.setShowDimensionTitle(false); // Optionally hide the dimension title headers.
 		
 		renderer.setCellSpacing(0);
 		renderer.setRowHeaderStyleClass("x-column-header-inner x-column-header x-column-header-align-left x-box-item x-column-header-default x-unselectable x-grid-header-ct x-docked x-grid-header-ct-default x-docked-top x-grid-header-ct-docked-top x-grid-header-ct-default-docked-top x-box-layout-ct x-docked-noborder-top x-docked-noborder-right x-docked-noborder-left x-pivot-header");
@@ -100,7 +100,6 @@ public class PivotJsonHTMLSerializer extends JsonSerializer<PivotModel> {
 		renderer.setCornerStyleClass("x-column-header-inner x-column-header x-column-header-align-left x-box-item x-column-header-default x-unselectable x-grid-header-ct x-docked x-grid-header-ct-default x-docked-top x-grid-header-ct-docked-top x-grid-header-ct-default-docked-top x-box-layout-ct x-docked-noborder-top x-docked-noborder-right x-docked-noborder-left x-pivot-header");
 		renderer.setCellStyleClass("x-grid-cell x-grid-td x-grid-cell-gridcolumn-1014 x-unselectable x-grid-cell-inner  x-grid-row-alt x-grid-data-row x-grid-with-col-lines x-grid-cell x-pivot-cell");
 		renderer.setTableStyleClass("x-panel-body x-grid-body x-panel-body-default x-box-layout-ct x-panel-body-default x-pivot-table");
-
 		
 		String drillDownModeValue = modelConfig.getDrillType(); 
 		
@@ -117,11 +116,14 @@ public class PivotJsonHTMLSerializer extends JsonSerializer<PivotModel> {
 		}else if(drillDownModeValue.equals(DrillDownCommand.MODE_MEMBER)){
 			renderer.addCommand(new DrillCollapseMemberCommand(renderer));
 			renderer.addCommand(new DrillExpandMemberCommand(renderer));	
+
 		}else if(drillDownModeValue.equals(DrillDownCommand.MODE_REPLACE)){
 			renderer.addCommand(new DrillDownReplaceCommand(renderer));
-			renderer.addCommand(new DrillUpReplaceCommand(renderer));		
-		}
-		
+			renderer.addCommand(new DrillUpReplaceCommand(renderer));
+			/*NOT TO BE CHANGED ---> used for drill-up in replace mode*/
+			renderer.setShowDimensionTitle(true); // Optionally hide the dimension title headers.
+			/*--------------------------------------------------------*/
+		}		
 		
 		renderer.setDrillDownMode(drillDownModeValue);
 		
