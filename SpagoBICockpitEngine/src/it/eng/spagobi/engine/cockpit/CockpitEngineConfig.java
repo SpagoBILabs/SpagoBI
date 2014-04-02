451d0ed420634077be0c02c4fc915789b650b768
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-04-02 15:15:38 +0000

    removed cache module from cockpit engine (it has been moved to spagobi core)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22540 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBICockpitEngine/src/it/eng/spagobi/engine/cockpit/CockpitEngineConfig.java
+++ SpagoBICockpitEngine/src/it/eng/spagobi/engine/cockpit/CockpitEngineConfig.java
@@ -5,23 +5,8 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.engine.cockpit;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
 import it.eng.spago.base.SourceBean;
-import it.eng.spago.error.EMFUserError;
-import it.eng.spagobi.commons.bo.Config;
-import it.eng.spagobi.commons.dao.DAOFactory;
-import it.eng.spagobi.commons.dao.IConfigDAO;
-import it.eng.spagobi.dataset.cache.CacheConfiguration;
-import it.eng.spagobi.dataset.cache.CacheFactory;
-import it.eng.spagobi.dataset.cache.ICache;
-import it.eng.spagobi.dataset.cache.impl.sqldbcache.SQLDBCache;
 import it.eng.spagobi.services.common.EnginConf;
-import it.eng.spagobi.tools.datasource.bo.IDataSource;
-import it.eng.spagobi.tools.datasource.dao.IDataSourceDAO;
 
 import org.apache.log4j.Logger;
 
@@ -68,106 +53,5 @@ public class CockpitEngineConfig {
 	public static SourceBean getConfigSourceBean() {
 		return getEngineConfig().getConfig();
 	}
-	
-	
-	//----- DataSets Cache Singleton -------------------------------------
-	private static ICache cache = null;
-	private static List<Properties> dimensionTypes = null;
-	
-	public static ICache getCache(){
-		if (cache == null){
-			initializeCache();
-		}  
-		return cache;
-	}	
-	
-	private static void initializeCache(){
-		try {
-			//Cache configuration parameters
-			String tableNamePrefix;
-			BigDecimal cacheSpaceAvailable;
-			Integer cachePercentageToClean;
-			
-			
-			CacheFactory cacheFactory = new CacheFactory();
-			IDataSourceDAO dataSourceDAO = DAOFactory.getDataSourceDAO();
-			IDataSource dataSource = dataSourceDAO.loadDataSourceWriteDefault();
-			if(dataSource == null) {
-				logger.warn("Impossible to initialize cache because there are no datasource defined as defualt write datasource");
-			} else {
-				
-				CacheConfiguration cacheConfiguration = new CacheConfiguration();
-
-				IConfigDAO configDao = DAOFactory.getSbiConfigDAO();
-				Config tableNamePrefixConfig = configDao.loadConfigParametersByLabel(CACHE_NAME_PREFIX_CONFIG);
-				if ((tableNamePrefixConfig != null) && (tableNamePrefixConfig.isActive())){
-					tableNamePrefix = tableNamePrefixConfig.getValueCheck();
-					cacheConfiguration.setTableNamePrefix(tableNamePrefix);
-				}
-				Config cacheSpaceAvailableConfig = configDao.loadConfigParametersByLabel(CACHE_SPACE_AVAILABLE_CONFIG);
-				if ((cacheSpaceAvailableConfig != null) && (cacheSpaceAvailableConfig.isActive())){
-					cacheSpaceAvailable = BigDecimal.valueOf(Double.valueOf(cacheSpaceAvailableConfig.getValueCheck()));
-					cacheConfiguration.setCacheSpaceAvailable(cacheSpaceAvailable);
-				}			
-				Config cacheSpaceCleanableConfig = configDao.loadConfigParametersByLabel(CACHE_LIMIT_FOR_CLEAN_CONFIG);
-				if ((cacheSpaceCleanableConfig != null) && (cacheSpaceAvailableConfig.isActive())){
-					cachePercentageToClean = Integer.valueOf(cacheSpaceCleanableConfig.getValueCheck());
-					cacheConfiguration.setCachePercentageToClean(cachePercentageToClean);
-				}				
-
-				cache = cacheFactory.getCache(dataSource,cacheConfiguration);
-				if (cache instanceof SQLDBCache){
-					((SQLDBCache)cache).setObjectsTypeDimension(getDimensionTypes());
-				}
-			}
-		} catch (Throwable t){
-			logger.error("An unexpected error occured while initializing cache");
-		}
-	}
-	
-	
-	public static List<Properties> getDimensionTypes() {
-		
-		if(dimensionTypes == null) {
-			initCacheConfiguration();
-		}
-		
-		return dimensionTypes;
-	}
-	
-	//--------------------------------------------------------------------
-	
-	// -- CORE SETTINGS ACCESSOR Methods---------------------------------
-	
-	
-	
-	
-	// -- PARSE Methods -------------------------------------------------
-	private final static String CACHE_CONFIG_TAG = "CACHE_CONFIG";
-	private final static String DATA_TYPES_TAG = "DATA_TYPES";
-	private final static String TYPE_TAG = "TYPE";
-
-	public static void initCacheConfiguration(){
-		try{ 
-			SourceBean configSB = (SourceBean) getConfigSourceBean().getAttribute(CACHE_CONFIG_TAG);
-			SourceBean typesSB = (SourceBean) configSB.getAttribute(DATA_TYPES_TAG);
-			List<SourceBean> typesList = typesSB.getAttributeAsList(TYPE_TAG);
-			dimensionTypes = new ArrayList<Properties>();
-			for(SourceBean type : typesList) {
-				String name = (String)type.getAttribute("name");
-				String bytes = (String)type.getAttribute("bytes");			
-				
-				Properties props = new Properties();
-				if(name != null) props.setProperty("name", name);
-				if(bytes != null) props.setProperty("bytes", bytes);
-				dimensionTypes.add(props);
-			}
-		} catch(Throwable t) {
-			throw new RuntimeException("An error occured while loading geo dimension levels' properties from file engine-config.xml", t);
-		} finally {
-			logger.debug("OUT");
-		}
-	}
-	
 	
 }
--- SpagoBICockpitEngine/test/it/eng/spagobi/dataset/cache/impl/sqldbcache/test/AbstractSQLDBCacheTest.java
+++ SpagoBICockpitEngine/test/it/eng/spagobi/dataset/cache/impl/sqldbcache/test/AbstractSQLDBCacheTest.java
@@ -21,49 +21,34 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.dataset.cache.impl.sqldbcache.test;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import org.apache.log4j.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import it.eng.qbe.dataset.QbeDataSet;
 import it.eng.spago.configuration.ConfigSingleton;
 import it.eng.spago.configuration.FileCreatorConfiguration;
 import it.eng.spagobi.commons.constants.SpagoBIConstants;
-import it.eng.spagobi.dataset.cache.CacheConfiguration;
-import it.eng.spagobi.dataset.cache.CacheFactory;
-import it.eng.spagobi.dataset.cache.ICache;
-import it.eng.spagobi.dataset.cache.ICacheMetadata;
-import it.eng.spagobi.dataset.cache.impl.sqldbcache.CacheItem;
-import it.eng.spagobi.dataset.cache.impl.sqldbcache.DataType;
-import it.eng.spagobi.dataset.cache.impl.sqldbcache.SQLDBCache;
 import it.eng.spagobi.dataset.cache.test.FakeDatamartRetriever;
 import it.eng.spagobi.dataset.cache.test.TestConstants;
 import it.eng.spagobi.tenant.Tenant;
 import it.eng.spagobi.tenant.TenantManager;
-import it.eng.spagobi.tools.dataset.bo.AbstractJDBCDataset;
 import it.eng.spagobi.tools.dataset.bo.FileDataSet;
 import it.eng.spagobi.tools.dataset.bo.FlatDataSet;
 import it.eng.spagobi.tools.dataset.bo.JDBCDataSet;
 import it.eng.spagobi.tools.dataset.bo.ScriptDataSet;
-import it.eng.spagobi.tools.dataset.common.datastore.DataStore;
-import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
 import it.eng.spagobi.tools.datasource.bo.DataSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * @author Marco Cortella (marco.cortella@eng.it)
  *
  */
 public abstract class AbstractSQLDBCacheTest extends TestCase {
-	protected static ICache cache = null;
 	
 	protected JDBCDataSet sqlDataset;
 	protected QbeDataSet qbeDataset;
@@ -88,377 +73,22 @@ public abstract class AbstractSQLDBCacheTest extends TestCase {
 		//Creating DataSources and DataSets
 		this.createDataSources();
 		this.createDatasets();
-		
-		if (cache == null){
-			CacheFactory cacheFactory = new CacheFactory();
-			
-			//Set configuration parameters for the cache (in SpagoBI Server this is the sbi_config table)
-			CacheConfiguration cacheConfiguration = new CacheConfiguration();
-			//table prefix for tables created by the cache
-			cacheConfiguration.setTableNamePrefix(TestConstants.CACHE_CONFIG_TABLE_PREFIX); 
-			//Dimension of cache in bytes
-			cacheConfiguration.setCacheSpaceAvailable(TestConstants.CACHE_CONFIG_CACHE_DIMENSION); 
-			//percentage of the cache to clean (from 0 to 100)
-			cacheConfiguration.setCachePercentageToClean(TestConstants.CACHE_CONFIG_PERCENTAGE_TO_CLEAN); 
-			
-			cache = cacheFactory.getCache(dataSourceWriting, cacheConfiguration);
-			if (cache instanceof SQLDBCache){
-				DataType dataType = new DataType(); //class used for setting data type dimension properties
-				((SQLDBCache)cache).setObjectsTypeDimension(dataType.getProps());
-			}
-		}
-
-
 	}
-	
-	//Test cases
-	
-	public void testCacheInit(){
-		assertNotNull("Cache correctly initialized", cache );
-	}
-	
-	public void testCachePutJDBCDataSet(){
-		IDataStore resultset;	
-		
-		sqlDataset.loadData();
-		resultset = sqlDataset.getDataStore();
-		cache.put(sqlDataset, sqlDataset.getSignature(), resultset);
-		assertNotNull(cache.get(sqlDataset.getSignature()));
-		logger.debug("JDBCDataset inserted inside cache");
-	}
-	
-	public void testCachePutFileDataSet(){
-		IDataStore resultset;
-
-		fileDataset.loadData();
-		resultset =	fileDataset.getDataStore();
-		cache.put(fileDataset, fileDataset.getSignature(), resultset);
-		assertNotNull(cache.get(fileDataset.getSignature()));
-		logger.debug("FileDataSet inserted inside cache");
-	}
-	
-	public void testCachePutQbeDataSet(){		
-		IDataStore resultset;
-
-		qbeDataset.loadData();
-		resultset =	qbeDataset.getDataStore();
-		cache.put(qbeDataset, qbeDataset.getSignature(), resultset);
-		assertNotNull(cache.get(qbeDataset.getSignature()));
-		logger.debug("QbeDataSet inserted inside cache");
-		
-	}
-	
-	public void testCachePutFlatDataSet(){		
-		IDataStore resultset;
-
-		flatDataset.loadData();
-		resultset =	flatDataset.getDataStore();
-		cache.put(flatDataset, flatDataset.getSignature(), resultset);
-		assertNotNull(cache.get(flatDataset.getSignature()));
-		logger.debug("FlatDataSet inserted inside cache");
-		
-	}
-	
-	public void testCachePutScriptDataSet(){		
-		IDataStore resultset;
-
-		scriptDataset.loadData();
-		resultset =	scriptDataset.getDataStore();
-		cache.put(scriptDataset, scriptDataset.getSignature(), resultset);
-		assertNotNull(cache.get(scriptDataset.getSignature()));
-		logger.debug("ScriptDataset inserted inside cache");
-		
-	}	
-	
-	public void testCacheGetJDBCDataSet(){
-		String signature = sqlDataset.getSignature();
-		IDataStore dataStore = cache.get(signature);
-		assertNull(dataStore);
-		if (dataStore == null){
-			sqlDataset.loadData();
-			dataStore =	sqlDataset.getDataStore();
-			cache.put(sqlDataset,signature,dataStore);
-		}
-		dataStore = cache.get(signature);
-		assertNotNull(dataStore);
-	}
-	
-	public void testCacheGetFileDataSet(){
-		String signature = fileDataset.getSignature();
-		IDataStore dataStore = cache.get(signature);
-		assertNull(dataStore);
-		if (dataStore == null){
-			fileDataset.loadData();
-			dataStore =	fileDataset.getDataStore();
-			cache.put(fileDataset,signature,dataStore);
-		}
-		dataStore = cache.get(signature);
-		assertNotNull(dataStore);
-	}
-	
-	public void testCacheGetQbeDataSet(){
-		String signature = qbeDataset.getSignature();
-		IDataStore dataStore = cache.get(signature);
-		assertNull(dataStore);
-		if (dataStore == null){
-			qbeDataset.loadData();
-			dataStore =	qbeDataset.getDataStore();
-			cache.put(qbeDataset,signature,dataStore);
-		}
-		dataStore = cache.get(signature);
-		assertNotNull(dataStore);
-	}	
-	
-	public void testCacheGetFlatDataSet(){
-		String signature = flatDataset.getSignature();
-		IDataStore dataStore = cache.get(signature);
-		assertNull(dataStore);
-		if (dataStore == null){
-			flatDataset.loadData();
-			dataStore =	flatDataset.getDataStore();
-			cache.put(flatDataset,signature,dataStore);
-		}
-		dataStore = cache.get(signature);
-		assertNotNull(dataStore);
-	}
-	
-	public void testCacheGetScriptDataSet(){
-		String signature = scriptDataset.getSignature();
-		IDataStore dataStore = cache.get(signature);
-		assertNull(dataStore);
-		if (dataStore == null){
-			scriptDataset.loadData();
-			dataStore =	scriptDataset.getDataStore();
-			cache.put(scriptDataset,signature,dataStore);
-		}
-		dataStore = cache.get(signature);
-		assertNotNull(dataStore);
-	}	
-	
-	
-	public void testCacheDeleteCachedDataset(){
-		IDataStore resultset;
-
-		fileDataset.loadData();
-		resultset =	fileDataset.getDataStore();
-		cache.put(fileDataset, fileDataset.getSignature(), resultset);
-		logger.debug("FileDataSet inserted inside cache");
-		String tableName = cache.getCacheMetadata().getCacheItemByResultsetSignature(fileDataset.getSignature()).getTable();
-		assertTrue(cache.delete(fileDataset.getSignature()));	
-		assertNull("Dataset still present in cache registry",cache.get(fileDataset.getSignature()));
-		IDataStore dataStore = null;
-		//Check that the table is not present, if this fail with an exception  we know that the table isn't on DB
-		try {
-			dataStore = dataSourceWriting.executeStatement("SELECT * FROM "+tableName, 0, 0);
-
-		} catch (Exception e){
-			logger.debug("The table ["+tableName+"] not found on cache database");
-			System.out.println("Delete successfull: The table ["+tableName+"] not found on cache database");
-
-		}
-		
-		assertNull("Delete fail: Dataset is still present on cache ",dataStore);
-		
-	}
-	
-	//Trying to delete a dataset that is not in the cache
-	public void testCacheDeleteUncachedDataset(){
-		IDataStore resultset;
-
-		fileDataset.loadData();
-		resultset =	fileDataset.getDataStore();
-		
-		assertFalse(cache.delete(fileDataset.getSignature()));	
-		
-	}
-	
-	public void testCacheDeleteAll(){
-		testCachePutJDBCDataSet();
-		testCachePutFileDataSet();
-		cache.deleteAll();
-		
-		ICacheMetadata cacheMetadata = cache.getCacheMetadata();
-		BigDecimal cacheSpaceAvaiable = cacheMetadata.getDimensionSpaceAvailable();
-		boolean cacheCleaned = false;
-		if (cacheSpaceAvaiable.compareTo(TestConstants.CACHE_CONFIG_CACHE_DIMENSION) == 0){
-			cacheCleaned = true;
-		}
-		assertTrue("The cache wasn't cleaned correctly",cacheCleaned);
-	}
-	
-	
-	public void testCacheDatasetDimension(){
-		ICacheMetadata cacheMetadata = cache.getCacheMetadata();
-		BigDecimal cacheSpaceAvaiable = cacheMetadata.getDimensionSpaceAvailable();
-		
-		IDataStore resultset;
-
-		fileDataset.loadData();
-		resultset =	fileDataset.getDataStore();
-		
-		BigDecimal estimatedDimension = cacheMetadata.getDimensionSpaceUsed(resultset);
-		
-		BigDecimal usedSpacePrevision = cacheSpaceAvaiable.subtract(estimatedDimension);
-		
-		boolean flag = false;
-		if (usedSpacePrevision.compareTo(BigDecimal.ZERO) >= 0) {
-			flag = true;
-			cache.put(fileDataset, fileDataset.getSignature(), resultset);
-			IDataStore cachedResultSet = cache.get(fileDataset.getSignature());
-			if (cachedResultSet != null){
-				flag = true;
-			} else {
-				flag = false;
-			}
-		} else {
-			flag = true;
-			logger.debug("Not enought space for inserting the dataset in cache");
-
-		}
-		assertTrue("Problem calculating space avaiable for inserting dataset in cache",flag);
-
-	}
-	
-	public void testCacheHasSpaceForDataset(){
-		IDataStore resultset;
-
-		fileDataset.loadData();
-		resultset =	fileDataset.getDataStore();
-		
-		
-		ICacheMetadata cacheMetadata = cache.getCacheMetadata();
-		if (cacheMetadata.hasSpaceForResultSet(resultset)){
-			cache.put(fileDataset, fileDataset.getSignature(), resultset);
-			IDataStore cachedResultSet = cache.get(fileDataset.getSignature());
-			assertNotNull("Checking space for dataset is wrong",cachedResultSet);
-
-		}
-	}
-	
-	
-	public void testCacheZeroSpaceAvaiable(){
-		
-		//Create a cache with space available equal to zero
-		ICache cacheZero = createCacheZero();
-		
-		IDataStore resultset;	
-		
-		sqlDataset.loadData();
-		resultset = sqlDataset.getDataStore();
-		cacheZero.put(sqlDataset, sqlDataset.getSignature(), resultset);
-		assertNull("Wrong behavior: dataset cached even if the space available is zero",cacheZero.get(sqlDataset.getSignature()));
-		
-		cacheZero.deleteAll();
-		
-	}
-	
-	public void testGetDimensionSpaceAvailable(){
-		ICacheMetadata cacheMetadata = cache.getCacheMetadata();
-		BigDecimal cacheSpaceAvaiable = cacheMetadata.getDimensionSpaceAvailable();
-		assertNotNull("Error calculating avaiable cache space", cacheSpaceAvaiable);
-		System.out.println(" >> Avaiable cache space: "+cacheSpaceAvaiable+" byte");
-	}
-	
-	public void testGetDimensionSpaceUsed(){
-		IDataStore resultset;
-
-		fileDataset.loadData();
-		resultset =	fileDataset.getDataStore();		
-		
-		ICacheMetadata cacheMetadata = cache.getCacheMetadata();
-		BigDecimal estimatedDatasetDimension = cacheMetadata.getDimensionSpaceUsed(resultset);
-		assertNotNull("Error calculating dimension of dataset", estimatedDatasetDimension);
-		System.out.println(" >> Estimated dataset dimension: "+estimatedDatasetDimension+" byte");
-	}
-	
-	public void testCountNumberOfObjects(){
-		testCachePutJDBCDataSet();
-		testCachePutFileDataSet();
-		testCachePutQbeDataSet();
-		
-		ICacheMetadata cacheMetadata = cache.getCacheMetadata();
-		boolean result = false;
-		if(cacheMetadata.getNumberOfObjects() == 3){
-			result = true;
-		}
-		assertTrue(result);
-	}
-	
-	public void testCacheCleaning(){
-		ICache cacheCustom = createCache(791449);
-		IDataStore resultset;
-
-		//Insert first dataset
-		qbeDataset.loadData();
-		resultset =	qbeDataset.getDataStore();
-		cacheCustom.put(qbeDataset, qbeDataset.getSignature(), resultset);
-		assertNotNull(cacheCustom.get(qbeDataset.getSignature()));
-		logger.debug("QbeDataSet inserted inside cache");
-		
-		ICacheMetadata cacheMetadata = cacheCustom.getCacheMetadata();
-		
-		//Second dataset (too big for avaiable space)
-		fileDataset.loadData();
-		resultset =	fileDataset.getDataStore();		
-		
-		assertFalse(cacheMetadata.hasSpaceForResultSet(resultset));
-		
-		cacheCustom.put(fileDataset, fileDataset.getSignature(), resultset);
-		assertNotNull("Not enought space on cache",cacheCustom.get(fileDataset.getSignature()));
-		
-		cacheCustom.deleteAll();
-		
-	}
-	
-	public void testSchemaName(){
-
-		String schemaName =  TestConstants.CACHE_CONFIG_SCHEMA_NAME;
-		
-		IDataStore resultset;
-		
-
-		qbeDataset.loadData();
-		resultset =	qbeDataset.getDataStore();
-		String signature = qbeDataset.getSignature();
-		cache.put(qbeDataset, qbeDataset.getSignature(), resultset);
-		logger.debug("QbeDataSet inserted inside cache");
-		
-		ICacheMetadata cacheMetadata = cache.getCacheMetadata();
-		LinkedHashMap<String,CacheItem> cacheRegistry = cacheMetadata.getCacheRegistry();
-		Iterator it = cacheRegistry.entrySet().iterator();
-	    while (it.hasNext()) {
-	    	BigDecimal size = null;
-	        Map.Entry<String,CacheItem> entry = (Map.Entry<String,CacheItem>)it.next();
-	        CacheItem cacheItem = entry.getValue();
-
-	        
-	        if (cacheItem.getSignature().equals(signature)){
-		        String tableName = entry.getKey();		        
-		        if (schemaName.isEmpty()){
-					IDataStore dataStore = dataSourceWriting.executeStatement("SELECT * FROM "+tableName, 0, 0);
-
-		        } else {
-					IDataStore dataStore = dataSourceWriting.executeStatement("SELECT * FROM "+schemaName+"."+tableName, 0, 0);
-		        }
-				break;
-	        }
-	        
-	    }
-
-	}
-	
-	
-	//------------------------------------------------------------------------------
-	
-	
 	
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#tearDown()
 	 */
 	protected void tearDown() throws Exception {
-		cache.deleteAll();
-		//clean cache in memory and on db
+		//clean 
 	}
+	
+	/*
+	* ----------------------------------------------------
+	* Test cases
+	* ----------------------------------------------------
+	*/
+
+	
 	
 	/*
 	* ----------------------------------------------------
@@ -552,57 +182,4 @@ public abstract class AbstractSQLDBCacheTest extends TestCase {
 		createScriptDataSet();
 
 	}
-	
-	public ICache createCacheZero(){
-		//Create a cache with space available equal to zero
-		/*
-		CacheFactory cacheFactory = new CacheFactory();
-		
-		//Set configuration parameters for the cache (in SpagoBI Server this is the sbi_config table)
-		CacheConfiguration cacheConfigurationZero = new CacheConfiguration();
-		//table prefix for tables created by the cache
-		cacheConfigurationZero.setTableNamePrefix(TestConstants.CACHE_CONFIG_TABLE_PREFIX); 
-		//Dimension of cache in bytes
-		cacheConfigurationZero.setCacheSpaceAvailable(new BigDecimal(0)); 
-		//percentage of the cache to clean (from 0 to 100)
-		cacheConfigurationZero.setCachePercentageToClean(TestConstants.CACHE_CONFIG_PERCENTAGE_TO_CLEAN); 
-		
-		ICache cacheZero = cacheFactory.getCache(dataSourceWriting, cacheConfigurationZero);
-		if (cacheZero instanceof SQLDBCache){
-			DataType dataType = new DataType(); //class used for setting data type dimension properties
-			((SQLDBCache)cacheZero).setObjectsTypeDimension(dataType.getProps());
-		}
-		return cacheZero;
-		*/
-		return createCache(0);
-	}
-	
-	public ICache createCache(int dimension ){
-		//Create a cache with space available equal to zero
-		
-		CacheFactory cacheFactory = new CacheFactory();
-		
-		//Set configuration parameters for the cache (in SpagoBI Server this is the sbi_config table)
-		CacheConfiguration cacheConfigurationCustom = new CacheConfiguration();
-		//table prefix for tables created by the cache
-		cacheConfigurationCustom.setTableNamePrefix(TestConstants.CACHE_CONFIG_TABLE_PREFIX); 
-		//Dimension of cache in bytes
-		cacheConfigurationCustom.setCacheSpaceAvailable(new BigDecimal(dimension)); 
-		//percentage of the cache to clean (from 0 to 100)
-		cacheConfigurationCustom.setCachePercentageToClean(TestConstants.CACHE_CONFIG_PERCENTAGE_TO_CLEAN); 
-		
-		ICache cacheCustom = cacheFactory.getCache(dataSourceWriting, cacheConfigurationCustom);
-		if (cacheCustom instanceof SQLDBCache){
-			DataType dataType = new DataType(); //class used for setting data type dimension properties
-			((SQLDBCache)cacheCustom).setObjectsTypeDimension(dataType.getProps());
-		}
-		return cacheCustom;
-	}
-	
-
-	
-	
-	
-	
-
 }
