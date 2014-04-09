5abe1833b1c51ff55aa5e460bdb0a401e236f673
antonella giachino <antonella.giachino@eng.it>
2014-04-09 22:43:42 +0000

    IN PROGRESS - issue SPAGOBI-1701: As SpagoBI User I want to associate the value of an anlaytical driver to a parameter exposed by a dataset or a widget in a cockpit document https://www.spagoworld.org/jira/browse/SPAGOBI-1701
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22600 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/api/DocumentResource.java
+++ SpagoBIProject/src/it/eng/spagobi/api/DocumentResource.java
@@ -11,6 +11,16 @@
  */
 package it.eng.spagobi.api;
 
+import it.eng.spagobi.analiticalmodel.document.AnalyticalModelDocumentManagementAPI;
+import it.eng.spagobi.behaviouralmodel.analyticaldriver.bo.BIObjectParameter;
+import it.eng.spagobi.commons.bo.UserProfile;
+import it.eng.spagobi.commons.serializer.SerializerFactory;
+import it.eng.spagobi.tools.dataset.bo.IDataSet;
+import it.eng.spagobi.utilities.exceptions.SpagoBIServiceException;
+
+import java.util.Iterator;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -19,12 +29,19 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
  * 
  */
 @Path("/1.0/documents")
-public class DocumentResource {
+public class DocumentResource extends AbstractSpagoBIResource {
+	
+	static private Logger logger = Logger.getLogger(DataSetResource.class);
+	
 	@GET
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
@@ -58,5 +75,49 @@ public class DocumentResource {
 	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
 	public String getDocumentMeta(@PathParam("label") String label) {
 		return null;
+	}
+	
+	@GET
+	@Path("/{label}/parameters")
+	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+	public String getDocumentParameters(@PathParam("label") String label) {
+		logger.debug("IN");		
+		try {		
+			List<JSONObject> parameters = getDocumentManagementAPI().getDocumentParameters(label);
+			JSONArray paramsJSON = writeParameters(parameters);
+			JSONObject resultsJSON = new JSONObject();
+			resultsJSON.put("results", paramsJSON);			
+			return resultsJSON.toString();	
+		} catch(Throwable t) {
+			throw new SpagoBIServiceException(this.request.getPathInfo(), "An unexpected error occured while executing service", t);
+		} finally {			
+			logger.debug("OUT");
+		}	
+	}
+	
+	public JSONArray writeParameters(List<JSONObject> params) throws Exception {		
+		JSONArray paramsJSON = new JSONArray();
+		
+		for (Iterator iterator = params.iterator(); iterator.hasNext();) {
+			JSONObject jsonObject = (JSONObject) iterator.next();
+			paramsJSON.put(jsonObject);
+		}
+
+
+		return paramsJSON;
+	}
+	
+	
+	// ===================================================================
+	// UTILITY METHODS
+	// ===================================================================
+	private UserProfile getUserProfile() {
+		UserProfile profile = this.getIOManager().getUserProfile();
+		return profile;
+	}
+	
+	private AnalyticalModelDocumentManagementAPI getDocumentManagementAPI() {
+		AnalyticalModelDocumentManagementAPI managementAPI = new AnalyticalModelDocumentManagementAPI(getUserProfile());
+		return managementAPI;
 	}
 }
