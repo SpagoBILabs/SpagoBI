066a222ed3935be4b269573b9cac953764343c92
aghedin <aghedin@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-04-24 09:59:56 +0000

    Build export service
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22691 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/api/ModelResource.java
+++ SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/api/ModelResource.java
@@ -16,6 +16,8 @@
  */
 package it.eng.spagobi.engines.whatif.api;
 
+import java.util.Date;
+
 import it.eng.spagobi.engines.whatif.WhatIfEngineInstance;
 import it.eng.spagobi.engines.whatif.common.AbstractWhatIfEngineService;
 import it.eng.spagobi.engines.whatif.model.SpagoBICellSetWrapper;
@@ -30,11 +32,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.apache.axis.utils.ByteArrayOutputStream;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
 import com.eyeq.pivot4j.PivotModel;
+import com.eyeq.pivot4j.export.poi.ExcelExporter;
 
 @Path("/1.0/model")
 public class ModelResource extends AbstractWhatIfEngineService {
@@ -43,6 +50,8 @@ public class ModelResource extends AbstractWhatIfEngineService {
 	
 	// input parameters
 	public static final String EXPRESSION = "expression";
+	
+	private static final String exportFileName = "SpagoBIOlapExport";
 	
 	/**
 	 * Executes the mdx query. If the mdx is null it executes the query of the model
@@ -125,11 +134,42 @@ public class ModelResource extends AbstractWhatIfEngineService {
 		if(mdx==null){
 			mdx = "";
 		}
-		
-		
+
 		logger.debug("OUT");
 		return mdx;
 		
 	}
+	
+	
+	/**
+	 * Exports the actual model in a xls format 
+	 * @return the response with the file embedded
+	 */
+    @GET
+    @Path("/exportXLS")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response exportXLS()
+    {
+    	
+		WhatIfEngineInstance ei = getWhatIfEngineInstance();
+		PivotModel model = ei.getPivotModel();
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+    	
+    	ExcelExporter exporter = new ExcelExporter(out);
+    	exporter.render(model);
+    	
+    	
+        byte[] outputByte = out.toByteArray();
+
+        String fileName = exportFileName+"-"+(new Date()).toLocaleString()+".xls";
+        
+    	return Response
+                .ok(outputByte, MediaType.APPLICATION_OCTET_STREAM)
+                .header("content-disposition","attachment; filename = "+fileName)
+                .build();
+    }
+	
+	
 }
 
