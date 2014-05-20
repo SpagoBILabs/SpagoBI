78f12ac2e2507af5cfd0e2c6b9683d5f92cf1bdc
Marco Cortella <macortel@eng.it>
2014-05-20 17:21:01 +0000

    added first draft of code for calculating member value from parser
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22778 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/parser/Lexer.java
+++ SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/parser/Lexer.java
@@ -61,9 +61,9 @@ public class Lexer implements java_cup.runtime.Scanner {
    */
   private static final String ZZ_CMAP_PACKED = 
     "\11\10\1\3\1\2\1\0\1\3\1\1\16\10\4\0\1\11\3\0"+
-    "\1\7\1\21\2\0\1\17\1\20\1\15\1\13\1\5\1\14\1\12"+
-    "\1\16\12\4\1\0\1\23\1\0\1\24\3\0\32\6\1\22\1\0"+
-    "\1\25\1\0\1\7\1\0\32\6\4\0\41\10\2\0\4\7\4\0"+
+    "\1\7\1\23\2\0\1\21\1\22\1\17\1\15\1\5\1\16\1\12"+
+    "\1\20\12\4\1\0\1\24\1\0\1\25\3\0\32\6\1\13\1\0"+
+    "\1\14\1\0\1\7\1\0\32\6\4\0\41\10\2\0\4\7\4\0"+
     "\1\7\2\0\1\10\7\0\1\7\4\0\1\7\5\0\27\7\1\0"+
     "\37\7\1\0\u01ca\7\4\0\14\7\16\0\5\7\7\0\1\7\1\0"+
     "\1\7\21\0\160\10\5\7\1\0\2\7\2\0\4\7\10\0\1\7"+
@@ -180,12 +180,12 @@ public class Lexer implements java_cup.runtime.Scanner {
   private static final int [] ZZ_ACTION = zzUnpackAction();
 
   private static final String ZZ_ACTION_PACKED_0 =
-    "\2\0\1\1\2\2\1\3\2\4\1\5\1\6\1\7"+
-    "\1\10\1\11\1\12\1\13\1\14\1\15\1\16\1\1"+
-    "\1\17\4\0\2\3\1\20\1\0\1\20";
+    "\2\0\1\1\2\2\1\3\2\4\1\1\1\5\1\6"+
+    "\1\7\1\10\1\11\1\12\1\13\1\14\1\15\5\0"+
+    "\2\3\1\16\3\0\1\16\3\0\2\16\2\0";
 
   private static int [] zzUnpackAction() {
-    int [] result = new int[29];
+    int [] result = new int[37];
     int offset = 0;
     offset = zzUnpackAction(ZZ_ACTION_PACKED_0, offset, result);
     return result;
@@ -211,12 +211,13 @@ public class Lexer implements java_cup.runtime.Scanner {
 
   private static final String ZZ_ROWMAP_PACKED_0 =
     "\0\0\0\26\0\54\0\102\0\54\0\130\0\156\0\204"+
-    "\0\54\0\54\0\54\0\54\0\54\0\54\0\54\0\54"+
-    "\0\54\0\54\0\232\0\54\0\260\0\232\0\306\0\334"+
-    "\0\260\0\362\0\u0108\0\u011e\0\u0134";
+    "\0\232\0\54\0\54\0\54\0\54\0\54\0\54\0\54"+
+    "\0\54\0\54\0\260\0\306\0\334\0\362\0\u0108\0\260"+
+    "\0\u011e\0\u0134\0\u014a\0\u0160\0\u0176\0\u018c\0\u01a2\0\u01b8"+
+    "\0\u01ce\0\u01e4\0\54\0\u01fa\0\u0210";
 
   private static int [] zzUnpackRowMap() {
-    int [] result = new int[29];
+    int [] result = new int[37];
     int offset = 0;
     offset = zzUnpackRowMap(ZZ_ROWMAP_PACKED_0, offset, result);
     return result;
@@ -240,19 +241,25 @@ public class Lexer implements java_cup.runtime.Scanner {
 
   private static final String ZZ_TRANS_PACKED_0 =
     "\1\3\1\4\2\5\1\6\1\3\1\7\1\10\1\3"+
-    "\1\5\1\3\1\11\1\12\1\13\1\14\1\15\1\16"+
-    "\1\17\1\20\1\21\1\22\5\3\1\23\1\3\1\23"+
-    "\16\3\1\24\30\0\1\5\27\0\1\6\1\25\1\26"+
-    "\2\0\1\26\1\27\17\0\1\7\1\0\1\7\2\10"+
-    "\1\26\1\30\17\0\1\10\1\0\3\10\21\0\1\26"+
-    "\1\0\1\26\2\0\1\26\1\30\17\0\1\31\25\0"+
-    "\1\32\1\0\1\33\23\0\1\33\1\0\1\33\23\0"+
-    "\1\32\1\0\1\33\2\0\1\33\1\34\17\0\1\33"+
-    "\1\0\1\33\2\0\1\33\1\34\17\0\1\35\1\0"+
-    "\1\35\23\0\1\35\1\0\1\35\2\0\1\35\14\0";
+    "\1\5\1\3\1\11\1\3\1\12\1\13\1\14\1\15"+
+    "\1\16\1\17\1\20\1\21\1\22\26\3\30\0\1\5"+
+    "\27\0\1\6\1\23\1\24\2\0\1\24\1\25\17\0"+
+    "\1\7\1\0\1\7\2\10\1\24\1\26\17\0\1\10"+
+    "\1\0\3\10\21\0\1\27\1\0\1\27\23\0\1\30"+
+    "\25\0\1\24\1\0\1\24\2\0\1\24\1\26\17\0"+
+    "\1\31\1\0\1\32\23\0\1\32\1\0\1\32\23\0"+
+    "\1\27\1\0\1\27\2\0\1\27\2\0\1\33\15\0"+
+    "\1\31\1\0\1\32\2\0\1\32\1\34\17\0\1\32"+
+    "\1\0\1\32\2\0\1\32\1\34\25\0\1\35\17\0"+
+    "\1\36\1\0\1\36\32\0\1\37\16\0\1\36\1\0"+
+    "\1\36\2\0\1\36\20\0\1\40\1\0\1\40\23\0"+
+    "\1\40\1\0\1\40\2\0\1\40\1\41\1\0\1\42"+
+    "\15\0\1\41\1\0\1\41\2\0\2\41\1\0\1\43"+
+    "\23\0\1\44\26\0\1\45\16\0\1\41\1\0\1\41"+
+    "\17\0";
 
   private static int [] zzUnpackTrans() {
-    int [] result = new int[330];
+    int [] result = new int[550];
     int offset = 0;
     offset = zzUnpackTrans(ZZ_TRANS_PACKED_0, offset, result);
     return result;
@@ -290,11 +297,11 @@ public class Lexer implements java_cup.runtime.Scanner {
   private static final int [] ZZ_ATTRIBUTE = zzUnpackAttribute();
 
   private static final String ZZ_ATTRIBUTE_PACKED_0 =
-    "\2\0\1\11\1\1\1\11\3\1\12\11\1\1\1\11"+
-    "\4\0\3\1\1\0\1\1";
+    "\2\0\1\11\1\1\1\11\4\1\11\11\5\0\3\1"+
+    "\3\0\1\1\3\0\1\1\1\11\2\0";
 
   private static int [] zzUnpackAttribute() {
-    int [] result = new int[29];
+    int [] result = new int[37];
     int offset = 0;
     offset = zzUnpackAttribute(ZZ_ATTRIBUTE_PACKED_0, offset, result);
     return result;
@@ -765,67 +772,59 @@ public class Lexer implements java_cup.runtime.Scanner {
         case 1: 
           { throw new Error("Illegal character <"+yytext()+">");
           }
-        case 17: break;
+        case 15: break;
         case 2: 
           { /* just skip what was found, do nothing */
           }
-        case 18: break;
+        case 16: break;
         case 3: 
           { if(verbose){System.out.println("NUMBER="+yytext());} parsedString.append("NUMBER="+yytext()); String val = yytext().replaceAll(",",".");return new Symbol(sym.NUMBER, new Double(val));
           }
-        case 19: break;
+        case 17: break;
         case 4: 
           { if(verbose){System.out.println("VARIABLE="+yytext());} parsedString.append("VARIABLE="+yytext()); return new Symbol(sym.VARIABLE, yytext());
           }
-        case 20: break;
+        case 18: break;
         case 5: 
           { if(verbose){System.out.println(" + ");} parsedString.append("+"); return symbol(sym.PLUS, "+");
           }
-        case 21: break;
+        case 19: break;
         case 6: 
           { if(verbose){System.out.println(" - ");} parsedString.append("-"); return symbol(sym.MINUS, "-");
           }
-        case 22: break;
+        case 20: break;
         case 7: 
           { if(verbose){System.out.println(" * ");} parsedString.append("*"); return symbol(sym.TIMES, "*");
           }
-        case 23: break;
+        case 21: break;
         case 8: 
           { if(verbose){System.out.println(" / ");} parsedString.append("/"); return symbol(sym.DIVIDE, "/");
           }
-        case 24: break;
+        case 22: break;
         case 9: 
           { if(verbose){System.out.println(" ( ");} parsedString.append("("); return symbol(sym.LPAREN, "(");
           }
-        case 25: break;
+        case 23: break;
         case 10: 
           { if(verbose){System.out.println(" ) ");} parsedString.append(")"); return symbol(sym.RPAREN, ")");
           }
-        case 26: break;
+        case 24: break;
         case 11: 
           { if(verbose){System.out.println(" % ");} parsedString.append("%"); return symbol(sym.PERCENT, "%");
           }
-        case 27: break;
+        case 25: break;
         case 12: 
-          { if(verbose){System.out.println(" [ ");} parsedString.append("[");  yybegin(MEMBER_DECLARATION);
-          }
-        case 28: break;
-        case 13: 
           { if(verbose){System.out.println(" ; ");} parsedString.append(";"); return symbol(sym.SEMI, ";");
           }
-        case 29: break;
-        case 14: 
+        case 26: break;
+        case 13: 
           { if(verbose){System.out.println(" = ");} parsedString.append("="); return symbol(sym.EQUAL, "=");
           }
-        case 30: break;
-        case 15: 
-          { if(verbose){System.out.println(" ] ");} parsedString.append("]"); yybegin(YYINITIAL);
-          }
-        case 31: break;
-        case 16: 
+        case 27: break;
+        case 14: 
           { if(verbose){System.out.println("MEMBER="+yytext());} parsedString.append("MEMBER="+yytext()); return new Symbol(sym.MEMBER, yytext());
           }
-        case 32: break;
+        case 28: break;
         default: 
           if (zzInput == YYEOF && zzStartRead == zzCurrentPos) {
             zzAtEOF = true;
