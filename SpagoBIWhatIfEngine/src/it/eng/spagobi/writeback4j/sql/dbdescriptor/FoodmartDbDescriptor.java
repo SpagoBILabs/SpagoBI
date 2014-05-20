1a8723e066377f779925e5fcdc41e0bf6eb81473
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-05-20 12:54:48 +0000

    Db Descriptors
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22776 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIWhatIfEngine/src/it/eng/spagobi/writeback4j/sql/dbdescriptor/FoodmartDbDescriptor.java
@@ -0,0 +1,31 @@
+package it.eng.spagobi.writeback4j.sql.dbdescriptor;
+
+import it.eng.spagobi.engines.whatif.common.WhatIfConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodmartDbDescriptor implements IDbSchemaDescriptor {
+
+
+	public List<String> getColumnNames(String table){
+
+		List<String> toReturn = new ArrayList<String>();
+		
+		toReturn.add("product_id");
+		toReturn.add("time_id");
+		toReturn.add("customer_id");
+		toReturn.add("promotion_id");
+		toReturn.add("store_id");
+		toReturn.add("store_sales");
+		toReturn.add("store_cost");
+		toReturn.add(WhatIfConstants.VERSION_COLUMN_NAME);
+		
+		return toReturn;
+		
+		
+	}
+	
+	
+	
+}
