12dc704b2e8a975900a738d4fbc3a67a8b8b5226
Alberto Ghedin <alberto.ghedin@eng.it>
2014-05-21 08:11:41 +0000

    OPEN - issue SPAGOBI-1736: Write back (in the DB) for the proportional algorithm https://www.spagoworld.org/jira/browse/SPAGOBI-1736
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22780 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/common/WhatIfConstants.java
+++ SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/common/WhatIfConstants.java
@@ -2,7 +2,7 @@ package it.eng.spagobi.engines.whatif.common;
 
 public class WhatIfConstants {
 
-	public static String VERSION_COLUMN_NAME = "versione"; 
+	public static String VERSION_COLUMN_NAME = "wbversion"; 
 	public static final String VERSION_DIMENSION_NAME = "[Version]";
 	
 }
