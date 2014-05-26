74a948d60ab4eae4ada61a7f7831a9b2947d9914
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-05-26 14:57:16 +0000

    Added locked and locker columns to sbi_artifacts table
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22833 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/ingres/Ingres_create.sql
+++ SpagoBIDatabaseScripts/ingres/Ingres_create.sql
@@ -2101,6 +2101,8 @@ CREATE TABLE SBI_ARTIFACTS (
        NAME                 VARCHAR(100) NOT NULL,
        DESCR                VARCHAR(500) NULL,
        TYPE                 VARCHAR(50) NULL,
+       LOCKED               BOOLEAN NULL,
+       LOCKER               VARCHAR(100) NULL;
        USER_IN              VARCHAR(100) NOT NULL,
        USER_UP              VARCHAR(100),
        USER_DE              VARCHAR(100),
