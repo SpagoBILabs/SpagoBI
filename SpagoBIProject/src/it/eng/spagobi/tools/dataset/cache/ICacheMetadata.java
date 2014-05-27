685060066e03783ea2d9a23c3e478e2d285569d5
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-05-27 14:42:42 +0000

    changed package of class CacheItem
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22850 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/tools/dataset/cache/ICacheMetadata.java
+++ SpagoBIProject/src/it/eng/spagobi/tools/dataset/cache/ICacheMetadata.java
@@ -21,7 +21,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.tools.dataset.cache;
 
-import it.eng.spagobi.tools.dataset.cache.impl.sqldbcache.CacheItem;
 import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
 
 import java.math.BigDecimal;
--- SpagoBIProject/src/it/eng/spagobi/tools/dataset/cache/impl/sqldbcache/SQLDBCacheMetadata.java
+++ SpagoBIProject/src/it/eng/spagobi/tools/dataset/cache/impl/sqldbcache/SQLDBCacheMetadata.java
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.tools.dataset.cache.impl.sqldbcache;
 
+import it.eng.spagobi.tools.dataset.cache.CacheItem;
 import it.eng.spagobi.tools.dataset.cache.ICacheMetadata;
 import it.eng.spagobi.tools.dataset.common.datastore.DataStore;
 import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
