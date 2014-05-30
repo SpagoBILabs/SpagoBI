2e31fb3eb8c724f468a72050c762baf773969efc
Giulio Gavardi <giulio.gavardi@eng.it>
2014-05-30 08:34:01 +0000

    Standalone url parameters
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@22878 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/common/WhatIfConstants.java
+++ SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/common/WhatIfConstants.java
@@ -4,6 +4,9 @@ public class WhatIfConstants {
 
 	public static final String VERSION_DIMENSION_NAME = "Version";
 	public static final String VERSION_DIMENSION_UNIQUENAME = "[Version]";
-	
+
+	public static final String STANDALONE_HOST = "http://localhost:8080/";
+	public static final String SATNDALONE_CONTEXT = "SpagoBIWhatIfEngine";
+
 	
 }
