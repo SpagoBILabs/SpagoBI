d91663c78b30d7a3d9e9502922371ce7af3a37dc
Davide Zerbetto <davide.zerbetto@eng.it>
2014-07-08 09:55:38 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23247 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- palo-gwt-widgets/src/com/tensegrity/palo/gwt/widgets/client/i18n/ILocalMessages.java
+++
@@ -1,6 +0,0 @@
-package com.tensegrity.palo.gwt.widgets.client.i18n;
-
-import com.google.gwt.i18n.client.Messages;
-
-public interface ILocalMessages extends Messages {
-}
--- palo-gwt-widgets/src/com/tensegrity/palo/gwt/widgets/client/palotable/MouseClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.tensegrity.palo.gwt.widgets.client.palotable;
-
-import com.tensegrity.palo.gwt.widgets.client.palotable.header.HeaderItem;
-
-public interface MouseClickListener {
-	public void leftClicked(HeaderItem item, int x, int y);
-	public void rightClicked(HeaderItem item, int x, int y);
-}
