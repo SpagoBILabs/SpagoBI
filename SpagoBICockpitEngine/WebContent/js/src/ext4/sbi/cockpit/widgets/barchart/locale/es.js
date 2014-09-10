4aafd85cd35222df535741c96f6377358af30402
Giulio Gavardi <giulio.gavardi@eng.it>
2014-09-10 16:06:38 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23582 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/cockpit/widgets/barchart/locale/es.js
+++ SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/cockpit/widgets/barchart/locale/es.js
@@ -4,10 +4,16 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
 
+Ext.ns("Sbi.locale");
 
-
-
-
- Ext.ns("Sbi.locale");
 Sbi.locale.ln = Sbi.locale.ln || new Array();
 
+Sbi.locale.ln['sbi.cockpit.widgets.table.barChartWidgetDesigner.title'] = 'Diseñador Bar Chart';
+Sbi.locale.ln['sbi.worksheet.designer.barchartdesignerpanel.form.orientation.title'] = 'Orientación';
+Sbi.locale.ln['sbi.worksheet.designer.barchartdesignerpanel.form.orientation.vertical'] = 'vertical';
+Sbi.locale.ln['sbi.worksheet.designer.barchartdesignerpanel.form.orientation.horizontal'] = 'horizontal';
+Sbi.locale.ln['sbi.worksheet.designer.barchartdesignerpanel.form.type.tooltip.side-by-side'] = 'De lado a lado';
+Sbi.locale.ln['sbi.worksheet.designer.barchartdesignerpanel.form.type.tooltip.stacked'] = 'Apilados';
+Sbi.locale.ln['sbi.worksheet.designer.barchartdesignerpanel.form.type.tooltip.percent-stacked'] = 'Porcentaje apilado';
+Sbi.locale.ln['sbi.worksheet.designer.barchartdesignerpanel.form.showvalues.title'] = 'Mostrar valores';
+Sbi.locale.ln['sbi.worksheet.designer.barchartdesignerpanel.form.showlegend.title'] = 'Mostrar leyenda';
