95ef09a99dc9c4291882c25673aa82134aedfc96
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-09-10 16:06:38 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23582 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/cockpit/widgets/piechart/locale/es.js
+++ SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/cockpit/widgets/piechart/locale/es.js
@@ -22,7 +22,20 @@ Ext.Msg.buttonText.no = 'No';
 Sbi.locale.ln['sbi.qbe.messagewin.yes'] = 'SÃ­';
 Sbi.locale.ln['sbi.qbe.messagewin.no'] = 'No';
 Sbi.locale.ln['sbi.qbe.messagewin.cancel'] = 'Cancelar';
+	Sbi.locale.ln['sbi.generic.label'] = 'Etiqueta';
+	Sbi.locale.ln['sbi.generic.name'] = 'Nombre';
+	Sbi.locale.ln['sbi.generic.descr'] = 'Descripción';
+	Sbi.locale.ln['sbi.generic.scope'] = 'Objetivo';
+	Sbi.locale.ln['sbi.generic.scope.private'] = 'Privado';
+	Sbi.locale.ln['sbi.generic.scope.public'] = 'Publico';
+	Sbi.locale.ln['sbi.generic.actions.save'] = 'Guardar';
 
+	Sbi.locale.ln['sbi.generic.wait'] = "Por favor espere...";
+	Sbi.locale.ln['sbi.generic.error'] = "Error";
+	Sbi.locale.ln['sbi.generic.success'] = "Éxito";
+	Sbi.locale.ln['sbi.generic.operationSucceded'] = "Operación correcta";
+	Sbi.locale.ln['sbi.generic.query.SQL'] = "Query SQL";
+	Sbi.locale.ln['sbi.generic.query.JPQL'] = "Query JPLQ";
 
 //===================================================================
 // MESSAGE WINDOW
@@ -36,3 +49,18 @@ Sbi.locale.ln['sbi.qbe.messagewin.info.title'] = 'Mensaje de informaciÃ³n';
 //SESSION EXPIRED
 //===================================================================
 Sbi.locale.ln['sbi.qbe.sessionexpired.msg'] = 'La sesiÃ³n ha caducado. Intente volver a ejecutar el documento';
+
+
+//===================================================================
+//PIECHART WIDGET PANEL
+//===================================================================
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.title'] = 'Diseñador gráfico de torta';
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.form.showvalues.title'] = 'Mostrar valores';
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.form.showlegend.title'] = 'Mostrar leyenda';
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.form.showpercentage.title'] = 'Mostrar valor de porcentaje';
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.categorypalette.title'] = 'Categoría paleta de colores';
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.form.legend.position.title'] = 'Posición Leyenda';
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.form.legend.position.bottom'] = 'Abajo';
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.form.legend.position.top'] = 'Arriba';
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.form.legend.position.left'] = 'Izquierda ';
+Sbi.locale.ln['sbi.cockpit.widgets.piechartwidgetdesigner.form.legend.position.right'] = 'Derecha';
