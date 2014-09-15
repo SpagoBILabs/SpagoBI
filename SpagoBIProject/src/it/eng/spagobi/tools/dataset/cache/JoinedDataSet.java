8f5abed22f4f8f71f1c00661bc9849517ed1d95e
Davide Zerbetto <davide.zerbetto@eng.it>
2014-09-15 10:48:13 +0000

    Bug fix: cockpit associations did not work when using Qbe datasets
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23619 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/tools/dataset/cache/JoinedDataSet.java
+++ SpagoBIProject/src/it/eng/spagobi/tools/dataset/cache/JoinedDataSet.java
@@ -11,21 +11,26 @@
  */
 package it.eng.spagobi.tools.dataset.cache;
 
+import it.eng.qbe.dataset.QbeDataSet;
 import it.eng.spagobi.commons.bo.UserProfile;
+import it.eng.spagobi.commons.constants.SpagoBIConstants;
 import it.eng.spagobi.commons.dao.DAOFactory;
 import it.eng.spagobi.container.ObjectUtils;
 import it.eng.spagobi.services.dataset.bo.SpagoBiDataSet;
 import it.eng.spagobi.tools.dataset.bo.AbstractDataSet;
 import it.eng.spagobi.tools.dataset.bo.IDataSet;
+import it.eng.spagobi.tools.dataset.bo.VersionedDataSet;
 import it.eng.spagobi.tools.dataset.common.association.AssociationGroup;
 import it.eng.spagobi.tools.dataset.common.association.AssociationGroupJSONSerializer;
 import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
 import it.eng.spagobi.tools.dataset.dao.IDataSetDAO;
+import it.eng.spagobi.tools.dataset.utils.datamart.SpagoBICoreDatamartRetriever;
 import it.eng.spagobi.tools.datasource.bo.IDataSource;
 import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -35,7 +40,7 @@ import org.json.JSONObject;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
- *
+ * 
  */
 public class JoinedDataSet extends AbstractDataSet {
 
@@ -46,65 +51,64 @@ public class JoinedDataSet extends AbstractDataSet {
 	AssociationGroup associationGroup;
 
 	private static transient Logger logger = Logger.getLogger(JoinedDataSet.class);
-	 	
-	public JoinedDataSet( SpagoBiDataSet dataSetConfig ) {
-	    super(dataSetConfig);
+
+	public JoinedDataSet(SpagoBiDataSet dataSetConfig) {
+		super(dataSetConfig);
 	}
-	
+
 	public JoinedDataSet(String label, String name, String description, String configuration) {
 		setName(name);
-    	setLabel(label);
-    	setDescription(description);
+		setLabel(label);
+		setDescription(description);
 		setConfiguration(configuration);
 	}
-	
-	public JoinedDataSet(String label, String name, String description, 
-			JSONObject configuration) {
+
+	public JoinedDataSet(String label, String name, String description, JSONObject configuration) {
 		setName(name);
-    	setLabel(label);
-    	setDescription(description);
+		setLabel(label);
+		setDescription(description);
 		setConfiguration(configuration);
 	}
-	
-	public JoinedDataSet(String label, String name, String description, 
-			AssociationGroup associationGroup) {
+
+	public JoinedDataSet(String label, String name, String description, AssociationGroup associationGroup) {
 		setName(name);
-    	setLabel(label);
-    	setDescription(description);
+		setLabel(label);
+		setDescription(description);
 		setAssociations(associationGroup);
 	}
-	
-	
+
+	@Override
 	public void setConfiguration(String configuration) {
-		 logger.trace("IN");
-		 
-		 try {
-			 JSONObject jsonConf = ObjectUtils.toJSONObject( this.configuration );
-			 setConfiguration(jsonConf);
-		 } catch(Throwable t) {
-			 throw new RuntimeException("An unexpected error occured while setting configuration", t);
-		 } finally {
-			 logger.trace("OUT");
-		 }
+		logger.trace("IN");
+
+		try {
+			JSONObject jsonConf = ObjectUtils.toJSONObject(this.configuration);
+			setConfiguration(jsonConf);
+		} catch (Throwable t) {
+			throw new RuntimeException("An unexpected error occured while setting configuration", t);
+		} finally {
+			logger.trace("OUT");
+		}
 	}
-	
+
 	public void setConfiguration(JSONObject configuration) {
 		logger.trace("IN");
-		 
-		 try { 
-			 AssociationGroupJSONSerializer deserializer = new AssociationGroupJSONSerializer();
-			 AssociationGroup associationGroup = deserializer.deserialize(configuration);
-			 setAssociations(associationGroup);			
-		 } catch(Throwable t) {
-			 throw new RuntimeException("An unexpected error occured while setting configuration", t);
-		 } finally {
-			 logger.trace("OUT");
-		 }
+
+		try {
+			AssociationGroupJSONSerializer deserializer = new AssociationGroupJSONSerializer();
+			AssociationGroup associationGroup = deserializer.deserialize(configuration);
+			setAssociations(associationGroup);
+		} catch (Throwable t) {
+			throw new RuntimeException("An unexpected error occured while setting configuration", t);
+		} finally {
+			logger.trace("OUT");
+		}
 	}
-	
+
 	/**
 	 * @return the configuration
 	 */
+	@Override
 	public String getConfiguration() {
 		JSONObject configuration = null;
 		try {
@@ -112,65 +116,89 @@ public class JoinedDataSet extends AbstractDataSet {
 			AssociationGroup associationGroup = getAssociations();
 			configuration = serializer.serialize(associationGroup);
 			return configuration.toString();
-		} catch(Throwable t) {
-			 throw new RuntimeException("An unexpected error occured while getting configuration", t);
+		} catch (Throwable t) {
+			throw new RuntimeException("An unexpected error occured while getting configuration", t);
 		}
 	}
-		 
+
 	public List<IDataSet> getDataSets() {
 		return this.joinedDataSets;
 	}
-	
+
 	public IDataSet getDataSet(String datasetLabel) {
 		IDataSet targetDataSet = null;
-		if(datasetLabel == null) return targetDataSet;
-		for(IDataSet dataSet: joinedDataSets) {
-			if(dataSet.getLabel().equals(datasetLabel)) {
+		if (datasetLabel == null)
+			return targetDataSet;
+		for (IDataSet dataSet : joinedDataSets) {
+			if (dataSet.getLabel().equals(datasetLabel)) {
 				targetDataSet = dataSet;
 				break;
 			}
 		}
 		return targetDataSet;
 	}
-	
+
 	private void setDataSetsByLabel(Collection<String> dataSetLabels) {
 		List<IDataSet> datsets = new ArrayList<IDataSet>();
-		for(String dataSetLabel : dataSetLabels) {
+		for (String dataSetLabel : dataSetLabels) {
 			IDataSet dataSet = this.getDataSetDAO().loadDataSetByLabel(dataSetLabel);
+			checkQbeDataset(dataSet);
 			datsets.add(dataSet);
 		}
 		setDataSets(datsets);
 	}
-	
+
 	private void setDataSets(List<IDataSet> joinedDataSets) {
 		this.joinedDataSets = joinedDataSets;
 	}
-	 
+
 	private IDataSetDAO getDataSetDAO() {
-		if(dataSetDao == null) {
+		if (dataSetDao == null) {
 			try {
 				dataSetDao = DAOFactory.getDataSetDAO();
-				if(getUserProfile() != null) {
+				if (getUserProfile() != null) {
 					dataSetDao.setUserProfile(userProfile);
 				}
 			} catch (Throwable t) {
 				throw new SpagoBIRuntimeException("An unexpected error occured while instatiating the DAO", t);
-			} 
+			}
 		}
 		return dataSetDao;
 	}
-	
+
+	private void checkQbeDataset(IDataSet dataSet) {
+
+		IDataSet ds = null;
+		if (dataSet instanceof VersionedDataSet) {
+			VersionedDataSet versionedDataSet = (VersionedDataSet) dataSet;
+			ds = versionedDataSet.getWrappedDataset();
+		} else {
+			ds = dataSet;
+		}
+
+		if (ds instanceof QbeDataSet) {
+			SpagoBICoreDatamartRetriever retriever = new SpagoBICoreDatamartRetriever();
+			Map parameters = ds.getParamsMap();
+			if (parameters == null) {
+				parameters = new HashMap();
+				ds.setParamsMap(parameters);
+			}
+			ds.getParamsMap().put(SpagoBIConstants.DATAMART_RETRIEVER, retriever);
+		}
+	}
+
 	public AssociationGroup getAssociations() {
 		return associationGroup;
 	}
 
 	private void setAssociations(AssociationGroup associationGroup) {
 		this.associationGroup = associationGroup;
-		setDataSetsByLabel( associationGroup.getDataSetLabels() );
+		setDataSetsByLabel(associationGroup.getDataSetLabels());
 	}
-	 
-	 
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#getUserProfileAttributes()
 	 */
 	public Map getUserProfileAttributes() {
@@ -178,58 +206,73 @@ public class JoinedDataSet extends AbstractDataSet {
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#setUserProfileAttributes(java.util.Map)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * it.eng.spagobi.tools.dataset.bo.IDataSet#setUserProfileAttributes(java
+	 * .util.Map)
 	 */
 	public void setUserProfileAttributes(Map<String, Object> attributes) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
+	@Override
 	public void loadData(int offset, int fetchSize, int maxResults) {
 		ICache cache = SpagoBICacheManager.getCache();
-		
+
 		// check if all joinedDataset have been succesfully stored in cache
 		List<IDataSet> dataSetNotStoredInCache = cache.getNotContained(joinedDataSets);
-		if(dataSetNotStoredInCache.size() > 0) {
+		if (dataSetNotStoredInCache.size() > 0) {
 			String dataSetLabels = "";
-			for(IDataSet dataSet: dataSetNotStoredInCache) {
+			for (IDataSet dataSet : dataSetNotStoredInCache) {
 				dataSetLabels += dataSet.getLabel() + ";";
 			}
-			logger.warn("In order to load data for joined store [" + this.getName() + "] " +
-					"the following datasets [" + dataSetLabels + "] need to be loaded before");
-		
+			logger.warn("In order to load data for joined store [" + this.getName() + "] " + "the following datasets [" + dataSetLabels
+					+ "] need to be loaded before");
+
 			cache.load(joinedDataSets, true);
 		}
-		
+
 		// qunado tutti i work sono finiti creo la tabella di join
 		joinedDataStore = cache.refresh(joinedDataSets, associationGroup);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#getDataStore()
 	 */
 	public IDataStore getDataStore() {
 		return joinedDataStore;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#setAbortOnOverflow(boolean)
 	 */
 	public void setAbortOnOverflow(boolean abortOnOverflow) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	/* (non-Javadoc)
-	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#addBinding(java.lang.String, java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * it.eng.spagobi.tools.dataset.bo.IDataSet#addBinding(java.lang.String,
+	 * java.lang.Object)
 	 */
 	public void addBinding(String bindingName, Object bindingValue) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#test()
 	 */
 	public IDataStore test() {
@@ -237,7 +280,9 @@ public class JoinedDataSet extends AbstractDataSet {
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#test(int, int, int)
 	 */
 	public IDataStore test(int offset, int fetchSize, int maxResults) {
@@ -245,53 +290,73 @@ public class JoinedDataSet extends AbstractDataSet {
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#getSignature()
 	 */
 	public String getSignature() {
 		String signature = "";
-		for(IDataSet dataSet : this.joinedDataSets)	{
+		for (IDataSet dataSet : this.joinedDataSets) {
 			signature += dataSet.getSignature();
 		}
 		return signature;
 	}
 
-	/* (non-Javadoc)
-	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#decode(it.eng.spagobi.tools.dataset.common.datastore.IDataStore)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * it.eng.spagobi.tools.dataset.bo.IDataSet#decode(it.eng.spagobi.tools.
+	 * dataset.common.datastore.IDataStore)
 	 */
 	public IDataStore decode(IDataStore datastore) {
 		throw new UnsupportedOperationException("Dataset implementation class [" + this.getClass().getName() + "] does not support method [decode]");
 	}
 
-	/* (non-Javadoc)
-	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#isCalculateResultNumberOnLoadEnabled()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * it.eng.spagobi.tools.dataset.bo.IDataSet#isCalculateResultNumberOnLoadEnabled
+	 * ()
 	 */
 	public boolean isCalculateResultNumberOnLoadEnabled() {
 		return false;
 	}
 
-	/* (non-Javadoc)
-	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#setCalculateResultNumberOnLoad(boolean)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * it.eng.spagobi.tools.dataset.bo.IDataSet#setCalculateResultNumberOnLoad
+	 * (boolean)
 	 */
 	public void setCalculateResultNumberOnLoad(boolean enabled) {
 		logger.warn("In [" + this.getClass().getName() + "] calculate result number on loadis always false");
-		
+
 	}
 
-	/* (non-Javadoc)
-	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#setDataSource(it.eng.spagobi.tools.datasource.bo.IDataSource)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * it.eng.spagobi.tools.dataset.bo.IDataSet#setDataSource(it.eng.spagobi
+	 * .tools.datasource.bo.IDataSource)
 	 */
 	public void setDataSource(IDataSource dataSource) {
 		logger.warn("In [" + this.getClass().getName() + "] datasource is not used");
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see it.eng.spagobi.tools.dataset.bo.IDataSet#getDataSource()
 	 */
 	public IDataSource getDataSource() {
 		return null;
 	}
-	
+
 	public UserProfile getUserProfile() {
 		return userProfile;
 	}
@@ -299,19 +364,20 @@ public class JoinedDataSet extends AbstractDataSet {
 	public void setUserProfile(UserProfile userProfile) {
 		this.userProfile = userProfile;
 	}
-	
+
 	public void setParamsMaps(Map<String, Map<String, String>> paramsMaps) {
 		Set<String> datasetLabels = paramsMaps.keySet();
-		for(String datasetLabel : datasetLabels) {
+		for (String datasetLabel : datasetLabels) {
 			Map paramsMap = paramsMaps.get(datasetLabel);
 			IDataSet dataSet = getDataSet(datasetLabel);
 			dataSet.setParamsMap(paramsMap);
 		}
 	}
-	
+
+	@Override
 	public void setParamsMap(Map paramsMap) {
 		super.setParamsMap(paramsMap);
-		for(IDataSet dataSet: joinedDataSets) {
+		for (IDataSet dataSet : joinedDataSets) {
 			dataSet.setParamsMap(paramsMap);
 		}
 	}
