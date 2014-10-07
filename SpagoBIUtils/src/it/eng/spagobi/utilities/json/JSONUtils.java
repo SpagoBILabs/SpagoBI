2951985adab69e46c63b9a0fd41290c80b9aaeaa
aghedin <aghedin@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-10-07 07:38:24 +0000

    OPEN - issue SPAGOBI-1881: As final user I want sort the values by rows and columns headers https://www.spagoworld.org/jira/browse/SPAGOBI-1881
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23725 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/utilities/json/JSONUtils.java
+++ SpagoBIUtils/src/it/eng/spagobi/utilities/json/JSONUtils.java
@@ -9,8 +9,10 @@ import it.eng.spagobi.utilities.assertion.Assert;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,28 +29,28 @@ import com.fasterxml.jackson.databind.node.ValueNode;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
- *
+ * 
  */
 public class JSONUtils {
 	public static List asList(JSONArray array) {
 		List list;
-		
-		if(array == null) {
+
+		if (array == null) {
 			throw new IllegalArgumentException();
 		}
 		list = new ArrayList();
-		
-		for(int i = 0; i < array.length(); i++) {
+
+		for (int i = 0; i < array.length(); i++) {
 			try {
-				list.add( array.get(i) );
+				list.add(array.get(i));
 			} catch (JSONException e) {
 				Assert.assertUnreachable("An out of bound error here is a signal of an internal JSON.org's bug");
 			}
 		}
-		
+
 		return list;
 	}
-	
+
 	public static String[] asStringArray(JSONArray jSONArray) throws JSONException {
 		if (jSONArray == null) {
 			return null;
@@ -60,7 +62,7 @@ public class JSONUtils {
 		}
 		return toReturn;
 	}
-	
+
 	public static JSONArray asJSONArray(String[] stringArray) throws JSONException {
 		if (stringArray == null) {
 			return null;
@@ -72,74 +74,94 @@ public class JSONUtils {
 		}
 		return toReturn;
 	}
-	
-	
-	public static JSONArray toJSONArray(String object)  throws JSONException, JsonMappingException, JsonParseException,IOException{
+
+	public static JSONArray toJSONArray(String object) throws JSONException, JsonMappingException, JsonParseException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		ArrayNode df = (ArrayNode) mapper.readValue(object, JsonNode.class);
 		return toJSONArray(df);
 	}
-	
-	public static JSONObject toJSONObject(String object) throws JSONException, JsonMappingException, JsonParseException,IOException{
+
+	public static JSONObject toJSONObject(String object) throws JSONException, JsonMappingException, JsonParseException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		ObjectNode df = (ObjectNode) mapper.readValue(object, JsonNode.class);
 		return toJSONObject(df);
 	}
 
-	public static JSONArray toJSONArray(ArrayNode df)  throws JSONException{
+	public static JSONArray toJSONArray(ArrayNode df) throws JSONException {
 		JSONArray toReturn = new JSONArray();
-		for(int i=0; i<df.size(); i++){
-			
+		for (int i = 0; i < df.size(); i++) {
+
 			toReturn.put(getValueFromJsonNode(df.get(i)));
 		}
 		return toReturn;
-	} 
-	
-	public static JSONObject toJSONObject(ObjectNode df) throws JSONException{
+	}
+
+	public static JSONObject toJSONObject(ObjectNode df) throws JSONException {
 		JSONObject toReturn = new JSONObject();
 		Iterator<String> namesIter = df.fieldNames();
-		while(namesIter.hasNext()){
+		while (namesIter.hasNext()) {
 			String key = namesIter.next();
 			JsonNode node = df.get(key);
 			Object value = getValueFromJsonNode(node);
 			toReturn.put(key, value);
 		}
 		return toReturn;
-	} 
-	
-	public static Object getValueFromJsonNode(JsonNode node) throws JSONException{
-		
+	}
+
+	public static Object getValueFromJsonNode(JsonNode node) throws JSONException {
+
 		Object value = null;
-		if(node instanceof TextNode){
-			value = ((TextNode)(node)).textValue();
-		}else if(node instanceof ObjectNode){
-			value = toJSONObject((ObjectNode)node);
-		}else if(node instanceof ArrayNode){
-			value = toJSONArray((ArrayNode)node);
-		}else if(node instanceof ValueNode){
-			value = ((ValueNode)node).asText();
+		if (node instanceof TextNode) {
+			value = ((TextNode) (node)).textValue();
+		} else if (node instanceof ObjectNode) {
+			value = toJSONObject((ObjectNode) node);
+		} else if (node instanceof ArrayNode) {
+			value = toJSONArray((ArrayNode) node);
+		} else if (node instanceof ValueNode) {
+			value = ((ValueNode) node).asText();
 		}
 		return value;
 	}
-	
-	public static String escapeJsonString(String object){
+
+	public static String escapeJsonString(String object) {
 		String toReturn = object;
-		
+
 		toReturn = toReturn.replaceAll("\n", "\\\\n");
 		toReturn = toReturn.replaceAll("\r", "\\\\r");
 		toReturn = toReturn.replaceAll("\t", "\\\\t");
 		toReturn = toReturn.replaceAll("\b", "\\\\b");
 		toReturn = toReturn.replaceAll("\f", "\\\\f");
-		//toReturn = toReturn.replaceAll("\"", "\\\\\"");
-		
-		//toReturn = toReturn.replaceAll("{", "\\{");		
-		//toReturn = toReturn.replaceAll("}", "\\}");
-		//toReturn = toReturn.replaceAll("(", "\\\\(");
-		//toReturn = toReturn.replaceAll(")", "\\\\)");
-		//toReturn = toReturn.replaceAll("[", "\\[");
-		//toReturn = toReturn.replaceAll("]", "\\]");
-		
+		// toReturn = toReturn.replaceAll("\"", "\\\\\"");
+
+		// toReturn = toReturn.replaceAll("{", "\\{");
+		// toReturn = toReturn.replaceAll("}", "\\}");
+		// toReturn = toReturn.replaceAll("(", "\\\\(");
+		// toReturn = toReturn.replaceAll(")", "\\\\)");
+		// toReturn = toReturn.replaceAll("[", "\\[");
+		// toReturn = toReturn.replaceAll("]", "\\]");
+
 		return toReturn;
 	}
-	
+
+	/**
+	 * Transform a jsonobject into a map
+	 * 
+	 * @param object
+	 * @return
+	 * @throws JSONException
+	 */
+	public static Map<String, Object> toMap(JSONObject object) throws JSONException {
+		Map<String, Object> toReturn = new HashMap<String, Object>();
+
+		if (object != null) {
+			String[] names = JSONObject.getNames(object);
+			for (int i = 0; i < names.length; i++) {
+				toReturn.put(names[i], object.get(names[i]));
+			}
+		}
+
+		return toReturn;
+
+	}
+
 }
