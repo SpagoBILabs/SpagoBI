c3b868adfba2c3fca4f7dfb3ffaa106e2a3ab288
Giulio Gavardi <giulio.gavardi@eng.it>
2014-10-15 12:45:47 +0000

    Cockpit table aggregation and visualization of last caching date
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23753 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datastore/IDataStore.java
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datastore/IDataStore.java
@@ -5,53 +5,71 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.tools.dataset.common.datastore;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
 import it.eng.spago.base.SourceBean;
 import it.eng.spago.base.SourceBeanException;
 import it.eng.spagobi.tools.dataset.common.metadata.IMetaData;
 
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 /**
- * @authors Angelo Bernabei (angelo.bernabei@eng.it)       
- *          Andrea Gioia (andrea.gioia@eng.it)
+ * @authors Angelo Bernabei (angelo.bernabei@eng.it) Andrea Gioia (andrea.gioia@eng.it)
  */
 public interface IDataStore {
 
 	IMetaData getMetaData();
-	
-	Iterator iterator();    
+
+	Iterator iterator();
+
 	boolean isEmpty();
+
 	long getRecordsCount();
-	
-    IRecord getRecordAt(int i);
-    IRecord getRecordByID(Object value);
-    
-    List<IRecord> findRecords(int fieldIndex, Object fieldValue) ;
-    List<IRecord> findRecords(final List fieldIndexes, final List fieldValues) ;
-    List<IRecord> findRecords(IRecordMatcher matcher);
-    
-    List getFieldValues(int fieldIndex);    
-    Set getFieldDistinctValues(int fieldIndex);
-    Set<String> getFieldDistinctValuesAsString(int fieldIndex);
-    
-    void sortRecords(int fieldIndex);    
-    void sortRecords(int fieldIndex, Comparator filedComparator);    
-    void sortRecords(Comparator recordComparator);
-        
-    void appendRecord(IRecord r);
-    void prependRecord(IRecord record);
+
+	IRecord getRecordAt(int i);
+
+	IRecord getRecordByID(Object value);
+
+	List<IRecord> findRecords(int fieldIndex, Object fieldValue);
+
+	List<IRecord> findRecords(final List fieldIndexes, final List fieldValues);
+
+	List<IRecord> findRecords(IRecordMatcher matcher);
+
+	List getFieldValues(int fieldIndex);
+
+	Set getFieldDistinctValues(int fieldIndex);
+
+	Set<String> getFieldDistinctValuesAsString(int fieldIndex);
+
+	void sortRecords(int fieldIndex);
+
+	void sortRecords(int fieldIndex, Comparator filedComparator);
+
+	void sortRecords(Comparator recordComparator);
+
+	void appendRecord(IRecord r);
+
+	void prependRecord(IRecord record);
+
 	void insertRecord(int recordIndex, IRecord record);
-    
+
 	/**
 	 * @deprecated use the proper DataWriter instead
 	 */
-    String toXml();
-    
-    /**
+	@Deprecated
+	String toXml();
+
+	/**
 	 * @deprecated use the proper DataWriter instead
 	 */
-    SourceBean toSourceBean() throws SourceBeanException;
+	@Deprecated
+	SourceBean toSourceBean() throws SourceBeanException;
+
+	Date getCacheDate();
+
+	void setCacheDate(Date cacheDate);
+
 }
