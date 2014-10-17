56f230cfa0594fde594ab49b899f4b754d392b75
Giorgio Federici <giorgio.federici@eng.it>
2014-10-17 08:09:50 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23785 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-5.0_to_5.1/MYSQL_upgradescript_5.0_to_5.1.sql
+++ SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-5.0_to_5.1/MYSQL_upgradescript_5.0_to_5.1.sql
@@ -2,7 +2,7 @@ alter table SBI_OBJ_METADATA CHANGE DESCRIPTION  DESCR  VARCHAR(100) ;
 
 INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN, SBI_VERSION_IN) VALUES 
 ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'), 
-'SPAGOBI.SOCIAL_ANALYSIS_URL', 'SPAGOBI SOCIAL ANALYSIS URL', 'SPAGOBI SOCIAL ANALYSIS URL', true, '/SpagoBISocialAnalysis',
+'SPAGOBI.SOCIAL_ANALYSIS_URL', 'SPAGOBI SOCIAL ANALYSIS URL', 'SPAGOBI SOCIAL ANALYSIS URL', true, '/SpagoBISocialAnalysis/restful-services/start/gui',
 (select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SOCIAL_CONFIGURATION', 'server', current_timestamp, '5.1');
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
 commit;
