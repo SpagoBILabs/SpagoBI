d45d98633890b5e2d74f08c775e127bba9e98e2e
Giorgio Federici <giorgio.federici@eng.it>
2014-10-17 08:06:48 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23781 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBISocialAnalysis/src/it/eng/spagobi/twitter/analysis/api/SpagoBISocialAnalysisStartGuiAPI.java
@@ -0,0 +1,41 @@
+/* SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package it.eng.spagobi.twitter.analysis.api;
+
+import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Giorgio Federici (giorgio.federici@eng.it)
+ */
+@Path("/start")
+public class SpagoBISocialAnalysisStartGuiAPI {
+
+	static final Logger logger = Logger.getLogger(SpagoBISocialAnalysisStartGuiAPI.class);
+
+	@GET
+	@Path("/gui")
+	public void startGUI(@Context HttpServletRequest request, @Context HttpServletResponse response) {
+
+		logger.debug("REST Service: /SpagoBISocialAnalysis/restful-services/startGUI - Start ");
+
+		try {
+
+			response.sendRedirect(request.getContextPath() + "/index.jsp");
+
+		} catch (Throwable t) {
+			throw new SpagoBIRuntimeException("Error in REST Service: /SpagoBISocialAnalysis/restful-services/startGUI", t);
+		}
+
+	}
+}
