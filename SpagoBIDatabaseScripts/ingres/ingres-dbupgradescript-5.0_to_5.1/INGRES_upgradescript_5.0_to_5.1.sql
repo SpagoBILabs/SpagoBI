e21f6285cdfe746e57dfc36ba34ab471deb60452
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-10-21 14:24:37 +0000

    added HierarchiesManagement authorizations
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23830 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-5.0_to_5.1/INGRES_upgradescript_5.0_to_5.1.sql
+++ SpagoBIDatabaseScripts/ingres/ingres-dbupgradescript-5.0_to_5.1/INGRES_upgradescript_5.0_to_5.1.sql
@@ -1,1 +1,10 @@
 ALTER TABLE SBI_OBJ_METADATA RENAME COLUMN DESCRIPTION  TO  DESCR;
+
+INSERT INTO SBI_AUTHORIZATIONS
+(ID, NAME, USER_IN, TIME_IN)
+values ((SELECT NEXT_VAL FROM hibernate_sequences WHERE SEQUENCE_NAME='SBI_AUTHORIZATIONS'),
+'HIERARCHIES_MANAGEMENT',
+'server', current_timestamp) ;
+commit;
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_AUTHORIZATIONS';
+commit; 
--- SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-5.0_to_5.1/MYSQL_upgradescript_5.0_to_5.1.sql
+++ SpagoBIDatabaseScripts/mysql/mysql-dbupgradescript-5.0_to_5.1/MYSQL_upgradescript_5.0_to_5.1.sql
@@ -13,3 +13,12 @@ INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, V
 (select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SOCIAL_CONFIGURATION', 'server', current_timestamp, '5.1');
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
 commit;
+
+INSERT INTO SBI_AUTHORIZATIONS
+(ID, NAME, USER_IN, TIME_IN)
+values ((SELECT NEXT_VAL FROM hibernate_sequences WHERE SEQUENCE_NAME='SBI_AUTHORIZATIONS'),
+'HIERARCHIES_MANAGEMENT',
+'server', current_timestamp) ;
+commit;
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_AUTHORIZATIONS';
+commit; 
