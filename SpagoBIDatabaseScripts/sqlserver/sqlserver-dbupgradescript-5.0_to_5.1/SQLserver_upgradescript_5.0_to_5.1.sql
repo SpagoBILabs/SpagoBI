f7157f8cbb1d77ee465b4dba9c5ac74aabe15476
Marco Cortella <macortel@eng.it>
2014-10-21 14:24:37 +0000

    added HierarchiesManagement authorizations
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23830 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/sqlserver/sqlserver-dbupgradescript-5.0_to_5.1/SQLserver_upgradescript_5.0_to_5.1.sql
+++ SpagoBIDatabaseScripts/sqlserver/sqlserver-dbupgradescript-5.0_to_5.1/SQLserver_upgradescript_5.0_to_5.1.sql
@@ -4,3 +4,13 @@ EXEC sp_rename 'SBI_OBJ_METADATA.DESCRIPTION', 'DESCR', 'COLUMN';
 --UPDATE SBI_OBJ_METADATA SET DESCR = DESCRIPTION;
 --COMMIT;
 --ALTER TABLE SBI_OBJ_METADATA DROP COLUMN DESCRIPTION;
+
+-- 10/09/2014 Marco: added HIERARCHIES_MANAGEMENT authorization
+INSERT INTO SBI_AUTHORIZATIONS
+(ID, NAME, USER_IN, TIME_IN)
+values ((SELECT NEXT_VAL FROM hibernate_sequences WHERE SEQUENCE_NAME='SBI_AUTHORIZATIONS'),
+'HIERARCHIES_MANAGEMENT',
+'server', current_timestamp) ;
+commit;
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_AUTHORIZATIONS';
+commit; 
