e8e39117f630d1dc719ed3fe0ada34b70cb0744b
giofeder <giofeder@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-10-22 13:11:08 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23852 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBISocialAnalysis/src/it/eng/spagobi/twitter/analysis/utilities/AnalysisUtility.java
+++ SpagoBISocialAnalysis/src/it/eng/spagobi/twitter/analysis/utilities/AnalysisUtility.java
@@ -17,8 +17,13 @@ import java.net.URLEncoder;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -374,5 +379,21 @@ public class AnalysisUtility {
 
 		return (double) NumberFormat.getNumberInstance(Locale.getDefault()).parse(str);
 
+	}
+
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+		List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+			@Override
+			public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+
+		Map<K, V> result = new LinkedHashMap<K, V>();
+		for (Map.Entry<K, V> entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+		return result;
 	}
 }
