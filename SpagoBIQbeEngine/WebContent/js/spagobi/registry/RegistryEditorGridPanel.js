fdc832c2e7f40fa49f6a36514c35b6392e0637f5
Giulio Gavardi <giulio.gavardi@eng.it>
2014-11-06 15:15:03 +0000

    fix sum to 2 decimals
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23982 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/WebContent/js/spagobi/registry/RegistryEditorGridPanel.js
+++ SpagoBIQbeEngine/WebContent/js/spagobi/registry/RegistryEditorGridPanel.js
@@ -1704,14 +1704,17 @@ Ext.extend(Sbi.registry.RegistryEditorGridPanel, Ext.grid.EditorGridPanel, {
 		  			previousTotal = parseFloat(cell.innerText);
 			  		// add to previous total the difference between new and old value
 			  		var newTotal = previousTotal - previousValue + parseFloat(newValue)
+			  		newTotal = newTotal.toFixed(2);
 			  		cell.innerText = newTotal;
 		  		}	
 		  		else{
 		  			previousTotal = parseFloat(cell.textContent);
 			  		// add to previous total the difference between new and old value
 			  		var newTotal = previousTotal - previousValue + parseFloat(newValue)
+			  		newTotal = newTotal.toFixed(2);
 			  		cell.textContent = newTotal;
 		  		}
+		  		
 		  		
 		  		foundTotal = true;
 		  		
