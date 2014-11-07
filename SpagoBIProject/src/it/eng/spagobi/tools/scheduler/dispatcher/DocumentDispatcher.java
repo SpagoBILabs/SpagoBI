8f6a432ba7852990ab335b92f09e1774b503a346
Giulio Gavardi <giulio.gavardi@eng.it>
2014-11-07 16:28:49 +0000

    Scheduler option to send only one mail with all document
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@23995 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/tools/scheduler/dispatcher/DocumentDispatcher.java
+++ SpagoBIProject/src/it/eng/spagobi/tools/scheduler/dispatcher/DocumentDispatcher.java
@@ -5,98 +5,102 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.tools.scheduler.dispatcher;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
 import it.eng.spagobi.tools.scheduler.to.DispatchContext;
 import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
 
-import org.apache.log4j.Logger;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.log4j.Logger;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
- *
+ * 
  */
 public class DocumentDispatcher {
-	
+
 	private boolean disposed;
 	private DispatchContext dispatchContext;
-	private List<IDocumentDispatchChannel> documentDispatchChannels;
-	
+	private final List<IDocumentDispatchChannel> documentDispatchChannels;
+
 	// logger component
-	private static Logger logger = Logger.getLogger(DocumentDispatcher.class); 
-	
+	private static Logger logger = Logger.getLogger(DocumentDispatcher.class);
+
 	public DocumentDispatcher(DispatchContext dispatchContext) {
 		this.disposed = false;
 		this.dispatchContext = dispatchContext;
-		
+
 		this.documentDispatchChannels = new ArrayList<IDocumentDispatchChannel>();
-		
+
 		IDocumentDispatchChannel dispatchChannel;
-		if(dispatchContext.isSnapshootDispatchChannelEnabled()) {
+		if (dispatchContext.isSnapshootDispatchChannelEnabled()) {
 			dispatchChannel = new SnapshootDocumentDispatchChannel(dispatchContext);
 			documentDispatchChannels.add(dispatchChannel);
 		}
 
-		if(dispatchContext.isFunctionalityTreeDispatchChannelEnabled()) {
+		if (dispatchContext.isFunctionalityTreeDispatchChannelEnabled()) {
 			dispatchChannel = new FunctionalityTreeDocumentDispatchChannel(dispatchContext);
 			documentDispatchChannels.add(dispatchChannel);
 		}
 
-		if(dispatchContext.isMailDispatchChannelEnabled()) {							
+		if (dispatchContext.isGlobalUniqueMail()) {
+			dispatchChannel = new UniqueMailDocumentDispatchChannel(dispatchContext);
+			documentDispatchChannels.add(dispatchChannel);
+		}
+
+		if (dispatchContext.isMailDispatchChannelEnabled() && !dispatchContext.isGlobalUniqueMail()) {
 			dispatchChannel = new MailDocumentDispatchChannel(dispatchContext);
 			documentDispatchChannels.add(dispatchChannel);
 		}
-		if(dispatchContext.isDistributionListDispatchChannelEnabled()) {
+		if (dispatchContext.isDistributionListDispatchChannelEnabled()) {
 			dispatchChannel = new DistributionListDocumentDispatchChannel(dispatchContext);
 			documentDispatchChannels.add(dispatchChannel);
 		}
 
-		if(dispatchContext.isJavaClassDispatchChannelEnabled()) {
+		if (dispatchContext.isJavaClassDispatchChannelEnabled()) {
 			dispatchChannel = new JavaClassDocumentDispatchChannel(dispatchContext);
 			documentDispatchChannels.add(dispatchChannel);
 		}
-		
-		if(dispatchContext.isFileSystemDispatchChannelEnabled()) {
+
+		if (dispatchContext.isFileSystemDispatchChannelEnabled()) {
 			dispatchChannel = new FileSystemDocumentDispatcher(dispatchContext);
 			documentDispatchChannels.add(dispatchChannel);
 		}
 	}
-	
+
 	public boolean dispatch(BIObject document, byte[] executionOutput) {
 		boolean dispatchedToAllChannels = true;
-		if(disposed) {
+		if (disposed) {
 			throw new SpagoBIRuntimeException("Impossible to dispatch document [" + document + "]. The dispatcher has been disposed.");
 		}
-		
-		for(IDocumentDispatchChannel dispatchChannel : documentDispatchChannels) {
-			boolean succesfullyDispatched =  dispatchChannel.dispatch(document, executionOutput);
+
+		for (IDocumentDispatchChannel dispatchChannel : documentDispatchChannels) {
+			boolean succesfullyDispatched = dispatchChannel.dispatch(document, executionOutput);
 			dispatchedToAllChannels = dispatchedToAllChannels && succesfullyDispatched;
 		}
 		return dispatchedToAllChannels;
 	}
-	
+
 	public boolean canDispatch(BIObject document) {
 		boolean canDispatch = false;
-		for(IDocumentDispatchChannel dispatchChannel : documentDispatchChannels) {
+		for (IDocumentDispatchChannel dispatchChannel : documentDispatchChannels) {
 			canDispatch = canDispatch || dispatchChannel.canDispatch(document);
-			
+
 		}
 		return canDispatch;
 	}
-	
+
 	public void dispose() {
-		for(IDocumentDispatchChannel dispatchChannel : documentDispatchChannels) {
+		for (IDocumentDispatchChannel dispatchChannel : documentDispatchChannels) {
 			dispatchChannel.close();
 		}
 		this.disposed = true;
 	}
-	
+
 	public void setDispatchContext(DispatchContext dispatchContext) {
 		this.dispatchContext = dispatchContext;
-		for(IDocumentDispatchChannel dispatchChannel : documentDispatchChannels) {
+		for (IDocumentDispatchChannel dispatchChannel : documentDispatchChannels) {
 			dispatchChannel.setDispatchContext(dispatchContext);
 		}
 	}
