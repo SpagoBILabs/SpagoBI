b7d75ced650c25c576293cbb293746ea7e63eb24
Giorgio Federici <giorgio.federici@eng.it>
2014-11-12 14:44:18 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24032 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/oracle/oracle-dbupgradescript-5.0_to_5.1/ORACLE_upgradescript_5.0_to_5.1.sql
+++ SpagoBIDatabaseScripts/oracle/oracle-dbupgradescript-5.0_to_5.1/ORACLE_upgradescript_5.0_to_5.1.sql
@@ -8,3 +8,11 @@ values ((SELECT NEXT_VAL FROM hibernate_sequences WHERE SEQUENCE_NAME='SBI_AUTHO
 commit;
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_AUTHORIZATIONS';
 commit; 
+
+INSERT INTO SBI_AUTHORIZATIONS
+(ID, NAME,  USER_IN, TIME_IN) 
+values ((SELECT NEXT_VAL FROM hibernate_sequences WHERE SEQUENCE_NAME='SBI_AUTHORIZATIONS'), 
+'VIEW_SOCIAL_ANALYSIS',  'server', current_timestamp) ;
+commit;
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_AUTHORIZATIONS';
+commit;
