93fe50c263d0d5299019498e0489c130a2c0545f
Giorgio Federici <giorgio.federici@eng.it>
2014-11-12 15:18:35 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24037 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBISocialAnalysis/src/it/eng/spagobi/twitter/analysis/rest/annotations/CheckFunctionalitiesParser.java
+++ SpagoBISocialAnalysis/src/it/eng/spagobi/twitter/analysis/rest/annotations/CheckFunctionalitiesParser.java
@@ -1,3 +1,8 @@
+/* SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.twitter.analysis.rest.annotations;
 
 import it.eng.spagobi.commons.bo.UserProfile;
@@ -5,31 +10,44 @@ import it.eng.spagobi.commons.bo.UserProfile;
 import java.lang.reflect.Method;
 import java.util.Collection;
 
+import org.apache.log4j.Logger;
+
+/**
+ * @author Giorgio Federici (giorgio.federici@eng.it)
+ */
 public class CheckFunctionalitiesParser {
+
+	static private Logger logger = Logger.getLogger(CheckFunctionalitiesParser.class);
 
 	public boolean isPublicService(Method method) throws Exception {
 
-		if (method.isAnnotationPresent(CheckFunctionalities.class)) {
+		logger.debug("Method isPublicService: Start");
+
+		if (method.isAnnotationPresent(UserConstraint.class)) {
+			logger.debug("Method isPublicService: The service is not public");
 			return false;
 		} else {
+			logger.debug("Method isPublicService: The service is public");
 			return true;
 		}
 	}
 
 	public boolean checkFunctionalitiesByAnnotation(Method method, UserProfile profile) throws Exception {
 
+		logger.debug("Method checkFunctionalitiesByAnnotation: Start");
 		boolean authorized = false;
 
-		if (method.isAnnotationPresent(CheckFunctionalities.class)) {
+		if (method.isAnnotationPresent(UserConstraint.class)) {
 
 			Collection functionalities = profile.getFunctionalities();
 
-			CheckFunctionalities checkFuncs = method.getAnnotation(CheckFunctionalities.class);
-			String[] funcsAnnotated = checkFuncs.funcs();
+			UserConstraint checkFuncs = method.getAnnotation(UserConstraint.class);
+			String[] funcsAnnotated = checkFuncs.functionalities();
 
 			for (int i = 0; i < funcsAnnotated.length; i++) {
 				if (functionalities.contains(funcsAnnotated[i])) {
 					authorized = true;
+					logger.debug("Method checkFunctionalitiesByAnnotation: Functionality found");
 					break;
 				}
 			}
@@ -37,6 +55,7 @@ public class CheckFunctionalitiesParser {
 			authorized = true;
 		}
 
+		logger.debug("Method checkFunctionalitiesByAnnotation: End");
 		return authorized;
 	}
 }
