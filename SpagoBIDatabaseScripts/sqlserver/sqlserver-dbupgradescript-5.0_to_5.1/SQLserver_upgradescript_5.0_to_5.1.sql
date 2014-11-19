9d5a28581ad96c4fcd5e652dc20317f1ca5919d7
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-11-19 14:16:51 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24118 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/sqlserver/sqlserver-dbupgradescript-5.0_to_5.1/SQLserver_upgradescript_5.0_to_5.1.sql
+++ SpagoBIDatabaseScripts/sqlserver/sqlserver-dbupgradescript-5.0_to_5.1/SQLserver_upgradescript_5.0_to_5.1.sql
@@ -6,6 +6,21 @@ EXEC sp_rename 'SBI_OBJ_METADATA.DESCRIPTION', 'DESCR', 'COLUMN';
 --ALTER TABLE SBI_OBJ_METADATA DROP COLUMN DESCRIPTION;
 
 -- 10/09/2014 Marco: added HIERARCHIES_MANAGEMENT authorization
+
+INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN, SBI_VERSION_IN) VALUES 
+((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'), 
+'SPAGOBI.SOCIAL_ANALYSIS_URL', 'SPAGOBI SOCIAL ANALYSIS URL', 'SPAGOBI SOCIAL ANALYSIS URL', true, '/SpagoBISocialAnalysis/restful-services/start',
+(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SOCIAL_CONFIGURATION', 'server', current_timestamp, '5.1');
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
+commit;
+
+INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN, SBI_VERSION_IN) VALUES 
+((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'), 
+'SPAGOBI.SOCIAL_ANALYSIS_IS_ACTIVE', 'SPAGOBI SOCIAL ANALYSIS STATUS', 'SPAGOBI SOCIAL ANALYSIS STATUS', true, 'true',
+(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SOCIAL_CONFIGURATION', 'server', current_timestamp, '5.1');
+update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
+commit;
+
 INSERT INTO SBI_AUTHORIZATIONS
 (ID, NAME, USER_IN, TIME_IN)
 values ((SELECT NEXT_VAL FROM hibernate_sequences WHERE SEQUENCE_NAME='SBI_AUTHORIZATIONS'),
