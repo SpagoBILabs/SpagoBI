ac10d9a4bd8b38b4721641638c214a146567068e
Giulio Gavardi <giulio.gavardi@eng.it>
2014-11-20 09:54:54 +0000

    back to previous slice behaviour
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24126 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/widgets/store/InMemoryFilteredStore.js
+++ SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/widgets/store/InMemoryFilteredStore.js
@@ -74,8 +74,7 @@ Ext.define('Sbi.widgets.store.InMemoryFilteredStore', {
     ,
     onLoadHandler: function(store, records, successful, eOpts, sorterFn) {
 		if (!this.inMemoryData) {
-			this.inMemoryData = this.data.items;
-			this.data.items = [];
+			this.inMemoryData = this.data.items.slice(0);//clone the items
 		}
 		var items = this.getFilteredItems(this.inMemoryData, this.filteredProperties, this.filterString, this.filteredObjects, this.filterSpecificProperty);
 		this.suspendEvents(false);
@@ -83,9 +82,7 @@ Ext.define('Sbi.widgets.store.InMemoryFilteredStore', {
 			items = Ext.Array.sort(items, sorterFn);
 		}
 		items = this.getPageItems(this.start, this.limit, items);
-		if (this.data.items.length > 0){
-			this.removeAll();
-		}
+		this.removeAll();
 		this.remoteSort = true; // this will prevent the add method to invoke the doSort method (an infinite loop will occur)
 		this.add(items);
 		this.resumeEvents();
