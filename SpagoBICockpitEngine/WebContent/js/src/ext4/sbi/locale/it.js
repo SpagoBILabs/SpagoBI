2ee57fd7dd8a71004af57846096f0b5f35144352
Giulio Gavardi <giulio.gavardi@eng.it>
2014-11-21 15:40:42 +0000

    Check Association Validity
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24135 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/locale/it.js
+++ SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/locale/it.js
@@ -218,6 +218,9 @@ Sbi.locale.ln['sbi.cockpit.association.editor.wizard.list.delete'] = 'Cancella';
 Sbi.locale.ln['sbi.cockpit.association.editor.wizard.list.delete.tooltip'] = 'Cancella Associazione';
 Sbi.locale.ln['sbi.cockpit.association.editor.wizard.list.autodetect'] = 'Rilevamento automatico';
 Sbi.locale.ln['sbi.cockpit.association.editor.wizard.list.autodetect.tooltip'] = 'Rilevamento automatico delle Associazioni';
+Sbi.locale.ln['sbi.data.editor.association.AssociationEditor.notValidAssociation'] ='L\'associazione definita ritorna troppi record per essere significativa';
+Sbi.locale.ln['sbi.data.editor.association.AssociationEditor.errorCheckingAssociation'] ='Errore durante il controllo dell\'associazione';
+
 
 Sbi.locale.ln['sbi.cockpit.association.editor.msg.modify'] = 'Per procedere con l\aggiornamento è necessario selezionare l\'associazione da modificare dalla lista!';
 Sbi.locale.ln['sbi.cockpit.association.editor.msg.duplicate'] = 'Associazione gia esistente!';
