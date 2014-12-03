761a09024e24a02ae7deb67d830a71d0477937fb
giofeder <giofeder@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-12-03 17:17:39 +0000

    Add user dataset persistence and filter preview (my data)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24275 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datareader/FileDatasetCsvDataReader.java
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datareader/FileDatasetCsvDataReader.java
@@ -1,28 +1,10 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.tools.dataset.common.datareader;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.util.Map;
-import java.util.StringTokenizer;
-
-import org.apache.log4j.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.supercsv.cellprocessor.ift.CellProcessor;
-import org.supercsv.io.CsvMapReader;
-import org.supercsv.io.ICsvMapReader;
-import org.supercsv.prefs.CsvPreference;
-
-import it.eng.spagobi.container.ObjectUtils;
 import it.eng.spagobi.tools.dataset.common.datastore.DataStore;
 import it.eng.spagobi.tools.dataset.common.datastore.Field;
 import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
@@ -32,13 +14,25 @@ import it.eng.spagobi.tools.dataset.common.datastore.Record;
 import it.eng.spagobi.tools.dataset.common.metadata.FieldMetadata;
 import it.eng.spagobi.tools.dataset.common.metadata.MetaData;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvMapReader;
+import org.supercsv.io.ICsvMapReader;
+import org.supercsv.prefs.CsvPreference;
+
 /**
- * @author Marco Cortella
- *         marco.cortella@eng.it
+ * @author Marco Cortella marco.cortella@eng.it
  */
 public class FileDatasetCsvDataReader extends AbstractDataReader {
-	
-	
+
 	private static transient Logger logger = Logger.getLogger(FileDatasetCsvDataReader.class);
 	public static final String CSV_FILE_DELIMITER_CHARACTER = "csvDelimiter";
 	public static final String CSV_FILE_QUOTE_CHARACTER = "csvQuote";
@@ -50,28 +44,28 @@ public class FileDatasetCsvDataReader extends AbstractDataReader {
 	public FileDatasetCsvDataReader(JSONObject jsonConf) {
 		super();
 
-		//Get File Dataset Configuration Options
-		if (jsonConf != null){
+		// Get File Dataset Configuration Options
+		if (jsonConf != null) {
 			try {
-				if (jsonConf.get(CSV_FILE_DELIMITER_CHARACTER) != null){
+				if (jsonConf.get(CSV_FILE_DELIMITER_CHARACTER) != null) {
 					csvDelimiter = jsonConf.get(CSV_FILE_DELIMITER_CHARACTER).toString();
 				} else {
-					csvDelimiter="";
+					csvDelimiter = "";
 				}
-				
-				if (jsonConf.get(CSV_FILE_QUOTE_CHARACTER) != null){
+
+				if (jsonConf.get(CSV_FILE_QUOTE_CHARACTER) != null) {
 					csvQuote = jsonConf.get(CSV_FILE_QUOTE_CHARACTER).toString();
 				} else {
-					csvQuote="";
+					csvQuote = "";
 				}
-				if (jsonConf.has(CSV_FILE_ENCODING)){
-					if (jsonConf.get(CSV_FILE_ENCODING) != null){
+				if (jsonConf.has(CSV_FILE_ENCODING)) {
+					if (jsonConf.get(CSV_FILE_ENCODING) != null) {
 						csvEncoding = jsonConf.get(CSV_FILE_ENCODING).toString();
 					} else {
-						csvEncoding="windows-1252"; //default
+						csvEncoding = "windows-1252"; // default
 					}
 				} else {
-					csvEncoding="windows-1252"; //default
+					csvEncoding = "windows-1252"; // default
 				}
 
 			} catch (JSONException e) {
@@ -84,102 +78,110 @@ public class FileDatasetCsvDataReader extends AbstractDataReader {
 
 		}
 
-		
 	}
 
-	public IDataStore read( Object data ) {
+	public IDataStore read(Object data) {
 		DataStore dataStore = null;
-		
+
 		InputStream inputDataStream;
 
-		
 		logger.debug("IN");
-		
-		inputDataStream = (InputStream)data;
-		
-		try {				
+
+		inputDataStream = (InputStream) data;
+
+		try {
 			dataStore = readWithCsvMapReader(inputDataStream);
 
-				
 		} catch (FileNotFoundException e) {
-			logger.error("Error reading CSV File: "+e);
+			logger.error("Error reading CSV File: " + e);
 			e.printStackTrace();
-		}catch (IOException e) {
-			logger.error("Error reading CSV File: "+e);
+		} catch (IOException e) {
+			logger.error("Error reading CSV File: " + e);
 			e.printStackTrace();
 		} catch (Exception e) {
-			logger.error("Error reading CSV File: "+e);
+			logger.error("Error reading CSV File: " + e);
 			e.printStackTrace();
 		}
-		
+
 		return dataStore;
-    }
-	
- 	private DataStore readWithCsvMapReader( InputStream inputDataStream ) throws Exception {
- 				
- 		InputStreamReader inputStreamReader = new InputStreamReader(inputDataStream,csvEncoding);
-    	DataStore dataStore = null;
+	}
+
+	private DataStore readWithCsvMapReader(InputStream inputDataStream) throws Exception {
+
+		InputStreamReader inputStreamReader = new InputStreamReader(inputDataStream, csvEncoding);
+		DataStore dataStore = null;
 		MetaData dataStoreMeta;
-    	dataStore = new DataStore();
+		dataStore = new DataStore();
 		dataStoreMeta = new MetaData();
 		dataStore.setMetaData(dataStoreMeta);
+		int maxResults = this.getMaxResults();
+		boolean checkMaxResults = false;
+		if ((maxResults > 0)) {
+			checkMaxResults = true;
+		}
 
-	        
-	        ICsvMapReader mapReader = null;
-	        
-	        try {
-	        	
-	        	 CsvPreference customPreference = new CsvPreference.Builder(csvQuote.charAt(0), csvDelimiter.charAt(0), "\n").build(); 
-	             // mapReader = new CsvMapReader(inputStreamReader, CsvPreference.STANDARD_PREFERENCE);
-	        	 	mapReader = new CsvMapReader(inputStreamReader, customPreference);  
-	                // the header columns are used as the keys to the Map
-	                final String[] header = mapReader.getHeader(true);
-	                
-	                int columnsNumber = mapReader.length();
-	                
-                	//Create Datastore Metadata with header file
-                    for (int i= 0; i<header.length;i++){
-                    	FieldMetadata fieldMeta = new FieldMetadata();
-                    	fieldMeta.setName(header[i]);
-                    	fieldMeta.setType(String.class);
-                    	dataStoreMeta.addFiedMeta(fieldMeta);
-	                }
+		ICsvMapReader mapReader = null;
 
-                	
-	                
-	                
-	                final CellProcessor[] processors = new CellProcessor[columnsNumber];
-	                for (int i= 0; i<processors.length;i++){
-	                	processors[i] = null;
-	                }
-	                
-	                Map<String, Object> contentsMap;
-	                while( (contentsMap = mapReader.read(header, processors)) != null ) {
-	                	//Create Datastore data 
+		try {
 
-	                	IRecord record = new Record(dataStore);
+			CsvPreference customPreference = new CsvPreference.Builder(csvQuote.charAt(0), csvDelimiter.charAt(0), "\n").build();
+			// mapReader = new CsvMapReader(inputStreamReader, CsvPreference.STANDARD_PREFERENCE);
+			mapReader = new CsvMapReader(inputStreamReader, customPreference);
+			// the header columns are used as the keys to the Map
+			final String[] header = mapReader.getHeader(true);
 
-	                        for (int i= 0; i<header.length;i++){
-	                        	 logger.debug(header[i]+" = "+contentsMap.get(header[i])); 
-	                        	 IField field = null;
-	                        	 if (contentsMap.get(header[i]) == null){
-	                        		 field = new Field("");
-	                        	 } else {
-	                        		 field = new Field(contentsMap.get(header[i]));
-	                        	 }
-	 							 record.appendField(field);
-	    	                }
-	                        dataStore.appendRecord(record);	
-	                        
-	                }
-	                
-	        }
-	        finally {
-	                if( mapReader != null ) {
-	                        mapReader.close();
-	                }
-	        }
-	        return dataStore;
+			int columnsNumber = mapReader.length();
+
+			// Create Datastore Metadata with header file
+			for (int i = 0; i < header.length; i++) {
+				FieldMetadata fieldMeta = new FieldMetadata();
+				fieldMeta.setName(header[i]);
+				fieldMeta.setType(String.class);
+				dataStoreMeta.addFiedMeta(fieldMeta);
+			}
+
+			final CellProcessor[] processors = new CellProcessor[columnsNumber];
+			for (int i = 0; i < processors.length; i++) {
+				processors[i] = null;
+			}
+
+			Map<String, Object> contentsMap;
+
+			int rowFetched = 0;
+			while ((contentsMap = mapReader.read(header, processors)) != null) {
+				// check if there is a limit for the rows to fetch in preview
+				if (checkMaxResults) {
+					if (rowFetched >= maxResults) {
+						break;
+					}
+				}
+
+				// Create Datastore data
+
+				IRecord record = new Record(dataStore);
+
+				for (int i = 0; i < header.length; i++) {
+					logger.debug(header[i] + " = " + contentsMap.get(header[i]));
+					IField field = null;
+					if (contentsMap.get(header[i]) == null) {
+						field = new Field("");
+					} else {
+						field = new Field(contentsMap.get(header[i]));
+					}
+					record.appendField(field);
+				}
+				dataStore.appendRecord(record);
+
+				rowFetched++;
+
+			}
+
+		} finally {
+			if (mapReader != null) {
+				mapReader.close();
+			}
+		}
+		return dataStore;
 	}
 
 }
