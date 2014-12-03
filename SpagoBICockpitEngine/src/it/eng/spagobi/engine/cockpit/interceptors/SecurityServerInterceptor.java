9c9adf43a3261bcd6263f05ba7a401d661223ff0
Davide Zerbetto <davide.zerbetto@eng.it>
2014-12-03 15:04:54 +0000

    Moved interceptors into a dedicated package
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24251 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBICockpitEngine/src/it/eng/spagobi/engine/cockpit/interceptors/SecurityServerInterceptor.java
+++ SpagoBICockpitEngine/src/it/eng/spagobi/engine/cockpit/interceptors/SecurityServerInterceptor.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-package it.eng.spagobi.engine.cockpit.api;
+package it.eng.spagobi.engine.cockpit.interceptors;
 
 import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.commons.bo.UserProfile;
@@ -11,6 +11,8 @@ import it.eng.spagobi.commons.utilities.GeneralUtilities;
 import it.eng.spagobi.commons.utilities.StringUtilities;
 import it.eng.spagobi.commons.utilities.UserUtilities;
 import it.eng.spagobi.engine.cockpit.CockpitEngineRuntimeException;
+import it.eng.spagobi.engine.cockpit.api.ExceptionUtilities;
+import it.eng.spagobi.engine.cockpit.api.SecurityServiceSupplierFactory;
 import it.eng.spagobi.security.ExternalServiceController;
 import it.eng.spagobi.services.common.SsoServiceFactory;
 import it.eng.spagobi.services.common.SsoServiceInterface;
