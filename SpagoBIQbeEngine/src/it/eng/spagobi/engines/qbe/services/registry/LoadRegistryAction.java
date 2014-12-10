509e26ad35e478196801392d11c23133795adee2
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-12-10 13:47:44 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24311 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIQbeEngine/src/it/eng/spagobi/engines/qbe/services/registry/LoadRegistryAction.java
+++ SpagoBIQbeEngine/src/it/eng/spagobi/engines/qbe/services/registry/LoadRegistryAction.java
@@ -205,7 +205,7 @@ public class LoadRegistryAction extends ExecuteQueryAction {
 			BigDecimal currValB = new BigDecimal(currValF.doubleValue());
 			BigDecimal prevValB = new BigDecimal(prevValF.doubleValue());
 			BigDecimal newValB = prevValB.add(currValB);
-			newValB.setScale(2, RoundingMode.CEILING);
+			newValB = newValB.setScale(2, RoundingMode.CEILING);
 			Float result = new Float(newValB.floatValue());
 			resultToReturn = result;
 		} else if (currentValue instanceof Double) {
@@ -215,14 +215,14 @@ public class LoadRegistryAction extends ExecuteQueryAction {
 			BigDecimal currValB = new BigDecimal(currValD.doubleValue());
 			BigDecimal prevValB = new BigDecimal(prevValD.doubleValue());
 			BigDecimal newValB = prevValB.add(currValB);
-			newValB.setScale(2, RoundingMode.CEILING);
+			newValB = newValB.setScale(2, RoundingMode.CEILING);
 			Double result = new Double(newValB.doubleValue());
 			resultToReturn = result;
 		} else if (currentValue instanceof BigDecimal) {
 			BigDecimal currVal = (BigDecimal) currentValue;
 			BigDecimal prevVal = (BigDecimal) previousValue;
 			BigDecimal result = prevVal.add(currVal);
-			result.setScale(2, RoundingMode.CEILING);
+			result = result.setScale(2, RoundingMode.CEILING);
 			resultToReturn = result;
 		} else {
 			logger.error("Error in measure type: trying to sum a value that is not a number");
