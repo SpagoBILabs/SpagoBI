c19cb6d04b1e34e3050015e2d2339ad18f2d66f2
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-12-15 15:56:02 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24333 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIDatabaseScripts/oracle/oracle-dbupgradescript-5.0_to_5.1/ORACLE_upgradescript_5.0_to_5.1.sql
+++ SpagoBIDatabaseScripts/oracle/oracle-dbupgradescript-5.0_to_5.1/ORACLE_upgradescript_5.0_to_5.1.sql
@@ -1,17 +1,15 @@
-RENAME COLUMN SBI_OBJ_METADATA.DESCRIPTION TO DESCR;
 
+ALTER TABLE SBI_OBJ_METADATA RENAME COLUMN DESCRIPTION TO DESCR;
 
-INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN, SBI_VERSION_IN) VALUES 
-((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'), 
-'SPAGOBI.SOCIAL_ANALYSIS_URL', 'SPAGOBI SOCIAL ANALYSIS URL', 'SPAGOBI SOCIAL ANALYSIS URL', true, '/SpagoBISocialAnalysis/restful-services/start',
-(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SOCIAL_CONFIGURATION', 'server', current_timestamp, '5.1');
+INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN, SBI_VERSION_IN) 
+VALUES ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'), 'SPAGOBI.SOCIAL_ANALYSIS_URL', 'SPAGOBI SOCIAL ANALYSIS URL', 'SPAGOBI SOCIAL ANALYSIS URL', 1, '/SpagoBISocialAnalysis/restful-services/start',(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SOCIAL_CONFIGURATION', 'server', SYSDATE, '5.1');
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
 commit;
 
 INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN, SBI_VERSION_IN) VALUES 
 ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'), 
-'SPAGOBI.SOCIAL_ANALYSIS_IS_ACTIVE', 'SPAGOBI SOCIAL ANALYSIS STATUS', 'SPAGOBI SOCIAL ANALYSIS STATUS', true, 'true',
-(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SOCIAL_CONFIGURATION', 'server', current_timestamp, '5.1');
+'SPAGOBI.SOCIAL_ANALYSIS_IS_ACTIVE', 'SPAGOBI SOCIAL ANALYSIS STATUS', 'SPAGOBI SOCIAL ANALYSIS STATUS', 1, 'true',
+(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'SOCIAL_CONFIGURATION', 'server', SYSDATE, '5.1');
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
 commit;
 
@@ -19,7 +17,7 @@ INSERT INTO SBI_AUTHORIZATIONS
 (ID, NAME, USER_IN, TIME_IN)
 values ((SELECT NEXT_VAL FROM hibernate_sequences WHERE SEQUENCE_NAME='SBI_AUTHORIZATIONS'),
 'HIERARCHIES_MANAGEMENT',
-'server', current_timestamp) ;
+'server', SYSDATE) ;
 commit;
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_AUTHORIZATIONS';
 commit; 
@@ -27,7 +25,7 @@ commit;
 INSERT INTO SBI_AUTHORIZATIONS
 (ID, NAME,  USER_IN, TIME_IN) 
 values ((SELECT NEXT_VAL FROM hibernate_sequences WHERE SEQUENCE_NAME='SBI_AUTHORIZATIONS'), 
-'VIEW_SOCIAL_ANALYSIS',  'server', current_timestamp) ;
+'VIEW_SOCIAL_ANALYSIS',  'server', SYSDATE) ;
 commit;
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_AUTHORIZATIONS';
 commit;
@@ -36,7 +34,7 @@ INSERT INTO SBI_AUTHORIZATIONS
 (ID, NAME, USER_IN, TIME_IN)
 values ((SELECT NEXT_VAL FROM hibernate_sequences WHERE SEQUENCE_NAME='SBI_AUTHORIZATIONS'),
 'ENABLE_DATASET_PERSISTENCE',
-'server', current_timestamp) ;
+'server', SYSDATE) ;
 commit;
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_AUTHORIZATIONS';
 commit; 
@@ -44,14 +42,14 @@ commit;
 
 INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN) VALUES 
 ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'), 
-'SPAGOBI.DATASET.PREVIEW_ROWS', 'Number of rows to show for dataset preview', 'Number of rows to show for dataset preview', true, '1000',
-(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'NUM' AND DOMAIN_CD = 'PAR_TYPE'), 'GENERIC_CONFIGURATION', 'biadmin', current_timestamp);
+'SPAGOBI.DATASET.PREVIEW_ROWS', 'Number of rows to show for dataset preview', 'Number of rows to show for dataset preview', 1, '1000',
+(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'NUM' AND DOMAIN_CD = 'PAR_TYPE'), 'GENERIC_CONFIGURATION', 'biadmin', SYSDATE);
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
 commit;
 
 INSERT INTO SBI_CONFIG ( ID, LABEL, NAME, DESCRIPTION, IS_ACTIVE, VALUE_CHECK, VALUE_TYPE_ID, CATEGORY, USER_IN, TIME_IN) VALUES
 ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_CONFIG'),
-'SPAGOBI.DATASET.PERSIST.TABLE_PREFIX', 'Table name prefix for dataset persistence', 'Table name prefix for dataset persistence', true, 'D_',
-(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'GENERIC_CONFIGURATION', 'biadmin', current_timestamp);
+'SPAGOBI.DATASET.PERSIST.TABLE_PREFIX', 'Table name prefix for dataset persistence', 'Table name prefix for dataset persistence', 1, 'D_',
+(select VALUE_ID from SBI_DOMAINS where VALUE_CD = 'STRING' AND DOMAIN_CD = 'PAR_TYPE'), 'GENERIC_CONFIGURATION', 'biadmin', SYSDATE);
 update hibernate_sequences set next_val = next_val+1 where sequence_name = 'SBI_CONFIG';
 commit;
