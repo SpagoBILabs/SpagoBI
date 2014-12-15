9573b3c0392cc85d7ce7ff1c0165ff073e0645ed
antonella giachino <antonella.giachino@eng.it>
2014-12-15 08:30:06 +0000

    Solved Issue SpagoBI-1988: "As a WhatIf User I want filter dimension values through analytical driver"
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24321 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/schema/SpagoBIFilterDynamicSchemaProcessor.java
+++ SpagoBIWhatIfEngine/src/it/eng/spagobi/engines/whatif/schema/SpagoBIFilterDynamicSchemaProcessor.java
@@ -19,14 +19,10 @@ import sun.misc.BASE64Decoder;
 /**
  * @author Davide Zerbetto (davide.zerbetto@eng.it)
  * 
- *         DATE CONTRIBUTOR/DEVELOPER NOTE 26/03/2013 Davide Zerbetto
- *         (davide.zerbetto@eng.it) SpagoBIFilterDynamicSchemaProcessor now
- *         extends Andrea Fantappie (andrea.fantappie@eng.it)
- *         LocalizingDynamicSchemaProcessor (instead of
- *         FilterDynamicSchemaProcessor) to support i18n
+ *         DATE CONTRIBUTOR/DEVELOPER NOTE 26/03/2013 Davide Zerbetto (davide.zerbetto@eng.it) SpagoBIFilterDynamicSchemaProcessor now extends Andrea Fantappie
+ *         (andrea.fantappie@eng.it) LocalizingDynamicSchemaProcessor (instead of FilterDynamicSchemaProcessor) to support i18n
  */
-public class SpagoBIFilterDynamicSchemaProcessor extends LocalizingDynamicSchemaProcessor
-{
+public class SpagoBIFilterDynamicSchemaProcessor extends LocalizingDynamicSchemaProcessor {
 	private static Logger logger = Logger.getLogger(SpagoBIFilterDynamicSchemaProcessor.class);
 
 	private static final BASE64Decoder DECODER = new BASE64Decoder();
@@ -38,29 +34,33 @@ public class SpagoBIFilterDynamicSchemaProcessor extends LocalizingDynamicSchema
 		logger.debug("IN");
 		String originalSchema = super.filter(schemaUrl, connectInfo, stream);
 		String modifiedSchema = originalSchema;
-		// search for profile attributes to substitute in schema definition,
-		// identified by $P{attribute_name}
-		indexProgression = Integer.valueOf(0);
-		String att = findProfileAttributeInSchema(originalSchema);
-		while (att != null) {
-			// if value is null I put null, if instead there is no the attribute
-			// name in connectInfo I don't substitute
-			if (connectInfo.get(att) != null) {
-				String attrValueBase64 = connectInfo.get(att);
-				logger.debug("Attribute value in Base64 encoding is " + attrValueBase64);
-				String value = null;
-				try {
-					value = new String(DECODER.decodeBuffer(attrValueBase64), "UTF-8");
-				} catch (UnsupportedEncodingException e) {
-					logger.error("UTF-8 encoding not supported!!!!!", e);
-					value = new String(DECODER.decodeBuffer(attrValueBase64));
-				}
-				logger.debug("change attribute " + att + " with  [" + value + "]");
-
-				modifiedSchema = modifiedSchema.replaceAll("\\$\\{" + att + "\\}", value);
-			}
-			att = findProfileAttributeInSchema(modifiedSchema);
-		}
+		// // search for profile attributes to substitute in schema definition,
+		// // identified by $P{attribute_name}
+		// indexProgression = Integer.valueOf(0);
+		// String att = findProfileAttributeInSchema(originalSchema);
+		// while (att != null) {
+		// // if value is null I put null, if instead there is no the attribute
+		// // name in connectInfo I don't substitute
+		// if (connectInfo.get(att) != null) {
+		// String attrValueBase64 = connectInfo.get(att);
+		// logger.debug("Attribute value in Base64 encoding is " + attrValueBase64);
+		// String value = null;
+		// try {
+		// value = new String(DECODER.decodeBuffer(attrValueBase64), "UTF-8");
+		// } catch (UnsupportedEncodingException e) {
+		// logger.error("UTF-8 encoding not supported!!!!!", e);
+		// value = new String(DECODER.decodeBuffer(attrValueBase64));
+		// }
+		// logger.debug("change attribute " + att + " with  [" + value + "]");
+		//
+		// modifiedSchema = modifiedSchema.replaceAll("\\$\\{" + att + "\\}", value);
+		// }
+		// att = findProfileAttributeInSchema(modifiedSchema);
+		// }
+		// Substitutes profile attributes values
+		modifiedSchema = substituteProfileValues(originalSchema, connectInfo);
+		// Substitutes parameters values (pass modifiedSchema for don't loose profile attribute values)
+		modifiedSchema = substituteParameterValues(modifiedSchema, connectInfo);
 		logger.debug("OUT: modified schema is:\n" + modifiedSchema);
 		return modifiedSchema;
 	}
@@ -86,8 +86,7 @@ public class SpagoBIFilterDynamicSchemaProcessor extends LocalizingDynamicSchema
 	 * 
 	 * @param schema
 	 * @param indexProgression
-	 *            . keeps track of the last found index to go always ahead in
-	 *            reading
+	 *            . keeps track of the last found index to go always ahead in reading
 	 * @return
 	 */
 	public String findProfileAttributeInSchema(String schema) {
@@ -103,4 +102,92 @@ public class SpagoBIFilterDynamicSchemaProcessor extends LocalizingDynamicSchema
 		return toReturn;
 	}
 
+	/**
+	 * return the first parameter in schema
+	 * 
+	 * @param schema
+	 * @param indexProgression
+	 *            . keeps track of the last found index to go always ahead in reading
+	 * @return
+	 */
+	public String findParameterInSchema(String schema) {
+		logger.debug("IN");
+		String toReturn = null;
+		int index = schema.indexOf("$P{", indexProgression);
+		if (index != -1) {
+			int indexEnd = schema.indexOf("}", index);
+			toReturn = schema.substring(index + 3, indexEnd);
+			indexProgression = new Integer(indexEnd);
+		}
+		logger.debug("OUT");
+		return toReturn;
+	}
+
+	/**
+	 * Search for profile attributes to substitute in schema definition, identified by $P{attribute_name}
+	 * 
+	 * @param originalSchema
+	 * @param connectInfo
+	 * @return modifiedSchema
+	 * @throws Exception
+	 */
+	private String substituteProfileValues(String originalSchema, Util.PropertyList connectInfo) throws Exception {
+		String modifiedSchema = originalSchema;
+		indexProgression = Integer.valueOf(0);
+		String att = findProfileAttributeInSchema(originalSchema);
+		while (att != null) {
+			// if value is null I put null, if instead there is no the attribute
+			// name in connectInfo I don't substitute
+			if (connectInfo.get(att) != null) {
+				String attrValueBase64 = connectInfo.get(att);
+				logger.debug("Attribute value in Base64 encoding is " + attrValueBase64);
+				String value = null;
+				try {
+					value = new String(DECODER.decodeBuffer(attrValueBase64), "UTF-8");
+				} catch (UnsupportedEncodingException e) {
+					logger.error("UTF-8 encoding not supported!!!!!", e);
+					value = new String(DECODER.decodeBuffer(attrValueBase64));
+				}
+				logger.debug("change attribute " + att + " with  [" + value + "]");
+
+				modifiedSchema = modifiedSchema.replaceAll("\\$\\{" + att + "\\}", value);
+			}
+			att = findProfileAttributeInSchema(modifiedSchema);
+		}
+		return modifiedSchema;
+	}
+
+	/**
+	 * Search for parameters to substitute in schema definition, identified by $P{attribute_name}
+	 * 
+	 * @param originalSchema
+	 * @param connectInfo
+	 * @return modifiedSchema
+	 * @throws Exception
+	 */
+	private String substituteParameterValues(String originalSchema, Util.PropertyList connectInfo) throws Exception {
+		String modifiedSchema = originalSchema;
+		indexProgression = Integer.valueOf(0);
+		String att = findParameterInSchema(originalSchema);
+		while (att != null) {
+			// if value is null I put null, if instead there is no the attribute
+			// name in connectInfo I don't substitute
+			if (connectInfo.get(att) != null) {
+				String attrValueBase64 = connectInfo.get(att);
+				logger.debug("Parameter value in Base64 encoding is " + attrValueBase64);
+				String value = null;
+				try {
+					value = new String(DECODER.decodeBuffer(attrValueBase64), "UTF-8");
+				} catch (UnsupportedEncodingException e) {
+					logger.error("UTF-8 encoding not supported!!!!!", e);
+					value = new String(DECODER.decodeBuffer(attrValueBase64));
+				}
+				logger.debug("change attribute " + att + " with  [" + value + "]");
+
+				modifiedSchema = modifiedSchema.replaceAll("\\$P\\{" + att + "\\}", value);
+			}
+			att = findParameterInSchema(modifiedSchema);
+		}
+		return modifiedSchema;
+	}
 }
