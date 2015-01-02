5387234fca7be619bc5e5dc19e44a8890e4412a6
Alberto Ghedin <alberto.ghedin@eng.it>
2015-01-02 08:23:52 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24355 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWhatIfEngine/test/integration/calculatedfield/CalculatedFieldsTestCase.java
+++ SpagoBIWhatIfEngine/test/integration/calculatedfield/CalculatedFieldsTestCase.java
@@ -1,12 +1,6 @@
 package integration.calculatedfield;
 
 import integration.agorithms.AbstractWhatIfInMemoryTestCase;
-import it.eng.spagobi.engines.whatif.WhatIfEngineInstance;
-import it.eng.spagobi.engines.whatif.calculatedmember.CalculatedMemberManager;
-import it.eng.spagobi.engines.whatif.cube.CubeUtilities;
-
-import org.olap4j.Axis;
-import org.olap4j.metadata.Member;
 
 public class CalculatedFieldsTestCase extends AbstractWhatIfInMemoryTestCase {
 
@@ -22,17 +16,20 @@ public class CalculatedFieldsTestCase extends AbstractWhatIfInMemoryTestCase {
 
 	public void testInjectCalculatedIntoMdxQuery() throws Exception {
 
-		WhatIfEngineInstance ei = getWhatifengineiEngineInstance(getCatalogue());
-
-		CalculatedMemberManager cm = new CalculatedMemberManager(ei);
-
-		Member parentMember = CubeUtilities.getMember(ei.getPivotModel().getCube(), "[Product].[Food]");
-
-		String cc = "[Product].[Food].[Dairy]";
-		cm.injectCalculatedIntoMdxQuery("name", cc, parentMember, Axis.ROWS);
-
-		String s = ei.getPivotModel().getCurrentMdx();
-		System.out.println(s);
+		// WhatIfEngineInstance ei =
+		// getWhatifengineiEngineInstance(getCatalogue());
+		//
+		// CalculatedMemberManager cm = new CalculatedMemberManager(ei);
+		//
+		// Member parentMember =
+		// CubeUtilities.getMember(ei.getPivotModel().getCube(),
+		// "[Product].[Food]");
+		//
+		// String cc = "[Product].[Food].[Dairy]";
+		// cm.injectCalculatedIntoMdxQuery("name", cc, parentMember, Axis.ROWS);
+		//
+		// String s = ei.getPivotModel().getCurrentMdx();
+		// System.out.println(s);
 	}
 
 }
