a4ebcbb7fb6ab2c3dd62bebce3f0f495966d7b63
giofeder <giofeder@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-02-24 11:24:58 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24549 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ ChefCookbooks/mysql-chef_gem/metadata.rb
@@ -0,0 +1,17 @@
+name 'mysql-chef_gem'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@getchef.com'
+license 'Apache 2.0'
+description 'Provides the mysql_chef_gem resource'
+
+version '1.0.0'
+
+supports 'amazon'
+supports 'redhat'
+supports 'centos'
+supports 'scientific'
+supports 'fedora'
+supports 'debian'
+supports 'ubuntu'
+
+depends 'build-essential'
