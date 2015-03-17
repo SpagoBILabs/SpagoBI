09d0e3e7d2f2e5826195a43ed4a02c15ccd4e012
lazarkostic <lazarkostic@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-03-17 14:50:59 +0000

    Analytical Driver User Interface Update
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24590 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/commons/serializer/JSONSerializer.java
+++ SpagoBIProject/src/it/eng/spagobi/commons/serializer/JSONSerializer.java
@@ -1,7 +1,7 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.commons.serializer;
 
@@ -13,6 +13,9 @@ import it.eng.spagobi.analiticalmodel.document.bo.SubObject;
 import it.eng.spagobi.analiticalmodel.document.bo.Viewpoint;
 import it.eng.spagobi.analiticalmodel.execution.service.GetParametersForExecutionAction;
 import it.eng.spagobi.analiticalmodel.functionalitytree.bo.LowFunctionality;
+import it.eng.spagobi.behaviouralmodel.analyticaldriver.bo.Parameter;
+import it.eng.spagobi.behaviouralmodel.analyticaldriver.bo.ParameterUse;
+import it.eng.spagobi.behaviouralmodel.lov.bo.ModalitiesValue;
 import it.eng.spagobi.commons.bo.Config;
 import it.eng.spagobi.commons.bo.Domain;
 import it.eng.spagobi.commons.bo.Role;
@@ -74,107 +77,109 @@ import org.json.JSONArray;
  * @author Andrea Gioia (andrea.gioia@eng.it)
  */
 public class JSONSerializer implements Serializer {
-	
+
 	Map<Class, Serializer> mappings;
-	
+
 	public JSONSerializer() {
 		mappings = new HashMap();
-		mappings.put( DataSource.class, new DataSourceJSONSerializer() );	
-		mappings.put( DataSourceModel.class, new DataSourceJSONSerializer() );	
-		mappings.put( Domain.class, new DomainJSONSerializer() );
-		mappings.put( Config.class, new ConfigJSONSerializer() );
-		mappings.put( Engine.class, new EngineJSONSerializer() );
-		mappings.put( Role.class, new RoleJSONSerializer() );
-		mappings.put( BIObject.class, new DocumentsJSONSerializer() );
-		mappings.put( LowFunctionality.class, new FoldersJSONSerializer() );
-		mappings.put( SubObject.class, new SubObjectsJSONSerializer() );
-		mappings.put( Viewpoint.class, new ViewpointJSONSerializer() );
-		mappings.put( Snapshot.class, new SnapshotJSONSerializer() );
-		mappings.put( RememberMe.class, new FavouritesJSONSerializer() );
-		mappings.put( DataStore.class, new DataStoreJSONSerializer() );
-		mappings.put( ObjNote.class, new ObjectNotesJSONSerializer() );
-		mappings.put( DocumentMetadataProperty.class, new MetadataJSONSerializer());	
-		mappings.put( ObjMetadata.class, new ShortMetadataJSONSerializer());	
-		mappings.put( SbiUser.class, new SbiUserJSONSerializer());
-		mappings.put( UserBO.class, new UserJSONSerializer());
-		mappings.put( SbiAttribute.class, new AttributeJSONSerializer());
-		mappings.put( SbiAlarm.class, new AlarmJSONSerializer());
-		mappings.put( SbiAlarmContact.class, new AlarmContactJSONSerializer());
-		mappings.put( ThresholdValue.class, new ThresholdValueJSONSerializer());
-		mappings.put( SbiDataSet.class, new SbiDataSetJSONSerializer());		
-		mappings.put( VersionedDataSet.class, new DataSetJSONSerializer());
-		mappings.put( CustomDataSet.class, new DataSetJSONSerializer());
-		mappings.put( JDBCDataSet.class, new DataSetJSONSerializer());
-		mappings.put( JDBCHBaseDataSet.class, new DataSetJSONSerializer());
-		mappings.put( JDBCHiveDataSet.class, new DataSetJSONSerializer());
-		mappings.put( ScriptDataSet.class, new DataSetJSONSerializer());
-		mappings.put( WebServiceDataSet.class, new DataSetJSONSerializer());
-		mappings.put( FileDataSet.class, new DataSetJSONSerializer());
+		mappings.put(DataSource.class, new DataSourceJSONSerializer());
+		mappings.put(DataSourceModel.class, new DataSourceJSONSerializer());
+		mappings.put(Domain.class, new DomainJSONSerializer());
+		mappings.put(Config.class, new ConfigJSONSerializer());
+		mappings.put(Engine.class, new EngineJSONSerializer());
+		mappings.put(Role.class, new RoleJSONSerializer());
+		mappings.put(BIObject.class, new DocumentsJSONSerializer());
+		mappings.put(LowFunctionality.class, new FoldersJSONSerializer());
+		mappings.put(SubObject.class, new SubObjectsJSONSerializer());
+		mappings.put(Viewpoint.class, new ViewpointJSONSerializer());
+		mappings.put(Snapshot.class, new SnapshotJSONSerializer());
+		mappings.put(RememberMe.class, new FavouritesJSONSerializer());
+		mappings.put(DataStore.class, new DataStoreJSONSerializer());
+		mappings.put(ObjNote.class, new ObjectNotesJSONSerializer());
+		mappings.put(DocumentMetadataProperty.class, new MetadataJSONSerializer());
+		mappings.put(ObjMetadata.class, new ShortMetadataJSONSerializer());
+		mappings.put(SbiUser.class, new SbiUserJSONSerializer());
+		mappings.put(UserBO.class, new UserJSONSerializer());
+		mappings.put(SbiAttribute.class, new AttributeJSONSerializer());
+		mappings.put(SbiAlarm.class, new AlarmJSONSerializer());
+		mappings.put(SbiAlarmContact.class, new AlarmContactJSONSerializer());
+		mappings.put(ThresholdValue.class, new ThresholdValueJSONSerializer());
+		mappings.put(SbiDataSet.class, new SbiDataSetJSONSerializer());
+		mappings.put(VersionedDataSet.class, new DataSetJSONSerializer());
+		mappings.put(CustomDataSet.class, new DataSetJSONSerializer());
+		mappings.put(JDBCDataSet.class, new DataSetJSONSerializer());
+		mappings.put(JDBCHBaseDataSet.class, new DataSetJSONSerializer());
+		mappings.put(JDBCHiveDataSet.class, new DataSetJSONSerializer());
+		mappings.put(ScriptDataSet.class, new DataSetJSONSerializer());
+		mappings.put(WebServiceDataSet.class, new DataSetJSONSerializer());
+		mappings.put(FileDataSet.class, new DataSetJSONSerializer());
 
-		mappings.put( RoleMetaModelCategory.class, new RoleMetaModelCategoryJSONSerializer());
-		
-		mappings.put( Resource.class, new ResourceJSONSerializer());
-		mappings.put( Threshold.class, new ThresholdJSONSerializer());
-		mappings.put( Kpi.class, new KpiJSONSerializer());
-		mappings.put( Periodicity.class, new PeriodicityJSONSerializer());
-		
-		mappings.put( Model.class, new ModelNodeJSONSerializer());	
-		mappings.put( ModelInstance.class, new ModelInstanceNodeJSONSerializer());
-		mappings.put( ModelResourcesExtended.class, new ModelResourcesExtendedJSONSerializer());
-		mappings.put( ModelExtended.class, new ModelExtendedJSONSerializer());
-		
-		mappings.put( GetParametersForExecutionAction.ParameterForExecution.class, new ParameterForExecutionJSONSerializer() );
-		mappings.put( SbiUdp.class, new UdpJSONSerializer());
-		mappings.put( SbiUdpValue.class, new UdpValueJSONSerializer());
-		
-		mappings.put( OrganizationalUnitGrant.class, new OrganizationalUnitGrantJSONSerializer());
-		mappings.put( OrganizationalUnit.class, new OrganizationalUnitJSONSerializer());
-		mappings.put( OrganizationalUnitHierarchy.class, new OrganizationalUnitHierarchyJSONSerializer());
-		mappings.put( OrganizationalUnitNodeWithGrant.class, new OrganizationalUnitNodeWithGrantJSONSerializer());
-		
-		mappings.put( GoalNode.class, new GoalNodeJSONSerializer());
-		mappings.put( Goal.class, new GoalJSONSerializer());
-		mappings.put( SbiKpiComments.class, new SbiKpiCommentSerializer());
-		mappings.put( MetaModel.class, new MetaModelJSONSerializer());
-		mappings.put( Artifact.class, new ArtifactJSONSerializer());
-		mappings.put( Content.class, new ContentJSONSerializer());
-		mappings.put( SbiTenant.class, new TenantJSONSerializer());
-		
-		mappings.put( Job.class, new JobJSONSerializer());
-		mappings.put( Trigger.class, new TriggerJSONSerializer());
+		mappings.put(ModalitiesValue.class, new ModalitiesValueJSONSerializer());
+		mappings.put(Parameter.class, new ParametersJSONSerialize());
+		mappings.put(ParameterUse.class, new ParametersUseJSONSerialize());
 
+		mappings.put(RoleMetaModelCategory.class, new RoleMetaModelCategoryJSONSerializer());
+
+		mappings.put(Resource.class, new ResourceJSONSerializer());
+		mappings.put(Threshold.class, new ThresholdJSONSerializer());
+		mappings.put(Kpi.class, new KpiJSONSerializer());
+		mappings.put(Periodicity.class, new PeriodicityJSONSerializer());
+
+		mappings.put(Model.class, new ModelNodeJSONSerializer());
+		mappings.put(ModelInstance.class, new ModelInstanceNodeJSONSerializer());
+		mappings.put(ModelResourcesExtended.class, new ModelResourcesExtendedJSONSerializer());
+		mappings.put(ModelExtended.class, new ModelExtendedJSONSerializer());
+
+		mappings.put(GetParametersForExecutionAction.ParameterForExecution.class, new ParameterForExecutionJSONSerializer());
+		mappings.put(SbiUdp.class, new UdpJSONSerializer());
+		mappings.put(SbiUdpValue.class, new UdpValueJSONSerializer());
+
+		mappings.put(OrganizationalUnitGrant.class, new OrganizationalUnitGrantJSONSerializer());
+		mappings.put(OrganizationalUnit.class, new OrganizationalUnitJSONSerializer());
+		mappings.put(OrganizationalUnitHierarchy.class, new OrganizationalUnitHierarchyJSONSerializer());
+		mappings.put(OrganizationalUnitNodeWithGrant.class, new OrganizationalUnitNodeWithGrantJSONSerializer());
+
+		mappings.put(GoalNode.class, new GoalNodeJSONSerializer());
+		mappings.put(Goal.class, new GoalJSONSerializer());
+		mappings.put(SbiKpiComments.class, new SbiKpiCommentSerializer());
+		mappings.put(MetaModel.class, new MetaModelJSONSerializer());
+		mappings.put(Artifact.class, new ArtifactJSONSerializer());
+		mappings.put(Content.class, new ContentJSONSerializer());
+		mappings.put(SbiTenant.class, new TenantJSONSerializer());
+
+		mappings.put(Job.class, new JobJSONSerializer());
+		mappings.put(Trigger.class, new TriggerJSONSerializer());
 
 	}
 
 	public Object serialize(Object o, Locale locale) throws SerializationException {
-		Object result = null;	
-		
+		Object result = null;
+
 		try {
-			if(o instanceof Collection) {
+			if (o instanceof Collection) {
 				JSONArray r = new JSONArray();
-				Collection c = (Collection)o;
+				Collection c = (Collection) o;
 				Iterator it = c.iterator();
-				while(it.hasNext()) {
-					r.put( serialize( it.next() ,locale) );
+				while (it.hasNext()) {
+					r.put(serialize(it.next(), locale));
 				}
 				result = r;
 			} else {
-				if( !mappings.containsKey(o.getClass())) {
+				if (!mappings.containsKey(o.getClass())) {
 					throw new SerializationException("JSONSerializer is unable to serialize object of type: " + o.getClass().getName());
 				}
-				
+
 				Serializer serializer = mappings.get(o.getClass());
-				result = serializer.serialize(o,locale);
-			}			
-			
+				result = serializer.serialize(o, locale);
+			}
+
 		} catch (Throwable t) {
 			throw new SerializationException("An error occurred while serializing object: " + o, t);
 		} finally {
-			
-		}
-		
-		return result;	
-	}
 
+		}
+
+		return result;
+	}
 
 }
