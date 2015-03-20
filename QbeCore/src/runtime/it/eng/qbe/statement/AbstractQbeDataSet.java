328703e1785f722fea6f96519db7aca7d3d283f5
Giulio Gavardi <giulio.gavardi@eng.it>
2015-03-20 13:52:00 +0000

    https://spago.eng.it/jira/browse/SPAGOBI-2075 Qbe profilation using drivers and attributes
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24601 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/runtime/it/eng/qbe/statement/AbstractQbeDataSet.java
+++ QbeCore/src/runtime/it/eng/qbe/statement/AbstractQbeDataSet.java
@@ -2,7 +2,7 @@
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
- * If a copy of the MPL was  not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.qbe.statement;
 
 import it.eng.qbe.datasource.AbstractDataSource;
@@ -387,6 +387,7 @@ public abstract class AbstractQbeDataSet extends AbstractDataSet {
 
 	public void setUserProfileAttributes(Map attributes) {
 		this.userProfileAttributes = attributes;
+		getStatement().setProfileAttributes(attributes);
 
 	}
 
