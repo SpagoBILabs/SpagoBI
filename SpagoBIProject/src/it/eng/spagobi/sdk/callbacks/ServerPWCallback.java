35a9e6bbbda2e8bac0e3d949e92cd7bfeeee8335
adaniele <adaniele@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-04-01 14:58:58 +0000

    Implemented SpagoBI Studio authentication through OAuth2 with FIWARE account
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24660 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/sdk/callbacks/ServerPWCallback.java
+++ SpagoBIProject/src/it/eng/spagobi/sdk/callbacks/ServerPWCallback.java
@@ -1,7 +1,7 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.sdk.callbacks;
 
@@ -22,55 +22,55 @@ import org.apache.ws.security.WSSecurityException;
 import org.apache.ws.security.handler.WSHandlerConstants;
 
 /**
- * 
+ *
  * @author zerbetto
  *
  */
 public class ServerPWCallback implements CallbackHandler {
 
 	static private Logger logger = Logger.getLogger(ServerPWCallback.class);
-	
-	public void handle(Callback[] callbacks) throws IOException,
-			UnsupportedCallbackException {
+
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
 		logger.debug("IN");
 		for (int i = 0; i < callbacks.length; i++) {
-            if (callbacks[i] instanceof WSPasswordCallback) {
-                WSPasswordCallback pc = (WSPasswordCallback) callbacks[i];
-                String userId = pc.getIdentifier();
-                logger.debug("UserId found from request: " + userId);
-                if (pc.getUsage() == WSPasswordCallback.DECRYPT) {
-                	logger.debug("WSPasswordCallback.DECRYPT=" + WSPasswordCallback.DECRYPT);
-                	pc.setPassword("security");
-//                } else if (pc.getUsage() == WSPasswordCallback.USERNAME_TOKEN) {
-//					logger.debug("WSPasswordCallback.USERNAME_TOKEN = " + pc.getUsage() + " callback usage");
-//					// for passwords sent in digest mode we need to provide the password,
-//					// because the original one can't be un-digested from the message
-//					String password = getPassword(userId);
-//					// this will throw an exception if the passwords don't match
-//					pc.setPassword(password);
+			if (callbacks[i] instanceof WSPasswordCallback) {
+
+				WSPasswordCallback pc = (WSPasswordCallback) callbacks[i];
+				String userId = pc.getIdentifier();
+				String userUniqueIdentifier = userId;
+				logger.debug("UserId found from request: " + userId);
+				if (pc.getUsage() == WSPasswordCallback.DECRYPT) {
+					logger.debug("WSPasswordCallback.DECRYPT=" + WSPasswordCallback.DECRYPT);
+					pc.setPassword("security");
+					// } else if (pc.getUsage() == WSPasswordCallback.USERNAME_TOKEN) {
+					// logger.debug("WSPasswordCallback.USERNAME_TOKEN = " + pc.getUsage() + " callback usage");
+					// // for passwords sent in digest mode we need to provide the password,
+					// // because the original one can't be un-digested from the message
+					// String password = getPassword(userId);
+					// // this will throw an exception if the passwords don't match
+					// pc.setPassword(password);
 				} else if (pc.getUsage() == WSPasswordCallback.USERNAME_TOKEN_UNKNOWN) {
 					logger.debug("WSPasswordCallback.USERNAME_TOKEN_UNKNOWN = " + pc.getUsage() + " callback usage");
 					// for passwords sent in clear-text mode we can compare passwords directly
-	                // Get the password that was sent
-	                String password = pc.getPassword();
-	                // Now pass them to your authentication mechanism
-	                authenticate(userId, password); // throws WSSecurityException.FAILED_AUTHENTICATION on failure
+					// Get the password that was sent
+					String password = pc.getPassword();
+					// Now pass them to your authentication mechanism
+					userUniqueIdentifier = authenticate(userId, password); // throws WSSecurityException.FAILED_AUTHENTICATION on failure
 				} else {
 					logger.error("WSPasswordCallback usage [" + pc.getUsage() + "] not treated.");
-                    throw new UnsupportedCallbackException(callbacks[i], "WSPasswordCallback usage [" + pc.getUsage() + "] not treated.");
+					throw new UnsupportedCallbackException(callbacks[i], "WSPasswordCallback usage [" + pc.getUsage() + "] not treated.");
 				}
-                // Put userId into MessageContext (for services that depend on profiling)
-        		MessageContext mc = MessageContext.getCurrentContext();
-        		mc.setProperty(WSHandlerConstants.USER, userId);
-            } else {
-            	logger.error("Unrecognized Callback");
-                throw new UnsupportedCallbackException(callbacks[i],
-                        "Unrecognized Callback");
-            }
+				// Put userId into MessageContext (for services that depend on profiling)
+				MessageContext mc = MessageContext.getCurrentContext();
+				mc.setProperty(WSHandlerConstants.USER, userUniqueIdentifier);
+			} else {
+				logger.error("Unrecognized Callback");
+				throw new UnsupportedCallbackException(callbacks[i], "Unrecognized Callback");
+			}
 		}
 	}
-	
-	private void authenticate(String userId, String password) throws WSSecurityException {
+
+	private String authenticate(String userId, String password) throws WSSecurityException {
 		logger.debug("IN: userId = " + userId);
 		try {
 			ISecurityServiceSupplier supplier = SecurityServiceSupplierFactory.createISecurityServiceSupplier();
@@ -79,6 +79,8 @@ public class ServerPWCallback implements CallbackHandler {
 				logger.error("Authentication failed for user " + userId);
 				throw new WSSecurityException(WSSecurityException.FAILED_AUTHENTICATION);
 			}
+
+			return profile.getUniqueIdentifier();
 		} catch (WSSecurityException e) {
 			throw e;
 		} catch (Exception e) {
@@ -88,19 +90,19 @@ public class ServerPWCallback implements CallbackHandler {
 			logger.debug("OUT");
 		}
 	}
-	
-//	private String getPassword(String userId) {
-//		logger.debug("IN: userId = " + userId);
-//		String password = null;
-//		try {
-//			ISecurityServiceSupplier supplier = SecurityServiceSupplierFactory.createISecurityServiceSupplier();
-//			password = supplier.getPassword(userId);
-//		} catch (Throwable t) {
-//			logger.error("Error while authenticating userId = " + userId, t);
-//		} finally {
-//			logger.debug("OUT");
-//		}
-//		return password;
-//	}
-	
+
+	// private String getPassword(String userId) {
+	// logger.debug("IN: userId = " + userId);
+	// String password = null;
+	// try {
+	// ISecurityServiceSupplier supplier = SecurityServiceSupplierFactory.createISecurityServiceSupplier();
+	// password = supplier.getPassword(userId);
+	// } catch (Throwable t) {
+	// logger.error("Error while authenticating userId = " + userId, t);
+	// } finally {
+	// logger.debug("OUT");
+	// }
+	// return password;
+	// }
+
 }
