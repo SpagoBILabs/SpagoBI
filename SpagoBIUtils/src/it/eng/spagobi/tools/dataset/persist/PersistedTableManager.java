2bb242d5b984b22751327e5af1af0ab1d482bc9b
Davide Zerbetto <davide.zerbetto@eng.it>
2015-04-01 13:52:25 +0000

    Improvement: when persisting a file dataset (and also CKAN dataset), the table columns names are c_0, c_1, ... instead of using the actual columns of the dataset (that might contain special characters or that can be too long)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24656 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/tools/dataset/persist/PersistedTableManager.java
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/persist/PersistedTableManager.java
@@ -8,6 +8,7 @@ package it.eng.spagobi.tools.dataset.persist;
 import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.commons.utilities.StringUtilities;
 import it.eng.spagobi.tools.dataset.bo.AbstractJDBCDataset;
+import it.eng.spagobi.tools.dataset.bo.CkanDataSet;
 import it.eng.spagobi.tools.dataset.bo.FileDataSet;
 import it.eng.spagobi.tools.dataset.bo.IDataSet;
 import it.eng.spagobi.tools.dataset.bo.VersionedDataSet;
@@ -120,7 +121,7 @@ public class PersistedTableManager {
 		dataset.setPersisted(false);
 		dataset.loadData();
 		IDataStore datastore = dataset.getDataStore();
-		persistDataset(datastore, dsPersist);
+		persistDataset(dataset, datastore, dsPersist);
 	}
 
 	public void persistDataset(IDataSet dataSet, IDataStore datastore, IDataSource datasource, String tableName) throws Exception {
@@ -140,7 +141,7 @@ public class PersistedTableManager {
 			logger.debug("Persistence management isn't implemented for " + getDialect() + ".");
 			throw new SpagoBIServiceException("", "sbi.ds.dsCannotPersistDialect");
 		}
-		persistDataset(datastore, datasource);
+		persistDataset(dataSet, datastore, datasource);
 	}
 
 	private IDataStore normalizeFileDataSet(IDataSet dataSet, IDataStore datastore) {
@@ -186,7 +187,7 @@ public class PersistedTableManager {
 		return datastore;
 	}
 
-	public void persistDataset(IDataStore datastore, IDataSource datasource) throws Exception {
+	public void persistDataset(IDataSet dataset, IDataStore datastore, IDataSource datasource) throws Exception {
 		logger.debug("IN");
 		Connection connection = null;
 		String dialect = datasource.getHibDialectClass();
@@ -201,9 +202,9 @@ public class PersistedTableManager {
 			}
 			// Steps #1: define prepared statement (and max column size for
 			// strings type)
-			PreparedStatement statement = defineStatements(datastore, datasource, connection);
+			PreparedStatement statement = defineStatements(dataset, datastore, datasource, connection);
 			// Steps #2: define create table statement
-			String createStmtQuery = getCreateTableQuery(datastore, datasource);
+			String createStmtQuery = getCreateTableQuery(dataset, datastore, datasource);
 			dropTableIfExists(datasource);
 			// Step #3: execute create table statament
 			executeStatement(createStmtQuery, datasource);
@@ -229,7 +230,7 @@ public class PersistedTableManager {
 		logger.debug("OUT");
 	}
 
-	private PreparedStatement defineStatements(IDataStore datastore, IDataSource datasource, Connection connection) {
+	private PreparedStatement defineStatements(IDataSet dataset, IDataStore datastore, IDataSource datasource, Connection connection) {
 		PreparedStatement toReturn;
 
 		IMetaData storeMeta = datastore.getMetaData();
@@ -254,7 +255,7 @@ public class PersistedTableManager {
 
 		for (int i = 0; i < fieldCount; i++) {
 			IFieldMetaData fieldMeta = storeMeta.getFieldMeta(i);
-			String columnName = getSQLColumnName(fieldMeta);
+			String columnName = getSQLColumnName(dataset, fieldMeta);
 			String escapedColumnName = AbstractJDBCDataset.encapsulateColumnName(columnName, datasource);
 
 			insertQuery += separator + escapedColumnName;
@@ -437,8 +438,13 @@ public class PersistedTableManager {
 		}
 	}
 
-	private String getSQLColumnName(IFieldMetaData fmd) {
-		String columnName = fmd.getAlias() != null ? fmd.getAlias() : fmd.getName();
+	private String getSQLColumnName(IDataSet dataset, IFieldMetaData fmd) {
+		String columnName = null;
+		if (dataset instanceof FileDataSet || dataset instanceof CkanDataSet) {
+			columnName = fmd.getName();
+		} else {
+			columnName = fmd.getAlias() != null ? fmd.getAlias() : fmd.getName();
+		}
 		logger.debug("Column name is " + columnName);
 		return columnName;
 	}
@@ -557,7 +563,7 @@ public class PersistedTableManager {
 		return toReturn;
 	}
 
-	private String getCreateTableQuery(IDataStore datastore, IDataSource dataSource) {
+	private String getCreateTableQuery(IDataSet dataset, IDataStore datastore, IDataSource dataSource) {
 		String toReturn = "create table " + tableName + " (";
 		IMetaData md = datastore.getMetaData();
 
@@ -569,7 +575,7 @@ public class PersistedTableManager {
 
 		for (int i = 0, l = md.getFieldCount(); i < l; i++) {
 			IFieldMetaData fmd = md.getFieldMeta(i);
-			String columnName = getSQLColumnName(fmd);
+			String columnName = getSQLColumnName(dataset, fmd);
 			toReturn += " " + AbstractJDBCDataset.encapsulateColumnName(columnName, dataSource) + getDBFieldType(dataSource, fmd);
 			toReturn += ((i < l - 1) ? " , " : "");
 		}
