9db3c5cc653377ad75d3118518e409aef7a6df9a
giofeder <giofeder@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-04-14 08:58:48 +0000

    Added font options and visualization mode
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24721 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/fonts/FontEditorWizardPanel.js
@@ -0,0 +1,109 @@
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
+
+Ext.define('Sbi.fonts.FontEditorWizardPanel', {
+		extend: 'Sbi.widgets.WizardPanel'
+
+	, config:{
+	   storesList: null
+	  , fonts: null
+	  , frame: false
+	  , border: false
+	}
+
+	/**
+	 * @property {Sbi.fonts.editor.main.FontEditorPage} fontEditorPage
+	 * The page that manages font editing
+	 */
+	, fontEditorPage: null
+
+	, constructor : function(config) {
+		Sbi.trace("[FontEditorWizardPanel.constructor]: IN");
+		this.initConfig(config);
+		this.callParent(arguments);
+		Sbi.trace("[FontEditorWizardPanel.constructor]: OUT");
+	}
+
+
+	// =================================================================================================================
+	// METHODS
+	// =================================================================================================================
+
+	// -----------------------------------------------------------------------------------------------------------------
+    // public methods
+	// -----------------------------------------------------------------------------------------------------------------
+
+	, getFontEditorPage: function() {
+		return this.fontEditorPage;
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------
+    // init methods
+	// -----------------------------------------------------------------------------------------------------------------
+
+	, initPages: function(){
+		Sbi.trace("[FontEditorWizardPanel.initPages]: IN");
+
+		this.pages = new Array();
+
+		this.initFontEditorPage();
+		this.pages.push(this.fontEditorPage);
+
+		Sbi.trace("[FontEditorWizardPanel.initPages]: font editor page succesfully adedd");
+
+		Sbi.trace("[FontEditorWizardPanel.initPages]: OUT");
+
+		return this.pages;
+	}
+
+	, initButtons: function(){
+		Sbi.trace("[FontEditorWizardPanel.initButtons]: IN");
+
+		this.buttons = new Array();
+
+		this.buttons.push('->');
+
+		this.buttons.push({
+			id: 'submit'
+			, hidden: false
+	        , text:  LN('sbi.ds.wizard.confirm')
+	        , handler: this.onSubmit
+	        , scope: this
+//	        , disabled: (this.activeItem == 0)?false:true
+//	        , disabled: true
+	    });
+
+		this.buttons.push({
+			id: 'cancel'
+	        , text:  LN('sbi.ds.wizard.cancel')
+	        , handler: this.onCancel
+	        , scope: this
+	    });
+
+		Sbi.trace("[FontEditorWizardPanel.initButtons]: association editor buttons succesfully adedd");
+
+		Sbi.trace("[FontEditorWizardPanel.initButtons]: OUT");
+
+		return this.buttons;
+	}
+
+	, initFontEditorPage: function() {
+		Sbi.trace("[FontEditorWizardPanel.initFontEditorPage]: IN");
+		this.fontEditorPage = Ext.create('Sbi.fonts.editor.main.FontEditorPage',{
+			storesList: this.storesList
+			  , fonts: this.fonts
+		});
+		Sbi.trace("[FontEditorWizardPanel.initFontEditorPage]: IN");
+		return this.fontEditorPage;
+	}
+
+
+
+	// -----------------------------------------------------------------------------------------------------------------
+    // utility methods
+	// -----------------------------------------------------------------------------------------------------------------
+
+});
