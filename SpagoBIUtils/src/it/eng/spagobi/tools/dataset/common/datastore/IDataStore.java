2483a6da57b42065e2f2fc03ebf19a4206d86857
Alessandro Portosa <alessandro.portosa@eng.it>
2015-04-30 13:44:37 +0000

    Added functionality for DataSet/DataStore aggregation and filtering via SQL statement
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24780 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datastore/IDataStore.java
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/datastore/IDataStore.java
@@ -1,7 +1,7 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.tools.dataset.common.datastore;
 
@@ -55,6 +55,8 @@ public interface IDataStore {
 	void prependRecord(IRecord record);
 
 	void insertRecord(int recordIndex, IRecord record);
+
+	IDataStore aggregateAndFilterRecords(String sqlQuery);
 
 	/**
 	 * @deprecated use the proper DataWriter instead
