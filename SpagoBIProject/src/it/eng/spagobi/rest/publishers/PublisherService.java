525f73cd2a68dec09275f92acfe3b7f0142564b8
lazarkostic <lazarkostic@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-05-27 13:38:31 +0000

    Changed way of calling jsp pages via PublisherService.java class.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24917 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/rest/publishers/PublisherService.java
+++ SpagoBIProject/src/it/eng/spagobi/rest/publishers/PublisherService.java
@@ -1,13 +1,13 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.rest.publishers;
 
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
@@ -18,10 +18,9 @@ import org.apache.log4j.Logger;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
 /**
- * PublisherService Rest service can be used to display a jsp. It can be called passing the
- * request parameter "PUBLISHER" containing the uri to the requested resource.
- * i.e. PUBLISHER=PUBLISHER=/WEB-INF/jsp/community/XXX.jsp 
- * 
+ * PublisherService Rest service can be used to display a jsp. It can be called passing the request parameter "PUBLISHER" containing the uri to the requested
+ * resource. i.e. PUBLISHER=PUBLISHER=/WEB-INF/jsp/community/XXX.jsp
+ *
  * @author franceschini
  *
  */
@@ -32,25 +31,36 @@ public class PublisherService {
 	private HttpServletResponse servletResponse;
 
 	private static Logger logger = Logger.getLogger(PublisherService.class);
-	private static String PUBLISHER ="PUBLISHER";
+	private static String PUBLISHER = "PUBLISHER";
+	private static Map<String, String> urls;
 
 	@GET
 	public void publish(@Context HttpServletRequest req) {
 
 		try {
-			
+
+			urls = new HashMap<String, String>();
+
+			urls.put("MULTITENANT_MANAGEMENT_HOME", "/WEB-INF/jsp/tools/multitenant/multitenantManagement.jsp");
+			urls.put("LIST_DATASOURCE_HOME", "/WEB-INF/jsp/tools/datasource/listDataSource.jsp");
+			urls.put("MEASURES_CATALOGUE_HOME", "/WEB-INF/jsp/tools/measure/measuresCatalogue.jsp");
+			urls.put("LAYER_CATALOGUE_HOME", "/WEB-INF/jsp/tools/layer/layerCatalogue.jsp");
+			urls.put("LIST_OF_VALUES_HOME", "/WEB-INF/jsp/behaviouralmodel/lov/listOfValues.jsp");
+			urls.put("ANALYTICAL_DRIVERS_HOME", "/WEB-INF/jsp/behaviouralmodel/analyticaldriver/analyticalDriver.jsp");
+			urls.put("MANAGE_COMMUNITY_HOME", "/WEB-INF/jsp/community/ManageCommunity.jsp");
+			urls.put("HIERARCHIES_EDITOR_HOME", "/WEB-INF/jsp/tools/hierarchieseditor/hierarchiesEditor.jsp");
+
 			HttpServletRequest request = ResteasyProviderFactory.getContextData(HttpServletRequest.class);
-		    HttpServletResponse response = ResteasyProviderFactory.getContextData(HttpServletResponse.class);
-			
+
+			HttpServletResponse response = ResteasyProviderFactory.getContextData(HttpServletResponse.class);
+
 			String publisher = request.getParameter(PUBLISHER);
 			if (publisher != null) {
-				request.getRequestDispatcher(publisher).forward(request,
-						response);
+				request.getRequestDispatcher(urls.get(publisher)).forward(request, response);
 			}
 
 		} catch (Exception e) {
 			logger.error("Error forwarding request", e);
 		}
 	}
-
 }
