57c891df0a1adee88eff2c15ae668af3851932bf
Davide Zerbetto <davide.zerbetto@eng.it>
2015-06-26 12:30:05 +0000

    RESOLVED SPAGOBI-2132 : Error while trying to create a Worksheet document starting from a dataset that uses profile attributes https://spagobi.eng.it/jira/browse/SPAGOBI-2132 RESOLVED SPAGOBI-2133 : Error while trying to define a dataset with Qbe starting from a dataset that uses profile attributes https://spagobi.eng.it/jira/browse/SPAGOBI-2133 Resolved the case when the datasource is read & write, that was not considered before
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24980 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIUtils/src/it/eng/spagobi/tools/dataset/bo/AbstractJDBCDataset.java
+++ SpagoBIUtils/src/it/eng/spagobi/tools/dataset/bo/AbstractJDBCDataset.java
@@ -1,7 +1,7 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package it.eng.spagobi.tools.dataset.bo;
@@ -30,91 +30,88 @@ import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
 public abstract class AbstractJDBCDataset extends ConfigurableDataSet {
-	
+
 	public static String DS_TYPE = "SbiQueryDataSet";
 	private static final String QUERY = "query";
 	private static final String QUERY_SCRIPT = "queryScript";
 	private static final String QUERY_SCRIPT_LANGUAGE = "queryScriptLanguage";
 	private static final String DATA_SOURCE = "dataSource";
-	
+
 	private static transient Logger logger = Logger.getLogger(AbstractJDBCDataset.class);
-    
-	
+
 	/**
-     * Instantiates a new empty JDBC data set.
-     */
-    public AbstractJDBCDataset() {
+	 * Instantiates a new empty JDBC data set.
+	 */
+	public AbstractJDBCDataset() {
 		super();
-		setDataProxy( new JDBCDataProxy() );
-		setDataReader( new JDBCStandardDataReader() );
-		addBehaviour( new QuerableBehaviour(this) );
+		setDataProxy(new JDBCDataProxy());
+		setDataReader(new JDBCStandardDataReader());
+		addBehaviour(new QuerableBehaviour(this));
 	}
-    
-    // cannibalization :D
-    public AbstractJDBCDataset(JDBCDataSet jdbcDataset) {
-    	this(jdbcDataset.toSpagoBiDataSet());
-    }
-    
-    public AbstractJDBCDataset(SpagoBiDataSet dataSetConfig) {
+
+	// cannibalization :D
+	public AbstractJDBCDataset(JDBCDataSet jdbcDataset) {
+		this(jdbcDataset.toSpagoBiDataSet());
+	}
+
+	public AbstractJDBCDataset(SpagoBiDataSet dataSetConfig) {
 		super(dataSetConfig);
-		
-		setDataProxy( new JDBCDataProxy() );
-		setDataReader( new JDBCStandardDataReader() );
-		
-		try{
-			setDataSource( DataSourceFactory.getDataSource( dataSetConfig.getDataSource() ) );
+
+		setDataProxy(new JDBCDataProxy());
+		setDataReader(new JDBCStandardDataReader());
+
+		try {
+			setDataSource(DataSourceFactory.getDataSource(dataSetConfig.getDataSource()));
 		} catch (Exception e) {
 			throw new RuntimeException("Missing right exstension", e);
 		}
-		try{
-    		String config = JSONUtils.escapeJsonString(dataSetConfig.getConfiguration());		
-    		JSONObject jsonConf  = ObjectUtils.toJSONObject(config);
+		try {
+			String config = JSONUtils.escapeJsonString(dataSetConfig.getConfiguration());
+			JSONObject jsonConf = ObjectUtils.toJSONObject(config);
 			setQuery((jsonConf.get(QUERY) != null) ? jsonConf.get(QUERY).toString() : "");
 			setQueryScript((jsonConf.get(QUERY_SCRIPT) != null) ? jsonConf.get(QUERY_SCRIPT).toString() : "");
 			setQueryScriptLanguage((jsonConf.get(QUERY_SCRIPT_LANGUAGE) != null) ? jsonConf.get(QUERY_SCRIPT_LANGUAGE).toString() : "");
 		} catch (Exception e) {
 			logger.error("Error while defining dataset configuration. Error:", e);
-			throw new SpagoBIRuntimeException("Error while defining dataset configuration", e);	
+			throw new SpagoBIRuntimeException("Error while defining dataset configuration", e);
 		}
-		
-		addBehaviour( new QuerableBehaviour(this) );
-	}
-    
-    
-    
-    /**
-     * Redefined for set schema name
-     * 
-     */
-	public void setUserProfileAttributes(Map userProfile)  {
-		this.userProfileParameters = userProfile;
-		if (getDataSource().checkIsMultiSchema()){
-			String schema=null;
-			try {
-				schema = (String)userProfile.get(getDataSource().getSchemaAttribute());
-				((JDBCDataProxy)dataProxy).setSchema(schema);
-				logger.debug("Set UP Schema="+schema);
-			} catch (Throwable t) {
-				throw new SpagoBIRuntimeException("An error occurred while reading schema name from user profile", t);	
-			}		
-		}
-	}
-	
 
-	
+		addBehaviour(new QuerableBehaviour(this));
+	}
+
+	/**
+	 * Redefined for set schema name
+	 * 
+	 */
+	@Override
+	public void setUserProfileAttributes(Map userProfile) {
+		this.userProfileParameters = userProfile;
+		if (getDataSource().checkIsMultiSchema()) {
+			String schema = null;
+			try {
+				schema = (String) userProfile.get(getDataSource().getSchemaAttribute());
+				((JDBCDataProxy) dataProxy).setSchema(schema);
+				logger.debug("Set UP Schema=" + schema);
+			} catch (Throwable t) {
+				throw new SpagoBIRuntimeException("An error occurred while reading schema name from user profile", t);
+			}
+		}
+	}
+
+	@Override
 	public SpagoBiDataSet toSpagoBiDataSet() {
 		SpagoBiDataSet toReturn;
 		JDBCDataProxy dataProxy;
-		
+
 		toReturn = super.toSpagoBiDataSet();
-		
-		toReturn.setType( DS_TYPE );
-			
-		dataProxy = (JDBCDataProxy)this.getDataProxy();
+
+		toReturn.setType(DS_TYPE);
+
+		dataProxy = this.getDataProxy();
 		toReturn.setDataSource(dataProxy.getDataSource().toSpagoBiDataSource());
-		
+
 		try {
-			JSONObject jsonConf  = new JSONObject();
+			JSONObject jsonConf = new JSONObject();
 			jsonConf.put(QUERY, (getQuery() == null) ? "" : getQuery());
 			jsonConf.put(QUERY_SCRIPT, (getQueryScript() == null) ? "" : getQueryScript());
 			jsonConf.put(QUERY_SCRIPT_LANGUAGE, (getQueryScriptLanguage() == null) ? "" : getQueryScriptLanguage());
@@ -123,51 +120,54 @@ public abstract class AbstractJDBCDataset extends ConfigurableDataSet {
 			logger.error("Error while defining dataset configuration. Error:", e);
 			throw new SpagoBIRuntimeException("Error while defining dataset configuration. Error:", e);
 		}
-		
+
 		return toReturn;
 	}
 
-	
+	@Override
 	public JDBCDataProxy getDataProxy() {
 		IDataProxy dataProxy;
-		
+
 		dataProxy = super.getDataProxy();
-		
-		if(dataProxy == null) {
-			setDataProxy( new JDBCDataProxy() );
+
+		if (dataProxy == null) {
+			setDataProxy(new JDBCDataProxy());
 			dataProxy = getDataProxy();
 		}
-		
-		if(!(dataProxy instanceof  JDBCDataProxy)) throw new RuntimeException("DataProxy cannot be of type [" + 
-				dataProxy.getClass().getName() + "] in JDBCDataSet");
-		
-		return (JDBCDataProxy)dataProxy;
+
+		if (!(dataProxy instanceof JDBCDataProxy))
+			throw new RuntimeException("DataProxy cannot be of type [" + dataProxy.getClass().getName() + "] in JDBCDataSet");
+
+		return (JDBCDataProxy) dataProxy;
 	}
-	
+
+	@Override
 	public void setDataSource(IDataSource dataSource) {
 		getDataProxy().setDataSource(dataSource);
 	}
-	
+
+	@Override
 	public IDataSource getDataSource() {
 		return getDataProxy().getDataSource();
 	}
-	
+
 	@Override
 	public IMetaData getMetadata() {
 		IMetaData metadata = null;
 		try {
 			DatasetMetadataParser dsp = new DatasetMetadataParser();
-			metadata =  dsp.xmlToMetadata( getDsMetadata() );
+			metadata = dsp.xmlToMetadata(getDsMetadata());
 		} catch (Exception e) {
-			logger.error("Error loading the metadata",e);
-			throw new SpagoBIEngineRuntimeException("Error loading the metadata",e);
+			logger.error("Error loading the metadata", e);
+			throw new SpagoBIEngineRuntimeException("Error loading the metadata", e);
 		}
 		return metadata;
 	}
 
+	@Override
 	public void setMetadata(IMetaData metadata) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -177,19 +177,20 @@ public abstract class AbstractJDBCDataset extends ConfigurableDataSet {
 	}
 
 	/**
-	 * Returns the signature of the dataset.
-	 * The signature composed by query signature (i.e. the SQL query where parameters and profile attributes are already replaced) 
-	 * plus the datasource signature (i.e. the JNDI url (with eventual schema name) or the JDBC connection string + JDBC user)
+	 * Returns the signature of the dataset. The signature composed by query signature (i.e. the SQL query where parameters and profile attributes are already
+	 * replaced) plus the datasource signature (i.e. the JNDI url (with eventual schema name) or the JDBC connection string + JDBC user)
+	 * 
 	 * @return the signature of the dataset
 	 */
+	@Override
 	public String getSignature() {
 		logger.debug("IN");
 		String toReturn = null;
-		
+
 		// get the query with parameters and profile attributes substituted by their values
-		QuerableBehaviour querableBehaviour = (QuerableBehaviour) this.getBehaviour(QuerableBehaviour.class.getName()) ;
+		QuerableBehaviour querableBehaviour = (QuerableBehaviour) this.getBehaviour(QuerableBehaviour.class.getName());
 		String statement = querableBehaviour.getStatement();
-		
+
 		// puts also the data-source signature
 		String datasourceSignature = null;
 		IDataSource dataSource = this.getDataSource();
@@ -199,7 +200,7 @@ public abstract class AbstractJDBCDataset extends ConfigurableDataSet {
 		} else {
 			datasourceSignature = dataSource.getUrlConnection() + "_" + dataSource.getUser();
 		}
-		
+
 		toReturn = statement + "_" + datasourceSignature;
 		logger.debug("OUT: returning " + toReturn);
 		return toReturn;
@@ -210,9 +211,12 @@ public abstract class AbstractJDBCDataset extends ConfigurableDataSet {
 		IDataSource datasetDataSource = getDataSource();
 		if (datasetDataSource.getLabel().equals(dataSource.getLabel())) {
 			logger.debug("Specified datasource is the dataset's datasource; using CREATE TABLE AS SELECT tecnique");
-			try{
+			try {
 				List<String> fields = this.getFieldsList();
-				return TemporaryTableManager.createTable(fields, (String)query, tableName, getDataSource());
+				// get the query with parameters and profile attributes substituted by their values
+				QuerableBehaviour querableBehaviour = (QuerableBehaviour) this.getBehaviour(QuerableBehaviour.class.getName());
+				String statement = querableBehaviour.getStatement();
+				return TemporaryTableManager.createTable(fields, statement, tableName, getDataSource());
 			} catch (Exception e) {
 				logger.error("Error peristing the temporary table", e);
 				throw new SpagoBIEngineRuntimeException("Error peristing the temporary table", e);
@@ -222,8 +226,8 @@ public abstract class AbstractJDBCDataset extends ConfigurableDataSet {
 			return super.persist(tableName, dataSource);
 		}
 	}
-	
-	public static String encapsulateColumnName (String columnName, IDataSource dataSource) {
+
+	public static String encapsulateColumnName(String columnName, IDataSource dataSource) {
 		logger.debug("IN");
 		String toReturn = null;
 		if (columnName != null) {
@@ -251,7 +255,5 @@ public abstract class AbstractJDBCDataset extends ConfigurableDataSet {
 		logger.debug("OUT: returning " + toReturn);
 		return toReturn;
 	}
-	
-		
-}
 
+}
