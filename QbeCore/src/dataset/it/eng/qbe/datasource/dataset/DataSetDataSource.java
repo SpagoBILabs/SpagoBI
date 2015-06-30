68d2a9e072b39d9f30206895e549925866a229ae
Alessandro Portosa <alessandro.portosa@eng.it>
2015-06-30 13:40:51 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@24984 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- QbeCore/src/dataset/it/eng/qbe/datasource/dataset/DataSetDataSource.java
+++ QbeCore/src/dataset/it/eng/qbe/datasource/dataset/DataSetDataSource.java
@@ -1,7 +1,7 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package it.eng.qbe.datasource.dataset;
@@ -30,34 +30,33 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
-public class DataSetDataSource  extends AbstractDataSource implements ISQLDataSource{
-	
-	
-	private List<IDataSet> datasets;
+public class DataSetDataSource extends AbstractDataSource implements ISQLDataSource {
+
+	private final List<IDataSet> datasets;
 	public static final String EMPTY_MODEL_NAME = "";
 	public static final String DATASETS = "DATASETS";
 	public Class statementType = SQLStatement.class;
-	
+
 	private static transient Logger logger = Logger.getLogger(JPADataSource.class);
 
 	protected DataSetDataSource(String dataSourceName, IDataSourceConfiguration configuration) {
 		logger.debug("Creating a new DataSetDataSource");
-		setName( dataSourceName );
+		setName(dataSourceName);
 		dataMartModelAccessModality = new AbstractModelAccessModality();
 		this.configuration = configuration;
-		datasets= new ArrayList<IDataSet>();
-		
+		datasets = new ArrayList<IDataSet>();
+
 		Assert.assertNotNull(configuration.loadDataSourceProperties(), "The properties of the datasource can not be empty");
-		
-//		// validate and set configuration
-		if(configuration instanceof DataSetDataSourceConfiguration){
+
+		// // validate and set configuration
+		if (configuration instanceof DataSetDataSourceConfiguration) {
 			datasets.add(((DataSetDataSourceConfiguration) configuration).getDataset());
-		} else if(configuration instanceof CompositeDataSourceConfiguration){
-			List<IDataSourceConfiguration> subConfigurations = ((CompositeDataSourceConfiguration)configuration).getSubConfigurations();
-			for(int i=0; i<subConfigurations.size(); i++){
-				IDataSourceConfiguration subConf = ((CompositeDataSourceConfiguration)configuration).getSubConfigurations().get(i);
-				if(subConf instanceof DataSetDataSourceConfiguration){
-					datasets.add(((DataSetDataSourceConfiguration)subConf).getDataset());
+		} else if (configuration instanceof CompositeDataSourceConfiguration) {
+			List<IDataSourceConfiguration> subConfigurations = ((CompositeDataSourceConfiguration) configuration).getSubConfigurations();
+			for (int i = 0; i < subConfigurations.size(); i++) {
+				IDataSourceConfiguration subConf = ((CompositeDataSourceConfiguration) configuration).getSubConfigurations().get(i);
+				if (subConf instanceof DataSetDataSourceConfiguration) {
+					datasets.add(((DataSetDataSourceConfiguration) subConf).getDataset());
 				} else {
 					Assert.assertUnreachable("Not suitable configuration to create a JPADataSource");
 				}
@@ -69,69 +68,70 @@ public class DataSetDataSource  extends AbstractDataSource implements ISQLDataSo
 		logger.debug("Created a new JPADataSource");
 		initStatementType();
 	}
-	
+
 	public DataSetDataSourceConfiguration getDataSetDataSourceConfiguration() {
-		return (DataSetDataSourceConfiguration)configuration;
+		return (DataSetDataSourceConfiguration) configuration;
 	}
-	
+
+	public List<IDataSet> getDatasets() {
+		return datasets;
+	}
 
 	public void open() {
 
 	}
-	
+
 	public boolean isOpen() {
 		return true;
 	}
-	
+
 	public void close() {
 
 	}
-	
+
+	@Override
 	public IDataSource getToolsDataSource() {
 		return getDataSourceForReading();
 	}
-	
-	
 
 	public IModelStructure getModelStructure() {
 		IModelStructureBuilder structureBuilder;
-		if(dataMartModelStructure == null) {			
+		if (dataMartModelStructure == null) {
 			structureBuilder = new DataSetModelStructureBuilder(this);
 			dataMartModelStructure = structureBuilder.build();
 		}
-		
+
 		return dataMartModelStructure;
 	}
-	
 
-	public ITransaction getTransaction(){
+	public ITransaction getTransaction() {
 		return new DataSetTransaction(this);
 	}
 
-	//TO-DO
+	// TO-DO
 	public IPersistenceManager getPersistenceManager() {
 		return null;
 	}
 
-	public List<IDataSet> getRootEntities(){
+	public List<IDataSet> getRootEntities() {
 		return datasets;
 	}
 
-	public Class getStatementType(){
+	public Class getStatementType() {
 		return statementType;
 	}
 
-	private void initStatementType(){
+	private void initStatementType() {
 		IDataSource datasourceForReading = this.getDataSourceForReading();
 		if (datasourceForReading != null) {
-			if(datasourceForReading.getHibDialectName().toLowerCase().contains("hive")){
+			if (datasourceForReading.getHibDialectName().toLowerCase().contains("hive")) {
 				statementType = HiveQLStatement.class;
 			}
 		}
 
 	}
-	
-	public IDataSource getDataSourceForReading(){
-		return ((IDataSet)datasets.get(0)).getDataSourceForReading();
+
+	public IDataSource getDataSourceForReading() {
+		return ((IDataSet) datasets.get(0)).getDataSourceForReading();
 	}
 }
