094eb6a961204a9b80857aa1ba4b7a2ebb81a7fc
giofeder <giofeder@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-07-22 09:31:17 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/branches/SpagoBI-5.1@25066 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIConsoleEngine/src/it/eng/spago/message/MessageBundle.java
+++ SpagoBIConsoleEngine/src/it/eng/spago/message/MessageBundle.java
@@ -50,7 +50,7 @@ public class MessageBundle {
 
 	/**
 	 * Retrieves the Locale of the current user checking for its language profile in session.
-	 * 
+	 *
 	 * @return The Locale corresponding to the current user.
 	 */
 	public static Locale getUserLocale() {
@@ -94,8 +94,8 @@ public class MessageBundle {
 
 	/**
 	 * Questo metodo recupera una stringa individuata da un codice e da alcune proprietà dell'utente che ha richiesto il servizio (language,country).
-	 * <p>
-	 * 
+	 *
+	 *
 	 * @param code
 	 *            il codice associato alla stringa.
 	 * @return la stringa recuperata da un file di properties.
@@ -106,8 +106,8 @@ public class MessageBundle {
 
 	/**
 	 * Questo metodo recupera una stringa individuata da un codice e da alcune proprietà dell'utente che ha richiesto il servizio (language,country).
-	 * <p>
-	 * 
+	 *
+	 *
 	 * @param code
 	 *            il codice associato alla stringa.
 	 * @param params
@@ -120,8 +120,8 @@ public class MessageBundle {
 
 	/**
 	 * Questo metodo recupera una stringa individuata da un codice e da alcune proprietà dell'utente che ha richiesto il servizio (language,country).
-	 * <p>
-	 * 
+	 *
+	 *
 	 * @param code
 	 *            il codice associato alla stringa.
 	 * @param bundle
@@ -139,8 +139,8 @@ public class MessageBundle {
 
 	/**
 	 * Questo metodo recupera una stringa individuata da un codice e da alcune proprietà dell'utente che ha richiesto il servizio (language,country).
-	 * <p>
-	 * 
+	 *
+	 *
 	 * @param code
 	 *            il codice associato alla stringa.
 	 * @param bundle
@@ -155,8 +155,8 @@ public class MessageBundle {
 
 	/**
 	 * Questo metodo recupera una stringa individuata da un codice e da alcune proprietà dell'utente che ha richiesto il servizio (language,country).
-	 * <p>
-	 * 
+	 *
+	 *
 	 * @param code
 	 *            il codice associato alla stringa.
 	 * @param bundle
@@ -214,8 +214,8 @@ public class MessageBundle {
 
 	/**
 	 * Questo metodo recupera una stringa individuata da un codice e da alcune proprietà dell'utente che ha richiesto il servizio (language,country).
-	 * <p>
-	 * 
+	 *
+	 *
 	 * @param code
 	 *            il codice associato alla stringa.
 	 * @param bundle
@@ -237,7 +237,7 @@ public class MessageBundle {
 
 	/**
 	 * Add a bundle name in the lookup list.
-	 * 
+	 *
 	 * @param bundleName
 	 *            Name of the bundle to add to the lookup list.
 	 */
@@ -251,7 +251,7 @@ public class MessageBundle {
 	 * Retrieve a message in a hierarchical way: first it search in the path "properties.action.<action name>.code" or "properties.page.<page name>.code", if
 	 * the search fails it searches in the path "action.<action name>.code" or "page.<page name>.code" in the files enlisted in messages.xml, and if it fails
 	 * again it searches in the path "code" in the files enlisted in messages.xml.
-	 * 
+	 *
 	 * @param code
 	 *            Identifier of the message
 	 * @param responseContainer
@@ -279,7 +279,7 @@ public class MessageBundle {
 
 	/**
 	 * Replace the placeholders with the elements supplied in the list
-	 * 
+	 *
 	 * @param message
 	 *            Original message
 	 * @param params
