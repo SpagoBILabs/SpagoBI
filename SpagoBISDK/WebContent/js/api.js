dff44a278ed0fb4281b1953ff9afefd867558ead
fabrizyo <fabrizyo@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-09-07 10:05:56 +0000

    API JSDoc main, jsonp and cors added
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@25130 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBISDK/WebContent/js/api.js
+++ SpagoBISDK/WebContent/js/api.js
@@ -1,12 +1,22 @@
-/** SpagoBI, the Open Source Business Intelligence suite
-
+/** 
+ * @license
+ * SpagoBI, the Open Source Business Intelligence suite
+ * 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
- * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
- 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 Sbi.sdk.namespace('Sbi.sdk.api');
 
-/* 
+/**
+* @namespace Sbi
+*/
+
+/**
+* @namespace Sbi.sdk
+*/
+
+/** 
  * Note that Sbi.sdk.api definition is defined in both api.js and api_jsonp.js.
  * In api_jsonp.js there are functions that uses jsonp to avoid the same-origin policy.
  * The same functions were also developed with CORS and they are defined in api_cors.js.
@@ -14,12 +24,13 @@ Sbi.sdk.namespace('Sbi.sdk.api');
  * jsonp is deprecated, it is highly recommended to use CORS instead of it.
  * 
  * NB: CORS functions are inside Sbi.sdk.cors.api namespace and have same names as jsonp counterpart.
- * */
+ * @namespace Sbi.sdk.api
+ */
 Sbi.sdk.apply(Sbi.sdk.api, {
 	
 	elId: 0
 	, dataSetList: {}
-	
+
 	, getIFrameHtml: function( serviceUrl, config ) {
 		
 		var html;
@@ -93,12 +104,71 @@ Sbi.sdk.apply(Sbi.sdk.api, {
 		return documentUrl;
 	}
 
+	 /**
+	 * It returns the HTML code of an iFrame containing document visualization. In particular config is an object that must contain at least one between documentId and documentLabel.
+	 * It can also have (optional) parameters (an object containing values of document parameters), executionRole, displayToolbar and iframe, an object containing the style, height and width of the iframe where the document will be rendered (height and width can also be put outside the iframe object).
+	 * @example
+	 * var html = Sbi.sdk.api.getDocumentHtml({
+	 * 		documentLabel: 'RPT_WAREHOUSE_PROF'
+	 * 		, executionRole: '/spagobi/user'
+	 * 		, parameters: {warehouse_id: 19}
+	 * 		, displayToolbar: false
+	 * 		, displaySliders: false
+	 * 		, iframe: {
+	 *     		height: '500px'
+	 *     		, width: '100%'
+	 * 			, style: 'border: 0px;'
+	 * 		}
+	 * 	});
+	 * 
+	 * @method Sbi.sdk.api.getDocumentHtml
+	 * @param {Object} config - the configuration
+	 * @param {String} config.documentId - the document id, must contain at least one between documentId and documentLabel
+	 * @param {String} config.documentLabel - the document label,  must contain at least one between documentId and documentLabel
+	 * @param {Object} [config.parameters] - the values of document parameters
+	 * @param {String} [config.executionRole] - the role of execution
+	 * @param {Boolean} [config.displayToolbar] - display or not the toolbar
+	 * @param {Boolean} [config.displaySliders] - display or not the sliders
+	 * @param {Object} [config.iframe] - the style object of iframe
+	 * @param {String} [config.iframe.height] - the height of iframe
+	 * @param {String} [config.iframe.width] - the width of iframe
+	 * @param {String} [config.iframe.style] - the style of iframe
+	 */
 	, getDocumentHtml: function( config ) {
 		
 		var serviceUrl = this.getDocumentUrl( config );
 		return this.getIFrameHtml(serviceUrl, config);
 	}
 	
+	 /**
+	 * It calls {@link Sbi.sdk.api.getDocumentHtml} and inject the generated iFrame inside a specified tag. If the target tag is not specified in config variable, it chooses the <body> tag as default.
+	 * It can also have (optional) parameters (an object containing values of document parameters), executionRole, displayToolbar and iframe, an object containing the style, height and width of the iframe where the document will be rendered (height and width can also be put outside the iframe object).
+	 * @see {@link Sbi.sdk.api.getDocumentHtml} 
+	 * @example
+	 * execTest8 = function() {
+ 	 *	Sbi.sdk.api.injectWorksheet({
+	 *		datasetLabel: 'DS_DEMO_51_COCKPIT'
+ 	 *		, target: 'worksheet'
+	 *		, height: '600px'
+	 *		, width: '1100px'
+	 *		, iframe: {
+	 *			style: 'border: 0px;'
+	 *		}
+	 *	});
+	 * };
+	 *@method Sbi.sdk.api.injectDocument
+	 * @param {Object} config - the configuration
+	 * @param {String} config.documentId - the document id, must contain at least one between documentId and documentLabel
+	 * @param {String} config.documentLabel - the document label,  must contain at least one between documentId and documentLabel
+	 * @param {Object} [config.parameters] - the values of document parameters
+	 * @param {String} [config.executionRole] - the role of execution
+	 * @param {Boolean} [config.displayToolbar] - display or not the toolbar
+	 * @param {Boolean} [config.displaySliders] - display or not the sliders
+	 * @param {Object} [config.iframe] - the style object of iframe
+	 * @param {String} [config.iframe.height] - the height of iframe
+	 * @param {String} [config.iframe.width] - the width of iframe
+	 * @param {String} [config.iframe.style] - the style of iframe
+	 */
 	, injectDocument: function( config ) {
 		
 		var serviceUrl = this.getDocumentUrl( config );
@@ -130,12 +200,42 @@ Sbi.sdk.apply(Sbi.sdk.api, {
 		return this.getAdHocReportingUrl(config);
 	}
 	
+	/**
+	 * It returns the HTML code of an iFrame containing worksheet visualization.
+	 * config is an object that must contain datasetLabel and iframe, an object containing the style, height and width of the iframe where the worksheet and qbe will be rendered (height and width can also be put outside the iframe object).
+	 * 
+	 * @method Sbi.sdk.api.getWorksheetHtml
+	 * @param {Object} config - the configuration
+	 * @param {String} config.target - the target
+	 * @param {String} config.documentLabel - the document label,  must contain at least one between documentId and documentLabel
+	 * @param {String} [config.height] - the height of iframe, can be put inside iframe object
+	 * @param {String} [config.width] - the width of iframe, can be put inside iframe object
+	 * @param {Object} config.iframe - the style object of iframe
+	 * @param {String} [config.iframe.height] - the height of iframe, can be put outside
+	 * @param {String} [config.iframe.width] - the width of iframe
+	 * @param {String} [config.iframe.style] - the style of iframe
+	 */
 	, getWorksheetHtml: function( config ) {
 		
 		var serviceUrl = this.getWorksheetUrl( config );
 		return this.getIFrameHtml(serviceUrl, config);
 	}
 	
+	/**
+	 * It calls {@link Sbi.sdk.api.getWorksheetHtml} and inject the generated iFrame inside a specified tag. If the target tag is not specified in config variable, it chooses the <body> tag as default
+	 * config is an object that must contain datasetLabel and iframe, an object containing the style, height and width of the iframe where the worksheet and qbe will be rendered (height and width can also be put outside the iframe object).
+	 * 
+	 * @method Sbi.sdk.api.injectWorksheet
+	 * @param {Object} config - the configuration
+	 * @param {String} [config.target="<body>"] - the target
+	 * @param {String} config.documentLabel - the document label,  must contain at least one between documentId and documentLabel
+	 * @param {String} [config.height] - the height of iframe, can be put inside iframe object
+	 * @param {String} [config.width] - the width of iframe, can be put inside iframe object
+	 * @param {Object} config.iframe - the style object of iframe
+	 * @param {String} [config.iframe.height] - the height of iframe, can be put outside
+	 * @param {String} [config.iframe.width] - the width of iframe
+	 * @param {String} [config.iframe.style] - the style of iframe
+	 */
 	, injectWorksheet: function( config ) {
 		
 		var serviceUrl = this.getWorksheetUrl( config );
@@ -147,12 +247,53 @@ Sbi.sdk.apply(Sbi.sdk.api, {
 		return this.getAdHocReportingUrl(config);
 	}
 	
+	/**
+	 * It returns the HTML code of an iFrame containing qbe visualization.
+	 * config is an object that must contain datasetLabel and iframe, an object containing the style, height and width of the iframe where the worksheet and qbe will be rendered (height and width can also be put outside the iframe object).
+	 * 
+	 * @method Sbi.sdk.api.getQbeHtml
+	 * @param {Object} config - the configuration
+	 * @param {String} config.target - the target
+	 * @param {String} config.documentLabel - the document label,  must contain at least one between documentId and documentLabel
+	 * @param {String} [config.height] - the height of iframe, can be put inside iframe object
+	 * @param {String} [config.width] - the width of iframe, can be put inside iframe object
+	 * @param {Object} config.iframe - the style object of iframe
+	 * @param {String} [config.iframe.height] - the height of iframe, can be put outside
+	 * @param {String} [config.iframe.width] - the width of iframe
+	 * @param {String} [config.iframe.style] - the style of iframe
+	 */
 	, getQbeHtml: function( config ) {
 		
 		var serviceUrl = this.getQbeUrl( config );
 		return this.getIFrameHtml(serviceUrl, config);
 	}
 	
+	/**
+	 * It calls {@link Sbi.sdk.api.getQbeHtml} and inject the generated iFrame inside a specified tag. If the target tag is not specified in config variable, it chooses the <body> tag as default.
+	 * config is an object that must contain datasetLabel and iframe, an object containing the style, height and width of the iframe where the worksheet and qbe will be rendered (height and width can also be put outside the iframe object).
+	 * @example
+	 *	execTest9 = function() {
+	 *		Sbi.sdk.api.injectQbe({
+	 *			datasetLabel: 'DS_DEMO_51_COCKPIT'
+	 *			, target: 'qbe'
+	 *			, height: '600px'
+	 *			, width: '1100px'
+	 *			, iframe: {
+	 *			style: 'border: 0px;'
+	 *		  }
+	 *		});
+	 *	};
+	 * @method Sbi.sdk.api.injectQbe
+	 * @param {Object} config - the configuration
+	 * @param {String} [config.target="<body>"] - the target
+	 * @param {String} config.documentLabel - the document label,  must contain at least one between documentId and documentLabel
+	 * @param {String} [config.height] - the height of iframe, can be put inside iframe object
+	 * @param {String} [config.width] - the width of iframe, can be put inside iframe object
+	 * @param {Object} config.iframe - the style object of iframe
+	 * @param {String} [config.iframe.height] - the height of iframe, can be put outside
+	 * @param {String} [config.iframe.width] - the width of iframe
+	 * @param {String} [config.iframe.style] - the style of iframe
+	 */
 	, injectQbe: function( config ) {
 		
 		var serviceUrl = this.getQbeUrl( config );
--- SpagoBISDK/WebContent/js/api_jsonp.js
+++ SpagoBISDK/WebContent/js/api_jsonp.js
@@ -1,32 +1,47 @@
-/** SpagoBI, the Open Source Business Intelligence suite
-
+/** 
+ * @license
+ * SpagoBI, the Open Source Business Intelligence suite
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
  
 Sbi.sdk.namespace('Sbi.sdk.api');
 
-/*
- * New api that gives the same functionalities of these functions were developed using CORS instead of jsonp.
- * There are three main advantages on using CORS over jsonp:
- *  1. all the methods are available while in jsonp only GET request can be done;
- *  2. if an error occurs it is possible to manage it with CORS, while in jsonp it is only possible to set a timeout;
- *  3. jsonp has security problems (see later for an example).
- * 
- * However, jsonp is supported by all browser while CORS doesn't work properly in Internet Explorer 8 and 9
- * (in IE 7 and earlier versions is not supported at all).
- * 
- * If you use the version with jsonp please take note about this security problem:
- * the authentication is made with a GET request, so users credentials are sent as query parameters.
- * It would be better to not use "authenticate" function (in that way, the user should already logged in
- * in order to use the api).
- * */
 Sbi.sdk.apply(Sbi.sdk.api, {
 	authenticate:  function ( config ) {	    
 		var serviceUrl = Sbi.sdk.services.getServiceUrl('authenticate', config.params);
 		Sbi.sdk.jsonp.asyncRequest(serviceUrl, config.callback.fn, config.callback.scope, config.callback.args);
 	}
 
+	/**
+	 * This callback is called a response is returned by the server.
+	 * @callback ResponseCallback
+	 * @param {String} json - json of response
+	 * @param {Array} args
+	 * @param {Boolean} success - true if it's a success response otherwise false 
+	 */
+
+	/**
+	 * It returns the list of datasets
+	 * @example
+	 * execTest6 = function() {
+	 *	    Sbi.sdk.api.getDataSetList({
+	 *	    	callback: function( json, args, success ) {
+	 *	    		if (success){
+	 *	    			var str = "";
+	 *	    			
+	 *	    			for (var key in json){
+	 *		    			str += "<tr><td>" + json[key].label + "</td><td>" + json[key].name + "</td><td>" + json[key].description + "</td></tr>";
+	 *	    			}
+	 *	    			
+	 *	    			document.getElementById('datasets').innerHTML = str;
+	 *	    		}
+	 *			}});
+	 *	};
+	 * @method Sbi.sdk.api.getDataSetList
+	 * @param {Object} config - the configuration
+	 * @param {ResponseCallback} config.callback - function to be called after the response is returned by the server
+	 */
 	, getDataSetList: function( config ) {
 		
 		Sbi.sdk.jsonp.timeout = 10000;
@@ -37,6 +52,49 @@ Sbi.sdk.apply(Sbi.sdk.api, {
 		Sbi.sdk.jsonp.asyncRequest(serviceUrl, config.callback, this);
 	}
 	
+	/**
+	 * It executes a dataset
+	 * @example
+	 * execTest7 = function() {
+	 *    Sbi.sdk.api.executeDataSet({
+	 *    	datasetLabel: 'DS_DEMO_EXTCHART'
+	 *    	, parameters: {
+	 *    		par_year: 2011,
+	 *    		par_family: 'Food'
+	 *    	}
+	 *    	, callback: function( json, args, success ) {
+	 *    		if (success){
+	 *    			var str = "<th>Id</th>";
+	 *    			
+	 *    			var fields = json.metaData.fields;
+	 *    			for(var fieldIndex in fields) {
+	 *    				if (fields[fieldIndex].hasOwnProperty('header'))
+	 *    					str += '<th>' + fields[fieldIndex]['header'] + '</th>';
+	 *    			}
+	 *    			
+	 *    			str += '<tbody>';
+	 *    			
+	 *    			var rows = json.rows;
+	 *    			for (var rowIndex in rows){
+	 *    				str += '<tr>';
+	 *    				for (var colIndex in rows[rowIndex]) {
+	 *    					str += '<td>' + rows[rowIndex][colIndex] + '</td>';
+	 *    				}
+	 *    				str += '</tr>';
+	 *    			}
+	 *    			
+	 *    			str += '</tbody>';
+	 *    			
+	 *    			document.getElementById('results').innerHTML = str;
+	 *    		}
+	 *		}});
+	 * };
+	 * @method Sbi.sdk.api.executeDataSet
+	 * @param {Object} config - the configuration
+	 * @param {String} config.documentLabel - the document label
+	 * @param {Object} [config.parameters] - the values of dataset parameters
+	 * @param {ResponseCallback} config.callback - function to be called after the response is returned by the server
+	 */
 	, executeDataSet: function( config ) {
 		
 		Sbi.sdk.jsonp.timeout = 20000;
