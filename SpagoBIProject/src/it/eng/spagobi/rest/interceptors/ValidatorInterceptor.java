d4a01348580e30ca0f3202b67a6a966be962f3f4
gvalenti <gvalenti@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-09-14 13:30:48 +0000

    https://www.spagoworld.org/jira/browse/SPAGOBI-2190
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@25157 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/rest/interceptors/ValidatorInterceptor.java
+++ SpagoBIProject/src/it/eng/spagobi/rest/interceptors/ValidatorInterceptor.java
@@ -13,7 +13,7 @@ import org.apache.log4j.Logger;
 import org.jboss.resteasy.annotations.interception.Precedence;
 import org.jboss.resteasy.annotations.interception.ServerInterceptor;
 import org.jboss.resteasy.core.Headers;
-import org.jboss.resteasy.core.ResourceMethod;
+import org.jboss.resteasy.core.ResourceMethodInvoker;
 import org.jboss.resteasy.core.ServerResponse;
 import org.jboss.resteasy.spi.Failure;
 import org.jboss.resteasy.spi.HttpRequest;
@@ -22,12 +22,11 @@ import org.jboss.resteasy.spi.interception.PreProcessInterceptor;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-/**Notice this interceptor is implementing an the interface AcceptedByMethod. We also have to implement 
- * the method accept, where we receive the target method information and we can decide whether  
- * if this interceptor will be executed or not.
- * The org.jboss.resteasy.spi.interception.PreProcessInterceptor runs after a JAX-RS resource 
- * method is found to invoke on, but before the actual invocation happens
- * 
+/**
+ * Notice this interceptor is implementing an the interface AcceptedByMethod. We also have to implement the method accept, where we receive the target method
+ * information and we can decide whether if this interceptor will be executed or not. The org.jboss.resteasy.spi.interception.PreProcessInterceptor runs after a
+ * JAX-RS resource method is found to invoke on, but before the actual invocation happens
+ *
  * @author Monica Franceschini (monica.franceschini@eng.it)
  *
  */
@@ -37,26 +36,27 @@ import org.json.JSONObject;
 public class ValidatorInterceptor implements PreProcessInterceptor, AcceptedByMethod {
 
 	static private Logger logger = Logger.getLogger(ValidatorInterceptor.class);
-	
+
 	public boolean accept(Class c, Method m) {
 
 		ToValidate validationAnnot = m.getAnnotation(ToValidate.class);
-		if(validationAnnot == null){
+		if (validationAnnot == null) {
 			return false;
-		}else{
-            if(validationAnnot.typeName() != null){
+		} else {
+			if (validationAnnot.typeName() != null) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
 
 		}
-		
+
 	}
+
 	/**
 	 * Preprocess all the REST requests..
 	 */
-	public ServerResponse preProcess(HttpRequest req, ResourceMethod resourceMethod)throws Failure, WebApplicationException {
+	public ServerResponse preProcess(HttpRequest req, ResourceMethodInvoker resourceMethod) throws Failure, WebApplicationException {
 
 		// start server response as null and perform the validations, if it gets
 		// some value, it will be a valid response and the interceptor will stop
@@ -64,11 +64,11 @@ public class ValidatorInterceptor implements PreProcessInterceptor, AcceptedByMe
 		ServerResponse response = null;
 		try {
 			FieldsValidatorFactory fvf = new FieldsValidatorFactory();
-			
+
 			IFieldsValidator validator = fvf.getValidator(resourceMethod.getMethod().getAnnotation(ToValidate.class).typeName());
 			JSONArray errors = validator.validateFields(req.getFormParameters());
 
-			if (errors != null && errors.length() !=0) {
+			if (errors != null && errors.length() != 0) {
 				JSONObject errorMsg = new JSONObject();
 				errorMsg.put("errors", errors);
 				errorMsg.put("message", "validation-error");
@@ -82,6 +82,5 @@ public class ValidatorInterceptor implements PreProcessInterceptor, AcceptedByMe
 
 		return response;
 	}
-	
 
 }
