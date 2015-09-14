d6863e2d5c152a06f0ee86c3d61853940caf4603
Giordano Valentini <giordano.valentini@eng.it>
2015-09-14 13:30:48 +0000

    https://www.spagoworld.org/jira/browse/SPAGOBI-2190
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@25157 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/src/it/eng/spagobi/rest/interceptors/AuditRestPostInterceptor.java
+++ SpagoBIProject/src/it/eng/spagobi/rest/interceptors/AuditRestPostInterceptor.java
@@ -1,7 +1,7 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package it.eng.spagobi.rest.interceptors;
@@ -23,66 +23,53 @@ import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.interception.PostProcessInterceptor;
 
 /**
- * 
+ *
  * @author Alberto Ghedin (alberto.ghedin@eng.it)
  *
- * Updates the audit log for the successful requests. For the services that throw exceptions look at RestExceptionMapper
+ *         Updates the audit log for the successful requests. For the services that throw exceptions look at RestExceptionMapper
  *
  */
 @Provider
 @ServerInterceptor
-public class AuditRestPostInterceptor implements PostProcessInterceptor{
-
+public class AuditRestPostInterceptor implements PostProcessInterceptor {
 
 	static private Logger logger = Logger.getLogger(AuditRestPostInterceptor.class);
-	
-	
+
 	@Context
 	private HttpServletRequest servletRequest;
 	@Context
 	private HttpRequest request;
-	
 
-	
 	/**
-	 * Postprocess all the REST requests..
-	 * Add an entry into the audit log for every rest service
+	 * Postprocess all the REST requests.. Add an entry into the audit log for every rest service
 	 */
-	public void postProcess(ServerResponse response){
+	public void postProcess(ServerResponse response) {
 		logger.debug("AuditRestInterceptor:postProcess IN");
-		
-		
-		
-		try {	
-			String serviceUrl = InterceptorUtilities.getServiceUrl(request);
-			HashMap<String,String> parameters = InterceptorUtilities.getRequestParameters(request, servletRequest);
-//			Object responseBody = response.getEntity();
-//			if(responseBody!=null){
-//				String responseBodyString = response.getEntity().toString();
-//				parameters.put("Response Body", responseBodyString);
-//			}
 
-			
+		try {
+			String serviceUrl = InterceptorUtilities.getServiceUrl(request);
+			HashMap<String, String> parameters = InterceptorUtilities.getRequestParameters(request, servletRequest);
+			// Object responseBody = response.getEntity();
+			// if(responseBody!=null){
+			// String responseBodyString = response.getEntity().toString();
+			// parameters.put("Response Body", responseBodyString);
+			// }
+
 			UserProfile profile = (UserProfile) servletRequest.getSession().getAttribute(IEngUserProfile.ENG_USER_PROFILE);
-				
-			String actionCode = "[Service:"+ serviceUrl +" ; Class:"+response.getResourceClass()+" ; Method:"+response.getResourceMethod()+"]";
-			String result ="";
-			if(response.getStatus()==200){
+
+			String actionCode = "[Service:" + serviceUrl + " ; Class:" + response.getEntityClass() /* + " ; Method:" + response.getResourceMethod() */+ "]";
+			String result = "";
+			if (response.getStatus() == 200) {
 				result = "OK";
-			}else{
-				result="ERR ("+response.getStatus()+")";
+			} else {
+				result = "ERR (" + response.getStatus() + ")";
 			}
-			AuditLogUtilities.updateAudit(servletRequest, profile, actionCode,	parameters, result);
+			AuditLogUtilities.updateAudit(servletRequest, profile, actionCode, parameters, result);
 		} catch (Exception e) {
 			logger.error("Error updating audit", e);
-		}finally{
+		} finally {
 			logger.debug("AuditRestInterceptor:postProcess OUT");
 		}
 	}
-	
-	
-
-	
-
 
 }
