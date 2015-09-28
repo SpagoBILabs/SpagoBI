5d10c9813d9aa0ed46ca79a953656203ef63c23f
giofeder <giofeder@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-09-28 15:05:01 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@25212 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/cockpit/MainPanel.js
+++ SpagoBICockpitEngine/WebContent/js/src/ext4/sbi/cockpit/MainPanel.js
@@ -566,9 +566,9 @@ Ext.extend(Sbi.cockpit.MainPanel, Ext.Panel, {
 				var params = {association: jsonObj};
 
 				Ext.Ajax.request({
-					url: Sbi.config.serviceReg.getServiceUrl('checkAssociation', {
+					url: encodeURI(Sbi.config.serviceReg.getServiceUrl('checkAssociation', {
 						pathParams: params
-					}),
+					})),
 					method: 'POST',
 					params: {
 						requestParam: 'notInRequestBody'
