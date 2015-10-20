66b703bac885435c55638769aa789daff722a246
Giorgio Federici <giorgio.federici@eng.it>
2015-10-20 14:10:55 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/branches/SpagoBI-5.1_Maven@25361 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIChartEngine/src/main/java/it/eng/spagobi/engines/chart/ChartEngineRuntimeException.java
@@ -0,0 +1,58 @@
+/* SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package it.eng.spagobi.engines.chart;
+
+import it.eng.spagobi.utilities.engines.SpagoBIEngineRuntimeException;
+
+/**
+ * @author Antonella Giachino (antonella.giachino@eng.it)
+ */
+
+/**
+ * The Class ChartEngineException.
+ */
+public class ChartEngineRuntimeException extends SpagoBIEngineRuntimeException {
+    
+	/** The hints. 
+	List hints;
+	*/
+	
+	ChartEngineInstance engineInstance;
+	
+	
+	/**
+	 * Builds a <code>ChartException</code>.
+	 * 
+	 * @param message Text of the exception
+	 */
+    public ChartEngineRuntimeException(String message) {
+    	super(message);
+    }
+	
+    /**
+     * Builds a <code>ChartEngineException</code>.
+     * 
+     * @param message Text of the exception
+     * @param ex previous Throwable object
+     */
+    public ChartEngineRuntimeException(String message, Throwable ex) {
+    	super(message, ex);
+    }
+    
+    public ChartEngineInstance getEngineInstance() {
+		return engineInstance;
+	}
+
+	public void setEngineInstance(ChartEngineInstance engineInstance) {
+		this.engineInstance = engineInstance;
+	}
+    
+   
+
+
+}
+
