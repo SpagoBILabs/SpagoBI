b031d9c0104fe38702ed2453aba8b78000236920
giofeder <giofeder@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-11-26 11:30:29 +0000

    [trunk_old 25235][fabrizyo]	chef cookbooks fixed with new release
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/branches/SpagoBI-5.1_Maven@25562 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- ChefCookbooks/spagobi/metadata.rb
+++ ChefCookbooks/spagobi/metadata.rb
@@ -1,17 +1,17 @@
-name             'Spagobi'
-maintainer       'Spagobi'
-maintainer_email 'Spagobi'
+name             'spagobi'
+maintainer       'Spagobi Labs'
+maintainer_email 'giorgio.federici@eng.it'
 license          'All rights reserved'
 description      'Installs/Configures spagobi'
 long_description 'Installs/Configures spagobi'
-version          '1.0.0'
+version          '1.0.2'
 
 depends 'mysql', '~> 6.0'
 depends 'database', '<= 2.3.1'
-depends 'java'
+depends 'java', '>= 1.29.0'
 
 supports 'ubuntu', '>= 12.04'
-supports 'debian', '>= 7.0'
+#supports 'debian', '>= 7.0'
 
 #centos in progress: problems with images space and random chef fails
 #supports 'centos', '>= 6.0'
