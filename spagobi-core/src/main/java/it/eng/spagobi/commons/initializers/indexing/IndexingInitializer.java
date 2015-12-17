7871c1067374c368c4d95b5118de994eb6a2634e
Giorgio Federici <giorgio.federici@eng.it>
2015-12-17 09:55:26 +0000

    Check added to avoid location and name null for index folder
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@25761 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- spagobi-core/src/main/java/it/eng/spagobi/commons/initializers/indexing/IndexingInitializer.java
+++ spagobi-core/src/main/java/it/eng/spagobi/commons/initializers/indexing/IndexingInitializer.java
@@ -1,7 +1,7 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.commons.initializers.indexing;
 
@@ -13,52 +13,59 @@ import it.eng.spagobi.commons.utilities.indexing.LuceneIndexer;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.apache.lucene.index.CorruptIndexException;
 
-/**Initializer class for Metadata Indexing
+/**
+ * Initializer class for Metadata Indexing
+ * 
  * @author franceschini
  *
  */
 public class IndexingInitializer implements InitializerIFace {
-	
+
 	static private Logger logger = Logger.getLogger(IndexingInitializer.class);
 	private SourceBean _config;
-	
+
+	@Override
 	public SourceBean getConfig() {
 		return _config;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see it.eng.spago.init.InitializerIFace#init(it.eng.spago.base.SourceBean)
 	 */
+	@Override
 	public void init(SourceBean config) {
 		logger.debug("IN");
 		_config = config;
 
-		    String jndiResourcePath = SingletonConfig.getInstance().getConfigValue("INDEX_INITIALIZATION.jndiResourcePath");
-		    String location = SpagoBIUtilities.readJndiResource(jndiResourcePath);
-		    String name = SingletonConfig.getInstance().getConfigValue("INDEX_INITIALIZATION.name");
-		    //first checks if iindex exists
-		    File idxFile = new File(location+name);
-		    if(!idxFile.exists()){
-		    	logger.debug("Creating index");
-			    LuceneIndexer indexer = new LuceneIndexer();
-			    try {
+		String jndiResourcePath = SingletonConfig.getInstance().getConfigValue("INDEX_INITIALIZATION.jndiResourcePath");
+		String location = SpagoBIUtilities.readJndiResource(jndiResourcePath);
+		String name = SingletonConfig.getInstance().getConfigValue("INDEX_INITIALIZATION.name");
+
+		if (location == null || name == null) {
+			logger.error("Impossible to read a correct location or index name!");
+		} else {
+			// first checks if iindex exists
+			File idxFile = new File(location + name);
+			if (!idxFile.exists()) {
+				logger.debug("Creating index");
+				LuceneIndexer indexer = new LuceneIndexer();
+				try {
 					indexer.createIndex(idxFile);
 				} catch (CorruptIndexException e) {
-					logger.error("Index corrupted "+e.getMessage(), e);
+					logger.error("Index corrupted " + e.getMessage(), e);
 				} catch (IOException e) {
 					logger.error(e.getMessage(), e);
 				}
-		    }else{
-		    	logger.debug("Index already exists");
-		    }
-
-		    
+			} else {
+				logger.debug("Index already exists");
+			}
+		}
 
 		logger.debug("OUT");
 	}
