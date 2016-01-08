9b450e6bbe675f6da995ee001507c0c82287fac3
Giulio Gavardi <giulio.gavardi@eng.it>
2016-01-08 11:33:24 +0000

    only style changes
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@25815 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- spagobi-core/src/main/java/it/eng/spagobi/sdk/datasources/impl/DataSourcesSDKServiceImpl.java
+++ spagobi-core/src/main/java/it/eng/spagobi/sdk/datasources/impl/DataSourcesSDKServiceImpl.java
@@ -1,9 +1,13 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.sdk.datasources.impl;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
 
 import it.eng.spagobi.commons.constants.SpagoBIConstants;
 import it.eng.spagobi.commons.dao.DAOFactory;
@@ -13,20 +17,17 @@ import it.eng.spagobi.sdk.exceptions.NotAllowedOperationException;
 import it.eng.spagobi.sdk.utilities.SDKObjectsConverter;
 import it.eng.spagobi.tools.datasource.bo.IDataSource;
 
-import java.util.List;
-
-import org.apache.log4j.Logger;
-
 public class DataSourcesSDKServiceImpl extends AbstractSDKService implements it.eng.spagobi.sdk.datasources.DataSourcesSDKService {
 
 	static private Logger logger = Logger.getLogger(DataSourcesSDKServiceImpl.class);
 
+	@Override
 	public SDKDataSource getDataSource(Integer dataSourceId) throws NotAllowedOperationException {
 		SDKDataSource toReturn = null;
 		logger.debug("IN: dataSourceId in input = " + dataSourceId);
-		
+
 		this.setTenant();
-		
+
 		try {
 			super.checkUserPermissionForFunctionality(SpagoBIConstants.DATASOURCE_MANAGEMENT, "User cannot see datasource congifuration.");
 			if (dataSourceId == null) {
@@ -39,9 +40,9 @@ public class DataSourcesSDKServiceImpl extends AbstractSDKService implements it.
 				return null;
 			}
 			toReturn = new SDKObjectsConverter().fromSpagoBiDataSourceToSDKDataSource(dataSource.toSpagoBiDataSource());
-		} catch(NotAllowedOperationException e) {
+		} catch (NotAllowedOperationException e) {
 			throw e;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			logger.error("Error while retrieving SDKDataSources list", e);
 			logger.debug("Returning null");
 			return null;
@@ -52,12 +53,13 @@ public class DataSourcesSDKServiceImpl extends AbstractSDKService implements it.
 		return toReturn;
 	}
 
+	@Override
 	public SDKDataSource[] getDataSources() throws NotAllowedOperationException {
 		SDKDataSource[] toReturn = null;
 		logger.debug("IN");
-		
+
 		this.setTenant();
-		
+
 		try {
 			super.checkUserPermissionForFunctionality(SpagoBIConstants.DATASOURCE_MANAGEMENT, "User cannot see datasources congifuration.");
 			List dataSourceList = DAOFactory.getDataSourceDAO().loadAllDataSources();
@@ -67,9 +69,9 @@ public class DataSourcesSDKServiceImpl extends AbstractSDKService implements it.
 				SDKDataSource sdkDataSource = new SDKObjectsConverter().fromSpagoBiDataSourceToSDKDataSource(dataSource.toSpagoBiDataSource());
 				toReturn[i] = sdkDataSource;
 			}
-		} catch(NotAllowedOperationException e) {
+		} catch (NotAllowedOperationException e) {
 			throw e;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			logger.error("Error while retrieving SDKSource list", e);
 			logger.debug("Returning null");
 			return null;
@@ -78,6 +80,6 @@ public class DataSourcesSDKServiceImpl extends AbstractSDKService implements it.
 			logger.debug("OUT");
 		}
 		return toReturn;
-	}	
+	}
 
 }
