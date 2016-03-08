c3288541cfbc669995ece05c05daabcee32ff7c4
Alessio Conese <alessio.conese@eng.it>
2016-03-08 10:13:52 +0000

    [what-if] add default constructor to ModelConfig for jackson serializer
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk@26064 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIWhatIfEngine/src/main/java/it/eng/spagobi/engines/whatif/model/ModelConfig.java
+++ SpagoBIWhatIfEngine/src/main/java/it/eng/spagobi/engines/whatif/model/ModelConfig.java
@@ -48,6 +48,8 @@ public class ModelConfig implements Serializable {
 
 	private SpagoBICrossNavigationConfig crossNavigation;
 
+	public ModelConfig() {}
+
 	public ModelConfig(PivotModel pivotModel) {
 		drillType = DrillDownCommand.MODE_POSITION;
 		showParentMembers = false;
