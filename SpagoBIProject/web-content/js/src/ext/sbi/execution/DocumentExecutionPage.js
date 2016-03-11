c5727919566eb233840c4371ded36cd27e8ba729
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2016-03-11 11:00:46 +0000

    Substituted Hot Link in Preferiti for italian language
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/branches/SpagoBI-5.1@26096 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIProject/web-content/js/src/ext/sbi/execution/DocumentExecutionPage.js
+++ SpagoBIProject/web-content/js/src/ext/sbi/execution/DocumentExecutionPage.js
@@ -969,7 +969,7 @@ Ext.extend(Sbi.execution.DocumentExecutionPage, Ext.Panel, {
 		if(this.favouritesWin === null){
 			this.hotlinksPanel = new Sbi.execution.HotlinksPanel();
 			this.favouritesWin = new Ext.Window({
-				title: "Favourites",
+				title: LN("sbi.rememberme.title"), //"Favourites",
 				layout: 'fit',
 				modal: true,
 				closeAction : 'hide',
