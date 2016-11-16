3555b8b12744968f62755017c2d30a24a503b7a3
aportosa <alessandro.portosa@eng.it>
2016-11-16 11:40:30 +0100

    Update installation.sh

--- ChefCookbooks/installation.sh
+++ ChefCookbooks/installation.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
-set -e
-set -x
+#SpagoBI VM installation script
 
 #install chef 12 client
-if [ ! -d "/opt/chef" ]; then
-	wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/10.04/x86_64/chef_12.5.1-1_amd64.deb
-	sudo dpkg -i chef_12.5.1-1_amd64.deb
-	rm -f chef_12.5.1-1_amd64.deb
-fi
+wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/10.04/x86_64/chef_12.5.1-1_amd64.deb
+dpkg -i chef_12.5.1-1_amd64.deb
+rm -f chef_12.5.1-1_amd64.deb
 
 #download recipes
-sudo apt-get update
-sudo apt-get -y install subversion
-rm -rf cookbooks
-svn checkout https://github.com/SpagoBILabs/SpagoBI/trunk/ChefCookbooks/ cookbooks
+wget https://github.com/SpagoBILabs/SpagoBI/releases/download/fiware-v5.2-6030e025b0/chef-cookbooks.zip
 
+#install zip
+apt-get install unzip
+
+unzip chef-cookbooks.zip
 
 #install spagobi
-if [ "$1" == "demo" ]; then
-	echo '{"demo":true}' | sudo chef-client -z -o 'recipe[spagobi::1.0.2_install]' -j /dev/stdin
-else
-	sudo chef-client -z -o 'recipe[spagobi::1.0.2_install]'
-fi
-
-#remove recipes
+mkdir /etc/chef
+chef-client -z -o 'recipe[spagobi::1.0.3_install]'
 rm -rf cookbooks
